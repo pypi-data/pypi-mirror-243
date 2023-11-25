f1d9c0fc8deb837aaf0e6129ef3a56b15f9d5bc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 14:03:08 +0000

    Add package it4u_http_request/1.0.17/it4u_http_request-v1.0.17.tar.gz

---
+++ packages/it4u_http_request/it4u_http_request-v1.0.17.tar.gz/it4u_http_request-v1.0.17/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name = 'it4u_http_request',
+    packages = ['it4u_http_request'],
+    version = 'v1.0.17',
+    description = 'HttpRequest will return response in cache if exists, else it will open request',
+    author = 'Louis',
+    author_email = 'thonh.it@gmail.com',
+    url = 'https://github.com/louis-it4u/http-request',
+    download_url = 'https://github.com/louis-it4u/http-request/archive/refs/tags/v1.0.16.tar.gz',
+    keywords = ['requests', 'cached_requests'],
+    classifiers = [],
+    install_requires=[
+        'requests',
+        'reactivex'
+    ]
+)
