1b6c721e112e17f490edcfd886c16ca8a20d5aa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 03:04:06 +0000

    Add package symbolicai/0.4.31/symbolicai-0.4.31.tar.gz

---
+++ packages/symbolicai/symbolicai-0.4.31.tar.gz/symbolicai-0.4.31/symai/extended/interfaces/whisper.py
@@ -0,0 +1,13 @@
+from ... import core
+from ...symbol import Expression
+
+
+class whisper(Expression):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def __call__(self, audio_path: str, operation: str = 'decode', **kwargs) -> "whisper":
+        @core.speech_to_text(audio=audio_path, prompt=operation, **kwargs)
+        def _func(_) -> str:
+            pass
+        return self.sym_return_type(_func(self))
