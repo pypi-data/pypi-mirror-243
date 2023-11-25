7f61698b5468f17dc61b530a7cedc8d0f5536338
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 14:02:42 +0000

    Add package docugami/0.0.5/docugami-0.0.5-py3-none-any.whl

---
+++ packages/docugami/docugami-0.0.5-py3-none-any.whl/docugami/resources/docsets/documents/__init__.py
@@ -0,0 +1,20 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from .dgmls import Dgmls, AsyncDgmls, DgmlsWithRawResponse, AsyncDgmlsWithRawResponse
+from .documents import (
+    Documents,
+    AsyncDocuments,
+    DocumentsWithRawResponse,
+    AsyncDocumentsWithRawResponse,
+)
+
+__all__ = [
+    "Dgmls",
+    "AsyncDgmls",
+    "DgmlsWithRawResponse",
+    "AsyncDgmlsWithRawResponse",
+    "Documents",
+    "AsyncDocuments",
+    "DocumentsWithRawResponse",
+    "AsyncDocumentsWithRawResponse",
+]
