d5f053b5ba5e5c166e04f455f7c58b6cd21ef3de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 14:03:04 +0000

    Add package pytest-subinterpreter/0.0.2/pytest-subinterpreter-0.0.2.tar.gz

---
+++ packages/pytest-subinterpreter/pytest-subinterpreter-0.0.2.tar.gz/pytest-subinterpreter-0.0.2/pytest_subinterpreter/plugin.py
@@ -0,0 +1,49 @@
+import pytest
+import _xxsubinterpreters as interpreters
+
+def pytest_addoption(parser):
+    group = parser.getgroup("subinterpreter")
+    group.addoption(
+        "--interpreter-per",
+        action="store",
+        dest="interpreter-per",
+        default="session",
+        help='Interpreter scope.',
+    )
+
+def wrap_pytest():
+    # args: tuple[str], plugins: tuple[str]
+    import sys
+
+    from _pytest.main import wrap_session, _main
+    from _pytest.config import get_config
+
+    config = get_config(list(args), plugins)
+    config.parse(list(args))
+
+    # The CPython faulthandler isn't supported, patch it with a dummy.
+    # Pytest requires that you import the module after importing pytest things.
+    from pytest_subinterpreter.faulthandler import FaultHandler
+    sys.modules["faulthandler"] = FaultHandler()
+
+    wrap_session(config, _main)
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_cmdline_main(config: pytest.Config):
+    scope = config.getoption("interpreter-per")
+
+    args = tuple(config.args)
+    plugins = tuple(config.invocation_params.plugins) if config.invocation_params.plugins else ()
+    interp = interpreters.create()
+
+    if scope == "session":
+        interpreters.run_func(interp, wrap_pytest, shared={
+            "args": args,
+            "plugins": plugins
+        })
+    else:
+        raise NotImplementedError(f"Scope {scope} not implemented")
+    
+    interpreters.destroy(interp)
+    return 1 # don't run main pytest script
---
+++ packages/pytest-subinterpreter/pytest-subinterpreter-0.0.2.tar.gz/pytest-subinterpreter-0.0.2/setup.py
@@ -0,0 +1,19 @@
+# sample ./setup.py file
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pytest-subinterpreter",
+    version="0.0.2",
+    description="Run pytest in a subinterpreter",
+    long_description=long_description,
+    packages=["pytest_subinterpreter"],
+    python_requires=">=3.13",
+    install_requires=["pytest>=7.0.0"],
+    # the following makes a plugin available to pytest
+    entry_points={"pytest11": ["subinterpreter = pytest_subinterpreter.plugin"]},
+    # custom PyPI classifier for pytest plugins
+    classifiers=["Framework :: Pytest"],
+)
