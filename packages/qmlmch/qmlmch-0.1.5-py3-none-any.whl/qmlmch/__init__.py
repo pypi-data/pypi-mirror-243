909eee9a6bd513d7b940b349131d85eb105b2c30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 14:02:50 +0000

    Add package qmlmch/0.1.5/qmlmch-0.1.5-py3-none-any.whl

---
+++ packages/qmlmch/qmlmch-0.1.5-py3-none-any.whl/qmlmch/__init__.py
@@ -0,0 +1,8 @@
+from .modulo import hydrogen_atom
+
+__version__ = "0.1.5"  # Puedes definir la versión de tu librería aquí
+
+__author__ = "M Luciano"  # Puedes especificar el autor de la librería
+
+__all__ = ["hydrogen_atom"]
+
