2eaafa7180d448b7d35fb05599cb326cb5f48dca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 14:03:05 +0000

    Add package qmlmch/0.1.0/qmlmch-0.1.0.tar.gz

---
+++ packages/qmlmch/qmlmch-0.1.0.tar.gz/qmlmch-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+with open('README.md',"r") as fh:
+    description = fh.read() 
+
+setup(
+    name='qmlmch',
+    version='0.1.0',
+    #packages=find_packages(exclude=['testsumm']),
+    packages=find_packages(),
+    description='library ML',
+    long_description=description,
+    long_description_content_type="text/markdown",
+    author='Luciano M', 
+    license='MIT',
+    install_requires=["numpy","plotly","scipy","matplotlib"],
+    python_requires='>=3.10.12',
+    author_email='luciano.munoz1@udea.edu.co'
+    #url = ''
+)
+
+#como crear el archivo requirements.txt
+#pip freeze > requirements.txt
+    
+    
