29894e65cc285b7ce734a0153db9efaa3bc1960c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 03:03:51 +0000

    Add package grss/1.5.0/grss-1.5.0.tar.gz

---
+++ packages/grss/grss-1.5.0.tar.gz/grss-1.5.0/include/grss.h
@@ -0,0 +1,34 @@
+#ifndef GRSS_H
+#define GRSS_H
+
+#include "gr15.h"
+
+void propSimulation::integrate() {
+    // check that xInteg has a valid size. If not, raise error
+    if (this->integParams.nInteg < 1) {
+        throw std::runtime_error(
+            "ERROR: There are no integration bodies in the "
+            "simulation. Need at least one body with a full state vector to "
+            "integrate.");
+    }
+
+    this->preprocess();
+    furnsh_c(this->DEkernelPath.c_str());
+    gr15(this);
+    unload_c(this->DEkernelPath.c_str());
+
+    // if backwards integration
+    if (this->integParams.t0 > this->integParams.tf) {
+        std::reverse(this->events.begin(), this->events.end());
+        std::reverse(this->xObserver.begin(), this->xObserver.end());
+        std::reverse(this->observerInfo.begin(), this->observerInfo.end());
+        std::reverse(this->tEval.begin(), this->tEval.end());
+        std::reverse(this->radarObserver.begin(), this->radarObserver.end());
+        std::reverse(this->lightTimeEval.begin(), this->lightTimeEval.end());
+        std::reverse(this->xIntegEval.begin(), this->xIntegEval.end());
+        std::reverse(this->opticalObsEval.begin(), this->opticalObsEval.end());
+        std::reverse(this->radarObsEval.begin(), this->radarObsEval.end());
+    }
+}
+
+#endif
