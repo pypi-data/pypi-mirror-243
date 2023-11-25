a98b8067714b8980bc8fb2fadb586b2d86c11caa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 14:02:50 +0000

    Add package django-restit/4.1.70/django_restit-4.1.70.tar.gz

---
+++ packages/django-restit/django_restit-4.1.70.tar.gz/django_restit-4.1.70/incident/periodic.py
@@ -0,0 +1,19 @@
+from rest.decorators import periodic
+from incident.models import Event, ServerOssecAlert, Incident
+from datetime import datetime, timedelta
+from rest import log
+
+logger = log.getLogger("incident", filename="incident.log")
+
+
+@periodic(minute=50, hour=8)
+def run_cleanup(force=False, verbose=False, now=None):
+    stale = datetime.now() - timedelta(days=90)
+    # delete all ossec alerts older then 90 days
+    count = ServerOssecAlert.objects.filter(created__lte=stale).delete()
+    if count:
+        logger.info(f"deleted {count} old ServerOssecAlert")
+    # delete all events older then 90 days
+    count = Event.objects.filter(created__lte=stale).delete()
+    if count:
+        logger.info(f"deleted {count} old Events")
