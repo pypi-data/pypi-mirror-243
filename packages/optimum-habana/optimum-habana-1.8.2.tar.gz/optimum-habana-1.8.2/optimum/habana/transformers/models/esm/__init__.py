12ccde020d074992ef8f168948f60786aa8b81dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 03:04:05 +0000

    Add package optimum-habana/1.8.2/optimum-habana-1.8.2.tar.gz

---
+++ packages/optimum-habana/optimum-habana-1.8.2.tar.gz/optimum-habana-1.8.2/optimum/habana/transformers/models/esm/__init__.py
@@ -0,0 +1,7 @@
+from .modeling_esm import gaudi_esmoutput_forward, gaudi_esmselfoutput_forward
+from .modeling_esmfold import (
+    gaudi_esm_for_protein_folding_forward,
+    gaudi_esmfolding_trunk_forward,
+    gaudi_rot_matmul,
+    gaudi_rot_vec_mul,
+)
