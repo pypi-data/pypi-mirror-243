128937a979aa5f5e044f564117f8be4e0a6d0ee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 03:04:06 +0000

    Add package symbolicai/0.4.19/symbolicai-0.4.19.tar.gz

---
+++ packages/symbolicai/symbolicai-0.4.19.tar.gz/symbolicai-0.4.19/symai/backend/mixin/openai.py
@@ -0,0 +1,82 @@
+SUPPORTED_MODELS = ['gpt-3.5-turbo', 'gpt-3.5-turbo-16k', 'gpt-4', 'gpt-4-16k', 'text-embedding-ada-002']
+
+
+class OpenAIMixin:
+    def api_pricing(self):
+        if self.model == 'gpt-3.5-turbo':
+            return {
+                'input':  0.0015 / 1_000,
+                'output': 0.0020 / 1_000
+            }
+
+        elif self.model == 'gpt-3.5-turbo-16k':
+            return {
+                'input':  0.003 / 1_000,
+                'output': 0.004 / 1_000
+            }
+
+        elif self.model == 'gpt-3.5-turbo-1106':
+            return {
+                'input':  0.001 / 1_000,
+                'output': 0.002 / 1_000
+            }
+
+        elif self.model == 'gpt-3.5-turbo-instruct':
+            return {
+                'input':  0.0015 / 1_000,
+                'output': 0.002 / 1_000
+            }
+
+        elif self.model == 'gpt-4':
+            return {
+                'input':  0.03 / 1_000,
+                'output': 0.06 / 1_000
+            }
+
+        elif self.model == 'gpt-4-16k':
+            return {
+                'input':  0.06 / 1_000,
+                'output': 0.12 / 1_000
+            }
+
+        elif self.model == 'gpt-4-1106-preview' or self.model == 'gpt-4-vision-preview':
+            return {
+                'input':  0.01 / 1_000,
+                'output': 0.03 / 1_000
+            }
+
+        elif self.model == 'text-embedding-ada-002':
+            return {
+                'usage': 0.0001 / 1_000
+            }
+
+    def api_max_tokens(self):
+        if self.model == 'gpt-3.5-turbo' or self.model == 'gpt-3.5-turbo-0613' or self.model == 'gpt-3.5-turbo-instruct':
+            return 4_096
+
+        elif self.model == 'gpt-3.5-turbo-16k' or self.model == 'gpt-3.5-turbo-16k-0613' or self.model == 'gpt-3.5-turbo-1106':
+            return 16_384
+
+        elif self.model == 'gpt-4' or self.model == 'gpt-4-0613':
+            return 8_192
+
+        elif self.model == 'gpt-4-1106-preview' or self.model == 'gpt-4-vision-preview':
+            return 128_000
+
+        elif self.model == 'gpt-4-32k' or self.model == 'gpt-4-32k-0613':
+            return 32_768
+
+        elif self.model == 'text-davinci-003' or self.model == 'text-davinci-002':
+            return 4_097
+
+        elif self.model == 'text-curie-001' or \
+             self.model == 'text-babbage-001' or \
+             self.model == 'text-ada-001' or \
+             self.model == 'davinci' or \
+             self.model == 'curie' or \
+             self.model == 'babbage' or \
+             self.model == 'ada':
+            return 2_049
+
+        elif self.model == 'text-embedding-ada-002':
+            return 8_191
---
+++ packages/symbolicai/symbolicai-0.4.19.tar.gz/symbolicai-0.4.19/symai/memory.py
@@ -0,0 +1,108 @@
+from json import JSONEncoder
+import json
+from typing import List
+from . import core
+from .symbol import Expression, Symbol
+from .components import Function
+
+
+class Memory(Expression):
+    def __init__(self):
+        super().__init__()
+
+    def store(self, query: str, *args, **kwargs):
+        raise NotImplementedError
+
+    def forget(self, query: str, *args, **kwargs):
+        raise NotImplementedError
+
+    def recall(self, query: str, *args, **kwargs):
+        raise NotImplementedError
+
+    def forward(self, query: str, *args, **kwargs):
+        return self.recall(query, *args, **kwargs)
+
+
+class SlidingWindowListMemory(Memory):
+    def __init__(self, window_size: int = 10, max_size: int = 1000):
+        super().__init__()
+        self._memory: List[str] = []
+        self._window_size: int  = window_size
+        self._max_size: int     = max_size
+
+    def store(self, query: str, *args, **kwargs):
+        self._memory.append(query)
+        if len(self._memory) > self._max_size:
+            self._memory = self._memory[-self._max_size:]
+
+    def forget(self, query: Symbol, *args, **kwargs):
+        self._memory.remove(query)
+
+    def recall(self, *args, **kwargs):
+        return self._memory[-self._window_size:]
+
+
+class SlidingWindowStringConcatMemory(Memory):
+    def __init__(self, token_ratio: float = 0.6, *args, **kwargs):
+        super().__init__()
+        self._memory: str       = ''
+        self.marker: str        = '[--++=|=++--]'
+        self.token_ratio: float  = token_ratio
+
+    @core.bind(engine='neurosymbolic', property='max_tokens')
+    def max_tokens(self): pass
+
+    def __getstate__(self):
+        state = super().__getstate__()
+        # Exclude the max_tokens property from being serialized
+        state.pop('_max_tokens', None)
+        return state
+
+    def __setstate__(self, state):
+        self.__dict__.update(state)
+        # Initialize _max_tokens as None, it should be set again after deserialization
+        @core.bind(engine='neurosymbolic', property='max_tokens')
+        def _max_tokens(self): pass
+        self.max_tokens = _max_tokens
+
+    def history(self):
+        return self._memory.split(self.marker)
+
+    def drop(self):
+        self._memory = ''
+
+    def store(self, query: str, *args, **kwargs):
+        # append to string to memory
+        self._memory += f'{str(query)}{self.marker}'
+
+    def forget(self, query: Symbol, *args, **kwargs):
+        # remove substring from memory
+        sym = Symbol(self._memory)
+        self._memory = str(sym - query)
+
+    def recall(self, query: str, *args, **kwargs) -> Symbol:
+        val  = self.history()
+        val  = '\n'.join(val)
+        func = Function(query)
+        func.static_context = self.static_context # TODO: consider dynamic context
+        return func(val, *args, **kwargs)
+
+
+class VectorDatabaseMemory(Memory):
+    def __init__(self, enabled: bool = True, top_k: int = 3):
+        super().__init__()
+        self.enabled: bool = enabled
+        self.top_k: int    = top_k
+
+    def store(self, query: str , *args, **kwargs):
+        if not self.enabled: return
+
+        self.add(Symbol(query).zip())
+
+    def recall(self, query: str, *args, **kwargs):
+        if not self.enabled: return
+
+        res = self.get(Symbol(query).embed().value, index_top_k=self.top_k).ast()
+
+        return [v['metadata']['text'] for v in res['matches']]
+
