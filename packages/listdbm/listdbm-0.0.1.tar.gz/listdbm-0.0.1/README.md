5ae3a7222293cd0a3dd85445f4b52f641283d071
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 15:02:51 +0000

    Add package listdbm/0.0.1/listdbm-0.0.1.tar.gz

---
+++ packages/listdbm/listdbm-0.0.1.tar.gz/listdbm-0.0.1/README.md
@@ -0,0 +1,20 @@
+# Explaining The Library:
+---
+#### This Library is Useful, Why?
+
+#### Because This Library can be used for `.JSON` Format Databases
+# Examples: 
+---
+Creating a User:
+```
+from listdbm import SetUser
+user = SetUser('rose', 'rosesecretpass1234', 'rosemail@gmail.com')
+```
+SetUser Args: `name`, `password` and `email`
+
+Result:
+```
+list: [202, True, {'uuid': '1d0552db-4280-4d7b-83e5-a97a825ba3f5', ...}]
+```
+
+# Understand More by Exploring...
---
+++ packages/listdbm/listdbm-0.0.1.tar.gz/listdbm-0.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'helps you manage databases'
+
+# Setting up
+setup(
+    name="listdbm",
+    version=VERSION,
+    author="Monil Darediya",
+    author_email="<monildarediya1@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'database', 'manage', 'db', 'list'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
