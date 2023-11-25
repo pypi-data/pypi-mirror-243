83fd2c519aaf07e5b31608f9aadab3aff181b15c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 03:04:05 +0000

    Add package lib-not-dr/0.2.0b2/lib_not_dr-0.2.0b2-py3-none-any.whl

---
+++ packages/lib-not-dr/lib_not_dr-0.2.0b2-py3-none-any.whl/lib_not_dr/__init__.py
@@ -0,0 +1,15 @@
+#  -------------------------------
+#  Difficult Rocket
+#  Copyright © 2020-2023 by shenjackyuanjie 3695888@qq.com
+#  All rights reserved
+#  -------------------------------
+
+__version__ = '0.2.0-beta.2'
+
+__all__ = [
+    '__version__',
+    'logger',
+    'nuitka',
+    'types',
+    'command'
+]
