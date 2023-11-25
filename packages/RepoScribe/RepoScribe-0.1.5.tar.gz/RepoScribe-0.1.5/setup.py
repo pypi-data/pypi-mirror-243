68ab255f5f652df6755ae6b0b885b056d70a75ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 03:04:17 +0000

    Add package RepoScribe/0.1.5/RepoScribe-0.1.5.tar.gz

---
+++ packages/RepoScribe/RepoScribe-0.1.5.tar.gz/RepoScribe-0.1.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="RepoScribe",
+    version='0.1.5',
+    author="Ruben Selander",
+    author_email="info@nordicintel.com",
+    description="A lightweight tool to compile project files and directories into a structured Markdown document",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/reversehobo/RepoScribe",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Documentation",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.8",  # Change as appropriate
+    entry_points={
+        "console_scripts": [
+            "reposcribe=reposcribe.cli:main",  # Assuming 'cli.py' contains the CLI interface
+        ],
+    },
+)
