5b1e81ca9cfe32dfb480be7d2c56e22deb3ffa48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 14:03:08 +0000

    Add package jackjtools/1.0.1/jackjtools-1.0.1.tar.gz

---
+++ packages/jackjtools/jackjtools-1.0.1.tar.gz/jackjtools-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='jackjtools',
+    version='1.0.1',
+    author='tomzmagic',
+    author_email='pi_sender@163.com',
+    description='jackjtools',
+    packages=['upload'],
+    install_requires=[
+        'requests'
+    ],
+)
