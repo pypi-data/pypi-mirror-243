6275da6da94b70084f0ead9eab53114e69f80bbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 03:04:05 +0000

    Add package fast-skimage/0.1.3/fast_skimage-0.1.3-py3-none-any.whl

---
+++ packages/fast-skimage/fast_skimage-0.1.3-py3-none-any.whl/fast_skimage/__init__.py
@@ -0,0 +1,23 @@
+from fast_skimage.Image import Image
+
+
+def zebra():
+    return Image('Pictures/zebra.jpg')
+
+def noisy_astronaut():
+    return Image('Pictures/astronaut_noisy.jpg')
+
+def new_york():
+    return Image('Pictures/nyc.jpg')
+
+def beach():
+    return Image('Pictures/etretat.jpg')
+
+def camera():
+    return Image('Pictures/camera.jpg')
+
+def walking():
+    return Image('Pictures/walking.jpg')
+
+def watermark_ULB():
+    return Image('Pictures/watermark.png')
