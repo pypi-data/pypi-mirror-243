2403e1bd303a950652c8cefb8757861051bcaf63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 14:02:54 +0000

    Add package it4u-web-crawler/1.0.12/it4u_web_crawler-1.0.12.tar.gz

---
+++ packages/it4u-web-crawler/it4u_web_crawler-1.0.12.tar.gz/it4u_web_crawler-1.0.12/README.md
@@ -0,0 +1,10 @@
+# web-crawler
+
+
+```shell
+# Build source
+python3 setup.py sdist bdist_wheel  
+
+# Upload to Pypi
+python3 -m twine upload -r pypi dist/*
+```
---
+++ packages/it4u-web-crawler/it4u_web_crawler-1.0.12.tar.gz/it4u_web_crawler-1.0.12/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name = 'it4u_web_crawler',
+    packages = ['it4u_web_crawler'],
+    version = 'v1.0.12',
+    description = 'WebCrawler will fetch all requests parallel',
+    author = 'Louis',
+    author_email = 'thonh.it@gmail.com',
+    url = 'https://github.com/louis-it4u/web-crawler',
+    download_url = 'https://github.com/louis-it4u/web-crawler/archive/refs/tags/v1.0.12.tar.gz',
+    keywords = ['requests', 'crawler'],
+    classifiers = [],
+    install_requires=[
+        'requests',
+        'reactivex',
+        'tqdm',
+        'it4u_http_request==1.0.17'
+    ]
+)
