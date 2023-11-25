f7574ad98130f10cabc1f13e3175a87066b112f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 14:02:53 +0000

    Add package nafes/0.0.3/nafes-0.0.3-py2.py3-none-any.whl

---
+++ packages/nafes/nafes-0.0.3-py2.py3-none-any.whl/nafes/__init__.py
@@ -0,0 +1,11 @@
+"""Top-level package for sofes."""
+
+__author__ = """Nana Abeka Otoo"""
+__email__ = 'abekaotoo@gmail.com'
+__version__ = '0.0.3'
+
+
+from .dataset import *
+from .mutate_labels import *
+from .mutated_validation import *
+from .nafes import *
