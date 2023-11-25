beb8b56518aef4e0dddc084dbbd81b8b8bc3da40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 14:03:08 +0000

    Add package PomidorAPI/1.6.0/PomidorAPI-1.6.0.tar.gz

---
+++ packages/PomidorAPI/PomidorAPI-1.6.0.tar.gz/PomidorAPI-1.6.0/setup.py
@@ -0,0 +1,18 @@
+#  https://github.com/SuSLiK-bozhe/pomidor_api_sdk/blob/main/README.md
+from setuptools import setup, find_packages
+
+def readme():
+  with open('README.md', 'r', encoding='utf-8') as f:
+    return f.read()
+
+setup(name='PomidorAPI',
+version='1.6.0',
+author='SuSLiK',
+description='SDK for easy contact with https://api.pomidorproject',
+long_description=readme(),
+long_description_content_type='text/markdown',
+url='https://t.me/pomidorik_sus',
+packages=find_packages(),
+install_requires=['requests>=2.25.1'],
+classifiers=['Programming Language :: Python :: 3.11','License :: OSI Approved :: MIT License','Operating System :: OS Independent'],
+python_requires='>=3.7')
