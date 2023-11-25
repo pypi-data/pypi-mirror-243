4a04a06ba4e0e9ec21856f363c9e20e040e58426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 14:02:48 +0000

    Add package dltoolbox/0.0.2/dltoolbox-0.0.2-py3-none-any.whl

---
+++ packages/dltoolbox/dltoolbox-0.0.2-py3-none-any.whl/dltoolbox/aa_code.py
@@ -0,0 +1,15 @@
+aa_code_123 = {
+    'A': 'ALA', 'R': 'ARG', 'N': 'ASN', 'D': 'ASP',
+    'C': 'CYS', 'Q': 'GLN', 'E': 'GLU', 'G': 'GLY',
+    'H': 'HIS', 'I': 'ILE', 'L': 'LEU', 'K': 'LYS',
+    'M': 'MET', 'F': 'PHE', 'P': 'PRO', 'S': 'SER',
+    'T': 'THR', 'W': 'TRP', 'Y': 'TYR', 'V': 'VAL',
+}
+
+aa_code_321 = {
+    'ALA': 'A', 'ARG': 'R', 'ASN': 'N', 'ASP': 'D', 
+    'CYS': 'C', 'GLN': 'Q', 'GLU': 'E', 'GLY': 'G', 
+    'HIS': 'H', 'ILE': 'I', 'LEU': 'L', 'LYS': 'K', 
+    'MET': 'M', 'PHE': 'F', 'PRO': 'P', 'SER': 'S', 
+    'THR': 'T', 'TRP': 'W', 'TYR': 'Y', 'VAL': 'V', 
+}
