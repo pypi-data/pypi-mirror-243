93d941df46f1a927093984017e8acd3e6a521d39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 14:02:51 +0000

    Add package PyBulma/0.1.1/PyBulma-0.1.1.tar.gz

---
+++ packages/PyBulma/PyBulma-0.1.1.tar.gz/PyBulma-0.1.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="PyBulma",
+    version='0.1.1',
+    url="https://gitlab.com/mnealer-public/pybulma",
+    author="Marc Nealer",
+    author_email="marcnealer@gmail.com",
+    license="GNU3",
+    description="Python wrappers around html elements, with bulma.io css classes, to create Jinja2/django templates",
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "jinja2"
+    ],
+    python_requires=">=3.6"
+)
