b57067a6cc139f6d9c20fa3145084012c73141f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 14:02:43 +0000

    Add package phone-footage-sorter/0.0.0/phone_footage_sorter-0.0.0.tar.gz

---
+++ packages/phone-footage-sorter/phone_footage_sorter-0.0.0.tar.gz/phone_footage_sorter-0.0.0/src/phone_footage_sorter/entry.py
@@ -0,0 +1,60 @@
+# Copyright (c) 2023 Benjamin Mummery
+
+"""Process android phone footage default names into plex-friendly filename."""
+
+import argparse
+import os
+from datetime import datetime
+
+
+def main():
+    """Search the dir for matching files and rename them."""
+
+    # Parse args
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "title",
+        type=str,
+        help="A custom name to be attached to the start of the new file names.",
+    )
+    args = parser.parse_args()
+
+    days: dict = {}
+    for file in os.listdir():
+        try:
+            if (
+                date := datetime.strptime(
+                    os.path.splitext(file)[0], "%Y%m%d_%H%M%S"
+                ).date()
+            ) not in days:
+                days[date] = [file]
+            else:
+                days[date].append(file)
+        except ValueError:
+            continue
+
+    if len(days) == 0:
+        return
+
+    print("Renaming files:")
+    for day in days:
+        if len(files := days[day]) == 1:
+            os.rename(
+                files[0],
+                f"{args.title} - "
+                f"{day.year}-{day.month:02d}-{day.day:02d}"
+                f"{os.path.splitext(file)[1]}",
+            )
+        else:
+            for i, file in enumerate(sorted(files)):
+                new_name = (
+                    f"{args.title} - "
+                    f"{day.year}-{day.month:02d}-{day.day:02d} - Part{i+1}"
+                    f"{os.path.splitext(file)[1]}"
+                )
+                os.rename(file, new_name)
+                print(f"  {file} --> {new_name}")
+
+
+if __name__ == "__main__":
+    main()
