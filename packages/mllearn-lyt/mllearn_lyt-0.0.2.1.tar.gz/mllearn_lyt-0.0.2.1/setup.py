0db1cb5fadf9d21a192c4815050c8d87658a958c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 14:02:53 +0000

    Add package mllearn-lyt/0.0.2.1/mllearn_lyt-0.0.2.1.tar.gz

---
+++ packages/mllearn-lyt/mllearn_lyt-0.0.2.1.tar.gz/mllearn_lyt-0.0.2.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='mllearn_lyt',
+    packages=['mllearn_lyt', 'mllearn_lyt.KNN'],
+    author='lyt0628',
+    author_email='lyt.0628@qq.com',
+    url='http://lyt0628.icu',
+    download_url='http://github.com//LYT0628/mllearn',
+    description='Machine Learning in Python'
+)
