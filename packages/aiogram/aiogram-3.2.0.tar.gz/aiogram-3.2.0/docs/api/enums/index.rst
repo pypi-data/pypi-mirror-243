3987918fb48f3613762e9293f3668961d481eb42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 03:04:05 +0000

    Add package aiogram/3.2.0/aiogram-3.2.0.tar.gz

---
+++ packages/aiogram/aiogram-3.2.0.tar.gz/aiogram-3.2.0/docs/api/enums/index.rst
@@ -0,0 +1,32 @@
+.. _enums:
+
+#####
+Enums
+#####
+
+Here is list of all available enums:
+
+
+.. toctree::
+    :maxdepth: 1
+
+    bot_command_scope_type
+    chat_action
+    chat_member_status
+    chat_type
+    content_type
+    currency
+    dice_emoji
+    encrypted_passport_element
+    inline_query_result_type
+    input_media_type
+    mask_position_point
+    menu_button_type
+    message_entity_type
+    parse_mode
+    passport_element_error_type
+    poll_type
+    sticker_format
+    sticker_type
+    topic_icon_color
+    update_type
---
+++ packages/aiogram/aiogram-3.2.0.tar.gz/aiogram-3.2.0/tests/test_utils/test_markdown.py
@@ -0,0 +1,62 @@
+from typing import Any, Callable, Optional, Tuple
+
+import pytest
+
+from aiogram.utils.markdown import (
+    bold,
+    code,
+    hbold,
+    hcode,
+    hide_link,
+    hitalic,
+    hlink,
+    hpre,
+    hstrikethrough,
+    hunderline,
+    italic,
+    link,
+    pre,
+    strikethrough,
+    text,
+    underline,
+)
+
+
+class TestMarkdown:
+    @pytest.mark.parametrize(
+        "func,args,sep,result",
+        [
+            [text, ("test", "test"), " ", "test test"],
+            [text, ("test", "test"), "\n", "test\ntest"],
+            [text, ("test", "test"), None, "test test"],
+            [bold, ("test", "test"), " ", "*test test*"],
+            [hbold, ("test", "test"), " ", "<b>test test</b>"],
+            [italic, ("test", "test"), " ", "_\rtest test_\r"],
+            [hitalic, ("test", "test"), " ", "<i>test test</i>"],
+            [code, ("test", "test"), " ", "`test test`"],
+            [hcode, ("test", "test"), " ", "<code>test test</code>"],
+            [pre, ("test", "test"), " ", "```\ntest test\n```"],
+            [hpre, ("test", "test"), " ", "<pre>test test</pre>"],
+            [underline, ("test", "test"), " ", "__\rtest test__\r"],
+            [hunderline, ("test", "test"), " ", "<u>test test</u>"],
+            [strikethrough, ("test", "test"), " ", "~test test~"],
+            [hstrikethrough, ("test", "test"), " ", "<s>test test</s>"],
+            [link, ("test", "https://aiogram.dev"), None, "[test](https://aiogram.dev)"],
+            [
+                hlink,
+                ("test", "https://aiogram.dev"),
+                None,
+                '<a href="https://aiogram.dev">test</a>',
+            ],
+            [
+                hide_link,
+                ("https://aiogram.dev",),
+                None,
+                '<a href="https://aiogram.dev">&#8203;</a>',
+            ],
+        ],
+    )
+    def test_formatter(
+        self, func: Callable[[Any], Any], args: Tuple[str], sep: Optional[str], result: str
+    ):
+        assert func(*args, **({"sep": sep} if sep is not None else {})) == result  # type: ignore
