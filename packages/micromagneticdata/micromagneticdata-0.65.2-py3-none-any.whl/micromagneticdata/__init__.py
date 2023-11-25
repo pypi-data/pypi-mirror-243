adec7bdb0e5c0c8008ced4d864e3727fc34ad28d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 15:03:19 +0000

    Add package micromagneticdata/0.65.2/micromagneticdata-0.65.2-py3-none-any.whl

---
+++ packages/micromagneticdata/micromagneticdata-0.65.2-py3-none-any.whl/micromagneticdata/__init__.py
@@ -0,0 +1,30 @@
+"""Analyse micromagnetic data."""
+import importlib.metadata
+
+import pytest
+
+from .abstract_drive import AbstractDrive
+from .combined_drive import CombinedDrive
+from .data import Data
+from .drive import Drive
+from .mumax3drive import Mumax3Drive
+from .oommfdrive import OOMMFDrive
+
+__version__ = importlib.metadata.version(__package__)
+
+
+def test():
+    """Run all package tests.
+
+    Examples
+    --------
+    1. Run all tests.
+
+    >>> import micromagneticdata
+    ...
+    >>> # micromagneticdata.test()
+
+    """
+    return pytest.main(
+        ["-v", "--pyargs", "micromagneticdata", "-l"]
+    )  # pragma: no cover
