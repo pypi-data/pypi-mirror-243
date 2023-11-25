9fed714c9f3217b4e107d76c571b1511c96d0bc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 03:03:54 +0000

    Add package symbolicai/0.4.30/symbolicai-0.4.30.tar.gz

---
+++ packages/symbolicai/symbolicai-0.4.30.tar.gz/symbolicai-0.4.30/symai/extended/interfaces/python.py
@@ -0,0 +1,14 @@
+from ... import core
+from ...symbol import Expression
+
+
+class python(Expression):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def __call__(self, expr: str, **kwargs) -> "python":
+        sym = self._to_symbol(expr)
+        @core.execute(**kwargs)
+        def _func(_):
+            pass
+        return _func(sym)
---
+++ packages/symbolicai/symbolicai-0.4.30.tar.gz/symbolicai-0.4.30/symai/ops/__init__.py
@@ -0,0 +1,19 @@
+from .symbol_primitives import *
+
+SYMBOL_PRIMITIVES = [
+    ContextualPrimitives,
+    ValueHandlingPrimitives,
+    ComparisonPrimitives,
+    ExpressionHandlingPrimitives,
+    DataHandlingPrimitives,
+    UniquenessPrimitives,
+    PatternMatchingPrimitives,
+    QueryHandlingPrimitives,
+    ExecutionControlPrimitives,
+    TemplateStylingPrimitives,
+    DataClusteringPrimitives,
+    PersistencePrimitives,
+    OutputHandlingPrimitives,
+    FineTuningPrimitives,
+]
+
