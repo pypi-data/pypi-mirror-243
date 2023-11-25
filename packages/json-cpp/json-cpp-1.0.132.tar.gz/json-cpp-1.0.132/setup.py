96e008f09368dc5ec8337abf567b66e3c8f8b3be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-25 03:03:48 +0000

    Add package json-cpp/1.0.132/json-cpp-1.0.132.tar.gz

---
+++ packages/json-cpp/json-cpp-1.0.132.tar.gz/json-cpp-1.0.132/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+import os
+if os.path.isfile('./json_cpp/README.md'):
+	with open('./json_cpp/README.md') as f:
+		long_description = f.read()
+else:
+	long_description = ''
+setup(name='json-cpp',description='a better json library',author='german espinosa',author_email='germanespinosa@gmail.com',long_description=long_description,long_description_content_type='text/markdown',packages=['json_cpp'],install_requires=['requests'],license='MIT',version='1.0.132',zip_safe=False)
