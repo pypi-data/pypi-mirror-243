a00972d11458494921eb6e2523d71a661447410c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 15:03:14 +0000

    Add package pyconiq/0.0.1/pyconiq-0.0.1.tar.gz

---
+++ packages/pyconiq/pyconiq-0.0.1.tar.gz/pyconiq-0.0.1/src/pyconiq/__init__.py
@@ -0,0 +1,7 @@
+r"""
+pyconiq: an unofficial Python package to interface with the payment processor Payconiq.
+"""
+
+from __future__ import annotations
+
+from pyconiq.concepts import merchant  # noqa: F401
