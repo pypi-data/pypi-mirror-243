1351b3391c79b208e41700ec55a8962a7064c4fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 14:06:34 +0000

    Add package metalarchivist/0.1.11/metalarchivist-0.1.11.tar.gz

---
+++ packages/metalarchivist/metalarchivist-0.1.11.tar.gz/metalarchivist-0.1.11/src/metalarchivist/interface/__init__.py
@@ -0,0 +1,8 @@
+
+from .band import BandProfile, BandLink, Genres, Themes
+from .album import AlbumProfile, AlbumRelease, AlbumLink, ReleasePage, ReleasePages
+
+
+__all__ = ['BandProfile', 'BandLink', 'Genres', 'Themes',
+           'AlbumProfile', 'AlbumRelease', 'AlbumLink', 
+           'ReleasePage', 'ReleasePages']
