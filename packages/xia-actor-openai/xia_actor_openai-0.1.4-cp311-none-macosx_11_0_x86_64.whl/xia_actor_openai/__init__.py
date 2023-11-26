eee0e7e3c17fe944ac63c4265522d7bf40a546ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 15:03:02 +0000

    Add package xia-actor-openai/0.1.4/xia_actor_openai-0.1.4-cp311-none-macosx_11_0_x86_64.whl

---
+++ packages/xia-actor-openai/xia_actor_openai-0.1.4-cp311-none-macosx_11_0_x86_64.whl/xia_actor_openai/__init__.py
@@ -0,0 +1,12 @@
+from xia_actor_openai.actor import GptActor, GptMindset
+from xia_actor_openai.graphist import PollinationActor, PollinationMindset
+from xia_actor_openai.graphist import DalleActor, DalleMindset
+
+
+__all__ = [
+    "GptActor", "GptMindset",
+    "PollinationActor", "PollinationMindset",
+    "DalleActor", "DalleMindset"
+]
+
+__version__ = "0.1.4"
