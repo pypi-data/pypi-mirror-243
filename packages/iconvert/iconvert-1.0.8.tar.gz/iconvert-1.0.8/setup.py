803b68abdf4e7c868229c53d4faf14f3c34b4e0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 15:04:06 +0000

    Add package iconvert/1.0.8/iconvert-1.0.8.tar.gz

---
+++ packages/iconvert/iconvert-1.0.8.tar.gz/iconvert-1.0.8/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='iconvert',
+    version='1.0.8',
+    author='nrdrch',
+    description='Convert images to .ico format for icon use in Windows',
+    keywords='image convert ico windows icon',
+    url='https://github.com/nrdrch/iconvert',
+    README='README.md',
+    packages=find_packages(where='src'),  # Ensure src directory is included
+    package_dir={'': 'src'},
+    py_modules=['iconvert'],
+    entry_points={
+        'console_scripts': [
+            'iconvert=iconvert:main',
+        ]
+    },
+    install_requires=[
+        'Pillow',
+    ],
+)
