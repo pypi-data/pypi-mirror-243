a536a2dcd874f350c8abd86a2c886e98e1953874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 16:03:25 +0000

    Add package VisOpy/0.0.2/VisOpy-0.0.2-py3-none-any.whl

---
+++ packages/VisOpy/VisOpy-0.0.2-py3-none-any.whl/VisOpy/aberturas.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+class Aberturas:
+    '''
+    Contiene las aberturas principales usadas en óptica, 
+    '''
+
+    def __init__(self,Lado:int,Nmuestras:int,radiox:float,radioy:float=False): #atributos
+        self.L = Lado
+        self.N = Nmuestras
+        self.wx = radiox
+        self.wy = radioy
+
+    def x(self):
+        return np.arange(-self.L/2,self.L/2,self.L/self.N)
+    
+    def xx(self):
+        return np.meshgrid(self.x(),self.x())
+    
+    def rho(self):
+        X,Y = self.xx()
+        return np.sqrt(X**2+Y**2)/self.wx
+    
+    def phi(self):
+        X,Y = self.xx()
+        return np.arctan2(Y,X)
+    
+    def circ(self):
+        X,Y = self.xx()
+        return (np.abs(np.sqrt(X**2+Y**2)/self.wx)<=1)*1
+
+    def rect(self):
+        X,Y = self.xx()
+        def rect2D(x): 
+            out = np.zeros((len(x),len(x)))
+            for cont in range(0,len(x)):
+                for cont2 in range(0,len(x)):
+                    out[cont,cont2]=int(np.abs(x[cont,cont2])<=1/2)
+            return out
+        if not(self.wy):
+            return rect2D(X/(2*self.wx))*rect2D(Y/(2*self.wx))
+        else:
+            return rect2D(X/(2*self.wx))*rect2D(Y/(2*self.wy))
