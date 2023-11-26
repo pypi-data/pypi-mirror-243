fe2ef6044b2b89e116e11be3d3f4cde6658b15ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 15:03:38 +0000

    Add package BanterBot/0.0.11/BanterBot-0.0.11.tar.gz

---
+++ packages/BanterBot/BanterBot-0.0.11.tar.gz/BanterBot-0.0.11/banterbot/gui/__init__.py
@@ -0,0 +1,4 @@
+from banterbot.gui.tk_multiplayer_interface import TKMultiplayerInterface
+from banterbot.gui.tk_simple_interface import TKSimpleInterface
+
+__all__ = ["TKMultiplayerInterface", "TKSimpleInterface"]
