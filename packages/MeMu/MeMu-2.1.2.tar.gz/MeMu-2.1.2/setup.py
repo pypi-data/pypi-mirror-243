3932d341aff25ac7358fd9b903a40c02c94553ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 14:04:44 +0000

    Add package MeMu/2.1.2/MeMu-2.1.2.tar.gz

---
+++ packages/MeMu/MeMu-2.1.2.tar.gz/MeMu-2.1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MeMu',
+    version='2.1.2',
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    package_data={"MeMu": ["*.ico", "*.png"]},
+    install_requires=[
+        'customtkinter>=5.1.3',
+        'Pillow>=9.4.0',
+        'pyboy>=1.6.6'
+    ],
+)
