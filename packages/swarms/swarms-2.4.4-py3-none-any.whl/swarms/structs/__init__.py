6f68f36789cdb76b869074cf996996f6881709e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 15:03:29 +0000

    Add package swarms/2.4.4/swarms-2.4.4-py3-none-any.whl

---
+++ packages/swarms/swarms-2.4.4-py3-none-any.whl/swarms/structs/__init__.py
@@ -0,0 +1,5 @@
+from swarms.structs.flow import Flow
+from swarms.structs.sequential_workflow import SequentialWorkflow
+from swarms.structs.autoscaler import AutoScaler
+
+__all__ = ["Flow", "SequentialWorkflow", "AutoScaler"]
