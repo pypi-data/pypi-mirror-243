8ae1441bdad99e2cbe1ed29d14138fc27ea25e00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 14:05:37 +0000

    Add package gitutils-cli/0.5.1/gitutils-cli-0.5.1.tar.gz

---
+++ packages/gitutils-cli/gitutils-cli-0.5.1.tar.gz/gitutils-cli-0.5.1/setup.py
@@ -0,0 +1,16 @@
+"""
+Generic setup for this python package
+"""
+from setuptools import find_packages, setup
+
+setup(
+    name="gitutils-cli",
+    version="0.5.01",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "gitutils-merge=cli.git_batch_merger:git_batch_merger",
+            "gitutils-cherry-pick=cli.git_batch_cherry_picker:git_batch_cherry_picker",
+        ],
+    },
+)
