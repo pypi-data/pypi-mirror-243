5f8c5347c4ab967d527594837ba6e50e645442fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 15:03:01 +0000

    Add package llm-guard/0.3.3/llm-guard-0.3.3.tar.gz

---
+++ packages/llm-guard/llm-guard-0.3.3.tar.gz/llm-guard-0.3.3/llm_guard/output_scanners/deanonymize.py
@@ -0,0 +1,33 @@
+from llm_guard.util import logger
+from llm_guard.vault import Vault
+
+from .base import Scanner
+
+
+class Deanonymize(Scanner):
+    """
+    A class for replacing placeholders in the model output with real values from a vault.
+
+    This class uses the Vault class to access stored values and replaces any placeholders
+    in the model's output with their corresponding values from the vault.
+    """
+
+    def __init__(self, vault: Vault):
+        """
+        Initializes an instance of the Deanonymize class.
+
+        Parameters:
+            vault (Vault): An instance of the Vault class which stores the real values.
+        """
+        self._vault = vault
+
+    def scan(self, prompt: str, output: str) -> (str, bool, float):
+        vault_items = self._vault.get()
+        if len(vault_items) == 0:
+            logger.warning("No items found in the Vault")
+
+        for vault_item in vault_items:
+            logger.debug(f"Replaced placeholder ${vault_item[0]} with real value")
+            output = output.replace(vault_item[0], vault_item[1])
+
+        return output, True, 0.0
