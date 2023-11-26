0f10e6478331130a574faf0c5da3a7bbd0ef227f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 15:03:05 +0000

    Add package promptengineers/0.1.8/promptengineers-0.1.8.tar.gz

---
+++ packages/promptengineers/promptengineers-0.1.8.tar.gz/promptengineers-0.1.8/setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='promptengineers',
+    version='0.1.8',
+    packages=find_packages(),
+    install_requires=[
+        'ujson',
+        'fastapi',
+        'uvicorn',
+    ],
+    extras_require={
+        'history': ['motor', 'pymongo', 'cryptography'],
+        'storage': [ 'minio', 'python-multipart'],
+        'chat': [
+            'langchain',
+            'openai',
+            'python-multipart',
+            'redis',
+            'pinecone-client',
+            'youtube-transcript-api',
+            'pypdf',
+            'numexpr',
+            'tiktoken',
+            'nest_asyncio',
+            'beautifulsoup4',
+        ],
+        'retrieval': [
+            'langchain',
+            'openai',
+            'python-multipart',
+            'redis',
+            'pinecone-client',
+            'youtube-transcript-api',
+            'pypdf',
+            'numexpr',
+            'tiktoken',
+            'nest_asyncio',
+            'beautifulsoup4',
+        ],
+    },
+    author='Ryan Eggleston',
+    author_email='kre8mymedia@gmail.com',
+    description='A collection of utilities by Prompt Engineers',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/promptengineers-ai/core',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
