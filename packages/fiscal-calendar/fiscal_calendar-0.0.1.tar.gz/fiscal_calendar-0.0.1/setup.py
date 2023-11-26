28de552237ce5b81644c4b129be1db84812dce58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 16:03:19 +0000

    Add package fiscal-calendar/0.0.1/fiscal_calendar-0.0.1.tar.gz

---
+++ packages/fiscal-calendar/fiscal_calendar-0.0.1.tar.gz/fiscal_calendar-0.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='fiscal_calendar',
+    version='0.0.1',
+    license='MIT',
+    description='Fiscal Retail Calendar - 4-5-4 week retail calendar',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Tony Hollaar',
+    author_email='thollaar@gmail.com',
+    url='https://github.com/tonyhollaar/fiscal_calendar',
+    download_url='https://github.com/tonyhollaar/fiscal_calendar/archive/refs/tags/v0.0.1.tar.gz',
+    packages=find_packages(),
+    install_requires=[
+        'pandas>=1.0.0',
+        'numpy>=1.18.0',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
