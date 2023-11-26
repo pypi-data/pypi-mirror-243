670def4edde623e7591ef8513128379749276aec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 15:02:52 +0000

    Add package dynamic-executor/1.0.0/dynamic_executor-1.0.0-py3-none-any.whl

---
+++ packages/dynamic-executor/dynamic_executor-1.0.0-py3-none-any.whl/dython/utils/exec_in_dynamic_mode.py
@@ -0,0 +1,33 @@
+import traceback
+from pathlib import Path
+from typing import Generator
+
+from src.dython.utils import get_modules
+from src.dython.utils.re_import import re_import_modules
+
+
+def exec_in_dynamic_mode(
+    local_vars: dict,
+    global_vars: dict,
+    executor_path: Path = None,
+    finnish_upon_success: bool = True,
+) -> Generator[str, None, None]:
+    if executor_path is None:
+        executor_path = Path("executor.py")
+    if not executor_path.exists():
+        executor_path.write_text("# Save mode executor")
+        yield f"Created an executor file in {executor_path.absolute()}"
+    done = False
+    while True:
+        try:
+            compiled = compile(executor_path.read_text(), executor_path.name, "exec")
+            exec(compiled, global_vars, local_vars)
+            done = True
+        except:
+            traceback.print_exc()
+            yield traceback.format_exc()
+        if not finnish_upon_success or not done:
+            modules = get_modules()
+            re_import_modules(modules, local_vars, global_vars)
+        else:
+            return
