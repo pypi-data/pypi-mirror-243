ccd2299f04e35897c7ce1161f0a483c0ad9e6fa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 15:03:37 +0000

    Add package robotic-transformer-pytorch/0.1.1/robotic-transformer-pytorch-0.1.1.tar.gz

---
+++ packages/robotic-transformer-pytorch/robotic-transformer-pytorch-0.1.1.tar.gz/robotic-transformer-pytorch-0.1.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'robotic-transformer-pytorch',
+  packages = find_packages(exclude=[]),
+  version = '0.1.1',
+  license='MIT',
+  description = 'Robotic Transformer - Pytorch',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/lucidrains/robotic-transformer-pytorch',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'transformers',
+    'attention mechanism',
+    'robotics'
+  ],
+  install_requires=[
+    'classifier-free-guidance-pytorch>=0.1.4',
+    'einops>=0.6',
+    'torch>=1.6',
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
