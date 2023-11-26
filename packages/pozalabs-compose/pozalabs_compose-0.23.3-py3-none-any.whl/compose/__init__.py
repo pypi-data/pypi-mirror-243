1ea32630d4135f53e83417f4df4a1864a55c5d1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 14:05:29 +0000

    Add package pozalabs-compose/0.23.3/pozalabs_compose-0.23.3-py3-none-any.whl

---
+++ packages/pozalabs-compose/pozalabs_compose-0.23.3-py3-none-any.whl/compose/__init__.py
@@ -0,0 +1,35 @@
+from . import (
+    command,
+    compat,
+    dependency,
+    entity,
+    event,
+    messaging,
+    pagination,
+    query,
+    repository,
+    result,
+    schema,
+    types,
+    uow,
+)
+from .container import BaseModel, TimeStampedModel
+
+__all__ = [
+    "BaseModel",
+    "TimeStampedModel",
+    "entity",
+    "field",
+    "schema",
+    "repository",
+    "query",
+    "types",
+    "command",
+    "event",
+    "dependency",
+    "compat",
+    "pagination",
+    "result",
+    "uow",
+    "messaging",
+]
