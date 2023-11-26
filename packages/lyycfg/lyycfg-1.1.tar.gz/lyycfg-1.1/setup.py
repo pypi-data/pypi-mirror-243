8549d2733c2cd46d79b81c0cd90067912c2203c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 14:04:16 +0000

    Add package lyycfg/1.1/lyycfg-1.1.tar.gz

---
+++ packages/lyycfg/lyycfg-1.1.tar.gz/lyycfg-1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='lyycfg',
+    version='1.1',
+    author='lyy',
+    author_email='',
+    description='calendar for  lyy',
+    #packages=find_packages(),
+    license="MIT",
+    install_requires=[
+
+
+    ],
+)
