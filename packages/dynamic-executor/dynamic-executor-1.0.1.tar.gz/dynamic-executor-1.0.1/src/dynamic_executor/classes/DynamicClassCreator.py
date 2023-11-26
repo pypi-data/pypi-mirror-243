68c589d276342b97d68ae31a664d302b43da0481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 15:02:53 +0000

    Add package dynamic-executor/1.0.1/dynamic-executor-1.0.1.tar.gz

---
+++ packages/dynamic-executor/dynamic-executor-1.0.1.tar.gz/dynamic-executor-1.0.1/src/dynamic_executor/classes/DynamicClassCreator.py
@@ -0,0 +1,26 @@
+from collections import OrderedDict
+from typing import Self
+import sys
+
+sys.modules = OrderedDict(sys.modules)
+
+
+def new_wrapper(new):
+    def wrapper(cls, *args, **kwargs):
+        new_instance = new(cls, *args, **kwargs)
+        if new_instance not in cls._instances:
+            cls._instances.append(new_instance)
+        return new_instance
+
+    return wrapper
+
+
+class DynamicClassCreator(type):
+    created_classes: list[Self] = []
+
+    def __new__(metacls, name, bases, namespace):
+        new_class = super().__new__(metacls, name, bases, namespace)
+        metacls.created_classes.append(new_class)
+        new_class._instances = []
+        new_class.__new__ = new_wrapper(new_class.__new__)
+        return new_class
