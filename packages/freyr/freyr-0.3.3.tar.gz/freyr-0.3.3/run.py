5fb8235056d671d92686ffde009941e4f50b5a73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 14:03:18 +0000

    Add package freyr/0.3.3/freyr-0.3.3.tar.gz

---
+++ packages/freyr/freyr-0.3.3.tar.gz/freyr-0.3.3/run.py
@@ -0,0 +1,19 @@
+import uvicorn
+
+from freyr.constants import constants
+
+
+def main() -> None:
+    uvicorn.run(
+        "freyr.__main__:app",
+        host=constants.settings.website.host,
+        port=constants.settings.website.port,
+        use_colors=True,
+        server_header=False,
+        reload=constants.settings.website.reload,
+        log_config=None,
+    )
+
+
+if __name__ == "__main__":
+    main()
