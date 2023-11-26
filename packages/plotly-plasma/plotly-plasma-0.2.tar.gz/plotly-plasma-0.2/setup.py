6838caeaa882bf202216743b8e9bc832df0aa224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 15:03:37 +0000

    Add package plotly-plasma/0.2/plotly-plasma-0.2.tar.gz

---
+++ packages/plotly-plasma/plotly-plasma-0.2.tar.gz/plotly-plasma-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="plotly-plasma",
+    version="0.2",
+    package_dir={"": "src"},
+    packages=find_packages(),
+    description="A small opinionated enhancement for Plotly",
+    author="Nilo Araujo",
+    author_email="renard.agne@gmail.com",
+    url="https://github.com/a0th/plotly",
+    install_requires=["plotly>=5.0.0", "pandas>=1.0.0"],
+)
