805a098d93aecf7700376e11c5f10d1a6f2d1415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 14:06:53 +0000

    Add package hexcolurs/1.4/hexcolurs-1.4.tar.gz

---
+++ packages/hexcolurs/hexcolurs-1.4.tar.gz/hexcolurs-1.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name="hexcolurs",
+    version='1.4',
+    license='Eclipse Public License 2.0',
+    authors=["smithsha"],
+    author_email="admin.gmail@gmail.com",
+    description="Bring Colorful code in python",
+    long_description='Documentation: https://github.com/kittyplot/hexcolurs',
+    packages=['hexcolurs']
+)
+
