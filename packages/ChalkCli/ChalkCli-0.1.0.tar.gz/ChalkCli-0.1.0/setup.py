8397c2825bce983bbbc8de002c891a17a9ef1411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 16:03:32 +0000

    Add package ChalkCli/0.1.0/ChalkCli-0.1.0.tar.gz

---
+++ packages/ChalkCli/ChalkCli-0.1.0.tar.gz/ChalkCli-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='ChalkCli',
+    version='0.1.0',
+    author='Ishan Oshada',
+    author_email='Ishan.kodithuwakku.official@gmail.com',
+    description='A Python package for terminal text styling and coloring.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ishanoshada/ChalkCli',
+    packages=find_packages(),
+     classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
