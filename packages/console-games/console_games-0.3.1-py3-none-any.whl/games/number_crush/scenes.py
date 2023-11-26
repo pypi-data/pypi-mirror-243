a539dffa4825b4137f05b3ada47cdd8a6ce9282a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 15:03:38 +0000

    Add package console-games/0.3.1/console_games-0.3.1-py3-none-any.whl

---
+++ packages/console-games/console_games-0.3.1-py3-none-any.whl/games/number_crush/scenes.py
@@ -0,0 +1,32 @@
+from games.screen import Scene
+from games.objects import Monologue
+from games.number_crush.objects import Numbers
+
+
+class Crush(Scene):
+    def init(self):
+        self.numbers = Numbers(x=self.screen.width / 2, y=0, player=self.player)
+        self.player.reset()
+
+    def start(self):
+        self.screen.add(self.player, self.numbers)
+
+    def escape_pressed(self):
+        self.controller.done = True
+
+
+class Intro(Scene):
+    def init(self):
+        self.intro = Monologue(self.controller.player.x, self.controller.player.y - 3,
+                               on_finish=self.next,
+                               texts=["Hi, I am {}!".format(self.controller.player.name),
+                                      "Most people don't like math, but not me.",
+                                      "I LOVE to crush them!! :D",
+                                      "Type the answer using number keys.",
+                                      "Ready? Let's CRUSH!!"])
+
+    def start(self):
+        self.screen.add(self.intro, self.controller.player)
+
+    def key_pressed(self, key):
+        self.next()
