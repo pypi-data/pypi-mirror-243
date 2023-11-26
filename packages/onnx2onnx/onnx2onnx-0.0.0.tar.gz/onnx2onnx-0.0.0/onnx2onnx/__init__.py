e360138c21b089e1daf6f619e5e4c70cfebe3622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 14:05:07 +0000

    Add package onnx2onnx/0.0.0/onnx2onnx-0.0.0.tar.gz

---
+++ packages/onnx2onnx/onnx2onnx-0.0.0.tar.gz/onnx2onnx-0.0.0/onnx2onnx/__init__.py
@@ -0,0 +1,10 @@
+"""An Open Neural Network Exchange (ONNX) Optimization and Transformation Tool.
+
+Copyright Wenyi Tang 2023
+
+:Author: Wenyi Tang
+:Email: wenyitang@outlook.com
+
+"""
+
+__version__ = "0.0.0"
