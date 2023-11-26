7c30597798bef410a49d64c2f99a7d892012b4d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 15:03:06 +0000

    Add package nbwipers/0.1.1/nbwipers-0.1.1.tar.gz

---
+++ packages/nbwipers/nbwipers-0.1.1.tar.gz/nbwipers-0.1.1/src/install.rs
@@ -0,0 +1,175 @@
+use anyhow::{anyhow, Error};
+
+use std::{fs, io::BufWriter, path::Path, path::PathBuf};
+
+use bstr::BStr;
+use gix_config::parse::section::Key;
+use gix_config::Source;
+use std::{collections::BTreeMap, io::Write};
+
+use itertools::Itertools;
+
+use crate::cli::GitConfigType;
+
+pub fn install_config(config_type: GitConfigType) -> Result<(), Error> {
+    let cur_exe = std::env::current_exe()?;
+    let cur_dir = std::env::current_dir()?;
+    let cur_exe_str = cur_exe
+        .to_str()
+        .ok_or_else(|| (anyhow!("Executable path cannot be converted to unicode")))?
+        .replace('\\', "/");
+
+    let source = match config_type {
+        GitConfigType::Global => Source::User,
+        GitConfigType::System => Source::System,
+        GitConfigType::Local => Source::Local,
+    };
+
+    // fails for sources without storage location. We don't use those ones.
+    #[allow(clippy::unwrap_used)]
+    let file_path: PathBuf = match config_type {
+        GitConfigType::Global | GitConfigType::System => source
+            .storage_location(&mut gix_path::env::var)
+            .as_deref()
+            .unwrap()
+            .to_owned(),
+        GitConfigType::Local => {
+            let dotgit = gix_discover::upwards(&cur_dir)?
+                .0
+                .into_repository_and_work_tree_directories()
+                .0;
+            dotgit.join(source.storage_location(&mut gix_path::env::var).unwrap())
+        }
+    };
+    let mut file = if file_path.is_file() {
+        gix_config::File::from_path_no_includes(file_path.clone(), source)?
+    } else {
+        gix_config::File::new(gix_config::file::Metadata::from(source))
+    };
+
+    // fails for invalid section names. This one is ok
+    #[allow(clippy::unwrap_used)]
+    let mut nbwipers_section = file
+        .section_mut_or_create_new("filter", Some("nbwipers".into()))
+        .unwrap();
+    // fails for invalid section names. This one is ok
+    #[allow(clippy::unwrap_used)]
+    nbwipers_section.set(
+        Key::try_from("clean").unwrap(),
+        BStr::new(format!("\"{}\" clean -", cur_exe_str.as_str()).as_str()),
+    );
+    #[allow(clippy::unwrap_used)]
+    nbwipers_section.set(Key::try_from("smudge").unwrap(), BStr::new("cat"));
+
+    // fails for invalid section names. This one is ok
+    #[allow(clippy::unwrap_used)]
+    let mut diff_section = file
+        .section_mut_or_create_new("diff", Some("nbwipers".into()))
+        .unwrap();
+
+    // fails for invalid section names. This one is ok
+    #[allow(clippy::unwrap_used)]
+    diff_section.set(
+        Key::try_from("textconv").unwrap(),
+        BStr::new(format!("\"{}\" clean -t", cur_exe_str.as_str()).as_str()),
+    );
+    println!("Writing to {}", file_path.display());
+    let mut writer = BufWriter::new(fs::File::create(file_path)?);
+    file.write_to(&mut writer)?;
+
+    Ok(())
+}
+
+fn resolve_attribute_file(
+    config_type: GitConfigType,
+    attribute_file: Option<&Path>,
+) -> Result<PathBuf, Error> {
+    if let Some(path) = attribute_file {
+        Ok(path.to_owned())
+    } else {
+        let cur_dir = std::env::current_dir()?;
+
+        let source = match config_type {
+            GitConfigType::Global => gix_attributes::Source::Git,
+            GitConfigType::Local => gix_attributes::Source::Local,
+            GitConfigType::System => gix_attributes::Source::System,
+        };
+
+        let file_path: PathBuf = match config_type {
+            #[allow(clippy::unwrap_used)]
+            GitConfigType::Global | GitConfigType::System => source
+                .storage_location(&mut gix_path::env::var)
+                .as_deref()
+                .unwrap()
+                .to_owned(),
+            GitConfigType::Local => {
+                let dotgit = gix_discover::upwards(&cur_dir)?
+                    .0
+                    .into_repository_and_work_tree_directories()
+                    .0;
+                #[allow(clippy::unwrap_used)]
+                dotgit.join(source.storage_location(&mut gix_path::env::var).unwrap())
+            }
+        };
+        Ok(file_path)
+    }
+}
+
+pub fn install_attributes(
+    config_type: GitConfigType,
+    attribute_file: Option<&Path>,
+) -> Result<(), Error> {
+    let file_path = resolve_attribute_file(config_type, attribute_file)?;
+    let to_add_lines = &[
+        "*.ipynb filter=nbwipers",
+        "*.zpln filter=nbwipers",
+        "*.ipynb diff=nbwipers",
+    ];
+    if file_path.is_file() {
+        let attribute_bytes = fs::read(&file_path)?;
+
+        // let to_add_str = to_add_lines.join("\n").as_bytes();
+        #[allow(clippy::unwrap_used)]
+        let to_add_values = to_add_lines
+            .iter()
+            .map(|x| gix_attributes::parse(x.as_bytes()).next().unwrap().unwrap())
+            .flat_map(|(kind, rhs, _)| {
+                rhs.filter_map(Result::ok)
+                    .map(move |a| (kind.clone(), a.to_owned()))
+            });
+
+        let mut to_add: BTreeMap<_, _> = to_add_values.zip(to_add_lines).collect();
+        let extra_newline = attribute_bytes.last() == Some(&b'\n');
+
+        let mut lines = gix_attributes::parse(&attribute_bytes);
+
+        for (kind, x, _) in lines.by_ref().filter_map(Result::ok) {
+            //
+            for ass in x.filter_map(Result::ok) {
+                to_add.remove(&(kind.clone(), ass.to_owned()));
+            }
+        }
+        if !to_add.is_empty() {
+            println!("Writing to {}", file_path.display());
+
+            let mut writer = fs::OpenOptions::new()
+                .create(true)
+                .append(true)
+                .open(&file_path)?;
+            let extra = if extra_newline { "" } else { "\n" };
+            writeln!(writer, "{}{}", extra, to_add.values().join("\n"))?;
+        }
+    } else {
+        println!("Writing to {}", file_path.display());
+        let mut writer = fs::File::create(file_path)?;
+
+        for line in to_add_lines {
+            writeln!(writer, "{line}")?;
+        }
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {}
---
+++ packages/nbwipers/nbwipers-0.1.1.tar.gz/nbwipers-0.1.1/src/main.rs
@@ -0,0 +1,141 @@
+#![warn(clippy::all, clippy::pedantic)]
+#![allow(clippy::module_name_repetitions)]
+#![warn(clippy::unwrap_used)]
+#![warn(missing_docs)]
+
+/*! nbwipers is a command line tool to wipe clean Jupyter Notebooks
+ *
+ *
+ */
+
+use std::path::{Path, PathBuf};
+
+use crate::settings::Settings;
+use anyhow::{anyhow, bail, Error};
+use clap::Parser;
+use cli::{CheckCommand, CleanAllCommand, CleanCommand, Commands, CommonArgs, InstallCommand};
+use colored::Colorize;
+use files::{find_notebooks, read_nb, read_nb_stdin, relativize_path, FoundNotebooks};
+use rayon::prelude::*;
+use strip::{strip_single, StripResult};
+
+mod cell_impl;
+mod check;
+mod cli;
+mod config;
+mod extra_keys;
+mod files;
+mod install;
+mod schema;
+mod settings;
+mod strip;
+mod utils;
+
+fn check_all(files: &[PathBuf], cli: CommonArgs) -> Result<(), Error> {
+    let (args, overrides) = cli.partition();
+    let settings = Settings::construct(args.config.as_deref(), &overrides)?;
+    let nbs = find_notebooks(files)?;
+    let check_results_by_file = match nbs {
+        FoundNotebooks::Stdin => match read_nb_stdin() {
+            Ok(nb) => vec![(Path::new("-"), check::check_nb(&nb, &settings))],
+            Err(e) => vec![(Path::new("-"), vec![e.into()])],
+        },
+        FoundNotebooks::Files(ref nbs) => {
+            nbs.par_iter()
+                .map(|nb_path| {
+                    // println!("{nb_path:?}");
+                    match read_nb(nb_path) {
+                        Ok(nb) => (nb_path.as_path(), check::check_nb(&nb, &settings)),
+                        Err(e) => (nb_path.as_path(), vec![e.into()]),
+                    }
+                })
+                .collect()
+        }
+    };
+    let mut check_results = Vec::new();
+
+    for (path, res) in &check_results_by_file {
+        check_results.extend(res.iter().map(|c| (path, c)));
+    }
+
+    for (path, res) in &check_results {
+        let rel_path = relativize_path(path).bold();
+        println!("{rel_path}:{res}");
+    }
+    if check_results.is_empty() {
+        Ok(())
+    } else {
+        let n_checks = check_results.len();
+        Err(anyhow!("Found {n_checks} items to strip"))
+    }
+}
+
+fn strip_all(files: &[PathBuf], dry_run: bool, yes: bool, cli: CommonArgs) -> Result<(), Error> {
+    let (args, overrides) = cli.partition();
+    let FoundNotebooks::Files(nbs) = find_notebooks(files)? else {
+        bail!("`strip-all` does not support stdin");
+    };
+    if !yes {
+        let ans = inquire::Confirm::new("Continue?")
+            .with_default(false)
+            .prompt()?;
+        if !ans {
+            return Ok(());
+        }
+    }
+
+    let settings = Settings::construct(args.config.as_deref(), &overrides)?;
+    let strip_results: Vec<StripResult> = nbs
+        .par_iter()
+        .map(|nb_path| strip_single(nb_path, dry_run, &settings).into())
+        .collect();
+
+    for (nb_path, res) in nbs.iter().zip(strip_results) {
+        let rel_path = relativize_path(nb_path).bold();
+
+        println!("{rel_path}: {res}");
+    }
+    Ok(())
+}
+
+fn strip(file: &Path, textconv: bool, cli: CommonArgs) -> Result<(), Error> {
+    let (args, overrides) = cli.partition();
+
+    let settings = Settings::construct(args.config.as_deref(), &overrides)?;
+    strip_single(file, textconv, &settings)?;
+
+    Ok(())
+}
+
+fn install(cmd: &InstallCommand) -> Result<(), Error> {
+    install::install_config(cmd.config_type)?;
+    install::install_attributes(cmd.config_type, cmd.attribute_file.as_deref())
+}
+
+fn main() -> Result<(), Error> {
+    let cli = cli::Cli::parse();
+
+    #[cfg(feature = "markdown-help")]
+    if cli.markdown_help {
+        clap_markdown::print_help_markdown::<cli::Cli>();
+        return Ok(());
+    }
+    match cli.command {
+        Commands::Clean(CleanCommand {
+            ref file,
+            textconv,
+            common,
+        }) => strip(file, textconv, common),
+        Commands::CleanAll(CleanAllCommand {
+            ref files,
+            dry_run,
+            yes,
+            common,
+        }) => strip_all(files, dry_run, yes, common),
+        Commands::Check(CheckCommand { ref files, common }) => check_all(files, common),
+        Commands::Install(ref cmd) => install(cmd),
+    }
+}
+
+#[cfg(test)]
+mod test {}
