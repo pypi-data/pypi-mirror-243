2ce89e5d390c1a7fbc0beb2b6d15f2f9489e2f87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 16:03:44 +0000

    Add package openllm/0.4.29/openllm-0.4.29-py3-none-any.whl

---
+++ packages/openllm/openllm-0.4.29-py3-none-any.whl/openllm/exceptions.py
@@ -0,0 +1,10 @@
+from openllm_core.exceptions import (
+  Error as Error,
+  FineTuneStrategyNotSupportedError as FineTuneStrategyNotSupportedError,
+  ForbiddenAttributeError as ForbiddenAttributeError,
+  GpuNotAvailableError as GpuNotAvailableError,
+  MissingAnnotationAttributeError as MissingAnnotationAttributeError,
+  MissingDependencyError as MissingDependencyError,
+  OpenLLMException as OpenLLMException,
+  ValidationError as ValidationError,
+)
