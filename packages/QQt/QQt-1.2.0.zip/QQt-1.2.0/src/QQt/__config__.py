c3d96fcf12b3536d38ee3414978676986957a6b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 15:03:38 +0000

    Add package QQt/1.2.0/QQt-1.2.0.zip

---
+++ packages/QQt/QQt-1.2.0.zip/QQt-1.2.0/src/QQt/__config__.py
@@ -0,0 +1,5 @@
+# Copyright (c) 2012-2023 Adam Karpierz
+# Licensed under the zlib/libpng License
+# https://opensource.org/licenses/Zlib
+
+origin = "qtpy"
---
+++ packages/QQt/QQt-1.2.0.zip/QQt-1.2.0/tests/test_main.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2012-2023 Adam Karpierz
+# Licensed under the zlib/libpng License
+# https://opensource.org/licenses/Zlib
+
+import unittest
+
+import QQt
+
+
+class MainTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test(self):
+        """Main test"""
