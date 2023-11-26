1c556b1272488edd4e03a55694c1b1461fcd8906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 16:03:44 +0000

    Add package openllm/0.4.29/openllm-0.4.29.tar.gz

---
+++ packages/openllm/openllm-0.4.29.tar.gz/openllm-0.4.29/src/openllm/serialisation/__init__.pyi
@@ -0,0 +1,19 @@
+'''Serialisation utilities for OpenLLM.
+
+Currently supports transformers for PyTorch, and vLLM.
+
+Currently, GGML format is working in progress.
+'''
+
+from typing import Any
+
+from bentoml import Model
+from openllm import LLM
+from openllm_core._typing_compat import M, T
+
+from . import constants as constants, ggml as ggml, transformers as transformers
+
+def load_tokenizer(llm: LLM[M, T], **attrs: Any) -> T: ...
+def get(llm: LLM[M, T]) -> Model: ...
+def import_model(llm: LLM[M, T], *args: Any, trust_remote_code: bool, **attrs: Any) -> Model: ...
+def load_model(llm: LLM[M, T], *args: Any, **attrs: Any) -> M: ...
