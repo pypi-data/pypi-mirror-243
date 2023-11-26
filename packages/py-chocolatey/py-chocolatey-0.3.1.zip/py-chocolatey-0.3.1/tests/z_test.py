ac462ccd1d5c3e9dcc34eb96415edcc1463817ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 15:03:05 +0000

    Add package py-chocolatey/0.3.1/py-chocolatey-0.3.1.zip

---
+++ packages/py-chocolatey/py-chocolatey-0.3.1.zip/py-chocolatey-0.3.1/tests/z_test.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+from functools import partial
+import logging
+# logging.basicConfig(level=logging.INFO)
+# logging.basicConfig(level=logging.DEBUG)
+
+from rich import print
+from rich.pretty import pprint
+pprint = partial(pprint, max_length=500)
+
+from chocolatey import Chocolatey
+choco = Chocolatey()
+
+sources = choco.sources()
+print(sources)
+
+input("Press a key...")
