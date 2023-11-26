de0e24e3c44e03c6b051a993062c8b4ead6705bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 14:06:21 +0000

    Add package pyqcolour/1.2/pyqcolour-1.2.tar.gz

---
+++ packages/pyqcolour/pyqcolour-1.2.tar.gz/pyqcolour-1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name="pyqcolour",
+    version='1.2',
+    license='Eclipse Public License 2.0',
+    authors=["danisec"],
+    author_email="dansecurity@gmail.com",
+    description="python library for colors",
+    long_description='Documentation: https://github.com/dansecurity/pyqcolour',
+    packages=['pyqcolour']
+)
+
