fba3e3c2103d9fe4cf314b2d2f5d2b1c24fd6e2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 14:05:23 +0000

    Add package ipa-tool/1.4.8/ipa_tool-1.4.8.tar.gz

---
+++ packages/ipa-tool/ipa_tool-1.4.8.tar.gz/ipa_tool-1.4.8/ipa_tool/analyze_macho.py
@@ -0,0 +1,47 @@
+import lief, os, re
+
+def parse_macho(plist, ipa_file):
+    '''
+    :param plist: formatted plist (dict)
+    :param ipa_file: zipfile object
+    :return: macho object
+    '''
+    exe_name=plist['CFBundleExecutable']
+
+    name_list = ipa_file.namelist()
+    # Use expression to match file
+    pt = r'Payload/[^/]*.app/' + exe_name
+    pattern = re.compile(pt)
+    # Tranversing files in ipa
+    for path in name_list:
+        path=path.encode("CP437").decode("UTF-8")
+        m = pattern.match(path)
+        if m is not None:
+            exec_path=m.group()
+            exec_path=exec_path.encode("UTF-8").decode("CP437")
+            break
+
+    # Read executable into memory
+    exec=ipa_file.read(exec_path)
+    # Use lief to parse executable
+    macho = lief.parse(exec)
+    # return
+    return macho
+
+def is_encrypted(macho):
+    '''
+    :param macho: macho object
+    :return: bool, is encrypted or not
+    '''
+    encry_info=macho.get(lief.MachO.LOAD_COMMAND_TYPES.ENCRYPTION_INFO)
+    if encry_info:
+        if encry_info.crypt_id == 1:
+            return True
+        else:
+            return False
+    else:
+        encry_info = macho.get(lief.MachO.LOAD_COMMAND_TYPES.ENCRYPTION_INFO_64)
+        if encry_info:
+            if encry_info.crypt_id == 1:
+                return True
+        return False
