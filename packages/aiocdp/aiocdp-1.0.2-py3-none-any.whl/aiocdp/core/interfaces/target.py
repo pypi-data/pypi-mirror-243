1233b775ba7a5f7bf117cf0dd15df6b3a63391b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 15:02:54 +0000

    Add package aiocdp/1.0.2/aiocdp-1.0.2-py3-none-any.whl

---
+++ packages/aiocdp/aiocdp-1.0.2-py3-none-any.whl/aiocdp/core/interfaces/target.py
@@ -0,0 +1,82 @@
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+
+from aiocdp.core.interfaces.stream import IEventStream
+
+if TYPE_CHECKING:
+    from aiocdp.core.interfaces.chrome import IChrome
+
+
+class ITargetInfo(ABC):
+    """
+    Represents information about a CDP target.
+    """
+
+    @classmethod
+    @abstractmethod
+    def init(
+        cls,
+        id_: str,
+        title: str,
+        description: str,
+        url: str,
+        type_: str,
+        web_socket_debugger_url: str
+    ):
+        """
+        Initializer method for ITargetInfo subclasses
+        """
+        pass
+
+
+class ITarget(ABC):
+    """
+    Represents a CDP target.
+    """
+
+    @classmethod
+    @abstractmethod
+    def init(
+        cls,
+        chrome: 'IChrome',
+        info: 'ITargetInfo'
+    ):
+        """
+        Initializer method for ITarget subclasses
+        """
+        pass
+
+    @abstractmethod
+    def close_stream(self, stream: 'IEventStream'):
+        """
+        Closes the given stream.
+        """
+        pass
+
+    @abstractmethod
+    def get_info(self):
+        """
+        Returns the associated ITargetInfo implementation
+        """
+        pass
+
+    @abstractmethod
+    def open_stream(self, events):
+        """
+        Opens a stream for the given events.
+        """
+        pass
+
+    @abstractmethod
+    def send(self, method, params):
+        """
+        Sends a message to the target.
+        """
+        pass
+
+    @abstractmethod
+    def send_and_await_response(self, method, params):
+        """
+        Sends a message to the target and awaits a response.
+        """
+        pass
