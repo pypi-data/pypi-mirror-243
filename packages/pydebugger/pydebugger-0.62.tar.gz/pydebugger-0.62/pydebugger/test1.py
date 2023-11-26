e9670a0e0a63aecbaff5b919cf4d2fdb448c8f32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 16:03:32 +0000

    Add package pydebugger/0.62/pydebugger-0.62.tar.gz

---
+++ packages/pydebugger/pydebugger-0.62.tar.gz/pydebugger-0.62/pydebugger/test1.py
@@ -0,0 +1,9 @@
+from debug import *
+import sys
+import traceback
+
+def function_test(param1, param2, param3):
+	debug(print_function_parameters=True)
+
+function_test("test_parameter_1", "test_parameter_2", "test_parameter_3")
+
