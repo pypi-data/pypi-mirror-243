fa4658b47cd4c80b62a6b9123b5da15404c0417b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 14:05:09 +0000

    Add package drf-emails/0.0.2/drf_emails-0.0.2-py3-none-any.whl

---
+++ packages/drf-emails/drf_emails-0.0.2-py3-none-any.whl/drf_emails/user/checks.py
@@ -0,0 +1,7 @@
+# Check that settings user is drf_emails.user.abstracts.AbstractUser instance (for verified and unique email)
+# Check that emails are drf_emails.user.email.CodeVerifyEmail instances
+# Check that the user model has an required email field
+
+# For views checks that user has an unique email field and boolean is_verified
+
+# 'rest_framework' and 'rest_framework.authtoken' inside INSTALLED APPS
