42a63a7bf65421834a631f36a0fe6773ef7a7ab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-26 14:05:35 +0000

    Add package toshona/0.0.1/toshona-0.0.1.tar.gz

---
+++ packages/toshona/toshona-0.0.1.tar.gz/toshona-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+        name="toshona",
+        version="0.0.1",
+        author="Eddington Chamunorwa",
+        author_email="dittonedo45@gmail.com",
+        description="Translate (int::) to shona word phrase.",
+        packages=find_packages(),
+        classifiers=[
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            ],
+        python_requires=">=3.3"
+        )
