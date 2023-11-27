39031599026cae3471a420a63f251151830512d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 03:04:05 +0000

    Add package invoice2data/0.4.5/invoice2data-0.4.5.tar.gz

---
+++ packages/invoice2data/invoice2data-0.4.5.tar.gz/invoice2data-0.4.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    include_package_data=True,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    license='MIT License',
+    package_data={
+        'invoice2data.extract': [
+            'templates/com/*.yml',
+            'templates/de/*.yml',
+            'templates/es/*.yml',
+            'templates/fr/*.yml',
+            'templates/nl/*.yml',
+            'templates/ch/*.yml'],
+    },
+)
