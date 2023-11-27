79ed837e16102a7c5dcefc0f5e0f1a7a4b2f7e00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 03:03:11 +0000

    Add package oct-vol/0.2.0/oct_vol-0.2.0-py3-none-any.whl

---
+++ packages/oct-vol/oct_vol-0.2.0-py3-none-any.whl/OCTVol/__init__.py
@@ -0,0 +1,12 @@
+"""
+OCT .vol Files Reader, Writer, and Cropper
+==========================================
+
+A utility package for handling OCT images in .vol format from Heidelberg Engineering.
+It provides streamlined functions to read, write, and crop.
+
+"""
+
+__version__ = '0.2.0'
+
+from .OCTVol import OCTVol
