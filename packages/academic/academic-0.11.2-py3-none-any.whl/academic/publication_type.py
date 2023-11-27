198618c207fb4c4ac4f526c2175da1a016577aab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 04:03:06 +0000

    Add package academic/0.11.2/academic-0.11.2-py3-none-any.whl

---
+++ packages/academic/academic-0.11.2-py3-none-any.whl/academic/publication_type.py
@@ -0,0 +1,18 @@
+# Map BibTeX publication types to universal CSL types
+PUB_TYPES_BIBTEX_TO_CSL = {
+    "article": "article-journal",
+    "book": "book",
+    "conference": "paper-conference",
+    "inbook": "chapter",
+    "incollection": "chapter",
+    "inproceedings": "paper-conference",
+    "manual": "book",
+    "mastersthesis": "thesis",
+    "patent": "patent",
+    "phdthesis": "thesis",
+    "proceedings": "book",
+    "report": "report",
+    "thesis": "thesis",
+    "techreport": "report",
+    "unpublished": "manuscript",
+}
