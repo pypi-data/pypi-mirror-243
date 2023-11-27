2786d7e0d3e21254e7e1b5b17ae6f458a4e75858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 15:04:21 +0000

    Add package django-rest-framework-query-tools/0.0.11/django_rest_framework_query_tools-0.0.11-py3-none-any.whl

---
+++ packages/django-rest-framework-query-tools/django_rest_framework_query_tools-0.0.11-py3-none-any.whl/libname/__init__.py
@@ -0,0 +1,5 @@
+# libname/__init__.py
+from libname.django_rest_framework_query_tools import (CustomOrderingFilter,
+                                                       URLFilter)
+
+__version__ = '0.0.11'
