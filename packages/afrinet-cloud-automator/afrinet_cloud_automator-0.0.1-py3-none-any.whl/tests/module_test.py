0d307903cd62a9b11f3815699c9bd37d53900b07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 03:03:43 +0000

    Add package afrinet-cloud-automator/0.0.1/afrinet_cloud_automator-0.0.1-py3-none-any.whl

---
+++ packages/afrinet-cloud-automator/afrinet_cloud_automator-0.0.1-py3-none-any.whl/tests/module_test.py
@@ -0,0 +1,10 @@
+from modules.src.config.user_class import UserClass
+
+
+user1 = UserClass("Eugene Parker", "Eugenio", "p$ssw0rd", "Basic")
+
+user1.getUserRecords()
+
+user2 = UserClass("Eugene Parker", "Eugenio", "p$ssw0rd", "Basic")
+
+user2.getUserRecords()
