0bd6814f6d3f8690c7afdb267939bbe3fcb607fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 04:03:06 +0000

    Add package kmac-automl/1.1.3/kmac_automl-1.1.3-py3-none-any.whl

---
+++ packages/kmac-automl/kmac_automl-1.1.3-py3-none-any.whl/kmac_automl/__init__.py
@@ -0,0 +1,8 @@
+from AutoML_Classification import Classification
+from AutoML_Clustering import Clustering
+from AutoML_Regression import Regression
+from AutoML_TimeSeries import TimeSeries
+from AutoML_AnomalyDetection import AnomalyDetection
+
+__all__ = ['Classification', 'Clustering', 'Regression', 'TimeSeries', 'AnomalyDetection']
+
