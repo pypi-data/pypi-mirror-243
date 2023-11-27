e3e9f5d25f809bf2a190eeaed5e89af4f4574fad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 04:03:46 +0000

    Add package mautrix-telegram/0.15.0/mautrix-telegram-0.15.0.tar.gz

---
+++ packages/mautrix-telegram/mautrix-telegram-0.15.0.tar.gz/mautrix-telegram-0.15.0/mautrix_telegram/version.py
@@ -0,0 +1,6 @@
+# Generated in setup.py
+
+git_tag = 'v0.15.0'
+git_revision = '562f646f'
+version = '0.15.0'
+linkified_version = '[0.15.0](https://github.com/mautrix/telegram/releases/tag/v0.15.0)'
