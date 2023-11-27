a07ea5977169392ac26a30f1bd44880990efa809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 03:03:00 +0000

    Add package wheel/0.42.0/wheel-0.42.0.tar.gz

---
+++ packages/wheel/wheel-0.42.0.tar.gz/wheel-0.42.0/tests/testdata/unicode.dist/setup.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+from setuptools import setup
+
+setup(
+    name="unicode.dist",
+    version="0.1",
+    description="A testing distribution \N{SNOWMAN}",
+    packages=["unicodedist"],
+)
