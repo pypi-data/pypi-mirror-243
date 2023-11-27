6df106b08341752791d08f47b8f1e5c077f27dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 14:03:53 +0000

    Add package common-web/0.1.6/common-web-0.1.6.tar.gz

---
+++ packages/common-web/common-web-0.1.6.tar.gz/common-web-0.1.6/constants/festival.py
@@ -0,0 +1,8 @@
+FESTIVAL_DAY = {
+    "yuandan": "New Year's Day",
+    "chunjie": "Chinese New Year (Spring Festival)",
+    "qingming": "Tomb-Sweeping Day",
+    "longzhou": "Dragon Boat Festival",
+    "zhongqiu": "Mid-Autumn Festival",
+    "guoqing": "National Day"
+}
