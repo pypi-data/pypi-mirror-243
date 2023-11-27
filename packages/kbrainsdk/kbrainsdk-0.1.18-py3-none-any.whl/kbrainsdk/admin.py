a22e66efbca1fc0ccfa6aad63eeb28b0f6ed8455
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 04:03:47 +0000

    Add package kbrainsdk/0.1.18/kbrainsdk-0.1.18-py3-none-any.whl

---
+++ packages/kbrainsdk/kbrainsdk-0.1.18-py3-none-any.whl/kbrainsdk/admin.py
@@ -0,0 +1,14 @@
+from kbrainsdk.base import APIBase
+
+class Admin(APIBase):
+
+    def create_account(self, req):
+        url = f"{self.base_url}/account/v1"
+        response = self.apiobject.call_endpoint(url, req, "post")
+        return response
+
+
+    def create_keys(self, req):
+        url = f"{self.base_url}/keys/v1"
+        response = self.apiobject.call_endpoint(url, req, "post")
+        return response
