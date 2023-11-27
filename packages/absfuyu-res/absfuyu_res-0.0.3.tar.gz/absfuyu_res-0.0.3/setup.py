fd326f9eda6939aa449ee8e63ca817a3297bd1da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 15:03:58 +0000

    Add package absfuyu-res/0.0.3/absfuyu_res-0.0.3.tar.gz

---
+++ packages/absfuyu-res/absfuyu_res-0.0.3.tar.gz/absfuyu_res-0.0.3/setup.py
@@ -0,0 +1,6 @@
+import setuptools
+
+
+setuptools.setup(
+    name="absfuyu_res",
+)
---
+++ packages/absfuyu-res/absfuyu_res-0.0.3.tar.gz/absfuyu_res-0.0.3/tests/test_load.py
@@ -0,0 +1,9 @@
+import pytest
+
+
+from absfuyu_res import is_loaded
+
+
+
+def test_loaded():
+    assert is_loaded()
