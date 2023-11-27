eff26ab0836b4e86c66802e173470097b519fee4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 03:04:05 +0000

    Add package certbot-dnshub/0.1.1/certbot-dnshub-0.1.1.tar.gz

---
+++ packages/certbot-dnshub/certbot-dnshub-0.1.1.tar.gz/certbot-dnshub-0.1.1/setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup
+from setuptools import find_packages
+
+version = '0.1.1'
+
+install_requires = [
+    'acme>=0.21.1',
+    'certbot>=0.21.1',
+    'dns-lexicon',
+    'mock',
+    'setuptools',
+    'zope.interface',
+]
+
+# Read the long description from the README
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='certbot-dnshub',
+    version=version,
+    description="Multi dns provider authentication plugin for certbot",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/warjiang/certbot-dnshub',
+    author="warjiang",
+    author_email='1096409085@qq.com',
+    license='Apache License 2.0',
+    python_requires='>=3.0',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Plugins',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Security',
+        'Topic :: System :: Installation/Setup',
+        'Topic :: System :: Networking',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities',
+    ],
+    packages=find_packages( exclude=["assets"] ),
+    include_package_data=True,
+    install_requires=install_requires,
+    entry_points={
+        'certbot.plugins': [
+            'dnshub = certbot_dnshub.dnshub:Authenticator',
+        ],
+    },
+)
