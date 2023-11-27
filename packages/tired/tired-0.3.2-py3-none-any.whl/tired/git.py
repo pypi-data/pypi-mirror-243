993f7889fd3bd8d1cc22f94146168464df1f5857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 03:05:05 +0000

    Add package tired/0.3.2/tired-0.3.2-py3-none-any.whl

---
+++ packages/tired/tired-0.3.2-py3-none-any.whl/tired/git.py
@@ -0,0 +1,44 @@
+import tired.command
+import tired.logging
+import tired.parse
+
+
+_LOG_CONTEXT = "tired.command"
+
+
+def get_current_branch_name():
+    command_string = "git rev-parse --abbrev-ref HEAD"
+    output, code = tired.command.get_output_with_code(command_string)
+
+    if code != 0:
+        tired.logging.error(_LOG_CONTEXT, f"Failed to execute command `{command_string}`")
+
+        raise Exception(f"Failed to execute command `{command_string}`, returned {code}")
+
+    return output.strip()
+
+
+def get_current_commit_hash():
+    command_string = "git rev-parse HEAD"
+    output, code = tired.command.get_output_with_code(command_string)
+
+    if code != 0:
+        tired.logging.error(_LOG_CONTEXT, f"Failed to execute command `{command_string}`")
+
+        raise Exception(f"Failed to execute command `{command_string}`, returned {code}")
+
+    return output.strip()
+
+
+def get_staged_file_paths():
+    command_string = "git diff --name-only --staged"
+    output, code = tired.command.get_output_with_code(command_string)
+
+    if code != 0:
+        tired.logging.error(_LOG_CONTEXT, f"Failed to execute command `{command_string}`")
+
+        raise Exception(f"Failed to execute command `{command_string}`, returned {code}")
+
+    output = output.strip()
+
+    return tired.parse.iterate_string_multiline(output)
