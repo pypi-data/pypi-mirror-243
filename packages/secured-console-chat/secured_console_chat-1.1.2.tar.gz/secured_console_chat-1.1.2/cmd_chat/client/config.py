e2d2416063aeb334fe899bcf73d42284fa07512c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 15:03:45 +0000

    Add package secured-console-chat/1.1.2/secured_console_chat-1.1.2.tar.gz

---
+++ packages/secured-console-chat/secured_console_chat-1.1.2.tar.gz/secured_console_chat-1.1.2/cmd_chat/client/config.py
@@ -0,0 +1,10 @@
+from colorama import Fore
+
+COLORS = {
+    "text_color": Fore.WHITE,
+    "my_username_color": Fore.MAGENTA,
+    "ip_color": Fore.MAGENTA,
+    "username_color": Fore.GREEN
+}
+
+RENDER_TIME = 0.05
