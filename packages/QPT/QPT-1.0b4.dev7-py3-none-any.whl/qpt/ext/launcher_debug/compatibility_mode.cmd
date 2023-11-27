8c246a548388ad8fa547fa4eac9ccd4abd88f768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 03:04:05 +0000

    Add package QPT/1.0b4.dev7/QPT-1.0b4.dev7-py3-none-any.whl

---
+++ packages/QPT/QPT-1.0b4.dev7-py3-none-any.whl/qpt/ext/launcher_debug/compatibility_mode.cmd
@@ -0,0 +1,14 @@
+chcp 65001
+cd /d %~dp0
+echo on
+set QPT_COLOR=False
+set QPT_MODE=Debug
+set PYTHONUNBUFFERED=1
+set PYTHONIOENCODING=utf-8
+set PROMPT=(QPT_VENV) %PROMPT%
+
+set QPT_PY_MAIN_FILE=NONE
+"./Python/python.exe" %QPT_PY_MAIN_FILE% %*
+
+cls
+pause
