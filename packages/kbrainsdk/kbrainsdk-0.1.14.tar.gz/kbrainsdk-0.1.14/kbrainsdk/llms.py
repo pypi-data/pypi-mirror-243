67721b28d7174242df4cbb36e7612304453d3815
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 03:03:12 +0000

    Add package kbrainsdk/0.1.14/kbrainsdk-0.1.14.tar.gz

---
+++ packages/kbrainsdk/kbrainsdk-0.1.14.tar.gz/kbrainsdk-0.1.14/kbrainsdk/llms.py
@@ -0,0 +1,33 @@
+import requests
+from kbrainsdk.validation.llms import validate_openai_llms
+
+class LLMs():
+    def __init__(self, base_url, headers):
+        self.base_url = base_url
+        self.headers = headers
+    
+    def openai_chat(self, messages, model_name, model_type, deployment_id, **kwargs):
+        payload = {
+            "messages": messages,
+            "model_name": model_name,
+            "model_type": model_type,
+            "deployment_id": deployment_id,
+            **kwargs
+        }
+        validate_openai_llms(payload, 'chat')
+        url = f"{self.base_url}/llms/openai/chat/v1"
+        response = self.call_endpoint(url, payload, "post")
+        return response
+
+    def openai_completion(self, prompt, model_name, model_type, deployment_id, **kwargs):
+        payload = {
+            "prompt": prompt,
+            "model_name": model_name,
+            "model_type": model_type,
+            "deployment_id": deployment_id,
+            **kwargs
+        }
+        validate_openai_llms(payload, 'completion')
+        url = f"{self.base_url}/llms/openai/completion/v1"
+        response = self.call_endpoint(url, payload, "post")
+        return response
