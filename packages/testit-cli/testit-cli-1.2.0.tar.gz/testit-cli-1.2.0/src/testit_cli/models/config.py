f277753fdaab5f97bbce033a75ff7d1d5012592d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 15:04:27 +0000

    Add package testit-cli/1.2.0/testit-cli-1.2.0.tar.gz

---
+++ packages/testit-cli/testit-cli-1.2.0.tar.gz/testit-cli-1.2.0/src/testit_cli/models/config.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Config:
+    url: str
+    token: str
+    project_id: str
+    configuration_id: str
+    testrun_id: str
+    testrun_name: str
+    separator: str
+    namespace: str
+    classname: str
+    results: str
+    is_debug: bool
+    output: str
+    paths_to_attachments: list
