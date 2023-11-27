f4629e2a76ceaf964c88d1eddf6d423ce88a8fdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 04:03:46 +0000

    Add package kbrainsdk/0.1.17/kbrainsdk-0.1.17.tar.gz

---
+++ packages/kbrainsdk/kbrainsdk-0.1.17.tar.gz/kbrainsdk-0.1.17/kbrainsdk/datasets.py
@@ -0,0 +1,23 @@
+from kbrainsdk.validation.datasets import validate_list_datasets
+
+class Datasets():
+    def __init__(self, kbrainapi, base_url, headers):
+        self.base_url = base_url
+        self.headers = headers
+        self.api = kbrainapi
+
+    def list_datasets(self, email, token, client_id, oauth_secret, tenant_id):
+        
+        payload = {
+            "email": email,
+            "token": token,
+            "client_id": client_id,
+            "oauth_secret": oauth_secret,
+            "tenant_id": tenant_id
+        }
+
+        validate_list_datasets(payload)
+
+        url = f"{self.base_url}/datasets/list/v1"
+        response = self.api.call_endpoint(url, payload, "post")
+        return response
