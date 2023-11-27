84637af02205b4678c3690ef3e9f52a47a1a407a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 03:04:05 +0000

    Add package tinyscript/1.29.5/tinyscript-1.29.5.tar.gz

---
+++ packages/tinyscript/tinyscript-1.29.5.tar.gz/tinyscript-1.29.5/tests/test_template.py
@@ -0,0 +1,23 @@
+# -*- coding: UTF-8 -*-
+"""Template module assets' tests.
+
+"""
+from tinyscript.template import new, TARGETS, TEMPLATE
+
+from utils import *
+
+
+class TestTemplate(TestCase):
+    def test_script_generation(self):
+        SCR = "test-script"
+        new(SCR)
+        self.assertTrue(exists(SCR + ".py"))
+        with open(SCR + ".py") as f:
+            self.assertIn("from tinyscript import *", f.read())
+        for t in TARGETS.keys():
+            new(SCR, t)
+            self.assertTrue(exists(SCR + ".py"))
+            with open(SCR + ".py") as f:
+                self.assertIn("from {} import {}".format(*t.split(".")), f.read())
+        remove(SCR + ".py")
+
