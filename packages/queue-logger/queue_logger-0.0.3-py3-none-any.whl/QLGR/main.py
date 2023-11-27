74477058bb6bc64b8a7b2dda9e7fa39e8b88730f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 03:04:20 +0000

    Add package queue-logger/0.0.3/queue_logger-0.0.3-py3-none-any.whl

---
+++ packages/queue-logger/queue_logger-0.0.3-py3-none-any.whl/QLGR/main.py
@@ -0,0 +1,11 @@
+import logging
+from handler import RabbitMQHandler
+
+
+def create_logger(name, log_level=logging.INFO):
+    rabbitmq_handler = RabbitMQHandler()
+    logger = logging.getLogger(name)
+    logger.propagate = False
+    logger.setLevel(log_level)
+    logger.addHandler(rabbitmq_handler)
+    return logger
