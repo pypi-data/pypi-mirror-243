b8b0b26fcc7c2df20970fd44829c3f74af8c4ff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 03:04:10 +0000

    Add package oneapi2langchain/0.0.102/oneapi2langchain-0.0.102.tar.gz

---
+++ packages/oneapi2langchain/oneapi2langchain-0.0.102.tar.gz/oneapi2langchain-0.0.102/oneapi2langchain/sparkai/api_resources/chat_completion.py
@@ -0,0 +1,136 @@
+import json
+import os
+import time
+from typing import Union, Optional, List, Callable, Tuple
+
+import websocket
+from openai.api_resources.abstract.engine_api_resource import EngineAPIResource
+from oneapi2langchain.sparkai.socket_mode.websocket_client import SparkMessageStatus, ResponseMessage
+
+from oneapi2langchain.sparkai.schema import ChatMessage
+from oneapi2langchain.sparkai.models.chat import ChatBody, ChatResponse
+
+from oneapi2langchain.sparkai.xf_util import build_auth_request_url
+from oneapi2langchain.sparkai.log.logger import logger
+
+
+class ChatCompletion(EngineAPIResource):
+    engine_required = False
+    OBJECT_NAME = "chat.completions"
+
+    @classmethod
+    def create(cls, *args, **kwargs):
+        """
+        启动一个once式的 WS短连接并返回会话响应
+        """
+        start = time.time()
+        timeout = kwargs.pop("timeout", None)
+        ws = websocket.WebSocket()
+
+
+class SparkOnceWebsocket():
+    def __init__(self, api_key=None,
+                 api_base=None,
+                 app_id=None,
+                 api_secret=None,
+                 messages=None,
+                 temperature=None,
+                 max_tokens=2048,
+                 top_p=None,
+                 request_id=None,
+                 api_version=None,
+                 organization=None):
+        self.ws_url = api_base
+        self.app_id = app_id
+        self.api_key = api_key
+        self.api_secret = api_secret
+        self.max_token = max_tokens
+        self.temperature = temperature
+        self.top_p = top_p
+
+        self.stopping = False
+        self.ws = websocket.WebSocket()
+
+    def connect(self):
+        self.ws.connect(
+            build_auth_request_url(self.ws_url, method="GET", api_key=self.api_key, api_secret=self.api_secret))
+
+    def send_messages(self, messages: List[ChatMessage]):
+        self.connect()
+
+        domain = os.environ.get("SPARK_DOMAIN", "general")
+        req_data = ChatBody(self.app_id, 
+                            messages, 
+                            domain=domain, 
+                            max_tokens=self.max_token,
+                            temperature=self.temperature,
+                            top_k=self.top_p).json()
+        self.ws.send(req_data)
+        lastFrame = False
+        full_msg_response = ''
+        code = 0
+        is_last = False
+        self.stopping = False
+        logger.debug(messages)
+        while not self.stopping and not lastFrame:
+            lastFrame, code, msg = self.handle_response(self.ws.recv())
+            if msg:
+                full_msg_response += msg.content
+            if code != 0 or lastFrame:
+                self.stopping = True
+                logger.info(full_msg_response)
+        return code, full_msg_response
+    def send_messages_generator(self, messages: List[ChatMessage]):
+        self.connect()
+
+        domain = os.environ.get("SPARK_DOMAIN", "general")
+        req_data = ChatBody(self.app_id, messages, domain=domain, max_tokens=self.max_token).json()
+        self.ws.send(req_data)
+        lastFrame = False
+        full_msg_response = ''
+        code = 0
+        is_last = False
+        self.stopping = False
+        logger.debug(messages)
+        while not self.stopping and not lastFrame:
+            lastFrame, code, msg = self.handle_response(self.ws.recv())
+            if msg:
+                full_msg_response += msg.content
+                yield msg.content
+            if code != 0 or lastFrame:
+                self.stopping = True
+                logger.info(full_msg_response)
+        self.close()
+        return code, full_msg_response
+
+    def handle_response(self, message) -> (int, str):
+        temp_result = json.loads(message)
+        # print("响应数据:{}\n".format(temp_result))
+
+        res = ChatResponse(**temp_result)
+        msg: ResponseMessage = ResponseMessage()
+        msg.set_status(res.header.status)
+        if res.header.status == SparkMessageStatus.DataBegin or res.header.status == SparkMessageStatus.DataContinue:
+            if len(res.payload.choices.text) >= 0:
+                msg.set_content(res.payload.choices.text[0].role, res.payload.choices.text[0].content)
+        elif res.header.status == SparkMessageStatus.DataEnd and res.payload:
+            if len(res.payload.choices.text) >= 0:
+                msg.set_content(res.payload.choices.text[0].role, res.payload.choices.text[0].content)
+                msg.set_usage(question_tokens=res.payload.usage.text.question_tokens,
+                              completion_tokens=res.payload.usage.text.completion_tokens,
+                              total_tokens=res.payload.usage.text.total_tokens,
+                              prompt_tokens=res.payload.usage.text.prompt_tokens)
+
+            self.chat_response = []
+        else:
+            logger.error(f"error status: not supported this status code, {res.header.status}")
+
+        if res.header.code != 0:
+            logger.error(f"sid: {res.header.sid}, error code:  {res.header.code}, {res.header.message}")
+        code = res.header.code
+        last = True if res.header.status == SparkMessageStatus.DataEnd else False
+        msg = msg
+        return last, code, msg
+
+    def close(self):
+        self.ws.close()
---
+++ packages/oneapi2langchain/oneapi2langchain-0.0.102.tar.gz/oneapi2langchain-0.0.102/oneapi2langchain/sparkai/models/chat/__init__.py
@@ -0,0 +1,127 @@
+from typing import List
+import json
+from oneapi2langchain.sparkai.memory import ChatMessageHistory
+
+
+class Header():
+
+    def __init__(self, code: int, message: str, sid: str, status: str):
+        self.code = code
+        self.message = message
+        self.sid = sid
+        self.status = status
+
+
+class Message():
+
+    def __init__(self, content: str, role: str, index: int):
+        self.content = content
+        self.role = role
+        self.index = index
+
+
+class Choices():
+
+    def __init__(self, status: int, seq: int, text: List[Message]):
+        self.status = status
+        self.seq = seq
+        tmp = []
+        if text:
+            for m in text:
+                tmp.append(Message(**m))
+        self.text = tmp
+
+
+class Text():
+    def __init__(self, completion_tokens: int, prompt_tokens: int, total_tokens: int, question_tokens: int):
+        self.completion_tokens: int = completion_tokens
+        self.prompt_tokens: int = prompt_tokens
+        self.total_tokens: int = total_tokens
+        self.question_tokens: int = question_tokens
+
+
+class Usage():
+    def __init__(self, text: Text):
+        if isinstance(text, Text):
+            self.text = text
+        else:
+            self.text = Text(**text)
+
+
+class Payload():
+
+    def __init__(self, choices: Choices = None, usage: Usage = None):
+        if isinstance(choices, dict):
+            self.choices = Choices(**choices)
+        else:
+            self.choices = choices
+        if usage:
+            if isinstance(usage, Usage):
+                self.usage = usage
+            elif isinstance(usage, dict):
+                self.usage = Usage(**usage)
+
+
+class ChatResponse():
+    def __init__(self, header: dict, payload: dict = None):
+        self.header = Header(**header)
+        if payload:
+            self.payload = Payload(**payload)
+        else:
+            self.payload = None
+
+
+class ChatBody():
+    def __init__(self, app_id, question, uid="12345", domain="plugin", random_threshold=0, max_tokens=2048,
+                 memory: ChatMessageHistory = None,top_k=1, temperature=0.4):
+        self.memory = memory
+        use_memory = False
+        self.req_data = {
+            "header": {
+                "app_id": app_id,
+                "uid": uid
+            },
+            "parameter": {
+                "chat": {
+                    "domain": domain,
+                    "random_threshold": random_threshold,
+                    "max_tokens": max_tokens,
+                    "auditing": "default",
+                    "temperature": temperature,
+                    "top_k": top_k
+                }
+            },
+            "payload": {
+                "message": {
+                    "text": [{
+                        "role": "user",
+                        "content": question
+                    }]
+                }
+            }
+        }
+        if memory:
+            use_memory = True
+
+        if isinstance(question, list):
+            # 如果用户传递自己的上下history list 则忽略memory todo need argument
+            use_memory = False
+            self.req_data['payload']['message']['text'] = question
+        else:
+            self.req_data['payload']['message']['text'][0]['content'] = question
+
+        if use_memory:
+            self.memory.add_user_message(question)
+            # self.memory.to_list()
+            self.req_data['payload']['message']['text'] = self.memory.to_list()
+
+    def json(self):
+        return json.dumps(self.req_data)
+
+
+if __name__ == '__main__':
+    a = {'header': {'code': 0, 'message': 'Success', 'sid': 'cht000c0e9c@dx187bb529b75a173541', 'status': 1},
+         'payload': {
+             'choices': {'status': 1, 'seq': 6, 'text': [{'content': '智能服务。', 'role': 'assistant', 'index': 0}]}}}
+    c = ChatResponse(**a)
+    print(c.payload.choices)
