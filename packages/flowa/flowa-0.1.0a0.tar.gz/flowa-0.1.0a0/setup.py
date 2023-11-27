0d84d46945b6b7c8b879a18423ccd140a40c002f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 04:03:26 +0000

    Add package flowa/0.1.0a0/flowa-0.1.0a0.tar.gz

---
+++ packages/flowa/flowa-0.1.0a0.tar.gz/flowa-0.1.0a0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flowa',
+    version='0.1.0a',
+    description='flowa pre-setup',
+    url='https://github.com/',
+    author='flowa',
+    author_email='flowa@flowa.com',
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[],
+    classifiers=[],
+)
