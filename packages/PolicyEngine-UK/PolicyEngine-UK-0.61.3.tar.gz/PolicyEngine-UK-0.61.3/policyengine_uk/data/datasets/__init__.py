45306f6fe3d0850f1a1800621709ab974cd6bb8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 03:02:59 +0000

    Add package PolicyEngine-UK/0.61.3/PolicyEngine-UK-0.61.3.tar.gz

---
+++ packages/PolicyEngine-UK/PolicyEngine-UK-0.61.3.tar.gz/PolicyEngine-UK-0.61.3/policyengine_uk/data/datasets/__init__.py
@@ -0,0 +1,25 @@
+from .frs import (
+    FRS_2018_19,
+    FRS_2019_20,
+    FRS_2020_21,
+    PooledFRS_2018_20,
+    SPIEnhancedFRS_2019_20,
+    SPIEnhancedPooledFRS_2018_20,
+    CalibratedFRS_2019_20,
+    CalibratedSPIEnhancedFRS_2019_20,
+    CalibratedSPIEnhancedPooledFRS_2018_20,
+    EnhancedFRS,
+)
+
+DATASETS = [
+    FRS_2018_19,
+    FRS_2019_20,
+    FRS_2020_21,
+    PooledFRS_2018_20,
+    SPIEnhancedFRS_2019_20,
+    SPIEnhancedPooledFRS_2018_20,
+    CalibratedFRS_2019_20,
+    CalibratedSPIEnhancedFRS_2019_20,
+    CalibratedSPIEnhancedPooledFRS_2018_20,
+    EnhancedFRS,
+]
---
+++ packages/PolicyEngine-UK/PolicyEngine-UK-0.61.3.tar.gz/PolicyEngine-UK-0.61.3/policyengine_uk/variables/gov/hmrc/child_benefit.py
@@ -0,0 +1,106 @@
+from policyengine_uk.model_api import *
+
+
+class child_benefit_reported(Variable):
+    label = "Child Benefit (reported amount)"
+    documentation = "Reported amount received for Child Benefit"
+    entity = Person
+    definition_period = YEAR
+    value_type = float
+    unit = GBP
+
+
+class would_claim_child_benefit(Variable):
+    label = "Would claim Child Benefit"
+    documentation = (
+        "Whether this benefit unit would claim Child Benefit if eligible"
+    )
+    entity = BenUnit
+    definition_period = YEAR
+    value_type = bool
+
+    def formula(benunit, period, parameters):
+        claims_benefits = benunit("claims_all_entitled_benefits", period)
+        already_claiming = add(benunit, period, ["child_benefit_reported"]) > 0
+        takeup_rate = parameters(period).gov.hmrc.child_benefit.takeup
+        baseline_cb = benunit("baseline_child_benefit_entitlement", period) > 0
+        eligible = benunit("child_benefit_entitlement", period) > 0
+        return select(
+            [already_claiming | claims_benefits, ~baseline_cb & eligible],
+            [
+                True,  # Claims Child Benefit in the baseline
+                random(benunit)
+                < takeup_rate,  # New CB eligibility from a reform
+            ],
+            default=False,  # Always non-claimant
+        )
+
+
+class child_benefit_respective_amount(Variable):
+    label = "Child Benefit (respective amount)"
+    documentation = "The amount of this benefit unit's Child Benefit which is in respect of this person"
+    entity = Person
+    definition_period = YEAR
+    value_type = float
+    unit = GBP
+    reference = (
+        "https://www.legislation.gov.uk/ukpga/1992/4/part/IX",
+        "https://www.legislation.gov.uk/uksi/2006/965/regulation/2",
+    )
+    defined_for = "is_child_or_QYP"
+
+    def formula(person, period, parameters):
+        eligible = True
+        if parameters(
+            period
+        ).gov.contrib.ubi_center.basic_income.interactions.withdraw_cb:
+            eligible &= person.benunit.sum(person("basic_income", period)) == 0
+        is_eldest = person("is_eldest_child", period)
+        child_benefit = parameters(period).gov.hmrc.child_benefit.amount
+        amount = where(
+            is_eldest, child_benefit.eldest, child_benefit.additional
+        )
+        return eligible * amount * WEEKS_IN_YEAR
+
+
+class child_benefit_entitlement(Variable):
+    label = "CB entitlement"
+    entity = BenUnit
+    definition_period = YEAR
+    value_type = float
+    unit = GBP
+
+    adds = ["child_benefit_respective_amount"]
+
+
+class child_benefit(Variable):
+    label = "Child Benefit"
+    documentation = "Total Child Benefit for the benefit unit"
+    entity = BenUnit
+    definition_period = YEAR
+    value_type = float
+    unit = GBP
+    category = BENEFIT
+    defined_for = "would_claim_child_benefit"
+    adds = ["child_benefit_entitlement"]
+
+
+class child_benefit_less_tax_charge(Variable):
+    label = "Child Benefit (less tax charge)"
+    documentation = (
+        "Child Benefit, minus the Child Benefit High-Income Tax Charge"
+    )
+    entity = BenUnit
+    definition_period = YEAR
+    value_type = float
+    unit = GBP
+
+    adds = ["child_benefit"]
+    subtracts = ["CB_HITC"]
+
+
+class baseline_child_benefit_entitlement(Variable):
+    label = "Child Benefit (baseline)"
+    entity = BenUnit
+    definition_period = YEAR
+    value_type = float
