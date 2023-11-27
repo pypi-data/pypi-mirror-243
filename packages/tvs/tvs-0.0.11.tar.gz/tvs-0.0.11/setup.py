ca2ea8353a27d2a828dc83a9bc5f48e4a2b4e821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 03:03:00 +0000

    Add package tvs/0.0.11/tvs-0.0.11.tar.gz

---
+++ packages/tvs/tvs-0.0.11.tar.gz/tvs-0.0.11/setup.py
@@ -0,0 +1,16 @@
+from io import open
+from setuptools import setup
+
+version = '0.0.11'
+
+setup(
+    name = 'tvs',
+    version=version,
+
+    author='Lohi',
+    author_email='9628782183oleg@gmail.com',
+
+    license='Free' ,
+    
+    packages= ['tvs']
+)
---
+++ packages/tvs/tvs-0.0.11.tar.gz/tvs-0.0.11/tvs/__init__.py
@@ -0,0 +1,1 @@
+from .TerVer import *
