4aa2455ee8461c672015304e13938f1b4ad79030
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 03:04:18 +0000

    Add package kbrainsdk/0.1.9/kbrainsdk-0.1.9.tar.gz

---
+++ packages/kbrainsdk/kbrainsdk-0.1.9.tar.gz/kbrainsdk-0.1.9/kbrainsdk/admin.py
@@ -0,0 +1,17 @@
+import requests
+
+class Admin():
+
+    def __init__(self, base_url, headers):
+        self.base_url = base_url
+        self.headers = headers
+
+    def create_account(self, req):
+        url = f"{self.base_url}/account/v1"
+        response = requests.post(url, json=req, headers=self.headers)
+        return response.json()
+
+    def create_keys(self, req):
+        url = f"{self.base_url}/keys/v1"
+        response = requests.post(url, json=req, headers=self.headers)
+        return response.json()
