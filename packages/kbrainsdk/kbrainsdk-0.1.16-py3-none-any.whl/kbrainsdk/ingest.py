8408c7345eeccf65113e1ce345af9b446c5a877f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 04:03:46 +0000

    Add package kbrainsdk/0.1.16/kbrainsdk-0.1.16-py3-none-any.whl

---
+++ packages/kbrainsdk/kbrainsdk-0.1.16-py3-none-any.whl/kbrainsdk/ingest.py
@@ -0,0 +1,56 @@
+from kbrainsdk.validation.ingest import validate_ingest_onedrive, validate_ingest_sharepoint, validate_ingest_status
+class Ingest():
+    def __init__(self, api, base_url, headers):
+        self.base_url = base_url
+        self.headers = headers
+        self.api = api
+
+    def ingest_onedrive(self, email, token, client_id, oauth_secret, tenant_id, environment):
+
+        payload = {
+            "email": email,
+            "token": token,
+            "environment": environment,
+            "client_id": client_id,
+            "oauth_secret": oauth_secret,
+            "tenant_id": tenant_id
+        }
+
+        validate_ingest_onedrive(payload)
+
+        url = f"{self.base_url}/ingest/onedrive/v1"
+        response = self.api.call_endpoint(url, payload, "post")
+        return response
+
+    def ingest_sharepoint(self, host, site, token, client_id, oauth_secret, tenant_id, environment):
+
+        payload = {
+            "host": host,
+            "site": site,
+            "token": token,
+            "environment": environment,
+            "client_id": client_id,
+            "oauth_secret": oauth_secret,
+            "tenant_id": tenant_id
+        }
+
+        validate_ingest_sharepoint(payload)
+
+        url = f"{self.base_url}/ingest/sharepoint/v1"
+        response = self.api.call_endpoint(url, payload, "post")
+        return response
+
+    def get_status(self, datasource):
+
+        payload = {
+            "datasource": datasource 
+        }
+
+        validate_ingest_status(payload)
+
+        url = f"{self.base_url}/ingest/status/v1"
+        response = self.api.call_endpoint(url, payload, "post")
+        return response
+
+    def convert_email_to_datasource(email):
+        return f"drive-{email.lower().replace('@', '-at-').replace('.', '-')}"
