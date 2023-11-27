5a1263795a594db7fc86813a9d47a75e3173358d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 16:03:54 +0000

    Add package ma2l/0.1.2/ma2l-0.1.2-py3-none-any.whl

---
+++ packages/ma2l/ma2l-0.1.2-py3-none-any.whl/ma2l/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for MindSpore with Ascend on ModelArts Launcher."""
+
+__author__ = """Genius Patrick"""
+__email__ = "74176172+geniuspatrick@users.noreply.github.com"
+__version__ = "0.1.2"
