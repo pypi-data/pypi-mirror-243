378bc15e5fa0c4d6d071fcb9acf247b90cdfd2a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 03:03:09 +0000

    Add package pylabel/0.1.55/pylabel-0.1.55.tar.gz

---
+++ packages/pylabel/pylabel-0.1.55.tar.gz/pylabel-0.1.55/setup.py
@@ -0,0 +1,28 @@
+# Test
+from setuptools import setup
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pylabel",
+    packages=["pylabel"],
+    version="0.1.55",
+    description="Transform, analyze, and visualize computer vision annotations.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pylabel-project/pylabel",
+    author="PyLabel Project",
+    license="MIT",
+    install_requires=[
+        "pandas",
+        "bbox_visualizer",
+        "matplotlib",
+        "opencv-python",
+        "scikit-learn",
+        "jupyter_bbox_widget",
+        "pyyaml",
+         "tqdm",
+    ]
+)
