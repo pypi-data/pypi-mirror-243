25bba8d507d81cc3fd452307b453cb16d5b6ccf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 15:05:00 +0000

    Add package Flask-CKEditor/0.5.1/Flask-CKEditor-0.5.1.tar.gz

---
+++ packages/Flask-CKEditor/Flask-CKEditor-0.5.1.tar.gz/Flask-CKEditor-0.5.1/README.md
@@ -0,0 +1,22 @@
+# Flask-CKEditor
+
+CKEditor integration for Flask, including image upload, code syntax highlighting, and more.
+
+
+## Links
+
+* [Documentation](https://flask-ckeditor.readthedocs.io/en/latest/)
+* [PyPI](https://pypi.org/project/Flask-CKEditor/)
+* [Examples](https://github.com/helloflask/flask-ckeditor/tree/master/examples)
+
+
+## Donate
+
+If you find Flask-CKEditor useful, please consider
+[donating today](https://opencollective.com/flask-ckeditor/donate). Your donation keeps
+Flask-CKEditor maintained and updated with CKEditor.
+
+
+## License
+
+This project is licensed under the MIT License (see the `LICENSE` file for details).
---
+++ packages/Flask-CKEditor/Flask-CKEditor-0.5.1.tar.gz/Flask-CKEditor-0.5.1/setup.py
@@ -0,0 +1,51 @@
+"""
+Flask-CKEditor
+--------------
+
+CKEditor integration for Flask, including image upload, code syntax
+highlighting, and more.
+
+Features:
+
+* Integrate with Flask-WTF/WTForms.
+* Configure CKEditor through Flask's configuration system.
+* Image upload support.
+* Code snippet highlighting.
+* Built-in CKEditor resources.
+"""
+from setuptools import setup
+
+setup(
+    name='Flask-CKEditor',
+    version='0.5.1',
+    url='http://github.com/helloflask/flask-ckeditor',
+    license='MIT',
+    author='Grey Li',
+    author_email='withlihui@gmail.com',
+    description='CKEditor integration for Flask.',
+    long_description=__doc__,
+    long_description_content_type='text/markdown',
+    packages=['flask_ckeditor'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    test_suite='test_flask_ckeditor',
+    install_requires=[
+        'Flask'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
