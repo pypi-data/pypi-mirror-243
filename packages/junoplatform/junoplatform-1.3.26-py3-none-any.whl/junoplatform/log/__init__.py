e89a3c8a2dc9f5304c273f027aad77209abd4cb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 15:04:36 +0000

    Add package junoplatform/1.3.26/junoplatform-1.3.26-py3-none-any.whl

---
+++ packages/junoplatform/junoplatform-1.3.26-py3-none-any.whl/junoplatform/log/__init__.py
@@ -0,0 +1,35 @@
+import logging
+from junoplatform.io import Storage
+from junoplatform.io.utils import junoconfig
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(filename)s %(lineno)d - %(message)s')
+
+
+class RTLogHandler(logging.Handler):
+    def __init__(self):
+        super(RTLogHandler, self).__init__()
+        self.io = Storage().cloud
+
+    def emit(self, record):
+        log_entry = {
+            'level': record.levelname,
+            'message': record.getMessage(),
+            'et': record.created,
+            'logger_name': record.name,
+            'file': record.filename,
+            'line_number': record.lineno,
+            'function_name': record.funcName
+        }
+
+        self.io.write('rtlog', log_entry)
+
+
+# try:
+#     logger = logging.getLogger('rtlog')
+#     logger.addHandler(RTLogHandler())
+#     logger.setLevel(logging.INFO)
+# except:
+#     pass
+
+logger = logging
