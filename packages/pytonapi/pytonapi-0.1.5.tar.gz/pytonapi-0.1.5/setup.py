fdf80587438d88dba60da66c493ecd9dd2f6a9ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 04:03:10 +0000

    Add package pytonapi/0.1.5/pytonapi-0.1.5.tar.gz

---
+++ packages/pytonapi/pytonapi-0.1.5.tar.gz/pytonapi-0.1.5/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pytonapi",
+    version="0.1.5",
+    author="nessshon",
+    description="Provide access to indexed TON blockchain.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nessshon/pytonapi/",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7, <3.11',
+    install_requires=[
+        "httpx>=0.25.1",
+        "websockets>=12.0",
+        "libscrc>=1.8.1",
+        "pydantic==2.*",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
