357ffada32d6ba6a14fb3df18c536364d7abc05c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 15:04:14 +0000

    Add package darli/0.1.0/darli-0.1.0-py3-none-any.whl

---
+++ packages/darli/darli-0.1.0-py3-none-any.whl/darli/models/_regressors.py
@@ -0,0 +1,12 @@
+# TODO:
+# all necesary functions are now depend on parameters
+
+
+# regressors
+
+# //////
+# Bodies
+# regressor.momentum
+# regressor.wrench
+# regressor.energy.potential
+# regressor.energy.kinetic
