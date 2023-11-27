688040f656a87ac5a71bbbc399979abc240f121e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 16:03:54 +0000

    Add package DrissionPage/4.0.0b14/DrissionPage-4.0.0b14.tar.gz

---
+++ packages/DrissionPage/DrissionPage-4.0.0b14.tar.gz/DrissionPage-4.0.0b14/DrissionPage/_commons/settings.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+"""
+@Author  :   g1879
+@Contact :   g1879@qq.com
+"""
+
+
+class Settings(object):
+    raise_when_ele_not_found = False
+    raise_when_click_failed = False
+    raise_when_wait_failed = False
+    NoneElement_value = None
