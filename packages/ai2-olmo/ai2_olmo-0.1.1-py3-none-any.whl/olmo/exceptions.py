851df34c8bc348b83c9a4074de55863bbcc75d0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 14:03:22 +0000

    Add package ai2-olmo/0.1.1/ai2_olmo-0.1.1-py3-none-any.whl

---
+++ packages/ai2-olmo/ai2_olmo-0.1.1-py3-none-any.whl/olmo/exceptions.py
@@ -0,0 +1,31 @@
+__all__ = ["OlmoError", "OlmoConfigurationError", "OlmoCliError", "OlmoNetworkError"]
+
+
+class OlmoError(Exception):
+    """
+    Base class for all custom OLMo exceptions.
+    """
+
+
+class OlmoConfigurationError(OlmoError):
+    """
+    An error with a configuration file.
+    """
+
+
+class OlmoCliError(OlmoError):
+    """
+    An error from incorrect CLI usage.
+    """
+
+
+class OlmoNetworkError(OlmoError):
+    """
+    An error with a network request.
+    """
+
+
+class OlmoThreadError(Exception):
+    """
+    Raised when a thread fails.
+    """
