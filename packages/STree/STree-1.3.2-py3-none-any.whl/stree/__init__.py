4c52a25df306e179524f1ed8e1f744d35ede3edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-27 15:04:21 +0000

    Add package STree/1.3.2/STree-1.3.2-py3-none-any.whl

---
+++ packages/STree/STree-1.3.2-py3-none-any.whl/stree/__init__.py
@@ -0,0 +1,8 @@
+from .Strees import Stree, Siterator
+
+__author__ = "Ricardo Montañana Gómez"
+__copyright__ = "Copyright 2020-2021, Ricardo Montañana Gómez"
+__license__ = "MIT License"
+__author_email__ = "ricardo.montanana@alu.uclm.es"
+
+__all__ = ["Stree", "Siterator"]
