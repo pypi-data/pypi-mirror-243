62e356a857535418e01720fe6ab378ddebc482ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 03:04:28 +0000

    Add package shieldb/0.4.7/shieldb-0.4.7-py3-none-any.whl

---
+++ packages/shieldb/shieldb-0.4.7-py3-none-any.whl/shieldb-0.4.7.data/data/app.py
@@ -0,0 +1,49 @@
+from application.core.enums import Actions
+from application.core.mask_utils import MaskUtils
+from application.core.utils import Utils
+
+
+def main():
+    connection = Utils.connect_db()
+    utils = Utils(connection)
+    args = utils.read_user_args()
+
+    if connection is None:
+        print("Could not connect to the database")
+        return
+
+    if args['action'] not in [Actions.DELETE.value, Actions.MASK.value]:
+        print("Invalid Action: ", args['action'])
+        return
+
+    mask_utils = MaskUtils(connection, args)
+    if args['action'] == Actions.DELETE.value:
+        confirm = input(f"{args['percentage']}% of the {args['table']} table will be "
+                        f"deleted, do "
+                        f"you confirm? (Y/N): ").lower()
+        if confirm == Actions.CONFIRM.value:
+            mask_utils.delete_rows_in_chunks()
+        else:
+            print("Operation canceled.")
+
+    elif args['action'] == Actions.MASK.value:
+        if not args['table']:
+            print("No table data to be deleted has been entered")
+            return
+
+        if args['place_to_mask'] == Actions.PLACE_TO_MASK.value:
+            confirm = input(
+                "All columns of the {} table will be masked, do you confirm? (Y/N): ".format(args['table'])).lower()
+        else:
+            confirm = input(
+                "The following columns will be masked: {}. Do you confirm? (Y/N): ".format(args['columns'])
+            ).lower()
+
+        if confirm == Actions.CONFIRM.value:
+            mask_utils.masking()
+        else:
+            print("Operation canceled.")
+
+
+if __name__ == "__main__":
+    main()
