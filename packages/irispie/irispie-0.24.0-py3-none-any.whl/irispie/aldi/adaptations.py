b03511a270001848026e6911f2652f5f8946a5e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 03:03:41 +0000

    Add package irispie/0.24.0/irispie-0.24.0-py3-none-any.whl

---
+++ packages/irispie/irispie-0.24.0-py3-none-any.whl/irispie/aldi/adaptations.py
@@ -0,0 +1,33 @@
+"""
+"""
+
+
+#[
+from __future__ import annotations
+
+import numpy as np_
+#]
+
+
+FUNCTION_ADAPTATIONS = [
+    "log", "exp", "sqrt", "maximum", "minimum"
+]
+
+
+for n in FUNCTION_ADAPTATIONS:
+    exec(
+        f"def {n}(x, *args, **kwargs): "
+        f"return x._{n}_(*args, **kwargs) if hasattr(x, '_{n}_') else np_.{n}(x, *args, **kwargs)"
+    )
+
+
+def add_function_adaptations_to_context(context: dict | None) -> dict:
+    """
+    """
+    #[
+    context = context if context else {}
+    for n in FUNCTION_ADAPTATIONS:
+        context[n] = globals()[n]
+    return context
+    #]
+
