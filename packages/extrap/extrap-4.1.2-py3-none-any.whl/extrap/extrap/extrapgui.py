d1420a240804e185513ed408ef32658ef3546411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 04:04:28 +0000

    Add package extrap/4.1.2/extrap-4.1.2-py3-none-any.whl

---
+++ packages/extrap/extrap-4.1.2-py3-none-any.whl/extrap/extrap/extrapgui.py
@@ -0,0 +1,252 @@
+# This file is part of the Extra-P software (http://www.scalasca.org/software/extra-p)
+#
+# Copyright (c) 2020-2023, Technical University of Darmstadt, Germany
+#
+# This software may be modified and distributed under the terms of a BSD-style license.
+# See the LICENSE file in the base directory for details.
+
+import argparse
+import logging
+import sys
+import threading
+import traceback
+import warnings
+
+from PySide6.QtCore import Qt, QCoreApplication
+from PySide6.QtGui import QPalette, QColor
+from PySide6.QtWidgets import QApplication, QMessageBox, QToolTip
+from matplotlib import font_manager
+
+import extrap
+from extrap.fileio.experiment_io import read_experiment
+from extrap.fileio.file_reader import all_readers
+from extrap.fileio.file_reader.cube_file_reader2 import CubeFileReader2
+from extrap.gui.MainWidget import MainWidget
+from extrap.util.exceptions import RecoverableError, CancelProcessError
+
+TRACEBACK = logging.DEBUG - 1
+logging.addLevelName(TRACEBACK, 'TRACEBACK')
+
+
+def main(*, args=None, test=False):
+    _update_mac_app_info()
+    # preload fonts for matplotlib
+    font_preloader = _preload_common_fonts()
+    arguments = parse_arguments(args)
+
+    # configure logging
+    log_level = min(logging.getLevelName(arguments.log_level.upper()), logging.INFO)
+    if arguments.log_file:
+        logging.basicConfig(format="%(levelname)s: %(asctime)s: %(message)s", level=log_level,
+                            filename=arguments.log_file)
+    else:
+        logging.basicConfig(format="%(levelname)s: %(asctime)s: %(message)s", level=log_level)
+    logging.getLogger().handlers[0].setLevel(logging.getLevelName(arguments.log_level.upper()))
+
+    QCoreApplication.setApplicationName(extrap.__title__)
+    QCoreApplication.setApplicationVersion(extrap.__version__)
+    app = QApplication(sys.argv) if not test else QApplication.instance()
+    apply_style(app)
+
+    window = MainWidget()
+
+    _init_warning_system(window, test)
+
+    window.show()
+
+    try:
+        load_from_command(arguments, window)
+    except CancelProcessError:
+        pass
+
+    if not test:
+        app.exec()
+        font_preloader.join()
+    else:
+        font_preloader.join()
+        return window, app
+
+
+def parse_arguments(args=None):
+    parser = argparse.ArgumentParser(description=extrap.__description__)
+    parser.add_argument("--log", action="store", dest="log_level", type=str.lower, default='critical',
+                        choices=['traceback', 'debug', 'info', 'warning', 'error', 'critical'],
+                        help="set program's log level (default: critical)")
+    parser.add_argument("--logfile", action="store", dest="log_file",
+                        help="set path of log file")
+    parser.add_argument("--version", action="version", version=extrap.__title__ + " " + extrap.__version__)
+
+    group = parser.add_mutually_exclusive_group(required=False)
+    for reader in all_readers.values():
+        group.add_argument(reader.CMD_ARGUMENT, action="store_true", default=False, dest=reader.NAME,
+                           help=reader.DESCRIPTION)
+
+    parser.add_argument("path", metavar="FILEPATH", type=str, action="store", nargs='?',
+                        help="specify a file path for Extra-P to work with")
+
+    parser.add_argument("--scaling", action="store", dest="scaling_type", default="weak", type=str.lower,
+                        choices=["weak", "strong"],
+                        help="set weak or strong scaling when loading data from cube files [weak (default), strong]")
+    arguments = parser.parse_args(args)
+    return arguments
+
+
+def load_from_command(arguments, window):
+    if arguments.path:
+        for reader in all_readers.values():
+            if getattr(arguments, reader.NAME):
+                file_reader = reader()
+                if file_reader is CubeFileReader2:
+                    file_reader.scaling_type = arguments.scaling_type
+                window.import_file(file_reader.read_experiment, file_name=arguments.path,
+                                   model=file_reader.GENERATE_MODELS_AFTER_LOAD)
+                return
+        window.import_file(read_experiment, file_name=arguments.path, model=False)
+
+
+def _init_warning_system(window, test=False):
+    open_message_boxes = []
+    current_warnings = set()
+
+    # save old handlers
+    _old_warnings_handler = warnings.showwarning
+    _old_exception_handler = sys.excepthook
+
+    def activate_box(box):
+        box.raise_()
+        box.activateWindow()
+
+    def display_messages(event):
+        for w in open_message_boxes:
+            w.raise_()
+            w.activateWindow()
+
+    if sys.platform.startswith('darwin'):
+        window.activate_event_handlers.append(display_messages)
+
+    def _warnings_handler(message: Warning, category, filename, lineno, file=None, line=None):
+        nonlocal current_warnings
+        message_str = str(message)
+        if message_str not in current_warnings:
+            warn_box = QMessageBox(QMessageBox.Warning, 'Warning', message_str, QMessageBox.Ok, window)
+            warn_box.setModal(False)
+            warn_box.setAttribute(Qt.WA_DeleteOnClose)
+            warn_box.destroyed.connect(
+                lambda x: (current_warnings.remove(message_str), open_message_boxes.remove(warn_box)))
+
+            if not test:
+                warn_box.show()
+                activate_box(warn_box)
+                open_message_boxes.append(warn_box)
+
+            current_warnings.add(message_str)
+            _old_warnings_handler(message, category, filename, lineno, file, line)
+
+        logging.warning(message_str)
+        logging.log(TRACEBACK, ''.join(traceback.format_stack()))
+        QApplication.processEvents()
+
+    def _exception_handler(type, value, traceback_):
+        traceback_text = ''.join(traceback.extract_tb(traceback_).format())
+
+        if issubclass(type, CancelProcessError):
+            logging.log(TRACEBACK, str(value))
+            logging.log(TRACEBACK, traceback_text)
+            return
+
+        parent, modal = _parent(window)
+        msg_box = QMessageBox(QMessageBox.Critical, 'Error', str(value), QMessageBox.Ok, parent)
+        print()
+        if hasattr(value, 'NAME'):
+            msg_box.setWindowTitle(getattr(value, 'NAME'))
+        msg_box.setDetailedText(traceback_text)
+        open_message_boxes.append(msg_box)
+
+        logging.error(str(value))
+        logging.log(TRACEBACK, traceback_text)
+
+        if test:
+            return _old_exception_handler(type, value, traceback_)
+        _old_exception_handler(type, value, traceback_)
+        if issubclass(type, RecoverableError):
+            msg_box.open()
+            activate_box(msg_box)
+        else:
+            activate_box(msg_box)
+            msg_box.exec()  # ensures waiting
+            exit(1)
+
+    warnings.showwarning = _warnings_handler
+    sys.excepthook = _exception_handler
+    warnings.simplefilter('always', UserWarning)
+
+
+def apply_style(app):
+    app.setStyle('Fusion')
+
+    palette = QPalette()
+    palette.setColor(QPalette.Window, QColor(200, 200, 200))
+    palette.setColor(QPalette.WindowText, Qt.black)
+    palette.setColor(QPalette.Base, QColor(220, 220, 220))
+    palette.setColor(QPalette.AlternateBase, QColor(10, 10, 10))
+    palette.setColor(QPalette.Text, Qt.black)
+    palette.setColor(QPalette.Button, QColor(220, 220, 220))
+    palette.setColor(QPalette.ButtonText, Qt.black)
+    palette.setColor(QPalette.Highlight, QColor(31, 119, 180))
+    palette.setColor(QPalette.HighlightedText, Qt.white)
+    palette.setColor(QPalette.ToolTipBase, QColor(230, 230, 230))
+    palette.setColor(QPalette.ToolTipText, Qt.black)
+    palette.setColor(QPalette.Disabled, QPalette.Text, QColor(80, 80, 80))
+    palette.setColor(QPalette.Disabled, QPalette.ButtonText, QColor(80, 80, 80))
+    palette.setColor(QPalette.Disabled, QPalette.Button, QColor(150, 150, 150))
+    palette.setColor(QPalette.ColorRole.Link, QColor(21, 83, 123))
+    app.setPalette(palette)
+    QToolTip.setPalette(palette)
+
+
+def _preload_common_fonts():
+    common_fonts = [
+        font_manager.FontProperties('sans\\-serif:style=normal:variant=normal:weight=normal:stretch=normal:size=10.0'),
+        'STIXGeneral', 'STIXGeneral:italic', 'STIXGeneral:weight=bold',
+        'STIXNonUnicode', 'STIXNonUnicode:italic', 'STIXNonUnicode:weight=bold',
+        'STIXSizeOneSym', 'STIXSizeTwoSym', 'STIXSizeThreeSym', 'STIXSizeFourSym', 'STIXSizeFiveSym',
+        'cmsy10', 'cmr10', 'cmtt10', 'cmmi10', 'cmb10', 'cmss10', 'cmex10',
+        'DejaVu Sans', 'DejaVu Sans:italic', 'DejaVu Sans:weight=bold', 'DejaVu Sans Mono', 'DejaVu Sans Display',
+        font_manager.FontProperties('sans\\-serif:style=normal:variant=normal:weight=normal:stretch=normal:size=12.0'),
+        font_manager.FontProperties('sans\\-serif:style=normal:variant=normal:weight=normal:stretch=normal:size=6.0')
+    ]
+
+    def _thread(fonts):
+        for f in fonts:
+            font_manager.findfont(f)
+
+    thread = threading.Thread(target=_thread, args=(common_fonts,))
+    thread.start()
+    return thread
+
+
+def _parent(window):
+    if not sys.platform.startswith('darwin'):
+        return window, False
+    modal = QApplication.activeModalWidget()
+    parent = modal if modal else window
+    return parent, bool(modal)
+
+
+def _update_mac_app_info():
+    if sys.platform.startswith('darwin'):
+        try:
+            from Foundation import NSBundle  # noqa
+            bundle = NSBundle.mainBundle()
+            if bundle:
+                app_info = bundle.localizedInfoDictionary() or bundle.infoDictionary()
+                if app_info:
+                    app_info['CFBundleName'] = extrap.__title__
+            from AppKit import NSWindow
+            NSWindow.setAllowsAutomaticWindowTabbing_(False)
+        except ImportError:
+            pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/extrap/extrap-4.1.2-py3-none-any.whl/extrap/modelers/abstract_modeler.py
@@ -0,0 +1,127 @@
+# This file is part of the Extra-P software (http://www.scalasca.org/software/extra-p)
+#
+# Copyright (c) 2020-2023, Technical University of Darmstadt, Germany
+#
+# This software may be modified and distributed under the terms of a BSD-style license.
+# See the LICENSE file in the base directory for details.
+
+import copy
+import logging
+import warnings
+from abc import ABC, abstractmethod
+from typing import Sequence, Optional
+
+from marshmallow import fields, post_load
+
+from extrap.entities.measurement import Measurement
+from extrap.entities.model import Model
+from extrap.util.classproperty import classproperty
+from extrap.util.progress_bar import DUMMY_PROGRESS
+from extrap.util.serialization_schema import BaseSchema
+
+
+class AbstractModeler(ABC):
+    def __init__(self, use_median: bool):
+        """Creates a new modeler object, that uses either the median or the mean when modeling.
+
+           :param use_median: use mean or median measurement values to calculate models
+        """
+        self._use_median = use_median
+
+    @property
+    def use_median(self) -> bool:
+        return self._use_median
+
+    @use_median.setter
+    def use_median(self, value: bool):
+        self._use_median = value
+
+    @abstractmethod
+    def model(self, measurements: Sequence[Sequence[Measurement]], progress_bar=DUMMY_PROGRESS) -> Sequence[Model]:
+        """ Creates a model for each sequence of measurements.
+
+        This method is the core of the modeling system.
+        It receives a sequence of measurement point sequences and returns a sequence of models.
+        For each sequence of measurement points one model is generated.
+        The measurement sequences are not guaranteed to have similar coordinates."""
+        raise NotImplementedError
+
+    @classproperty
+    @abstractmethod
+    def NAME(cls) -> str:  # noqa
+        """ This attribute is the unique display name of the modeler.
+
+        It is used for selecting the modeler in the GUI and CLI.
+        You must override this only in concrete modelers, you should do so by setting the class variable NAME."""
+        raise NotImplementedError
+
+    @classproperty
+    def DESCRIPTION(cls) -> Optional[str]:  # noqa
+        """ This attribute is the description of the modeler.
+
+        It is shown as additional information in the GUI and CLI.
+        You should override this by setting the class variable DESCRIPTION."""
+        return None
+
+
+class SingularModeler(AbstractModeler, ABC):
+    def model(self, measurements: Sequence[Sequence[Measurement]], progress_bar=DUMMY_PROGRESS) -> Sequence[Model]:
+        return [self.create_model(m) for m in progress_bar(measurements)]
+
+    @abstractmethod
+    def create_model(self, measurements: Sequence[Measurement]) -> Model:
+        raise NotImplementedError
+
+
+class MultiParameterModeler(AbstractModeler, ABC):
+
+    def __init__(self, use_median, single_parameter_modeler):
+        super().__init__(use_median)
+        single_parameter_modeler.use_median = use_median
+        self._default_single_parameter_modeler = single_parameter_modeler
+        self.single_parameter_modeler: AbstractModeler = copy.copy(single_parameter_modeler)
+
+    def reset_single_parameter_modeler(self):
+        self.single_parameter_modeler = copy.copy(self._default_single_parameter_modeler)
+
+    @AbstractModeler.use_median.setter
+    def use_median(self, value):
+        self._use_median = value
+        self.single_parameter_modeler.use_median = value
+
+
+class ModelerSchema(BaseSchema):
+    use_median = fields.Bool()
+
+    def on_missing_sub_schema(self, type_, data, **kwargs):
+        warnings.warn(f"Loaded unknown modeler of type {type_}")
+        data['type_'] = type_
+        return super(BaseSchema, self).load(data, **kwargs)
+
+    def create_object(self):
+        logging.debug(f"Created placeholder for unknown modeler")
+        return _EmptyModeler(False)
+
+
+class _EmptyModeler(AbstractModeler):
+    NAME = "<Empty>"
+
+    def model(self, measurements: Sequence[Sequence[Measurement]], progress_bar=DUMMY_PROGRESS) -> Sequence[Model]:
+        raise NotImplementedError()
+
+    def __eq__(self, o: object) -> bool:
+        return o is self or isinstance(o, _EmptyModeler)
+
+
+EMPTY_MODELER = _EmptyModeler(False)
+
+
+class _EmptyModelerSchema(ModelerSchema):
+    use_median = fields.Constant(False, load_only=True, dump_only=True)
+
+    @post_load
+    def unpack_to_object(self, data, **kwargs):
+        return EMPTY_MODELER
+
+    def create_object(self):
+        return EMPTY_MODELER
