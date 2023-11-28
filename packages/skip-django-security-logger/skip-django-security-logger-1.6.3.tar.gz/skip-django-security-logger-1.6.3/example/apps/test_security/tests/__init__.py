ca875ea79e0b9594e17458609470ce4823f66d9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 05:03:21 +0000

    Add package skip-django-security-logger/1.6.3/skip-django-security-logger-1.6.3.tar.gz

---
+++ packages/skip-django-security-logger/skip-django-security-logger-1.6.3.tar.gz/skip-django-security-logger-1.6.3/example/apps/test_security/tests/__init__.py
@@ -0,0 +1,6 @@
+from .celery_log import CeleryLogTestCase
+from .command_log import CommandLogTestCase
+from .input_request_log import InputRequestLogTestCase
+from .output_request_log import OutputRequestLogTestCase
+from .commands import CommandTestCase
+from .utils import UtilsTestCase
