19fa1d972eb13cf6a9fa76cbd83f4d821250289c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 16:04:41 +0000

    Add package pyCADD/1.6.8/pyCADD-1.6.8-py3-none-any.whl

---
+++ packages/pyCADD/pyCADD-1.6.8-py3-none-any.whl/pyCADD/__init__.py
@@ -0,0 +1,7 @@
+__version__= '1.6.8'
+__update_date__ = '2023-11-21'
+__author__ = 'YH. W'
+__institution__ = 'Xiamen University, China'
+
+from pyCADD.utils.log import _init_log
+_init_log('pyCADD')
