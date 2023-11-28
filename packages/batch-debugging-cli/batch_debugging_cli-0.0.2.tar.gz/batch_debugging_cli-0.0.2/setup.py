3ae58d620dab07fb868a4e3c653b86980fc9520e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 03:04:31 +0000

    Add package batch-debugging-cli/0.0.2/batch_debugging_cli-0.0.2.tar.gz

---
+++ packages/batch-debugging-cli/batch_debugging_cli-0.0.2.tar.gz/batch_debugging_cli-0.0.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+DESCRIPTION = 'A CLI to help debug issues in batch enviornments accross the cloud'
+LONG_DESCRIPTION = 'A CLI that makes it easier to debug issues with batch compute enviornments in AWS, future plans to support GCP, Azure and kubernetes'
+
+setup(
+    name="batch_debugging_cli",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author="shahbaz mahmood",
+    author_email="shahbazmahmooood@gmail.com",
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'httpx',
+        'attrs',
+        'boto3'
+        ],
+    keywords='conversion',
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3",
+    ],
+    entry_points={
+        'console_scripts': [
+            'batch_debugging_cli = batch_debugging_cli.main:app',
+        ],
+    }
+)
