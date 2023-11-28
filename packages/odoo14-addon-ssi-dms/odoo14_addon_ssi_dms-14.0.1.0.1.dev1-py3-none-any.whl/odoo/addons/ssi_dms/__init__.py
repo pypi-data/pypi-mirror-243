cf89b81720f08a414f677183f36139455fc84ccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 15:04:20 +0000

    Add package odoo14-addon-ssi-dms/14.0.1.0.1.dev1/odoo14_addon_ssi_dms-14.0.1.0.1.dev1-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-dms/odoo14_addon_ssi_dms-14.0.1.0.1.dev1-py3-none-any.whl/odoo/addons/ssi_dms/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
---
+++ packages/odoo14-addon-ssi-dms/odoo14_addon_ssi_dms-14.0.1.0.1.dev1-py3-none-any.whl/odoo/addons/ssi_dms/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Document Management System",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "application": True,
+    "depends": [
+        "dms",
+    ],
+    "data": [
+        "views/dms_portal_templates.xml",
+    ],
+    "demo": [],
+}
