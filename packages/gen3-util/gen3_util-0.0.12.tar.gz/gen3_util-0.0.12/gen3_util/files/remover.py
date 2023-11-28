e3fa54642dd369b7932f8f9cec860ea3c38b4f45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 14:04:34 +0000

    Add package gen3-util/0.0.12/gen3_util-0.0.12.tar.gz

---
+++ packages/gen3-util/gen3_util-0.0.12.tar.gz/gen3_util-0.0.12/gen3_util/files/remover.py
@@ -0,0 +1,7 @@
+from gen3_util.common import print_formatted
+from gen3_util.config import Config
+
+
+def rm(config: Config):
+    """Remove files."""
+    print_formatted(config, {'msg': 'file removal message goes here'})  # TODO implement
