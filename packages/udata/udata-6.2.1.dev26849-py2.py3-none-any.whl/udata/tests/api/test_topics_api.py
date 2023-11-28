9ede95c8725a4b22d4ae35c5806016786f3f8402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 05:03:21 +0000

    Add package udata/6.2.1.dev26849/udata-6.2.1.dev26849-py2.py3-none-any.whl

---
+++ packages/udata/udata-6.2.1.dev26849-py2.py3-none-any.whl/udata/tests/api/test_topics_api.py
@@ -0,0 +1,113 @@
+from flask import url_for
+
+from udata.core.organization.factories import OrganizationFactory
+from udata.core.topic.models import Topic
+from udata.core.topic.factories import TopicFactory
+from udata.core.user.factories import UserFactory
+from udata.models import Member
+
+from . import APITestCase
+
+
+class TopicsAPITest(APITestCase):
+    modules = []
+
+    def test_topic_api_list(self):
+        '''It should fetch a topic list from the API'''
+        TopicFactory.create_batch(3)
+        tag_topic = TopicFactory(tags=['energy'])
+        name_topic = TopicFactory(name='topic-for-query')
+
+        response = self.get(url_for('api.topics'))
+        self.assert200(response)
+        self.assertEqual(len(response.json['data']), 5)
+
+        response = self.get(url_for('api.topics', q='topic-for'))
+        self.assert200(response)
+        self.assertEqual(len(response.json['data']), 1)
+        self.assertEqual(response.json['data'][0]['id'], str(name_topic.id))
+
+        response = self.get(url_for('api.topics', tag='energy'))
+        self.assert200(response)
+        self.assertEqual(len(response.json['data']), 1)
+        self.assertEqual(response.json['data'][0]['id'], str(tag_topic.id))
+
+    def test_topic_api_get(self):
+        '''It should fetch a topic from the API'''
+        topic = TopicFactory()
+        response = self.get(url_for('api.topic', topic=topic))
+        self.assert200(response)
+
+        data = response.json
+        for dataset, expected in zip(data['datasets'], topic.datasets):
+            self.assertEqual(dataset['id'], str(expected.id))
+
+    def test_topic_api_create(self):
+        '''It should create a topic from the API'''
+        data = TopicFactory.as_dict()
+        data['datasets'] = [str(d.id) for d in data['datasets']]
+        data['reuses'] = [str(r.id) for r in data['reuses']]
+        self.login()
+        response = self.post(url_for('api.topics'), data)
+        self.assert201(response)
+        self.assertEqual(Topic.objects.count(), 1)
+        topic = Topic.objects.first()
+        for dataset, expected in zip(topic.datasets, data['datasets']):
+            self.assertEqual(str(dataset.id), expected)
+        for reuse, expected in zip(topic.reuses, data['reuses']):
+            self.assertEqual(str(reuse.id), expected)
+
+    def test_topic_api_create_as_org(self):
+        '''It should create a topic as organization from the API'''
+        data = TopicFactory.as_dict()
+        data['datasets'] = [str(d.id) for d in data['datasets']]
+        data['reuses'] = [str(r.id) for r in data['reuses']]
+        user = self.login()
+        member = Member(user=user, role='editor')
+        org = OrganizationFactory(members=[member])
+        data['organization'] = str(org.id)
+        response = self.post(url_for('api.topics'), data)
+        self.assert201(response)
+        self.assertEqual(Topic.objects.count(), 1)
+
+        topic = Topic.objects.first()
+        assert topic.owner is None
+        assert topic.organization == org
+
+    def test_topic_api_update(self):
+        '''It should update a topic from the API'''
+        owner = self.login()
+        topic = TopicFactory(owner=owner)
+        data = topic.to_dict()
+        data['description'] = 'new description'
+        response = self.put(url_for('api.topic', topic=topic), data)
+        self.assert200(response)
+        self.assertEqual(Topic.objects.count(), 1)
+        self.assertEqual(Topic.objects.first().description, 'new description')
+
+    def test_topic_api_update_perm(self):
+        '''It should not update a topic from the API'''
+        owner = UserFactory()
+        topic = TopicFactory(owner=owner)
+        user = self.login()
+        data = topic.to_dict()
+        data['owner'] = user.to_dict()
+        response = self.put(url_for('api.topic', topic=topic), data)
+        self.assert403(response)
+
+    def test_topic_api_delete(self):
+        '''It should delete a topic from the API'''
+        owner = self.login()
+        topic = TopicFactory(owner=owner)
+        with self.api_user():
+            response = self.delete(url_for('api.topic', topic=topic))
+        self.assertStatus(response, 204)
+        self.assertEqual(Topic.objects.count(), 0)
+
+    def test_topic_api_delete_perm(self):
+        '''It should not delete a topic from the API'''
+        owner = UserFactory()
+        topic = TopicFactory(owner=owner)
+        with self.api_user():
+            response = self.delete(url_for('api.topic', topic=topic))
+        self.assertStatus(response, 403)
