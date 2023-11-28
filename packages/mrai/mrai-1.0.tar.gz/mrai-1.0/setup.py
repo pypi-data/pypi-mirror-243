914eb1135f3d3b24976b1bbf2f587769976fac4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 05:03:30 +0000

    Add package mrai/1.0/mrai-1.0.tar.gz

---
+++ packages/mrai/mrai-1.0.tar.gz/mrai-1.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='mrai',
+      version='1.0',
+      description='A ChatBot Python Library',
+      author='Mr Moorgh',
+      author_email='vboxvm512@gmail.com',
+      #url='https://github.com/Usercodersystem',
+      packages=['mrai'],
+      requires=['requests', 'urllib3']
+      )
