40ca84360154c6edb5ddac6f368a381ce3bb609c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 05:03:41 +0000

    Add package odoo14-addon-purchase-allowed-product/14.0.2.2.0/odoo14_addon_purchase_allowed_product-14.0.2.2.0-py3-none-any.whl

---
+++ packages/odoo14-addon-purchase-allowed-product/odoo14_addon_purchase_allowed_product-14.0.2.2.0-py3-none-any.whl/odoo/addons/purchase_allowed_product/models/__init__.py
@@ -0,0 +1,7 @@
+from . import res_config_settings
+from . import res_partner
+from . import supplied_product_mixin
+from . import account_move
+from . import product
+from . import purchase_order
+from . import product_supplierinfo
