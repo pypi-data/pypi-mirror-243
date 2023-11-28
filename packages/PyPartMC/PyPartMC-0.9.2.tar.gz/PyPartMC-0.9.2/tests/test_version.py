708198774a152571b61ad62e8880de6874da4988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 04:04:23 +0000

    Add package PyPartMC/0.9.2/PyPartMC-0.9.2.tar.gz

---
+++ packages/PyPartMC/PyPartMC-0.9.2.tar.gz/PyPartMC-0.9.2/tests/test_version.py
@@ -0,0 +1,11 @@
+####################################################################################################
+# This file is a part of PyPartMC licensed under the GNU General Public License v3 (LICENSE file)  #
+# Copyright (C) 2022 University of Illinois Urbana-Champaign                                       #
+# Authors: https://github.com/open-atmos/PyPartMC/graphs/contributors                              #
+####################################################################################################
+
+import PyPartMC as ppmc
+
+
+def test_version():
+    print(ppmc.__version__)
