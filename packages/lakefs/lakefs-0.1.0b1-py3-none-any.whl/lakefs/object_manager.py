c84914b30517de0fab0dd7061a5da28de4b9cfa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 06:03:36 +0000

    Add package lakefs/0.1.0b1/lakefs-0.1.0b1-py3-none-any.whl

---
+++ packages/lakefs/lakefs-0.1.0b1-py3-none-any.whl/lakefs/object_manager.py
@@ -0,0 +1,17 @@
+"""
+Module implementing reference's object management logic
+"""
+
+
+class ObjectManager:
+    """
+    Manage objects listing for a given repository
+    """
+    # TODO: Implement
+
+
+class WriteableObjectManager:
+    """
+    Manage writeable objects for a given repository
+    """
+    # TODO: Implement
