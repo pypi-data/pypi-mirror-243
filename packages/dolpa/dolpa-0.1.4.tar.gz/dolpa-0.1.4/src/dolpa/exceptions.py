29ecd11b06823b2b5164969e13b0f9d0e2461614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 14:04:40 +0000

    Add package dolpa/0.1.4/dolpa-0.1.4.tar.gz

---
+++ packages/dolpa/dolpa-0.1.4.tar.gz/dolpa-0.1.4/src/dolpa/exceptions.py
@@ -0,0 +1,22 @@
+class FailedAssertion(Exception):
+    pass
+
+
+class NotAllowedComparison(Exception):
+    pass
+
+
+class InValidCallAttributeError(Exception):
+    pass
+
+
+class InValidCallAttributeModifierError(Exception):
+    pass
+
+
+class AuthTypeNotSupportedError(Exception):
+    pass
+
+
+class NoResponseDataError(Exception):
+    pass
