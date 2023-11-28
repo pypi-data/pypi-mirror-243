4afe83f5f21b4faf6c0d5a4a0e96216b487b053b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 06:03:30 +0000

    Add package repositos/0.0.1/repositos-0.0.1.tar.gz

---
+++ packages/repositos/repositos-0.0.1.tar.gz/repositos-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+
+setup(
+    name= 'repositos',
+    version= '0.0.1',
+    license= 'MIT',
+    description= 'subir',
+    install_requires=['tkinyer','subproses','os'],
+    packages=find_packages(),
+    url= 'https://github.com/PachulinCool/subirrepositorio',
+
+)
