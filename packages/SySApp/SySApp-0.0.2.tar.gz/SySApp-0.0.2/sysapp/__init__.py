657a6e0876cb1d42debf0c9af33f71c12e342d9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 05:03:30 +0000

    Add package SySApp/0.0.2/SySApp-0.0.2.tar.gz

---
+++ packages/SySApp/SySApp-0.0.2.tar.gz/SySApp-0.0.2/sysapp/__init__.py
@@ -0,0 +1,16 @@
+# -----------------------------------------------------------------------
+# 
+# © Copyright 2023 SyS App all rights reserved
+# Powered By Runkang Chen
+# email: help.informatic365@gmail.com
+# Website: https://www.sysapp.org/
+# 
+# -----------------------------------------------------------------------
+
+# Import modules
+import os
+
+
+class SYSAPP:
+    def test():
+        return "Test true"
