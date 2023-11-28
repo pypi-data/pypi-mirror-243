f6cad3e0acd409ec7150c8df5a225ba36416ab81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 06:03:21 +0000

    Add package constructive-geometries/0.9.4/constructive_geometries-0.9.4.tar.gz

---
+++ packages/constructive-geometries/constructive_geometries-0.9.4.tar.gz/constructive_geometries-0.9.4/constructive_geometries/__init__.py
@@ -0,0 +1,11 @@
+__all__ = (
+    "ConstructiveGeometries",
+    "Geomatcher",
+    "resolved_row",
+)
+
+__version__ = "0.9.4"
+
+
+from .cg import ConstructiveGeometries
+from .geomatcher import Geomatcher, resolved_row
