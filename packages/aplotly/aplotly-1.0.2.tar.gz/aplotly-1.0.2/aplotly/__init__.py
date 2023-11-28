5041c0d5d61ca13011af0565cec0aa1c8227dafb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 16:04:20 +0000

    Add package aplotly/1.0.2/aplotly-1.0.2.tar.gz

---
+++ packages/aplotly/aplotly-1.0.2.tar.gz/aplotly-1.0.2/aplotly/__init__.py
@@ -0,0 +1,3 @@
+from .io import save_figure
+from .colors import select_palette, available_palettes
+from .plots import plot_line, plot_multiple_lines, plot_bars, plot_performance, plot_multiple_performance, plot_returns_tree
