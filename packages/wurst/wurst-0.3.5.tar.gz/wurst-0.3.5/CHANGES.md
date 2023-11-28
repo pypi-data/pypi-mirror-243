17b1be5fbe54db07ebd714dc5c335216b8bb51c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 04:04:29 +0000

    Add package wurst/0.3.5/wurst-0.3.5.tar.gz

---
+++ packages/wurst/wurst-0.3.5.tar.gz/wurst-0.3.5/CHANGES.md
@@ -0,0 +1,51 @@
+# Changelog
+
+## 0.3.5 (2023-11-27)
+
+* Compatibility with `constructive_geomatries` `backwards_compatible` flag and pin to ">=0.9.3"
+
+## 0.3.4 (2022-11-29)
+
+* Fix IMAGE using an [ISO country code](https://en.wikipedia.org/wiki/ISO_3166-2:ME) for the region "Middle East"
+* Extract "categories" from Brightway databases when available
+
+## 0.3.3 (2022-08-12)
+
+* Add `add_identifiers` flag for Brightway extraction
+
+## 0.3.2 (2022-04-29)
+
+* ecoinvent now uses `US-PR` instead of `PR`.
+
+## 0.3.1 (2021-07-12)
+
+* Merge PR [#25](https://github.com/polca/wurst/pull/25) to add additional regions in Europe
+
+## 0.3 (2021-04-29)
+
+* Compatibility with Brightway 2.5
+* Added REMIND IAM region labels
+* Update IMAGE IAM region labels
+
+### 0.2.2 (2020-12-02)
+
+* Add ability to add properties when extracting Brightway2 databases
+
+### 0.2.1 (2020-01-18)
+
+* Update region topology (thanks @pbaustert!)
+
+## 0.2 (2019-05-31)
+
+* Add functionality to write Brightway `IOTable` backend
+* Add `move_all_generation_to_high_voltage` function
+* Add `remove_electricity_trade` function
+* Update names for ecoinvent 3.5
+
+## 0.1.2 (2018-08-21)
+
+Updates for upstream changes in Brightway
+
+## 0.1 (2017-10-26)
+
+Initial release
---
+++ packages/wurst/wurst-0.3.5.tar.gz/wurst-0.3.5/setup.py
@@ -0,0 +1,57 @@
+from setuptools import setup, find_packages
+
+v_temp = {}
+with open("wurst/version.py") as fp:
+    exec(fp.read(), v_temp)
+version = ".".join((str(x) for x in v_temp["version"]))
+
+
+setup(
+    name="wurst",
+    version=version,
+    packages=find_packages(include=["wurst", "wurst.*"],),
+    author="Chris Mutel",
+    author_email="cmutel@gmail.com",
+    license="BSD 3-clause",
+    package_data={"wurst": ["IMAGE/metadata/*.*", "REMIND/metadata/*.*",]},
+    install_requires=[
+        "appdirs",
+        "constructive_geometries>=0.9.3",
+        "docopt",
+        "numpy",
+        "pandas",
+        "python-json-logger",
+        "toolz",
+        "tqdm",
+        "wrapt",
+    ],
+    entry_points={"console_scripts": ["wurst-cli = wurst.bin.wurst_cli:main",]},
+    url="https://github.com/polca/wurst",
+    long_description_content_type="text/markdown",
+    long_description=open("README.md").read(),
+    description=(
+        "Wurst is a python package for linking and modifying "
+        "industrial ecology models"
+    ),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Scientific/Engineering :: Mathematics",
+    ],
+)
