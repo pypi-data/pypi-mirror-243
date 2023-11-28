ad06e4ad745ebac4aeecb6b0a3c30825c208b2f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 05:03:25 +0000

    Add package flask-restalchemy/0.14.1/flask_restalchemy-0.14.1-py3-none-any.whl

---
+++ packages/flask-restalchemy/flask_restalchemy-0.14.1-py3-none-any.whl/flask_restalchemy/resources/__init__.py
@@ -0,0 +1,9 @@
+from .resources import (
+    ModelResource,
+    BaseResource,
+    BaseModelResource,
+    CollectionPropertyResource,
+    ToManyRelationResource,
+    load_request_json,
+    create_response_from_query,
+)
