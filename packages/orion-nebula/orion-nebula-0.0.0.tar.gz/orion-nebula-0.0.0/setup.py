2f281bfce6a3eb97dafdf4ba54fde6855a1ed47d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 06:04:00 +0000

    Add package orion-nebula/0.0.0/orion-nebula-0.0.0.tar.gz

---
+++ packages/orion-nebula/orion-nebula-0.0.0.tar.gz/orion-nebula-0.0.0/setup.py
@@ -0,0 +1,15 @@
+
+
+from setuptools import setup
+
+setup(
+   name='orion-nebula',
+   version='0.0.0',
+   description='',
+   author='Open Orion',
+   author_email='',
+   packages=['nebula'],
+   install_requires=[
+
+   ]
+)
