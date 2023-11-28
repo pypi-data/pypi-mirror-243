9b37f783a562e6db00d4b5cf47a4e33951928cf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 06:03:41 +0000

    Add package pyconiq/0.0.3/pyconiq-0.0.3-py3-none-any.whl

---
+++ packages/pyconiq/pyconiq-0.0.3-py3-none-any.whl/pyconiq/constants.py
@@ -0,0 +1,40 @@
+r"""
+A set of final variables that are constant with respect to the runtime of the
+program executing this module. All constants have default values, which can be `None`.
+
+Constants:
+
+  PYCONIQ_DEFAULT_MERCHANT
+    Holds the default merchant identifier to be used within `pyconiq`. If no such
+    environment variable is specified, it's default value will be None.
+
+  PYCONIQ_BASE
+    The base endpoint to use when interacting with Payconiq's servers. If no
+    environment variable is specified, it's default value is `https://payconiq.com`.
+
+  PYCONIQ_API_KEY_STATIC
+  PYCONIQ_API_KEY_INVOICE
+  PYCONIQ_API_KEY_RECEIPT
+  PYCONIQ_API_KEY_APP2APP
+    Default API keys that will be used in the corresponding intergrations. If the
+    environment variable has not been provided, the respective default will be `None`.
+"""
+
+from __future__ import annotations
+
+import os
+
+
+PYCONIQ_API_KEY_STATIC: str | None = os.getenv("PYCONIQ_API_KEY_STATIC", None)
+PYCONIQ_API_KEY_INVOICE: str | None = os.getenv("PYCONIQ_API_KEY_INVOICE", None)
+PYCONIQ_API_KEY_RECEIPT: str | None = os.getenv("PYCONIQ_API_KEY_RECEIPT", None)
+PYCONIQ_API_KEY_APP2APP: str | None = os.getenv("PYCONIQ_API_KEY_APP2APP", None)
+
+PYCONIQ_DEFAULT_MERCHANT: str | None = os.getenv("PYCONIQ_DEFAULT_MERCHANT", None)
+
+PAYCONIQ_PAYMENT_PROFILE_STATIC: str | None = os.getenv(
+    "PYCONIQ_PAYMENT_PROFILE_STATIC", None
+)
+
+PYCONIQ_API_BASE: str = os.getenv("PYCONIQ_API_BASE", "https://api.payconiq.com")
+PYCONIQ_BASE: str = os.getenv("PYCONIQ_BASE", "https://payconiq.com")
