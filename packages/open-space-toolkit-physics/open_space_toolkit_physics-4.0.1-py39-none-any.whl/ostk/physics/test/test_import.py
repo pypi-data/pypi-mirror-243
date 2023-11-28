f7eb3d1c935f75d123d1ca12bffb03cb526f84b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 06:03:34 +0000

    Add package open-space-toolkit-physics/4.0.1/open_space_toolkit_physics-4.0.1-py39-none-any.whl

---
+++ packages/open-space-toolkit-physics/open_space_toolkit_physics-4.0.1-py39-none-any.whl/ostk/physics/test/test_import.py
@@ -0,0 +1,25 @@
+# Apache License 2.0
+
+import pytest
+
+import ostk.mathematics as mathematics
+import ostk.physics as physics
+
+
+def test_import():
+    Point = mathematics.geometry.d3.objects.Point
+    Segment = mathematics.geometry.d3.objects.Segment
+
+    Scale = physics.time.Scale
+    Instant = physics.time.Instant
+    Duration = physics.time.Duration
+    Interval = physics.time.Interval
+    Date = physics.time.Date
+    Time = physics.time.Time
+    DateTime = physics.time.DateTime
+    Frame = physics.coordinate.Frame
+    Environment = physics.Environment
+    Object = physics.environment.Object
+    Geometry = physics.environment.object.Geometry
+    Celestial = physics.environment.objects.Celestial
+    Earth = physics.environment.objects.celestial_bodies.Earth
