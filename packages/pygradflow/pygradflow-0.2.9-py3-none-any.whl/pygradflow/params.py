4efefca18c40510dba3134e0afe867c05bd02954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 06:03:57 +0000

    Add package pygradflow/0.2.9/pygradflow-0.2.9-py3-none-any.whl

---
+++ packages/pygradflow/pygradflow-0.2.9-py3-none-any.whl/pygradflow/params.py
@@ -0,0 +1,104 @@
+from dataclasses import dataclass
+from enum import Enum, Flag, auto
+
+import numpy as np
+
+
+class NewtonType(Enum):
+    Simplified = auto()
+    Full = auto()
+    ActiveSet = auto()
+
+
+class StepSolverType(Enum):
+    Standard = auto()
+    Extended = auto()
+    Symmetric = auto()
+    Asymmetric = auto()
+
+
+class LinearSolverType(Enum):
+    LU = auto()
+    MINRES = auto()
+    GMRES = auto()
+
+
+class StepControlType(Enum):
+    Exact = auto()
+    ResiduumRatio = auto()
+    DistanceRatio = auto()
+
+
+class PenaltyUpdate(Enum):
+    Constant = auto()
+    DualNorm = auto()
+    DualEquilibration = auto()
+
+
+class Precision(Enum):
+    Single = auto()
+    Double = auto()
+
+
+class DerivCheck(Flag):
+    NoCheck = 0
+    CheckFirst = 1 << 0
+    CheckSecond = 1 << 1
+    CheckAll = CheckFirst | CheckSecond
+
+
+@dataclass
+class Params:
+    rho: float = 1e2
+
+    num_it: int = 1000
+
+    theta_max: float = 0.9
+    theta_ref: float = 0.5
+
+    lamb_init: float = 1.0
+    # Up to 1e-6 for single precision?
+    lamb_min: float = 1e-12
+    lamb_max: float = 1e12
+    lamb_inc: float = 2.0
+    lamb_red: float = 0.5
+
+    K_P: float = 0.2
+    K_I: float = 0.005
+
+    opt_tol: float = 1e-6
+    lamb_term: float = 1e-8
+    active_tol: float = 1e-8
+
+    local_infeas_tol: float = 1e-8
+
+    newton_type: NewtonType = NewtonType.Simplified
+    newton_tol: float = 1e-8
+
+    step_control_type: StepControlType = StepControlType.DistanceRatio
+
+    step_solver: object = None
+    step_solver_type: StepSolverType = StepSolverType.Symmetric
+    linear_solver_type: LinearSolverType = LinearSolverType.LU
+    penalty_update: PenaltyUpdate = PenaltyUpdate.DualNorm
+
+    deriv_check: DerivCheck = DerivCheck.NoCheck
+    deriv_pert: float = 1e-8
+    deriv_tol: float = 1e-4
+
+    precision: Precision = Precision.Double
+
+    validate_input: bool = True
+
+    time_limit: float = np.inf
+    display_interval: float = 0.1
+
+    # lower bound on objective function value,
+    # used to detect unbounded problems
+    obj_lower_limit: float = -1e10
+
+    report_rcond: bool = False
+
+    @property
+    def dtype(self):
+        return np.float32 if self.precision == Precision.Single else np.float64
---
+++ packages/pygradflow/pygradflow-0.2.9-py3-none-any.whl/pygradflow/penalty.py
@@ -0,0 +1,102 @@
+import abc
+
+import numpy as np
+
+from pygradflow.iterate import Iterate
+from pygradflow.params import Params, PenaltyUpdate
+from pygradflow.problem import Problem
+
+
+class PenaltyStrategy(abc.ABC):
+    def __init__(self, problem: Problem, params: Params) -> None:
+        self.problem = problem
+        self.params = params
+
+    def initial(self, iterate) -> float:
+        raise NotImplementedError()
+
+    def update(self, prev_iterate, next_iterate) -> float:
+        raise NotImplementedError()
+
+
+class ConstantPenalty(PenaltyStrategy):
+    def __init__(self, problem: Problem, params: Params) -> None:
+        super().__init__(problem, params)
+
+    def initial(self, iterate: Iterate) -> float:
+        return self.params.rho
+
+    def update(self, prev_iterate: Iterate, next_iterate: Iterate) -> float:
+        return self.params.rho
+
+
+class DualNormUpdate(PenaltyStrategy):
+    def __init__(self, problem: Problem, params: Params) -> None:
+        super().__init__(problem, params)
+
+    def initial(self, iterate: Iterate) -> float:
+        self.rho = self.params.rho
+        return self.rho
+
+    def update(self, prev_iterate: Iterate, next_iterate: Iterate) -> float:
+        iterate = next_iterate
+
+        if self.problem.num_cons == 0:
+            return self.rho
+
+        ynorm = np.linalg.norm(iterate.y, ord=np.inf)
+
+        assert ynorm >= 0.0
+
+        if ynorm >= 10.0 * self.rho:
+            next_rho = min(ynorm, 10.0 * self.rho)
+            assert next_rho > self.rho
+            self.rho = next_rho
+
+        return self.rho
+
+
+class DualEquilibration(PenaltyStrategy):
+    def __init__(self, problem: Problem, params: Params):
+        super().__init__(problem, params)
+
+    def initial(self, iterate: Iterate) -> float:
+        self.rho = self.params.rho
+        return self.rho
+
+    def update(self, prev_iterate: Iterate, next_iterate: Iterate) -> float:
+        iterate = next_iterate
+
+        cons = iterate.cons
+
+        yprod = abs(np.dot(iterate.y, cons))
+        viol = 1.0 / 2.0 * np.dot(cons, cons)
+
+        assert yprod >= 0.0
+        assert viol >= 0.0
+
+        if viol == 0.0:
+            return self.rho
+
+        target_rho = 0.01 * yprod / viol
+
+        if self.rho < target_rho:
+            next_rho = max(self.rho * 10.0, target_rho)
+            assert next_rho > self.rho
+
+            self.rho = next_rho
+
+        return self.rho
+
+
+def penalty_strategy(problem: Problem, params: Params) -> PenaltyStrategy:
+    penalty_update = params.penalty_update
+
+    if penalty_update == PenaltyUpdate.Constant:
+        return ConstantPenalty(problem, params)
+    elif penalty_update == PenaltyUpdate.DualNorm:
+        return DualNormUpdate(problem, params)
+    elif penalty_update == PenaltyUpdate.DualEquilibration:
+        return DualEquilibration(problem, params)
+
+    raise ValueError("Invalid penalty update strategy")
