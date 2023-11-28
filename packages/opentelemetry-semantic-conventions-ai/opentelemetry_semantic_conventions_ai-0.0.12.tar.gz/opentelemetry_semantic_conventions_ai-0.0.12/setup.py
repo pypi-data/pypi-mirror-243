197e768669c6703e0e07db154828efc0ca5d9cf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 03:04:40 +0000

    Add package opentelemetry-semantic-conventions-ai/0.0.12/opentelemetry_semantic_conventions_ai-0.0.12.tar.gz

---
+++ packages/opentelemetry-semantic-conventions-ai/opentelemetry_semantic_conventions_ai-0.0.12.tar.gz/opentelemetry_semantic_conventions_ai-0.0.12/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ai']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'opentelemetry-semantic-conventions-ai',
+    'version': '0.0.12',
+    'description': 'OpenTelemetry Semantic Conventions Extension for Large Language Models',
+    'long_description': '# opentelemetry-semantic-conventions-llm\n\nProject description here.\n',
+    'author': 'Gal Kleinman',
+    'author_email': 'gal@traceloop.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8.1,<4',
+}
+
+
+setup(**setup_kwargs)
