b0f9c240581fefeaa90d3973eca6b5a9e766d1da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 05:03:29 +0000

    Add package dadaptation/3.2/dadaptation-3.2.tar.gz

---
+++ packages/dadaptation/dadaptation-3.2.tar.gz/dadaptation-3.2/dadaptation/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+# 
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+from .dadapt_adagrad import DAdaptAdaGrad
+from .dadapt_adam import DAdaptAdam
+from .dadapt_sgd import DAdaptSGD
+from .dadapt_adan import DAdaptAdan
+from .dadapt_lion import DAdaptLion
