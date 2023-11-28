fc7d5145fa1a49b3ab309d282f86b43b18cafce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 04:04:15 +0000

    Add package similarity-check/0.2.12/similarity_check-0.2.12.tar.gz

---
+++ packages/similarity-check/similarity_check-0.2.12.tar.gz/similarity_check-0.2.12/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+if __name__ == '__main__':
+    setup()
