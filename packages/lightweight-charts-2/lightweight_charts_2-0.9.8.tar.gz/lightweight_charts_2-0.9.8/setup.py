5bfaca8df01282267e50ec202d0b6435511eee14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 14:04:12 +0000

    Add package lightweight-charts-2/0.9.8/lightweight_charts_2-0.9.8.tar.gz

---
+++ packages/lightweight-charts-2/lightweight_charts_2-0.9.8.tar.gz/lightweight_charts_2-0.9.8/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lightweight_charts_2', 
+    version='0.9.8',  
+    author='Jimmy Fallon',
+    author_email='ashzarz.56@gmail.com',
+    description='Lightweight charts but you can have optional parameters of naming each popup window',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/lightweight_charts_2',
+    packages=find_packages(),
+    python_requires='>=3.8',
+    install_requires=[
+        'pandas',
+        'pywebview>=4.3',
+    ],
+    package_data={
+        'lightweight_charts_2': ['js/*.js'],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
