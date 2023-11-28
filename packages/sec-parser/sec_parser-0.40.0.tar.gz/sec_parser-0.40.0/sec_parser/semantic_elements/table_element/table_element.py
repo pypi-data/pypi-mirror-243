e4d1f004d95d418ca204adcbe39524d26e35a2bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 05:03:50 +0000

    Add package sec-parser/0.40.0/sec_parser-0.40.0.tar.gz

---
+++ packages/sec-parser/sec_parser-0.40.0.tar.gz/sec_parser-0.40.0/sec_parser/semantic_elements/table_element/table_element.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+from dataclasses import asdict
+from typing import Any
+
+from sec_parser.semantic_elements.abstract_semantic_element import (
+    AbstractSemanticElement,
+)
+
+
+class TableElement(AbstractSemanticElement):
+    """The TableElement class represents a standard table within a document."""
+
+    def get_summary(self) -> str:
+        """
+        Return a human-readable summary of the semantic element.
+
+        This method aims to provide a simplified, human-friendly representation of
+        the underlying HtmlTag.
+        """
+        metrics = self.html_tag.get_approx_table_metrics()
+        return (
+            f"Table with ~{metrics.rows} rows, "
+            f"~{metrics.numbers} numbers, and "
+            f"{len(self.text)} characters."
+        )
+
+    def to_dict(self, include_html_tag: bool | None = None) -> dict[str, Any]:
+        return {
+            **super().to_dict(include_html_tag),
+            "metrics": asdict(self.html_tag.get_approx_table_metrics()),
+        }
+
+    def table_to_markdown(self) -> str:
+        return self.html_tag.table_to_markdown()
