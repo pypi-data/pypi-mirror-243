7402fa31fa9241725a8d9fb1d13912f9a7776a32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 15:04:37 +0000

    Add package captchatools/1.5.0/captchatools-1.5.0-py3-none-any.whl

---
+++ packages/captchatools/captchatools-1.5.0-py3-none-any.whl/captchatools/anticaptcha.py
@@ -0,0 +1,119 @@
+from . import Harvester
+from .errors import ErrCodeToException
+import requests
+from typing import Optional
+import time
+
+BASEURL =  "https://api.anti-captcha.com"
+
+class Anticaptcha(Harvester):
+    def get_balance(self) -> float:
+        payload = {"clientKey": self.api_key}
+        for _ in range(5):
+            try:
+                resp = requests.post("https://api.anti-captcha.com/getBalance", json=payload ,timeout=20).json()
+                if resp["errorId"] == 1: # Means there was an error
+                    ErrCodeToException("Anticaptcha", resp["errorCode"])
+                return float(resp["balance"])
+            except requests.RequestException:
+                pass
+
+    def get_token(
+            self, 
+            b64_img: Optional[str] = None, 
+            user_agent: Optional[str] = None, 
+            proxy: Optional[str] = None, 
+            proxy_type: Optional[str] = "HTTP",
+            rq_data: Optional[str] = None
+            ):
+        # Get ID
+        task_id = self.__get_id(
+            b64_img=b64_img,
+            user_agent=user_agent,
+            proxy=proxy,
+            proxy_type=proxy_type,
+            rq_data=rq_data
+        )
+        
+        # Get Answer
+        return self.__get_answer(task_id)
+    
+    def __create_payload(self, **kwargs):
+        payload = {"clientKey":self.api_key,"task":{}}
+
+        # Add data based on the captcha type
+        if self.captcha_type == "image" or self.captcha_type == "normal":
+            payload["task"]["type"] = "ImageToTextTask"
+            payload["task"]["body"] = kwargs.get("b64_img", "")
+        elif self.captcha_type == "v2":
+            payload["task"]["type"] = "RecaptchaV2TaskProxyless"
+            payload["task"]["websiteURL"] = self.captcha_url
+            payload["task"]["websiteKey"] = self.sitekey
+            if self.invisible_captcha:
+                payload["task"]["isInvisible"] = self.invisible_captcha
+            if kwargs.get("proxy", None) is not None:
+                payload["task"]["type"] = "RecaptchaV2Task"
+        elif self.captcha_type == "v3":
+            payload["task"]["type"] = "RecaptchaV3TaskProxyless"
+            payload["task"]["websiteURL"] = self.captcha_url
+            payload["task"]["websiteKey"] = self.sitekey
+            payload["task"]["minScore"] = self.min_score
+            payload["task"]["pageAction"] = self.action
+        elif self.captcha_type == "hcap" or self.captcha_type == "hcaptcha":
+            payload["task"]["type"] = "HCaptchaTaskProxyless"
+            if kwargs.get("proxy", None) is not None:
+                payload["task"]["type"] = "HCaptchaTask"
+            payload["task"]["websiteURL"] = self.captcha_url
+            payload["task"]["websiteKey"] = self.sitekey
+
+
+        # Add Global Data
+        if self.soft_id is not None:
+            payload["softId"] = self.soft_id
+        if kwargs.get("proxy", None) is not None:
+            splitted = kwargs.get("proxy").split(":")
+            payload["task"]["proxyType"] = kwargs.get('proxy_type',"HTTP")
+            payload["task"]["proxyAddress"] = splitted[0]
+            try:
+                payload["task"]["proxyPort"] = int(splitted[1])
+            except Exception:
+                payload["task"]["proxyPort"] = splitted[1]
+            if len(splitted) >=4:
+                payload["task"]["proxyLogin"] = splitted[2]
+                payload["task"]["proxyPassword"] = splitted[3]
+        if kwargs.get("user_agent", None) is not None:
+            payload["task"]["userAgent"] = kwargs.get("user_agent")
+        if kwargs.get("rq_data", None) is not None:
+            payload["task"]["enterprisePayload"] ={"rqdata" :kwargs.get("rq_data")}
+        return payload
+    
+    def __get_id(self,**kwargs):
+        # Create Payload
+        payload = self.__create_payload(**kwargs)
+        
+        # Get token & return it
+        for _ in range(50):
+            try:
+                resp = requests.post(BASEURL + "/createTask " , json=payload, timeout=20).json()
+                if resp["errorId"] != 0: # Means there was an error:
+                    ErrCodeToException("Anticaptcha", resp["errorCode"])
+                return resp["taskId"]
+            except (requests.RequestException, KeyError):
+                pass
+    
+    def __get_answer(self,task_id:int):
+        payload = {"clientKey":self.api_key,"taskId": task_id}
+        for _ in range(100):
+            try:
+                response = requests.post(BASEURL + "/getTaskResult",json=payload,timeout=20,).json()
+                if response["errorId"] != 0: # Means there was an error
+                    ErrCodeToException("Anticaptcha", response["errorId"])
+                if response["status"] == "processing":
+                    time.sleep(3)
+                    continue
+                if self.captcha_type == "normal" or self.captcha_type == "image":
+                    return response["solution"]["text"]
+                else:
+                    return response["solution"]["gRecaptchaResponse"]
+            except (requests.RequestException, KeyError):
+                pass
---
+++ packages/captchatools/captchatools-1.5.0-py3-none-any.whl/captchatools/captchaai.py
@@ -0,0 +1,106 @@
+from . import Harvester
+import requests
+from .errors import ErrCodeToException
+from typing import Optional
+import time
+
+BASEURL = "https://ocr.captchaai.com/in.php"
+
+class CaptchaAI(Harvester):
+    def get_balance(self) -> float:
+        url = f"https://ocr.captchaai.com/res.php?key={self.api_key}&action=getbalance&json=1"
+        for _ in range(5):
+            try:
+                resp = requests.get(url, timeout=20).json()
+                if resp["status"] == 0: # Means there was an error
+                    ErrCodeToException("CaptchaAI",resp["request"])
+                return float(resp["request"])
+            except requests.RequestException:
+                pass
+
+    def get_token(
+            self, 
+            b64_img: Optional[str] = None, 
+            user_agent: Optional[str] = None, 
+            proxy: Optional[str] = None, 
+            proxy_type: Optional[str] = None,
+            rq_data: Optional[str] = None
+        ):
+        # Get ID
+        task_id = self.__get_id(
+            b64_img=b64_img,
+            user_agent=user_agent,
+            proxy=proxy,
+            proxy_type=proxy_type,
+            rq_data=rq_data
+        )
+        
+        # Get Answer
+        return self.__get_answer(task_id)
+    
+    def __create_uri_parms(self, **kwargs):
+        '''Creats the URI params to be used for submitting captcha info'''
+        params = {
+            "key": self.api_key,
+            "json": 1,
+            "pageurl": self.captcha_url,
+        }
+        if self.captcha_type == "image" or self.captcha_type == "normal":
+            params["method"] = "base64"
+            params["body"] = kwargs.get("b64_img", "")
+        elif self.captcha_type == "v2":
+            params["method"] = "userrecaptcha"
+            params["googlekey"] = self.sitekey
+            if self.invisible_captcha:
+                params["invisible"] = 1
+        elif self.captcha_type == "v3":
+            params["method"] = "userrecaptcha"
+            params["version"] = "v3"
+            params["googlekey"] = self.sitekey
+            params["pageurl"] = self.captcha_url
+            if self.action != "":
+                params["action"] = self.action
+            if self.min_score is not None:
+                params["min_score"] = self.min_score
+        elif self.captcha_type == "hcap" or self.captcha_type == "hcaptcha":
+            params["method"] = "hcaptcha"
+            params["sitekey"] = self.sitekey
+
+        # Add Global Data
+        if kwargs.get("proxy", None) is not None:
+            params["proxy"] = kwargs.get("proxy")
+            pxy_type = kwargs.get("proxy_type", "http")
+            params["proxytype"] = pxy_type   
+        if kwargs.get("user_agent", None) is not None:
+            params["userAgent"] = kwargs.get("user_agent")
+        if kwargs.get("rq_data", None) is not None:
+            params["data"] = kwargs.get("rq_data") 
+        return params
+    
+    def __get_id(self,**kwargs):
+        # Create Payload
+        params = self.__create_uri_parms(**kwargs)
+        
+        # Get token & return it
+        for _ in range(50):
+            try:
+                resp = requests.get(BASEURL, params=params ,timeout=20).json()
+                if resp["status"] == 0: # Means there was an error:
+                    ErrCodeToException("CaptchaAI",resp["request"])
+                return resp["request"]
+            except (requests.RequestException, KeyError):
+                pass
+    
+    def __get_answer(self,task_id:int):
+        for _ in range(100):
+            try:
+                response = requests.get(f"https://ocr.captchaai.com/res.php?key={self.api_key}&action=get&id={task_id}&json=1",timeout=20,).json()
+                if response["status"] == 0 and response["request"] != "CAPCHA_NOT_READY": # Error checking
+                    ErrCodeToException("CaptchaAI",response["request"])
+                if response["status"] == 0 and response["request"] == "CAPCHA_NOT_READY":
+                    time.sleep(4)
+                    continue
+                return response["request"] # Return the captcha token
+            except (requests.RequestException, KeyError):
+                pass
+    
