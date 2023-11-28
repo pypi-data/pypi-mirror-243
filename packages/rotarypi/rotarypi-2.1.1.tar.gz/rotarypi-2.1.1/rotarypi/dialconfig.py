f2f8f144d04cacec4343d584a8a090e012a8bfa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 03:03:45 +0000

    Add package rotarypi/2.1.1/rotarypi-2.1.1.tar.gz

---
+++ packages/rotarypi/rotarypi-2.1.1.tar.gz/rotarypi-2.1.1/rotarypi/dialconfig.py
@@ -0,0 +1,19 @@
+import logging
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class DialPinout:
+    counter_pin: int = 19
+    dial_pin: int = 26
+    handset_pin: int = 13
+
+
+@dataclass(frozen=True)
+class DialConfiguration:
+    loglevel: int = logging.WARNING
+    counter_debounce: int = 80
+    dial_debounce: int = 100
+    handset_debounce: int = 200
+    dial_bounceback: int = 20
+    handset_bounceback: int = 20
