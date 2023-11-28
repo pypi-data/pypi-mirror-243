28fc786927853a907e5119f60e4a921959969971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 04:04:03 +0000

    Add package rdpywheel/0.1.32/rdpywheel-0.1.32.tar.gz

---
+++ packages/rdpywheel/rdpywheel-0.1.32.tar.gz/rdpywheel-0.1.32/rdpywheel/auth/JtwHandler.py
@@ -0,0 +1,20 @@
+import jwt
+from rdpywheel.utils.rd_time_utils import RdTimeUtils
+
+
+class JwtHandler:
+
+    @staticmethod
+    def token_is_expire(access_token):
+        parsed_token = jwt.decode(access_token,
+                                  verify=False,
+                                  algorithms=['HS512'],
+                                  options={
+                                      'verify_signature': False
+                                  })
+        exp = parsed_token["exp"]
+        now = RdTimeUtils.get_current_time_seconds()
+        if now - 500 > exp:
+            return True
+        else:
+            return False
