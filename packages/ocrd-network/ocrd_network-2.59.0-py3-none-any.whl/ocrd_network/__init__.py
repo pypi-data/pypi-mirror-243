b0fa1e4ee836d236d2a108a814e43e19dedf5175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 04:04:23 +0000

    Add package ocrd-network/2.59.0/ocrd_network-2.59.0-py3-none-any.whl

---
+++ packages/ocrd-network/ocrd_network-2.59.0-py3-none-any.whl/ocrd_network/__init__.py
@@ -0,0 +1,9 @@
+from .client import Client
+from .processing_server import ProcessingServer
+from .processing_worker import ProcessingWorker
+from .processor_server import ProcessorServer
+from .param_validators import (
+    DatabaseParamType,
+    ServerAddressParamType,
+    QueueServerParamType
+)
