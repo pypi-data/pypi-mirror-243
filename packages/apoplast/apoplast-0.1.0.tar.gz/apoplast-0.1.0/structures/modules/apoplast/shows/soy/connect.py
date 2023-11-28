5725b4433cd4e2027edb41021c1a6b0c92601dae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 14:04:06 +0000

    Add package apoplast/0.1.0/apoplast-0.1.0.tar.gz

---
+++ packages/apoplast/apoplast-0.1.0.tar.gz/apoplast-0.1.0/structures/modules/apoplast/shows/soy/connect.py
@@ -0,0 +1,13 @@
+
+'''
+	import apoplast.shows.soy.connect as connect
+	[ r, c ] = connect.it ()
+'''
+
+import lymphatic.system.connect as ly_connect
+
+def it ():
+	
+	[ r, c ] = ly_connect.start ()
+	
+	return [ r, c ]
