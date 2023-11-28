81b3050361ab64a463c13de1dd5f875d0877789c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 04:04:17 +0000

    Add package larvaworld/0.0.399/larvaworld-0.0.399.tar.gz

---
+++ packages/larvaworld/larvaworld-0.0.399.tar.gz/larvaworld-0.0.399/src/larvaworld/lib/util/__init__.py
@@ -0,0 +1,9 @@
+"""
+Methods for fitting and sampling between simulated and experimental datasets
+"""
+
+
+from .data_aux import *
+from .fitting import *
+
+__displayname__ = 'Dataset fitting & sampling'
