8127d9513cbcbe7b9169a542d77eb05cbc1faeed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 05:03:18 +0000

    Add package eggi/0.1.0/eggi-0.1.0-py3-none-any.whl

---
+++ packages/eggi/eggi-0.1.0-py3-none-any.whl/eggi/main.py
@@ -0,0 +1,11 @@
+def say_something(something: str) -> str:
+    """
+    Say something.
+
+    Args:
+        something: Something to say.
+
+    Returns: something said.
+
+    """
+    return f"Something: {something}"
