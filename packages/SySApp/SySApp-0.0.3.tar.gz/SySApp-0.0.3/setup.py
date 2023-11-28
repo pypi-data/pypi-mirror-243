0f8892d76981c26d164d15cfd0e19c6184d56319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 05:03:30 +0000

    Add package SySApp/0.0.3/SySApp-0.0.3.tar.gz

---
+++ packages/SySApp/SySApp-0.0.3.tar.gz/SySApp-0.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import *
+from Cython.Build import *
+
+to_pyc_module = ["sysapp/adv_module.py", "sysapp/dll.py"]
+
+setup(
+    name="SySApp",
+    version="0.0.3",
+    description="SyS App official module in Python. (Working in progress...)",
+    author="Runkang Chen",
+    author_email="admin@sysapp.org",
+    packages=["sysapp"],
+    zip_save=False,
+    ext_modules=cythonize(to_pyc_module),
+    install_requires=[
+        "Cython"
+    ]
+)
