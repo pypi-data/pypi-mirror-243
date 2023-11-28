005b6de0dd03ac43d01cb0de9ee18e75195cf2fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 03:03:42 +0000

    Add package PythonDictionary/0.0.3/PythonDictionary-0.0.3.tar.gz

---
+++ packages/PythonDictionary/PythonDictionary-0.0.3.tar.gz/PythonDictionary-0.0.3/PythonDictionary/pyDictionary.py
@@ -0,0 +1,32 @@
+import requests
+from .constants import API_URL
+
+class PyDictionary:
+    def __init__(self, word):
+        self.word = word
+    
+    def getDictionary(self):
+        return requests.get(API_URL + self.word).json()
+    
+    def getDefinitions(self, partOfSpeech):
+        
+        self.partOfSpeech = partOfSpeech
+        
+        meanings = self.getDictionary()[0]["meanings"]
+        definitions = []
+        synonyms = []
+        for x in meanings:
+            if x['partOfSpeech'] == self.partOfSpeech:
+                for y in x['definitions']:
+                    definitions.append(y['definition'])
+                    synonyms.append(y['synonyms'])
+                    
+        return definitions, synonyms
+    
+    def getAudio(self):
+        
+        audios = self.getDictionary()[0]["phonetics"]
+        audio_urls = []
+        for a in audios:
+            audio_urls.append(a['audio'])
+        return audio_urls
