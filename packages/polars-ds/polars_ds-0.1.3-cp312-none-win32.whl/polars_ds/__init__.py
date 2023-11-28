9ae5694b8843ea40a618dfe08879eb6252cf2076
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 15:04:50 +0000

    Add package polars-ds/0.1.3/polars_ds-0.1.3-cp312-none-win32.whl

---
+++ packages/polars-ds/polars_ds-0.1.3-cp312-none-win32.whl/polars_ds/__init__.py
@@ -0,0 +1,7 @@
+version = "0.1.3"
+
+from polars_ds.num_ext import NumExt  # noqa: E402
+from polars_ds.str_ext import StrExt  # noqa: E402
+from polars_ds.stats_ext import StatsExt  # noqa: E402
+
+__all__ = ["NumExt", "StrExt", "StatsExt"]
