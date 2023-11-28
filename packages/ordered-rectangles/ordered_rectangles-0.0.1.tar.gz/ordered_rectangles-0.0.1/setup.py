48e688c114961cdf18583028c5785bc89ac8e375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 03:04:15 +0000

    Add package ordered-rectangles/0.0.1/ordered_rectangles-0.0.1.tar.gz

---
+++ packages/ordered-rectangles/ordered_rectangles-0.0.1.tar.gz/ordered_rectangles-0.0.1/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+
+def parse_requirements(requirements: str):
+    with open(requirements) as f:
+        return [
+            l.strip('\n') for l in f if l.strip('\n') and not l.startswith('#')
+        ]
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ordered_rectangles", 
+    version="0.0.1",
+    author="Demetry Pascal",
+    author_email="qtckpuhdsa@gmail.com",
+    maintainer='Demetry Pascal',
+    description="Environment to python dictionary parser util",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PasaOpasen/rectangles-text-view-util",
+    license='MIT',
+    keywords=['rectangles'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    install_requires=parse_requirements('./requirements.txt')
+    
+)
