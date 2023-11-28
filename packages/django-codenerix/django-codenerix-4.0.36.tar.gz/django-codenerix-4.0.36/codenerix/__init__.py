62a0fbe6ee557e49f298c241671dee8a3a6acb40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 04:04:18 +0000

    Add package django-codenerix/4.0.36/django-codenerix-4.0.36.tar.gz

---
+++ packages/django-codenerix/django-codenerix-4.0.36.tar.gz/django-codenerix-4.0.36/codenerix/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "4.0.36"
+
+__authors__ = [
+    "Juanmi Taboada",
+    "Juan Soler Ruiz",
+]
+
+__authors_email__ = [
+    "juanmi@juanmitaboada.com",
+    "soleronline@gmail.com",
+]
