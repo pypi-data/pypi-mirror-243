fbeae0f92f1d2e3ed8773d97e8a15c375147e402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 03:04:30 +0000

    Add package caosadvancedtools/0.9.0/caosadvancedtools-0.9.0.tar.gz

---
+++ packages/caosadvancedtools/caosadvancedtools-0.9.0.tar.gz/caosadvancedtools-0.9.0/src/caosadvancedtools/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM caosadvancedtools SETUP.PY
+#
+short_version = '0.9.0'
+version = '0.9.0'
+full_version = '0.9.0'
+git_revision = '517b489f921fddb6c41aff7f11e02f5b9dbb1a0d'
+release = True
+
+if not release:
+    version = full_version
