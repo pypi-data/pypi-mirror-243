6e28b96f2b7f5bd5d96cb7a900f137a8bd1f60e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 04:04:22 +0000

    Add package forcha/0.1.5/forcha-0.1.5.tar.gz

---
+++ packages/forcha/forcha-0.1.5.tar.gz/forcha-0.1.5/forcha/components/nodes/federated_node.py
@@ -0,0 +1,129 @@
+from typing import Any
+from datasets import arrow_dataset
+from forcha.models.federated_model import FederatedModel
+from forcha.utils.loggers import Loggers
+
+node_logger = Loggers.node_logger()
+
+class FederatedNode:
+    def __init__(self, 
+                 node_id: int,
+                 settings: dict) -> None:
+        """An abstract object representing a single node in the federated training.
+        ------------
+        Arguments:
+            node_id (int): an int identifier of a node
+            settings (dict): a dictionary containing settings for the node
+        ------------
+        Returns:
+            None"""
+        self.state = 1 # Attribute controlling the state of the object.
+                         # 0 - initialized, resting
+                         # 1 - initialized, in run-time
+        
+        self.node_id = node_id
+        self.model = None
+        self.train_data = None
+        self.test_data = None
+        self.settings = settings
+        self.state = 0
+
+
+    def prepare_node(self, 
+                     model: Any, 
+                     data: arrow_dataset.Dataset):
+       """Prepares node for the training, given the passed model 
+       and dataset.
+       ------------
+       Arguments:
+            model (Any): compiled or pre-compiled model to be trained
+            dataset (list[datasets.arrow_dataset.Dataset, 
+                        datasets.arrow_dataset.Dataset]): a list[train_data, test_data]
+                        wrapped in a pre-compiled HuggingFace object.
+        ------------
+        Returns:
+            None"""
+       
+       self.state = 1
+       self.train_data = data[0]
+       self.test_data = data[1]
+       
+       self.model = FederatedModel(
+           settings=self.settings["model_settings"],
+           net = model,
+           local_dataset = data,
+           node_name=self.node_id
+       )
+
+       if self.model != None and self.train_data != None \
+        and self.test_data != None:
+           self.state = 0
+       else:
+           # TODO: LOGGING INFO
+           pass
+    
+
+    def train_local_model(
+        self,
+        mode: str
+    ) -> tuple[list[float], list[float], list[float]]:
+        """This function starts the server phase of the federated learning.
+        In particular, it trains the model locally and then sends the weights.
+        Then the updated weights are received and used to update
+        the local model.
+        -------------
+        Args:
+        node (FederatedNode object): Node that we want to train.
+        mode (str): Mode of the training. 
+            Mode = 'weights': Node will return model's weights.
+            Mode = 'gradients': Node will return model's gradients.
+        -------------
+        Returns:
+            Tuple[List[float], List[float], List[float]]: _description_
+        """
+        node_logger.info(f"Starting training on node {self.node_id}")
+        loss_list: list[float] = []
+        accuracy_list: list[float] = []
+
+        local_epochs = self.settings['local_epochs']
+
+        if mode == 'gradients':
+            self.model.preserve_initial_model()
+        
+        for _ in range(local_epochs):
+            metrics = self.local_training()
+            loss_list.append(metrics["loss"])
+            accuracy_list.append(metrics["accuracy"])
+        
+        node_logger.info(f"Results of training on node {self.node_id}: {accuracy_list}")
+        if mode == 'weights:':
+            return (
+                self.node_id,
+                self.model.get_weights()
+                )
+        elif mode == 'gradients':
+            return (
+                self.node_id,
+                self.model.get_gradients()
+            )
+        else:
+            node_logger.info("No mode was provided, returning only model's weights")
+            return (
+                self.node_id,
+                self.model.get_weights()
+                )
+
+    def local_training(
+        self,
+    ) -> dict[int, int]:
+        """Helper method for performing one epoch of local training.
+        Performs one round of Federated Training and pack the
+        results (metrics) into the appropiate data structure.
+        Args:
+            self
+        Returns
+        -------
+            dict[int, int]: metrics from the training.
+        """
+        loss, accuracy = self.model.train()
+        return {"loss": loss, "accuracy": accuracy}
---
+++ packages/forcha/forcha-0.1.5.tar.gz/forcha-0.1.5/forcha/utils/handlers.py
@@ -0,0 +1,109 @@
+from forcha.components.nodes.federated_node import FederatedNode
+import logging, csv
+from typing import Any
+import os
+
+class Handler:
+    """Common class for various utilities handling the data logs."""
+    @staticmethod
+    def log_model_metrics(iteration: int, 
+                          model: Any,
+                          logger,
+                          ) -> None:
+        """Used to log the model's metrics (on the orchestrator level).
+        
+        Args:
+            iteration (int): Current iteration of the training.
+            node_id (FederatedNode object): Federated Node object that we want to evaluate metrics on.
+        """
+        try:
+            (
+                loss,
+                accuracy,
+                fscore,
+                precision,
+                recall,
+                test_accuracy_per_class,
+                true_positive_rate,
+                false_positive_rate
+            ) = model.evaluate_model()
+            metrics = {"loss":loss, 
+                        "accuracy": accuracy, 
+                        "fscore": fscore, 
+                        "precision": precision,
+                        "recall": recall, 
+                        "test_accuracy_per_class": test_accuracy_per_class, 
+                        "true_positive_rate": true_positive_rate,
+                        "false_positive_rate": false_positive_rate,
+                        "epoch": iteration}
+            logger.info(f"Evaluating model after iteration {iteration} on node {model.node_name}. Results: {metrics}")
+        except Exception as e:
+            logger.warning(f"Unable to compute metrics. {e}")
+    
+
+    @staticmethod
+    def save_model_metrics(iteration: int, 
+                           model: Any,
+                           logger = None,
+                           saving_path: str = None,
+                           file_name: str = 'metrics.csv',
+                           log_to_screen: bool = False) -> None:
+        """Used to save the model metrics.
+        Args:
+            - iteration (int): Current iteration of the training.
+            - node (FederatedNode): FederatedNode Object which metrics we want to save.
+            - participants (list[int]): List of id's of participants
+                that are participating in this simulation. By default,
+                it will be equal to all available nodes -> self.nodes_id
+            - saving_path (str or path-like object): the saving path of the
+                csv file - if none, the file will be saved in the current 
+                working directory.
+            - file_name (str): a desired file name for the metrics, default to
+                'metrics.csv'.
+        Returns:
+            None"""
+        try:
+            (
+                loss,
+                accuracy,
+                fscore,
+                precision,
+                recall,
+                test_accuracy_per_class,
+                true_positive_rate,
+                false_positive_rate
+            ) = model.evaluate_model()
+            metrics = {"epoch": iteration,
+                       "node": model.node_name,
+                        "loss":loss, 
+                        "accuracy": accuracy, 
+                        "fscore": fscore, 
+                        "precision": precision,
+                        "recall": recall, 
+                        "test_accuracy_per_class": test_accuracy_per_class, 
+                        "true_positive_rate": true_positive_rate,
+                        "false_positive_rate": false_positive_rate,
+                        "epoch": iteration}
+            if log_to_screen == True:
+                logger.info(f"Evaluating model after iteration {iteration} on node {model.node_name}. Results: {metrics}")
+        except Exception as e:
+            logger.warning(f"Unable to compute metrics. {e}")
+        path = os.path.join(saving_path, file_name)
+        with open(path, 'a+', newline='') as saved_file:
+                writer = csv.DictWriter(saved_file, list(metrics.keys()))
+                if os.path.getsize(path) == 0:
+                    writer.writeheader()
+                writer.writerow(metrics)
+    
+    
+    @staticmethod
+    def save_csv_file(file,
+                      saving_path: str = None,
+                      file_name: str = 'metrics.csv') -> None:
+        """Used to preserve the content of a csv file."""
+        path = os.path.join(saving_path, file_name)
+        with open(path, 'a+', newline='') as csv_file:
+            writer = csv.DictWriter(csv_file, list(file[0].keys()))
+            writer.writeheader()
+            for row in file:
+                writer.writerow(row)
