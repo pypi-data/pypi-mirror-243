7144f3e6fc18ae0a823f0ff14a0dcf0f5b6f5ead
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 14:04:06 +0000

    Add package Accuinsight/3.5.20231124/Accuinsight-3.5.20231124.tar.gz

---
+++ packages/Accuinsight/Accuinsight-3.5.20231124.tar.gz/Accuinsight-3.5.20231124/Accuinsight/__about__.py
@@ -0,0 +1,11 @@
+"""Contains meta-information about the modeler package.
+"""
+
+__all__ = ("__version__", "__author__", "__url__")
+
+__version__ = "3.5.20231124"
+
+__author__ = "SK"
+
+__url__ = ""
+
