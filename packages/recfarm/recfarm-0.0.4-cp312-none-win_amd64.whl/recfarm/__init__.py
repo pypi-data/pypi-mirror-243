72e4ec3c72a8264d12a2a87efbc4796cfe7bcc71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 03:03:53 +0000

    Add package recfarm/0.0.4/recfarm-0.0.4-cp312-none-win_amd64.whl

---
+++ packages/recfarm/recfarm-0.0.4-cp312-none-win_amd64.whl/recfarm/__init__.py
@@ -0,0 +1,10 @@
+from recfarm import recfarm
+from recfarm.recfarm import (
+    UserCF,
+    __version__,
+    build_consumed_unique,
+    load_user_cf,
+    save_user_cf,
+)
+
+__all__ = ["recfarm", "UserCF"]
