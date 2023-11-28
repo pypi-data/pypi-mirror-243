9febef201458b493b129d4aab350734dee6b8bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 14:04:05 +0000

    Add package pgp-reconstruction/0.0.6.1/pgp_reconstruction-0.0.6.1.tar.gz

---
+++ packages/pgp-reconstruction/pgp_reconstruction-0.0.6.1.tar.gz/pgp_reconstruction-0.0.6.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+# Setting up
+setup(
+    name="pgp_reconstruction",
+    version='0.0.6.1',
+    author="rcolpo (Rodrigo Amarante Colpo)",
+    author_email="<rodrigocolpo@gmail.com>",
+    description='Pathway-Guided Pruning Reconstruction of constraint-based metabolic models',
+    long_description_content_type="text/markdown",
+    long_description = readme,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["reframed>=1.2.1", "pandas>=1.0", "cobra"],
+	license="Apache Software License 2.0",
+	keywords=['python', 'carveme', 'PGP Reconstruction', 'gapseq'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console', 
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+)
