fdd933bef4a4c159ef129b6af4b5b121d42c2ae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 04:03:49 +0000

    Add package pynpm/0.2.0/pynpm-0.2.0.tar.gz

---
+++ packages/pynpm/pynpm-0.2.0.tar.gz/pynpm-0.2.0/pynpm/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of PyNPM
+# Copyright (C) 2017 CERN.
+#
+# PyNPM is free software; you can redistribute it and/or modify
+# it under the terms of the Revised BSD License; see LICENSE file for
+# more details.
+
+"""PyNPM is a small API to help you invoke NPM from inside Python."""
+
+from __future__ import absolute_import, print_function
+
+from .package import NPMPackage, YarnPackage
+
+__version__ = "0.2.0"
+
+__all__ = ("__version__", "NPMPackage", "YarnPackage")
