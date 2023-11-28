a57bf681d647c6ab9041ec6aed5466eb3e346aa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 04:03:51 +0000

    Add package so-ml-tools/0.1.107/so-ml-tools-0.1.107.tar.gz

---
+++ packages/so-ml-tools/so-ml-tools-0.1.107.tar.gz/so-ml-tools-0.1.107/so_ml_tools/__init__.py
@@ -0,0 +1,33 @@
+from . import (
+    data,
+    nlp,
+    pd,
+    tf,
+    util,
+    evaluate,
+    shap,
+    sklearn
+)
+
+# We follow Semantic Versioning (https://semver.org/spec/v2.0.0.html)
+_MAJOR_VERSION = '0'
+_MINOR_VERSION = '1'
+_PATCH_VERSION = '107'
+
+__version__ = '.'.join([
+    _MAJOR_VERSION,
+    _MINOR_VERSION,
+    _PATCH_VERSION,
+])
+
+__all__ = [
+    "data",
+    "nlp",
+    "pd",
+    "shap",
+    "tf",
+    "util",
+    "evaluate",
+    "sklearn",
+    "__version__"
+]
