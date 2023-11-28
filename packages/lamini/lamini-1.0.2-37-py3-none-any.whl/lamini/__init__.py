7ccd532804e974c3df31cc3bcd601bbc0bf57172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 16:04:48 +0000

    Add package lamini/1.0.2/lamini-1.0.2-37-py3-none-any.whl

---
+++ packages/lamini/lamini-1.0.2-37-py3-none-any.whl/lamini/__init__.py
@@ -0,0 +1,10 @@
+from llama import *
+
+import os
+
+api_key = os.environ.get("LAMINI_API_KEY", None)
+
+
+MISSING_API_KEY_MESSAGE = """LAMINI_API_KEY not found.
+Please set it as an environment variable LAMINI_API_KEY, set it as lamini.api_key, or set it in ~/.lamini/configure.yaml
+Find your LAMINI_API_KEY at https://app.lamini.ai/account"""
