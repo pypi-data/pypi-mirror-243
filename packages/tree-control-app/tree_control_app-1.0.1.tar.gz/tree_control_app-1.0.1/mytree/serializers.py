e132a419a8211349f46c5b5b76a8abdd20681ffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 15:04:35 +0000

    Add package tree-control-app/1.0.1/tree_control_app-1.0.1.tar.gz

---
+++ packages/tree-control-app/tree_control_app-1.0.1.tar.gz/tree_control_app-1.0.1/mytree/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import TreeNode, Tree
+
+class TreeNodeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TreeNode
+        fields = ['id', 'data', 'parent', 'children']
+
+
+    children = serializers.SerializerMethodField()
+
+    def get_children(self, obj):
+        # This method will be used to serialize the children
+        children = obj.children.all()  # Assuming 'children' is the related name in the model
+        return TreeNodeSerializer(children, many=True).data
+
+class TreeSerializer(serializers.ModelSerializer):
+    root_node = TreeNodeSerializer(read_only=True)
+
+    class Meta:
+        model = Tree
+        fields = ['id', 'root_node']
