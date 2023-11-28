dbbdbf8b0b9efa4c9ef60fbfb3d20c27f7cc909d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 04:04:15 +0000

    Add package v8unpack/0.16.2/v8unpack-0.16.2.tar.gz

---
+++ packages/v8unpack/v8unpack-0.16.2.tar.gz/v8unpack-0.16.2/src/v8unpack/MetaDataObject/versions/Form803Elements/Group.py
@@ -0,0 +1,40 @@
+from .FormElement import FormElement, check_count_element, calc_offset
+from ....helper import FuckingBrackets, str_decode
+from ....ext_exception import ExtException
+
+
+class Group(FormElement):
+    pass
+
+    @classmethod
+    def get_name_node_offset(cls, raw_data):
+        return calc_offset([(3, 1), (1, 1), (2, 0)], raw_data)
+
+    @classmethod
+    def decode(cls, form, path, raw_data):
+        try:
+            size = check_count_element([
+                (3, 1), (1, 1), (17, 2)
+            ], raw_data)
+        except Exception as err:
+            raise ExtException(parent=err)
+        if raw_data[0] == '22' and size < 20:
+            raise FuckingBrackets(detail=cls.__name__)
+
+        data = super().decode(form, path, raw_data)
+        index = calc_offset([(3, 1), (1, 1), (17, 0)], raw_data)
+        data['child'] = cls.decode_list(form, raw_data, index, f"{path}/{data['name']}")
+        return data
+
+    @classmethod
+    def encode(cls, form, path, data):
+        try:
+            child = data['child']
+        except KeyError:
+            child = []
+        raw_data = super().encode(form, path, data)
+        if not child:
+            return raw_data
+        index = calc_offset([(3, 1), (1, 1), (17, 0)], raw_data)
+        cls.encode_list(form, child, raw_data, index, f"{path}/{data['name']}")
+        return raw_data
