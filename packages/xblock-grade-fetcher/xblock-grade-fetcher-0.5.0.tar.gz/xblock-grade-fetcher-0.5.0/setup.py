a093a41b6c21ff8881610985dc8dc8c479863823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 15:04:31 +0000

    Add package xblock-grade-fetcher/0.5.0/xblock-grade-fetcher-0.5.0.tar.gz

---
+++ packages/xblock-grade-fetcher/xblock-grade-fetcher-0.5.0.tar.gz/xblock-grade-fetcher-0.5.0/setup.py
@@ -0,0 +1,42 @@
+"""Setup for gradefetcher XBlock."""
+
+
+import os
+
+from setuptools import setup
+
+
+def package_data(pkg, roots):
+    """Generic function to find package_data.
+
+    All of the files under each of the `roots` will be declared as package
+    data for package `pkg`.
+
+    """
+    data = []
+    for root in roots:
+        for dirname, _, files in os.walk(os.path.join(pkg, root)):
+            for fname in files:
+                data.append(os.path.relpath(os.path.join(dirname, fname), pkg))
+
+    return {pkg: data}
+
+
+setup(
+    name='xblock-grade-fetcher',
+    version='0.5.0',
+    description='Grade Fetcher',
+    license='AGPL v3',
+    packages=[
+        'gradefetcher',
+    ],
+    install_requires=[
+        'XBlock',
+    ],
+    entry_points={
+        'xblock.v1': [
+            'gradefetcher = gradefetcher:GradeFetcherXBlock',
+        ]
+    },
+    package_data=package_data("gradefetcher", ["static", "public", "translations"]),
+)
