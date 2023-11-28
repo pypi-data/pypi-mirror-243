e91437ddf51d1e22c2e88d762dac44b376067237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 06:03:57 +0000

    Add package ezregex/1.5.0/ezregex-1.5.0-py3-none-any.whl

---
+++ packages/ezregex/ezregex-1.5.0-py3-none-any.whl/ezregex/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+__version__ = '1.5.0'
+from .invert import invertRegex as invert
+from .EZRegexMember import EZRegexMember
+from .invert import invertRegex as invert
+from .elements import *
+from .psuedonymns import *
+from re import RegexFlag, escape
+# import .docs as _docs
+# import .meta as _meta
