342091b50174cef548de43ea12f64be0048d6b4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 06:03:57 +0000

    Add package fastvs/0.1.5/fastvs-0.1.5-py3-none-any.whl

---
+++ packages/fastvs/fastvs-0.1.5-py3-none-any.whl/fastvs/__init__.py
@@ -0,0 +1,16 @@
+from fastvs.helpers import (
+    search_arrow,
+    search_pandas,
+    apply_distance_pandas,
+    apply_distance_arrow,
+)
+from . import knn, distance
+
+__all__ = [
+    "search_arrow",
+    "search_pandas",
+    "knn",
+    "distance",
+    "apply_distance_pandas",
+    "apply_distance_arrow",
+]
