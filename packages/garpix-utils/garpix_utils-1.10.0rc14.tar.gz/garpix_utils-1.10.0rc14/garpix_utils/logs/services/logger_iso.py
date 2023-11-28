f88bd3f9500d82cd5fd4a6a701680c0814bb36bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 05:03:29 +0000

    Add package garpix-utils/1.10.0rc14/garpix_utils-1.10.0rc14.tar.gz

---
+++ packages/garpix-utils/garpix_utils-1.10.0rc14.tar.gz/garpix_utils-1.10.0rc14/garpix_utils/logs/services/logger_iso.py
@@ -0,0 +1,54 @@
+from datetime import datetime, timezone
+
+from django.conf import settings
+import logging
+
+from garpix_utils.logs.enums.get_enums import ActionType, ActionLevel, ActionResult
+
+
+class ActionElement:
+
+    def __init__(self, action_id: int, action_type: ActionType, name: str, level: ActionLevel):
+        self.id = action_id
+        self.name = name
+        self.type = action_type
+        self.level = level
+
+
+class LoggerIso:
+
+    def __init__(self, logger_name=settings.ISO_LOGS_NAME):
+        self.logger = logging.getLogger(logger_name)
+
+    def create_log(self, action: ActionElement, obj, obj_address, result: ActionResult,
+                   params=None, sbj=None, sbj_address=None, msg=''):
+
+        dt = datetime.now(timezone.utc).astimezone()
+
+        log = f'time = {dt.isoformat()} | id={action.id} | act=\"{action.name}\"'
+        log += f' | sbj=\"{sbj}\"' if sbj else ''
+        log += f' | sbj_addr=\"{sbj_address}\"' \
+               f' | act_type=\"{action.type.value}\"' \
+               f' | lvl={action.level.value} | obj={obj}' \
+               f' | obj_addr=\"{obj_address}\"' \
+               f' | result={result.value}'
+        log += f' | change=\"{params}\"' if params else ''
+        log += f' | msg=\"{msg}\"'
+        return log
+
+    def write(self, act: ActionElement, obj, obj_address, result: ActionResult, params=None, sbj=None, sbj_address=None, msg=""):
+        log = self.create_log(act, obj, obj_address, result, params, sbj, sbj_address, msg)
+        self.logger.info(log)
+
+    def write_string(self, string):
+        self.logger.info(string)
+
+    @staticmethod
+    def get_client_ip(request):
+        # Используется для поулчения адреса субъекта
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
