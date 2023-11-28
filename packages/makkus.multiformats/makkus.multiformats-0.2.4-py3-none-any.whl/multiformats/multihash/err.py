b107a63d84db43eb8076fd00de2f47b25830d10d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 16:04:47 +0000

    Add package makkus.multiformats/0.2.4/makkus.multiformats-0.2.4-py3-none-any.whl

---
+++ packages/makkus.multiformats/makkus.multiformats-0.2.4-py3-none-any.whl/multiformats/multihash/err.py
@@ -0,0 +1,11 @@
+"""
+    Errors for the :mod:`~multiformats.multihash` module.
+"""
+
+import builtins
+
+class MultihashKeyError(builtins.KeyError): # pylint: disable = redefined-builtin
+    """ Class for :mod:`~multiformats.multihash` key errors. """
+
+class MultihashValueError(builtins.ValueError): # pylint: disable = redefined-builtin
+    """ Class :mod:`~multiformats.multihash` value errors. """
