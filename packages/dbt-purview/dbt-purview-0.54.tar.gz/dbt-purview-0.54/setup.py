9234595dcf9c749358b037264b46e269731185ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 15:04:59 +0000

    Add package dbt-purview/0.54/dbt-purview-0.54.tar.gz

---
+++ packages/dbt-purview/dbt-purview-0.54.tar.gz/dbt-purview-0.54/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dbt-purview',
+    version='0.54',
+    packages=find_packages(),
+    install_requires=[
+        'click',
+        'requests',
+        'apache-airflow',
+    ],
+    entry_points={
+        'console_scripts': [
+            'dbtpurview = dbtpurview.main:dbtpurview',
+        ],
+    },
+)
