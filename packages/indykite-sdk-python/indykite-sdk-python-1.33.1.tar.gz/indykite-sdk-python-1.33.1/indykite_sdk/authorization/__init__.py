f7f8bdcdc6ec6a02c18c0a289f7ab24bceb5067f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-28 16:04:39 +0000

    Add package indykite-sdk-python/1.33.1/indykite-sdk-python-1.33.1.tar.gz

---
+++ packages/indykite-sdk-python/indykite-sdk-python-1.33.1.tar.gz/indykite-sdk-python-1.33.1/indykite_sdk/authorization/__init__.py
@@ -0,0 +1,20 @@
+import sys
+from indykite_sdk.utils import jwt_credentials
+from indykite_sdk.utils.logger import handle_excepthook, logger_error
+
+
+class AuthorizationClient(object):
+
+    def __init__(self, token_source=None):
+        sys.excepthook = handle_excepthook
+        try:
+            self.channel, self.stub, self.credentials, self.token_source = jwt_credentials.get_credentials(
+                client="authz",
+                token_source=token_source
+            )
+        except Exception as exception:
+            return logger_error(exception)
+    # Imported methods
+    from .is_authorized import is_authorized_token, is_authorized_digital_twin, is_authorized_property_filter
+    from .what_authorized import what_authorized_token, what_authorized_digital_twin, what_authorized_property_filter
+    from .who_authorized import who_authorized
---
+++ packages/indykite-sdk-python/indykite-sdk-python-1.33.1.tar.gz/indykite-sdk-python-1.33.1/indykite_sdk/authorization/is_authorized.py
@@ -0,0 +1,92 @@
+from indykite_sdk.indykite.authorization.v1beta1 import authorization_service_pb2 as pb2
+from indykite_sdk.indykite.objects.v1beta1 import struct_pb2 as pb2_struct
+from indykite_sdk.indykite.authorization.v1beta1 import model_pb2 as pb2_model
+from indykite_sdk.model.is_authorized import IsAuthorizedResponse
+import sys
+import indykite_sdk.utils.logger as logger
+
+
+def is_authorized_digital_twin(self, digital_twin_id, resources=[], input_params={}, policy_tags=[]):
+    sys.excepthook = logger.handle_excepthook
+    try:
+        response = self.stub.IsAuthorized(
+            pb2.IsAuthorizedRequest(
+                subject=pb2_model.Subject(
+                    digital_twin_id=pb2_model.DigitalTwin(
+                        id=str(digital_twin_id)
+                    )
+                ),
+                resources=request_resource(resources),
+                input_params=request_input_params(input_params),
+                policy_tags=policy_tags
+            )
+        )
+    except Exception as exception:
+        return logger.logger_error(exception)
+
+    if not response:
+        return None
+
+    return IsAuthorizedResponse.deserialize(response)
+
+
+def is_authorized_token(self, access_token, resources=[], input_params={}, policy_tags=[]):
+    sys.excepthook = logger.handle_excepthook
+    try:
+        response = self.stub.IsAuthorized(
+            pb2.IsAuthorizedRequest(
+                subject=pb2_model.Subject(
+                    indykite_access_token=str(access_token)
+                ),
+                resources=request_resource(resources),
+                input_params=request_input_params(input_params),
+                policy_tags=policy_tags
+            )
+        )
+    except Exception as exception:
+        return logger.logger_error(exception)
+
+    if not response:
+        return None
+
+    return IsAuthorizedResponse.deserialize(response)
+
+
+def is_authorized_property_filter(self, type_filter, value, resources=[], input_params={}, policy_tags=[]):
+    sys.excepthook = logger.handle_excepthook
+    try:
+        response = self.stub.IsAuthorized(
+            pb2.IsAuthorizedRequest(
+                subject=pb2_model.Subject(
+                    digital_twin_property=pb2_model.Property(
+                        type=str(type_filter),
+                        value=pb2_struct.Value(string_value=value)
+                    )
+                ),
+                resources=request_resource(resources),
+                input_params=request_input_params(input_params),
+                policy_tags=policy_tags
+            )
+        )
+    except Exception as exception:
+        return logger.logger_error(exception)
+
+    if not response:
+        return None
+
+    return IsAuthorizedResponse.deserialize(response)
+
+
+def request_resource(resources):
+    return [
+        pb2.IsAuthorizedRequest.Resource(external_id=r.external_id, type=r.type, actions=list(r.actions))
+        for r in resources
+    ]
+
+
+def request_input_params(input_params):
+    input_params_dict = {
+        k: pb2_model.InputParam(string_value=str(v))
+        for k, v in input_params.items()
+    }
+    return input_params_dict
