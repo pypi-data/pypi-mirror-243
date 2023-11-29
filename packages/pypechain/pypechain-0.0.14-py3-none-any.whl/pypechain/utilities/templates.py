d22a46e1ae3c9fbdcc824bc1c0dd7a42d5f8b70c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 18:08:08 +0000

    Add package pypechain/0.0.14/pypechain-0.0.14-py3-none-any.whl

---
+++ packages/pypechain/pypechain-0.0.14-py3-none-any.whl/pypechain/utilities/templates.py
@@ -0,0 +1,13 @@
+"""Utilities for templating."""
+import os
+
+from jinja2 import Environment, FileSystemLoader
+
+
+def get_jinja_env() -> Environment:
+    """Returns the jinja environment."""
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    # Construct the path to your templates directory.
+    templates_dir = os.path.join(script_dir, "../templates")
+    env = Environment(loader=FileSystemLoader(templates_dir))
+    return env
