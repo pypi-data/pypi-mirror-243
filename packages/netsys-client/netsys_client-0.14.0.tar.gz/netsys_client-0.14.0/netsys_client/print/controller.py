9aa243c563277f9d078de41e6096013a251a6132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 14:04:35 +0000

    Add package netsys-client/0.14.0/netsys_client-0.14.0.tar.gz

---
+++ packages/netsys-client/netsys_client-0.14.0.tar.gz/netsys_client-0.14.0/netsys_client/print/controller.py
@@ -0,0 +1,7 @@
+from . import PrintMethods, PrintLayout
+
+class PrintController(PrintMethods, PrintLayout):
+
+    def __init__(self):
+
+        pass
---
+++ packages/netsys-client/netsys_client-0.14.0.tar.gz/netsys_client-0.14.0/netsys_client/response.py
@@ -0,0 +1,32 @@
+import asyncio
+import websockets
+
+class Response:
+
+    async def connector(self, message):
+
+        uri = "ws://localhost:12345"
+
+        async with websockets.connect(uri) as websocket:
+
+            print('aqui')
+
+            await websocket.send(f'{message}')
+
+            while True:
+
+                print('aqui')
+
+                self._response_message = await websocket.recv()
+
+                print(self._response_message)
+
+    def send_message(self, message):
+
+        asyncio.run(self.connector(message))
+
+    def get_response_message(self):
+
+        return self._response_message
+
+    
