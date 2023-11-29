3ae77975910f6a7ccd2797426e8a846c33b62d45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 04:04:49 +0000

    Add package mind-the-gaps/0.2.2/mind_the_gaps-0.2.2.tar.gz

---
+++ packages/mind-the-gaps/mind_the_gaps-0.2.2.tar.gz/mind_the_gaps-0.2.2/src/mind_the_gaps/__init__.py
@@ -0,0 +1,13 @@
+"""A library for unions, intersections, subtractions, and xors of intervals (gaps)."""
+from .gaps import Endpoint, Gaps, NegativeInfinity, PositiveInfinity
+from .var import x
+
+__all__ = [
+    "Endpoint",
+    "Gaps",
+    "NegativeInfinity",
+    "PositiveInfinity",
+    "x",
+]
+
+__version__ = "0.2.2"
