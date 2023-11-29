9565675d3a8064d5edc03de5353074595a451e2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 05:04:03 +0000

    Add package netsys-client/0.9.0/netsys_client-0.9.0.tar.gz

---
+++ packages/netsys-client/netsys_client-0.9.0.tar.gz/netsys_client-0.9.0/netsys_client/gateway.py
@@ -0,0 +1,32 @@
+from . import ConfController, PrintController, Response
+import json
+
+class Gateway:
+
+    _erros = []
+
+    def __init__(self):
+
+        self.response = Response()
+
+    def change_config(self, data):
+        
+        conf = ConfController(data)
+        
+        if conf.get_erros():
+
+            print(conf.get_erros())
+
+    def search_printers(self):
+
+        print = PrintController()
+        message = print.get_printer_list(self._token)
+        self.response.send_message(json.dumps(message))
+    
+    def set_erro(self, erro):
+
+        self._erros.append(erro)
+
+    def set_token(self, token):
+
+        self._token = token
