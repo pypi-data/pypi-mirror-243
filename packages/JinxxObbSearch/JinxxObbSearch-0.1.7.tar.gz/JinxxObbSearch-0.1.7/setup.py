46cd219b57265f8390a118eeac5c10b027a76b04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 16:05:03 +0000

    Add package JinxxObbSearch/0.1.7/JinxxObbSearch-0.1.7.tar.gz

---
+++ packages/JinxxObbSearch/JinxxObbSearch-0.1.7.tar.gz/JinxxObbSearch-0.1.7/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='JinxxObbSearch',
+    version='0.1.7',
+    packages=find_packages(),
+    install_requires=[
+        'tqdm==4.62.2',
+    ],
+    entry_points={
+        'console_scripts': [
+            'JinxxObbSearch = JinxxObbSearch.search:main',
+        ],
+    },
+)
