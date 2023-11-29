9373d2e2796b9efede15c2d52edd10f930a1bf72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 16:05:16 +0000

    Add package klimalogger/0.6.9/klimalogger-0.6.9.tar.gz

---
+++ packages/klimalogger/klimalogger-0.6.9.tar.gz/klimalogger-0.6.9/klimalogger/store/__init__.py
@@ -0,0 +1,24 @@
+import logging
+
+from injector import Module, provider, singleton
+
+from .client import StoreClient
+from ..config import Config
+
+log = logging.getLogger(__name__)
+
+class StoreModule(Module):
+    @provider
+    @singleton
+    def store_provider(self, config: Config) -> StoreClient:
+        log.info("store provider type: %s", config.store_type)
+
+        if config.store_type == 'queue':
+            from .queue import CombinedStore
+            return CombinedStore(config)
+        elif config.store_org is not None:
+            from .influxdb2 import InfluxDbStore
+            return InfluxDbStore(config)
+        else:
+            from .influxdb import InfluxDbStore
+            return InfluxDbStore(config)
