2439bd906677549378095780d99b76b1c3bfe7b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 05:04:07 +0000

    Add package ds-caselaw-marklogic-api-client/17.3.0/ds_caselaw_marklogic_api_client-17.3.0-py3-none-any.whl

---
+++ packages/ds-caselaw-marklogic-api-client/ds_caselaw_marklogic_api_client-17.3.0-py3-none-any.whl/caselawclient/client_helpers/search_helpers.py
@@ -0,0 +1,35 @@
+from caselawclient.Client import MarklogicApiClient
+from caselawclient.responses.search_response import SearchResponse
+from caselawclient.search_parameters import SearchParameters
+
+
+def search_judgments_and_parse_response(
+    api_client: MarklogicApiClient, search_parameters: SearchParameters
+) -> SearchResponse:
+    """
+    Search for judgments using the given search parameters and parse the response into a SearchResponse object.
+
+    :param api_client: An instance of MarklogicApiClient used to make the search request
+    :param search_parameters: An instance of SearchParameters containing the search parameters
+
+    :return: The parsed search response as a SearchResponse object
+    """
+    return SearchResponse.from_response_string(
+        api_client.search_judgments_and_decode_response(search_parameters)
+    )
+
+
+def search_and_parse_response(
+    api_client: MarklogicApiClient, search_parameters: SearchParameters
+) -> SearchResponse:
+    """
+    Search using the given search parameters and parse the response into a SearchResponse object.
+
+    :param api_client: An instance of MarklogicApiClient used to make the search request
+    :search_parameters: An instance of SearchParameters containing the search parameters
+
+    :return: The parsed search response as a SearchResponse object
+    """
+    return SearchResponse.from_response_string(
+        api_client.search_and_decode_response(search_parameters)
+    )
