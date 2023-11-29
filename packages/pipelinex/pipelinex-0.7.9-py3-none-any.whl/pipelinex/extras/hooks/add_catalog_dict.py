6724007caa1959bdfdd43b54d459c53d6837de62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 04:03:49 +0000

    Add package pipelinex/0.7.9/pipelinex-0.7.9-py3-none-any.whl

---
+++ packages/pipelinex/pipelinex-0.7.9-py3-none-any.whl/pipelinex/extras/hooks/add_catalog_dict.py
@@ -0,0 +1,30 @@
+from typing import Dict
+
+from kedro.io import DataCatalog
+from kedro_datasets._io import AbstractDataset as AbstractDataSet
+
+try:
+    from kedro.framework.hooks import hook_impl
+except ModuleNotFoundError:
+
+    def hook_impl(func):
+        return func
+
+
+class AddCatalogDictHook:
+    """Hook to add data sets."""
+
+    def __init__(
+        self,
+        catalog_dict: Dict[str, AbstractDataSet],
+    ):
+        """
+        Args:
+            catalog_dict: catalog_dict to add.
+        """
+        assert isinstance(catalog_dict, dict), "{} is not a dict.".format(catalog_dict)
+        self._catalog_dict = catalog_dict
+
+    @hook_impl
+    def after_catalog_created(self, catalog: DataCatalog) -> None:
+        catalog.add_feed_dict(self._catalog_dict)
