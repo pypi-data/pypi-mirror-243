46779574f3625709a507a63ebfba14652152ebf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 18:07:20 +0000

    Add package jaguardb-http-client/3.4.0/jaguardb-http-client-3.4.0.tar.gz

---
+++ packages/jaguardb-http-client/jaguardb-http-client-3.4.0.tar.gz/jaguardb-http-client-3.4.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name='jaguardb-http-client',
+    version='3.4.0',
+    author = 'JaguarDB',
+    description = 'Http client for Jaguar vector database',
+    url = 'http://www.jaguardb.com',
+    license = 'Apache 2.0',
+    python_requires = '>=3.0',
+    packages=find_packages(),
+    include_package_data=True,
+    data_files=[
+        ('jaguardb', ['jaguardb-http-client/LICENSE', 'jaguardb-http-client/README-http.md', 'jaguardb-http-client/JaguarHttpClient.py'])
+    ],
+)
