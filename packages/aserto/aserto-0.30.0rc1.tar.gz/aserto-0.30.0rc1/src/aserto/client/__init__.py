0b59aa9605620ff735881c38e63134a2192b9957
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 04:03:21 +0000

    Add package aserto/0.30.0rc1/aserto-0.30.0rc1.tar.gz

---
+++ packages/aserto/aserto-0.30.0rc1.tar.gz/aserto-0.30.0rc1/src/aserto/client/__init__.py
@@ -0,0 +1,9 @@
+from aserto.client.options import AuthorizerOptions
+from aserto.client.identity import Identity
+from aserto.client.resource_context import ResourceContext
+
+__all__ = [
+    "AuthorizerOptions",
+    "Identity",
+    "ResourceContext",
+]
