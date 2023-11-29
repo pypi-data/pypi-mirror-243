81a9a7df0b183dbaf5c92af2e8162dbdaad6a18d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 15:04:35 +0000

    Add package execdata/5.0.1/execdata-5.0.1-py3-none-any.whl

---
+++ packages/execdata/execdata-5.0.1-py3-none-any.whl/execdata/format/__init__.py
@@ -0,0 +1,14 @@
+'''
+Date         : 2023-11-02 17:34:28
+Author       : BDFD,bdfd2005@gmail.com
+Github       : https://github.com/bdfd
+LastEditTime : 2023-11-28 14:03:08
+LastEditors  : BDFD
+Description  : 
+FilePath     : \execdata\format\__init__.py
+Copyright (c) 2023 by BDFD, All Rights Reserved. 
+'''
+from ._data_conversion import convint
+from ._data_conversion import convfloat
+
+from ._format_data import fill_zeros
