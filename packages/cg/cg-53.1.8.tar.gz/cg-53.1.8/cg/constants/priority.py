b973909ae976cca774b88e756191e409fe9a1615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 18:07:16 +0000

    Add package cg/53.1.8/cg-53.1.8.tar.gz

---
+++ packages/cg/cg-53.1.8.tar.gz/cg-53.1.8/cg/constants/priority.py
@@ -0,0 +1,41 @@
+"""Priority specific constants"""
+from enum import IntEnum, StrEnum
+
+
+class SlurmQos(StrEnum):
+    MAINTENANCE: str = "maintenance"
+    LOW: str = "low"
+    NORMAL: str = "normal"
+    HIGH: str = "high"
+    EXPRESS: str = "express"
+
+
+class PriorityTerms(StrEnum):
+    EXPRESS: str = "express"
+    PRIORITY: str = "priority"
+    RESEARCH: str = "research"
+    STANDARD: str = "standard"
+    CLINICAL_TRIALS: str = "clinical_trials"
+
+
+class Priority(IntEnum):
+    research: int = 0
+    standard: int = 1
+    priority: int = 2
+    express: int = 3
+    clinical_trials: int = 4
+
+
+PRIORITY_TO_SLURM_QOS = {
+    Priority.research: SlurmQos.LOW,
+    Priority.standard: SlurmQos.NORMAL,
+    Priority.priority: SlurmQos.HIGH,
+    Priority.express: SlurmQos.EXPRESS,
+    Priority.clinical_trials: SlurmQos.NORMAL,
+}
+
+
+class SlurmAccount(StrEnum):
+    PRODUCTION: str = "production"
+    DEVELOPMENT: str = "development"
+    STAGE: str = "stage"
