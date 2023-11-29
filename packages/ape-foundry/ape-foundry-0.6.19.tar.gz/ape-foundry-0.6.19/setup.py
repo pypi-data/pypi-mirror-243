886915110b9d700547f2e3be84cb684d82c9058c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 16:04:36 +0000

    Add package ape-foundry/0.6.19/ape-foundry-0.6.19.tar.gz

---
+++ packages/ape-foundry/ape-foundry-0.6.19.tar.gz/ape-foundry-0.6.19/setup.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+extras_require = {
+    "test": [  # `test` GitHub Action jobs uses this
+        "pytest>=6.0",  # Core testing package
+        "pytest-xdist",  # multi-process runner
+        "pytest-cov",  # Coverage analyzer plugin
+        "hypothesis>=6.2.0,<7.0",  # Strategy-based fuzzer
+        "ape-alchemy",  # For running fork tests
+        "ape-polygon",  # For running polygon fork tests
+    ],
+    "lint": [
+        "black>=23.10.1,<24",  # auto-formatter and linter
+        "mypy>=1.6.1,<2",  # Static type analyzer
+        "types-requests",  # Needed due to mypy typeshed
+        "types-setuptools",  # Needed due to mypy typeshed
+        "types-PyYAML",  # Needed due to mypy typeshed
+        "flake8>=6.0.1,<7",  # Style linter
+        "isort>=5.10.1,<6",  # Import sorting linter
+        "mdformat>=0.7.17",  # Auto-formatter for markdown
+        "mdformat-gfm>=0.3.5",  # Needed for formatting GitHub-flavored markdown
+        "mdformat-frontmatter>=0.4.1",  # Needed for frontmatters-style headers in issue templates
+    ],
+    "doc": [
+        "Sphinx>=6.1.3,<7",  # Documentation generator
+        "sphinx_rtd_theme>=1.2.0,<2",  # Readthedocs.org theme
+        "towncrier>=19.2.0, <20",  # Generate release notes
+    ],
+    "release": [  # `release` GitHub Action job uses this
+        "setuptools",  # Installation tool
+        "setuptools-scm",  # Installation tool
+        "wheel",  # Packaging tool
+        "twine",  # Package upload tool
+    ],
+    "dev": [
+        "commitizen",  # Manage commits and publishing releases
+        "pre-commit",  # Ensure that linters are run prior to committing
+        "IPython",  # Console for interacting
+        "ipdb",  # Debugger (Must use `export PYTHONBREAKPOINT=ipdb.set_trace`)
+    ],
+}
+
+# NOTE: `pip install -e .[dev]` to install package
+extras_require["dev"] = (
+    extras_require["test"]
+    + extras_require["lint"]
+    + extras_require["doc"]
+    + extras_require["release"]
+    + extras_require["dev"]
+)
+
+
+with open("./README.md") as readme:
+    long_description = readme.read()
+
+
+setup(
+    name="ape-foundry",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    description="""ape-foundry: Ape network provider for Foundry""",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="ApeWorX Ltd.",
+    author_email="admin@apeworx.io",
+    url="https://github.com/ApeWorX/ape-foundry",
+    include_package_data=True,
+    install_requires=[
+        "eth-ape>=0.6.24,<0.7",
+        "evm-trace",  # Use same version as ape
+        "hexbytes",  # Use same version as ape
+        "web3",  # Use same version as ape
+        "yarl",  # Use same version as ape
+    ],
+    python_requires=">=3.8,<4",
+    extras_require=extras_require,
+    py_modules=["ape_foundry"],
+    license="Apache-2.0",
+    zip_safe=False,
+    keywords="ethereum",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    package_data={"ape_foundry": ["py.typed"]},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
