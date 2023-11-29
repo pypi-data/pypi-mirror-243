6509915f06ed29e392ad09748a5eca8114eafd2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 17:05:11 +0000

    Add package openai-utilties/3.0/openai_utilties-3.0-py3-none-any.whl

---
+++ packages/openai-utilties/openai_utilties-3.0-py3-none-any.whl/openai_utils/__init__.py
@@ -0,0 +1,35 @@
+import openai
+
+def AskGPT(model: str, Context, temperature: float = 1) -> str:
+    if type(Context) == list:
+
+        response = openai.chat.completions.create(
+            model=model,
+            messages=Context,
+            temperature=temperature
+        )
+        return response.choices[0].message.content
+
+    elif type(Context) == str:
+
+        response = openai.chat.completions.create(
+            model=model,
+            messages=[{"role": "user", "content": Context}],
+            temperature=temperature
+        )
+        return response.choices[0].message.content
+
+
+def CreateEmbedding(String: str) -> list:
+    reply = openai.Embedding.create(
+        model="text-embedding-ada-002",
+        input=String
+    )
+    return reply.data[0].embedding
+
+def ParseResponse(Response):
+    MainParse = {}
+    MainParse['text'] = Response.choices[0].message.content
+    MainParse['model'] = Response.model
+    MainParse['function_call'] = Response.choices[0].message.function_call
+    return MainParse
