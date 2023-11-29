1de3d80fe6eecbcbbb1bea3df2b7f09c38cecb1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 14:04:38 +0000

    Add package jinja2-cli-tddschn/0.9.2/jinja2_cli_tddschn-0.9.2.tar.gz

---
+++ packages/jinja2-cli-tddschn/jinja2_cli_tddschn-0.9.2.tar.gz/jinja2_cli_tddschn-0.9.2/jinja2cli/__init__.py
@@ -0,0 +1,11 @@
+"""
+jinja2-cli
+==========
+
+License: BSD, see LICENSE for more details.
+"""
+
+__author__ = "Matt Robenolt, Teddy Xinyuan Chen"
+__version__ = "0.9.2"
+
+from .cli import main  # NOQA
