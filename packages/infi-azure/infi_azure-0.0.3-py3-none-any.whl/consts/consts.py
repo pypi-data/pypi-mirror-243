d742bd99bf18f087df00f9e7c6e8c14f8740c026
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 05:04:07 +0000

    Add package infi-azure/0.0.3/infi_azure-0.0.3-py3-none-any.whl

---
+++ packages/infi-azure/infi_azure-0.0.3-py3-none-any.whl/consts/consts.py
@@ -0,0 +1,8 @@
+class Const:
+    ACCOUNT_NAME_INDEX: int = 1
+    ACCOUNT_KEY_INDEX: int = 1
+
+    ACCOUNT_NAME_PREFIX: str = "AccountName="
+    ACCOUNT_KEY_PREFIX: str = "AccountKey="
+    PROTOCOL_PREFIX: str = "DefaultEndpointsProtocol"
+    
