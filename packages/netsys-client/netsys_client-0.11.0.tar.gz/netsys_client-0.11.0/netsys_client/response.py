9b5ea97575c9f0434565eabe735cb85398c766e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 05:03:39 +0000

    Add package netsys-client/0.11.0/netsys_client-0.11.0.tar.gz

---
+++ packages/netsys-client/netsys_client-0.11.0.tar.gz/netsys_client-0.11.0/netsys_client/response.py
@@ -0,0 +1,26 @@
+import asyncio
+import websockets
+
+class Response:
+
+    async def connector(self, message):
+
+        uri = "ws://localhost:12345"
+
+        async with websockets.connect(uri) as websocket:
+
+            await websocket.send(f'{message}')
+
+            self._response_message = await websocket.recv()
+
+            print(self._response_message)
+
+    def send_message(self, message):
+
+        asyncio.get_event_loop().run_until_complete(self.connector(message))
+
+    def get_response_message(self):
+
+        return self._response_message
+
+    
