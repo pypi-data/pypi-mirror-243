21cff07a8dd33bd03bbd746e051f567c31c0d56b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 19:05:17 +0000

    Add package svn2git/0.5.1a1/svn2git-0.5.1a1.tar.gz

---
+++ packages/svn2git/svn2git-0.5.1a1.tar.gz/svn2git-0.5.1a1/svn2git/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.5.1-alpha.1"
+__all__ = ["__version__", "SVN2GitOptions", "GitMigrationHelper", "GitBranches"]
+
+from svn2git.git import GitBranches, GitMigrationHelper
+from svn2git.options import SVN2GitOptions
