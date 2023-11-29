d81bd61818b3d0874f2d928c7318962b3ceaca96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 17:05:21 +0000

    Add package deuces/0.2.1/deuces-0.2.1.tar.gz

---
+++ packages/deuces/deuces-0.2.1.tar.gz/deuces-0.2.1/setup.py
@@ -0,0 +1,30 @@
+"""
+Deuces: A pure Python poker hand evaluation library
+"""
+
+from setuptools import setup
+
+setup(
+    name='deuces',
+    version='0.2.1',
+    description=__doc__,
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Will Drevo',
+    url='https://github.com/worldveil/deuces',
+    license='MIT',
+    packages=['deuces'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Games/Entertainment'
+    ],
+    #
+    # https://docs.pytest.org/en/latest/goodpractices.html#integrating-with-setuptools-python-setup-py-test-pytest-runner
+    #
+    setup_requires=['pytest-runner'],
+    tests_require=['mock', 'pytest', 'pytest-cov']
+)
