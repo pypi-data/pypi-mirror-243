6e08697ce24d982a9c1cfaf66c7c280754397811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 04:03:48 +0000

    Add package pebbleauthclient/0.1.3/pebbleauthclient-0.1.3.tar.gz

---
+++ packages/pebbleauthclient/pebbleauthclient-0.1.3.tar.gz/pebbleauthclient-0.1.3/pebbleauthclient/models/AuthenticatedLicence.py
@@ -0,0 +1,19 @@
+from pebbleauthclient.models.User import User
+from pebbleauthclient.datatypes import AuthenticatedLicenceObject
+
+
+class AuthenticatedLicence(AuthenticatedLicenceObject):
+
+    def __init__(self, token_object: AuthenticatedLicenceObject):
+        self.app: str = token_object.app
+        self.id: str = token_object.id
+        self.tenant_id: str = token_object.tenant_id
+        self.user: User = token_object.user
+
+    def get_user(self) -> User:
+        """
+        Return the user who own the licence
+
+        :return: User
+        """
+        return self.user
