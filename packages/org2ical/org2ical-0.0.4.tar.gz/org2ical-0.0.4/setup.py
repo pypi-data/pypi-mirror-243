5c256ad6716bb643544bd6d5c83061c64d41520b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 05:03:54 +0000

    Add package org2ical/0.0.4/org2ical-0.0.4.tar.gz

---
+++ packages/org2ical/org2ical-0.0.4.tar.gz/org2ical-0.0.4/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+github_url = "https://github.com/j3soon/org2ical"
+
+setuptools.setup(
+    name="org2ical",
+    version="0.0.4",
+    author="Johnson",
+    author_email="j3.soon777@gmail.com",
+    description="Generate a iCalendar (.ics) file based on a OrgMode (.org) file.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=github_url,
+    project_urls={
+        "Issues": f"{github_url}/issues",
+        "Source Code": github_url,
+    },
+    keywords=(
+        "package, parser, converter, python, orgmode, icalendar"
+    ),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where=".", exclude=("tests*",)),
+    install_requires=[
+        "orgparse>=0.3.2"
+    ],
+    extras_require={
+        "testing": ["pytest", "mypy", "flake8", "pylint", "icalendar", "python-dateutil"],
+    },
+)
+
+# Note: PyPI seems to only recognize double-quoted strings
