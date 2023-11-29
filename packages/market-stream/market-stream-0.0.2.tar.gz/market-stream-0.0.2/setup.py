ea98ce51f82014337ce505fcb34787333769f83a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 14:04:29 +0000

    Add package market-stream/0.0.2/market-stream-0.0.2.tar.gz

---
+++ packages/market-stream/market-stream-0.0.2.tar.gz/market-stream-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='market-stream',
+    version='0.0.2',
+    description='A centralized market stream manager',
+    author='Yi Te',
+    author_email='coastq22889@icloud.com',
+    packages=find_packages(include=['client', 'generated']),
+    install_requires=[
+        'websockets',
+        'redis',
+        'aioredis'
+    ],
+)
