23f7cbb54c7377263388b9bbd071816118d47329
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 18:07:42 +0000

    Add package deepepochs/0.5.4/deepepochs-0.5.4.tar.gz

---
+++ packages/deepepochs/deepepochs-0.5.4.tar.gz/deepepochs-0.5.4/deepepochs/__init__.py
@@ -0,0 +1,15 @@
+"""
+@author: hitlic
+TODO:
+    - Accelerate下利用tensorboard日志记录指标
+    - 在Notebook中启动Accelerate程序
+    - 混合精度训练测试
+"""
+__version__ = '0.5.4'
+
+from .loops import *
+from .trainer import Trainer, TrainerBase, EpochTask
+from .tools import *
+from .callbacks import *
+from .optimizer import Optimizer, Optimizers
+from .patches import PatchBase, ValuePatch, TensorPatch, MeanPatch, ConfusionPatch
