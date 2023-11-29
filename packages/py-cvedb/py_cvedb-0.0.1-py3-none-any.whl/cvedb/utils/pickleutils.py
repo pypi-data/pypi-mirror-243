d97818ce0a46bcf4845d535825038a87c3e87c8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 17:05:33 +0000

    Add package py-cvedb/0.0.1/py_cvedb-0.0.1-py3-none-any.whl

---
+++ packages/py-cvedb/py_cvedb-0.0.1-py3-none-any.whl/cvedb/utils/pickleutils.py
@@ -0,0 +1,28 @@
+import zlib
+import pickle
+
+
+def compress(data: bytes):
+    return zlib.compress(data)
+
+
+def decompress(data: bytes):
+    return zlib.decompress(data)
+
+
+def serialize(obj: object):
+    return pickle.dumps(obj)
+
+
+def deserialize(data: bytes):
+    return pickle.loads(data)
+
+
+def pickle_dump(path, obj):
+    with open(path, "wb") as file:
+        pickle.dump(obj, file)
+
+
+def pickle_load(path):
+    with open(path, "rb") as file:
+        return pickle.load(file)
