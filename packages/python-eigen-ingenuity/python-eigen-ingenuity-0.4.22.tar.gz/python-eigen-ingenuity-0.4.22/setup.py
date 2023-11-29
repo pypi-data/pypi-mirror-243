be66b2721b7ca65b1809594a2273070776aea2a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 04:04:40 +0000

    Add package python-eigen-ingenuity/0.4.22/python-eigen-ingenuity-0.4.22.tar.gz

---
+++ packages/python-eigen-ingenuity/python-eigen-ingenuity-0.4.22.tar.gz/python-eigen-ingenuity-0.4.22/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup,find_packages
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+pkgname = 'python-eigen-ingenuity'
+
+# Invoke setup
+setup(
+    name=pkgname,
+    version='0.4.22',
+    author='Murray Callander',
+    author_email='info@eigen.co',
+    url='https://www.eigen.co/',
+    description="A python library used to query data from the Eigen Ingenuity system",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    packages=find_packages("."),
+    license='Apache License 2.0',
+    install_requires=[],
+)
