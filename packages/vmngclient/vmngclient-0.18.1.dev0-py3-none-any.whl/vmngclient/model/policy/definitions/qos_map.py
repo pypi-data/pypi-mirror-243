3f9df38dfa29ceeeeba5ae2928f13633c230f887
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 04:04:30 +0000

    Add package vmngclient/0.18.1.dev0/vmngclient-0.18.1.dev0-py3-none-any.whl

---
+++ packages/vmngclient/vmngclient-0.18.1.dev0-py3-none-any.whl/vmngclient/model/policy/definitions/qos_map.py
@@ -0,0 +1,111 @@
+from enum import Enum
+from typing import Dict, List, Optional, Union
+
+from pydantic.v1 import BaseModel, Field, root_validator, validator
+
+from vmngclient.model.policy.policy_definition import PolicyDefinitionHeader
+
+
+class QoSSchedulingEnum(str, Enum):
+    LLQ = "llq"
+    WRR = "wrr"
+
+
+class QoSDropEnum(str, Enum):
+    TAIL = "tail-drop"
+    RANDOM_EARLY = "red-drop"
+
+
+class QoSScheduler(BaseModel):
+    queue: str
+    class_map_ref: str = Field(alias="classMapRef")
+    bandwidth_percent: str = Field("1", alias="bandwidthPercent")
+    buffer_percent: str = Field("1", alias="bufferPercent")
+    burst: Optional[str] = None
+    scheduling: QoSSchedulingEnum = QoSSchedulingEnum.WRR
+    drops: QoSDropEnum = QoSDropEnum.TAIL
+    temp_key_values: Optional[str] = Field(None, alias="tempKeyValues")
+
+    @staticmethod
+    def get_default_control_scheduler() -> "QoSScheduler":
+        return QoSScheduler(  # type: ignore[call-arg]
+            queue="0",
+            class_map_ref="",
+            bandwidth_percent="100",
+            buffer_percent="100",
+            burst="15000",
+            scheduling=QoSSchedulingEnum.LLQ,
+            drops=QoSDropEnum.TAIL,
+        )
+
+    class Config:
+        allow_population_by_field_name = True
+
+    @validator("queue")
+    def check_queue(cls, queue_str: str):
+        queue = int(queue_str)
+        if queue < 0 or queue > 7:
+            raise ValueError("queue should be in range 0-7")
+        return queue_str
+
+    @validator("bandwidth_percent", "buffer_percent")
+    def check_bandwidth_and_buffer_percent(cls, percent_str: str):
+        percent = int(percent_str)
+        if percent < 1 or percent > 100:
+            raise ValueError("bandwidth/buffer percent should be in range 1-100")
+        return percent_str
+
+    @validator("burst")
+    def check_burst(cls, burst_val: Union[str, None]):
+        if burst_val is not None:
+            burst = int(burst_val)
+            if burst < 5000 or burst > 10_000_000:
+                raise ValueError("burst should be in range 5000-10000000")
+        return burst_val
+
+
+class QoSMapDefinition(BaseModel):
+    qos_schedulers: List[QoSScheduler] = Field(alias="qosSchedulers")
+
+    class Config:
+        allow_population_by_field_name = True
+
+
+class QoSMap(PolicyDefinitionHeader):
+    type: str = Field(default="qosMap", const=True)
+    definition: QoSMapDefinition = QoSMapDefinition(qosSchedulers=[])
+
+    class Config:
+        allow_population_by_field_name = True
+
+    def add_scheduler(
+        self,
+        queue: int,
+        class_map_ref: str,
+        bandwidth: int = 1,
+        buffer: int = 1,
+        scheduling: QoSSchedulingEnum = QoSSchedulingEnum.WRR,
+        drops: QoSDropEnum = QoSDropEnum.TAIL,
+        burst: Optional[int] = None,
+    ) -> None:
+        self.definition.qos_schedulers.append(
+            QoSScheduler(  # type: ignore[call-arg]
+                queue=str(queue),
+                class_map_ref=class_map_ref,
+                bandwidth_percent=str(bandwidth),
+                buffer_percent=str(buffer),
+                burst=str(burst) if burst is not None else None,
+                scheduling=scheduling,
+                drops=drops,
+            )
+        )
+
+    @root_validator  # type: ignore[call-overload]
+    def generate_default_control_scheduler(cls, values) -> Dict:
+        definition: QoSMapDefinition = values.get("definition")
+        if not definition.qos_schedulers:
+            # Only when creating (not when value obtained from remote is present)
+            values["definition"] = QoSMapDefinition(  # type: ignore[call-arg]
+                qos_schedulers=[QoSScheduler.get_default_control_scheduler()]
+            )
+        return values
---
+++ packages/vmngclient/vmngclient-0.18.1.dev0-py3-none-any.whl/vmngclient/model/policy/policy.py
@@ -0,0 +1,51 @@
+import datetime
+from typing import Optional, Sequence
+
+from pydantic.v1 import BaseModel, Field
+
+
+class PolicyId(BaseModel):
+    policy_id: str = Field(alias="policyId")
+
+
+class AssemblyItem(BaseModel):
+    definition_id: str = Field(alias="definitionId")
+    type: str
+
+    class Config:
+        allow_population_by_field_name = True
+
+
+class PolicyDefinition(BaseModel):
+    assembly: Sequence[AssemblyItem]
+
+
+class PolicyCreationPayload(BaseModel):
+    policy_name: str = Field(
+        alias="policyName",
+        regex="^[a-zA-Z0-9_-]{1,127}$",
+        description="Can include only alpha-numeric characters, hyphen '-' or underscore '_'; maximum 127 characters",
+    )
+    policy_description: str = Field("Default description", alias="policyDescription")
+    policy_type: str = Field(alias="policyType")
+    policy_definition: PolicyDefinition = Field(alias="policyDefinition")
+    is_policy_activated: bool = Field(default=False, alias="isPolicyActivated")
+
+    class Config:
+        allow_population_by_field_name = True
+
+
+class PolicyEditPayload(PolicyCreationPayload, PolicyId):
+    pass
+
+
+class PolicyInfo(PolicyEditPayload):
+    created_by: str = Field(alias="createdBy")
+    created_on: datetime.datetime = Field(alias="createdOn")
+    last_updated_by: str = Field(alias="lastUpdatedBy")
+    last_updated_on: datetime.datetime = Field(alias="lastUpdatedOn")
+    policy_version: Optional[str] = Field(None, alias="policyVersion")
+
+
+class PolicyPreview(BaseModel):
+    preview: str
