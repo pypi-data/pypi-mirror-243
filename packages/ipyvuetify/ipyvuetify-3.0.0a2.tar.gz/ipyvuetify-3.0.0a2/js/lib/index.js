7e530df064a836f5edb0d46dbc01bfc61c2c97fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 05:03:41 +0000

    Add package ipyvuetify/3.0.0a2/ipyvuetify-3.0.0a2.tar.gz

---
+++ packages/ipyvuetify/ipyvuetify-3.0.0a2.tar.gz/ipyvuetify-3.0.0a2/js/lib/index.js
@@ -0,0 +1,10 @@
+import "typeface-roboto";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+import "@mdi/font/css/materialdesignicons.css";
+import "./styles.css";
+export { VuetifyView } from "./VuetifyView";
+export * from "./Widgets";
+export { HtmlModel } from "./Html";
+export { VuetifyTemplateModel } from "./VuetifyTemplate";
+export { ThemeModel, ThemeColorsModel } from "./Themes";
+export { version } from "./version";
---
+++ packages/ipyvuetify/ipyvuetify-3.0.0a2.tar.gz/ipyvuetify-3.0.0a2/js/lib/nodepsEmbed.js
@@ -0,0 +1,7 @@
+import "./styles.css";
+export { VuetifyView } from "./VuetifyView";
+export * from "./Widgets";
+export { HtmlModel } from "./Html";
+export { VuetifyTemplateModel } from "./VuetifyTemplate";
+export { ThemeModel, ThemeColorsModel } from "./Themes";
+export { version } from "./version";
