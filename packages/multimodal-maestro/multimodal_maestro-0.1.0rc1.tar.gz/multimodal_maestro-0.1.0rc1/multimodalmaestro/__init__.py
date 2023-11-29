b07a0b865db6ad5c64ec2c13d1a4a155a61864d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 14:04:25 +0000

    Add package multimodal-maestro/0.1.0rc1/multimodal_maestro-0.1.0rc1.tar.gz

---
+++ packages/multimodal-maestro/multimodal_maestro-0.1.0rc1.tar.gz/multimodal_maestro-0.1.0rc1/multimodalmaestro/__init__.py
@@ -0,0 +1,25 @@
+import importlib.metadata as importlib_metadata
+
+try:
+    # This will read version from pyproject.toml
+    __version__ = importlib_metadata.version(__package__ or __name__)
+except importlib_metadata.PackageNotFoundError:
+    __version__ = "development"
+
+from multimodalmaestro.lmms.gpt4 import prompt_image
+from multimodalmaestro.markers.sam import SegmentAnythingMarker
+from multimodalmaestro.postprocessing.mask import (
+    compute_mask_iou_vectorized,
+    mask_non_max_suppression,
+    filter_masks_by_relative_area,
+    adjust_mask_features_by_relative_area,
+    FeatureType,
+    masks_to_marks,
+    refine_masks
+)
+from multimodalmaestro.postprocessing.text import (
+    extract_marks_in_brackets,
+    extract_relevant_masks
+)
+from multimodalmaestro.visualizers import MarkVisualizer
+from multimodalmaestro.primitives import MarkMode
