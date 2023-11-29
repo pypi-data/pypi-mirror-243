158499d1d16a5ea9b2af70ecf3eb81eba6541d59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 16:05:13 +0000

    Add package django-tinymce4-widget/7.1.1/django_tinymce4_widget-7.1.1.tar.gz

---
+++ packages/django-tinymce4-widget/django_tinymce4_widget-7.1.1.tar.gz/django_tinymce4_widget-7.1.1/src/tinymce/__init__.py
@@ -0,0 +1,14 @@
+"""
+TinyMCE Django widget.
+
+This application provides a rich-text WYSIWYG `TinyMCE 4`_ widget
+for Django forms and models.
+
+.. _TinyMCE 4: https://www.tinymce.com/
+"""
+
+from .models import HTMLField
+from .widgets import AdminTinyMCE, TinyMCE
+
+__version__ = "7.1.1"
+__all__ = ["HTMLField", "TinyMCE", "AdminTinyMCE"]
