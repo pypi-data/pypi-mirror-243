6106e2df428879ee5ca89f31374e21ff94e2c3e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 14:04:59 +0000

    Add package braintrust/0.0.75/braintrust-0.0.75-py3-none-any.whl

---
+++ packages/braintrust/braintrust-0.0.75-py3-none-any.whl/braintrust/__init__.py
@@ -0,0 +1,39 @@
+"""
+A Python library for logging data to Braintrust. `braintrust` is distributed as
+a [library on PyPI](https://pypi.org/project/braintrust/).
+
+### Quickstart
+
+Install the library with pip.
+
+```bash
+pip install braintrust
+```
+
+Then, run a simple experiment with the following code (replace `YOUR_API_KEY` with
+your Braintrust API key):
+
+```python
+import braintrust
+
+experiment = braintrust.init(project="PyTest", api_key="YOUR_API_KEY")
+experiment.log(
+    inputs={"test": 1},
+    output="foo",
+    expected="bar",
+    scores={
+        "n": 0.5,
+    },
+    metadata={
+        "id": 1,
+    },
+)
+print(experiment.summarize())
+```
+
+### API Reference
+"""
+
+from .framework import *
+from .logger import *
+from .oai import wrap_openai
