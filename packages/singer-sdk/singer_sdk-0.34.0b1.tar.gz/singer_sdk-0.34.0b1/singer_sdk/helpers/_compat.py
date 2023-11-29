b5683a93ba3f6a404cd432d853271c4f6d7b1047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 16:05:07 +0000

    Add package singer-sdk/0.34.0b1/singer_sdk-0.34.0b1.tar.gz

---
+++ packages/singer-sdk/singer_sdk-0.34.0b1.tar.gz/singer_sdk-0.34.0b1/singer_sdk/helpers/_compat.py
@@ -0,0 +1,24 @@
+"""Compatibility helpers."""
+
+from __future__ import annotations
+
+import sys
+
+if sys.version_info < (3, 8):
+    import importlib_metadata as metadata
+    from typing_extensions import final
+else:
+    from importlib import metadata
+    from typing import final  # noqa: ICN003
+
+if sys.version_info < (3, 12):
+    from importlib_metadata import entry_points
+else:
+    from importlib.metadata import entry_points
+
+if sys.version_info < (3, 9):
+    import importlib_resources as resources
+else:
+    from importlib import resources
+
+__all__ = ["metadata", "final", "resources", "entry_points"]
