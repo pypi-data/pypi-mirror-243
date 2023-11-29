b60305200b9bae094d971b7b38693c2a461ac884
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 04:03:47 +0000

    Add package aiovodafone/0.4.3/aiovodafone-0.4.3-py3-none-any.whl

---
+++ packages/aiovodafone/aiovodafone-0.4.3-py3-none-any.whl/aiovodafone/const.py
@@ -0,0 +1,15 @@
+"""Constants for Vodafone Station."""
+import logging
+
+_LOGGER = logging.getLogger(__package__)
+
+LOGIN = [
+    "-",
+    "logged",
+    "already logged",
+    "credential error",
+    "credential error",
+    "password mismatch",
+]
+
+USER_ALREADY_LOGGED_IN = "MSG_LOGIN_150"
