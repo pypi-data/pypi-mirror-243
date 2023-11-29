7777ad01799ac7ac32223c2011e268d510147acd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 18:07:52 +0000

    Add package pygwalker/0.3.17a1/pygwalker-0.3.17a1-py3-none-any.whl

---
+++ packages/pygwalker/pygwalker-0.3.17a1-py3-none-any.whl/pygwalker/templates/dist/index.d.ts
@@ -0,0 +1,8 @@
+import { IAppProps, PreviewAppProps } from './interfaces';
+declare function GWalker(props: IAppProps, id: string): void;
+declare function PreviewApp(props: PreviewAppProps, id: string): void;
+declare const _default: {
+    GWalker: typeof GWalker;
+    PreviewApp: typeof PreviewApp;
+};
+export default _default;
