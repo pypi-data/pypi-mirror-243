51e4adb8d1c09696712f2317cc8107dcdac9416c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 04:04:50 +0000

    Add package openclimatedata/0.4/openclimatedata-0.4-py3-none-any.whl

---
+++ packages/openclimatedata/openclimatedata-0.4-py3-none-any.whl/openclimatedata/__init__.py
@@ -0,0 +1,6 @@
+from ._primap_hist import PRIMAPHIST
+from ._gcb import GCB
+from ._gcb_national import GCB_National_Emissions
+from ._gcb_fossil import GCB_Fossil_Emissions
+
+__all__ = ["PRIMAPHIST", "GCB", "GCB_National_Emissions", "GCB_Fossil_Emissions"]
