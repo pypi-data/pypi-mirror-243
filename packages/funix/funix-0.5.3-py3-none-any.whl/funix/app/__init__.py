3bfea3a5abb84dbc3e0953d21b0f9468d57d40e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 05:03:47 +0000

    Add package funix/0.5.3/funix-0.5.3-py3-none-any.whl

---
+++ packages/funix/funix-0.5.3-py3-none-any.whl/funix/app/__init__.py
@@ -0,0 +1,39 @@
+"""
+Save the app instance here
+"""
+import re
+from secrets import token_hex
+
+from flask import Flask, Response, abort, request
+from flask_sock import Sock
+
+app = Flask(__name__)
+app.secret_key = token_hex(16)
+app.config.update(
+    SESSION_COOKIE_PATH="/",
+    SESSION_COOKIE_SAMESITE="Lax",
+)
+sock = Sock(app)
+
+
+@app.after_request
+def funix_auto_cors(response: Response) -> Response:
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers[
+        "Access-Control-Allow-Methods"
+    ] = "GET, HEAD, POST, OPTIONS, PUT, PATCH, DELETE"
+    response.headers["Access-Control-Allow-Headers"] = "*"
+    return response
+
+
+regex_string = None
+
+
+def enable_funix_host_checker(regex: str):
+    global regex_string
+    regex_string = regex
+
+    @app.before_request
+    def funix_host_check():
+        if len(re.findall(regex_string, request.host)) == 0:
+            abort(403)
