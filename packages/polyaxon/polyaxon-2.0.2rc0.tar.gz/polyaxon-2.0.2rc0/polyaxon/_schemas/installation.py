f780996073aa2ece93500c4cb6a4e0c561237e3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 04:03:50 +0000

    Add package polyaxon/2.0.2rc0/polyaxon-2.0.2rc0.tar.gz

---
+++ packages/polyaxon/polyaxon-2.0.2rc0.tar.gz/polyaxon-2.0.2rc0/polyaxon/_schemas/installation.py
@@ -0,0 +1,15 @@
+from typing import List, Optional
+
+from clipped.compact.pydantic import StrictStr
+
+from polyaxon._schemas.base import BaseSchemaModel
+
+
+class V1Installation(BaseSchemaModel):
+    key: Optional[StrictStr]
+    version: Optional[StrictStr]
+    dist: Optional[StrictStr]
+    host: Optional[StrictStr]
+    hmac: Optional[StrictStr]
+    mode: Optional[StrictStr]
+    auth: Optional[List[StrictStr]]
