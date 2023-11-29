c21b743bd1b87475c9f3897c468cc6a82ee93c8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 18:07:34 +0000

    Add package pygwalker/0.3.17a0/pygwalker-0.3.17a0-py3-none-any.whl

---
+++ packages/pygwalker/pygwalker-0.3.17a0-py3-none-any.whl/pygwalker/templates/dist/components/preview/index.d.ts
@@ -0,0 +1,6 @@
+import React from "react";
+import { PreviewAppProps } from "@/interfaces";
+interface IPreview extends PreviewAppProps {
+}
+declare const Preview: React.FC<IPreview>;
+export default Preview;
