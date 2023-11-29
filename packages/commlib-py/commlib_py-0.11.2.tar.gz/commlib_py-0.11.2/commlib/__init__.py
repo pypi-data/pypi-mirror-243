0a102fc9c9982eccf21aced1c49eb8d056498ac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 16:04:30 +0000

    Add package commlib-py/0.11.2/commlib_py-0.11.2.tar.gz

---
+++ packages/commlib-py/commlib_py-0.11.2.tar.gz/commlib_py-0.11.2/commlib/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for commlib-py."""
+
+__author__ = """Konstantinos Panayiotou"""
+__email__ = "klpanagi@gmail.com"
+__version__ = "0.11.2"
+
+from .node import Node
+from .rest_proxy import RESTProxy
