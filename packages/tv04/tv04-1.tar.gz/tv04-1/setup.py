0112d9d34c72e72ec054cba7e37f11302c301018
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 16:05:23 +0000

    Add package tv04/1/tv04-1.tar.gz

---
+++ packages/tv04/tv04-1.tar.gz/tv04-1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='tv04',
+      version='1',
+      description='none',
+      packages=['tv04'],
+      author_email='zuszusov676@gmail.com',
+      zip_safe=False)
