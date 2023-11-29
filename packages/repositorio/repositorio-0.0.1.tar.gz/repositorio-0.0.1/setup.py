6fb36c7455a3b94155aca5b0808caa20c387a7eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 05:03:58 +0000

    Add package repositorio/0.0.1/repositorio-0.0.1.tar.gz

---
+++ packages/repositorio/repositorio-0.0.1.tar.gz/repositorio-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name='repositorio',
+    version='0.0.1',
+    description='subir un repositorio automaticamente',
+    author='pachulin50',
+    packages=find_packages(),
+    url='https://github.com/PachulinCool/subeturepositorio'
+
+
+)
