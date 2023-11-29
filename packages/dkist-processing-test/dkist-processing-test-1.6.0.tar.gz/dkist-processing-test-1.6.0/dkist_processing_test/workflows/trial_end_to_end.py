9abd8985d1ea21c724ff6ce1c3501c0f2e20eea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 15:04:21 +0000

    Add package dkist-processing-test/1.6.0/dkist-processing-test-1.6.0.tar.gz

---
+++ packages/dkist-processing-test/dkist-processing-test-1.6.0.tar.gz/dkist-processing-test-1.6.0/dkist_processing_test/workflows/trial_end_to_end.py
@@ -0,0 +1,27 @@
+"""
+Workflow which exercises the common tasks end to end in a trial scenario
+"""
+from dkist_processing_common.tasks import TransferL0Data
+from dkist_processing_common.tasks import TrialTeardown
+from dkist_processing_core import Workflow
+
+from dkist_processing_test.tasks import GenerateCalibratedData
+from dkist_processing_test.tasks import ParseL0TestInputData
+from dkist_processing_test.tasks import TestQualityL0Metrics
+from dkist_processing_test.tasks import TransferTestTrialData
+from dkist_processing_test.tasks import WriteL1Data
+
+trial = Workflow(
+    input_data="input",
+    output_data="output",
+    category="test",
+    detail="trial-e2e",
+    workflow_package=__package__,
+)
+trial.add_node(task=TransferL0Data, upstreams=None)
+trial.add_node(task=ParseL0TestInputData, upstreams=TransferL0Data)
+trial.add_node(task=TestQualityL0Metrics, upstreams=ParseL0TestInputData)
+trial.add_node(task=GenerateCalibratedData, upstreams=TestQualityL0Metrics)
+trial.add_node(task=WriteL1Data, upstreams=GenerateCalibratedData)
+trial.add_node(task=TransferTestTrialData, upstreams=WriteL1Data)
+trial.add_node(task=TrialTeardown, upstreams=TransferTestTrialData)
