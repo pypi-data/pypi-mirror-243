5c4fc2f9f370f949b403291f5dd492a6e83165c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 04:04:46 +0000

    Add package toytrack/0.1.9/toytrack-0.1.9.tar.gz

---
+++ packages/toytrack/toytrack-0.1.9.tar.gz/toytrack-0.1.9/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+# Read the contents of your README file
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='toytrack',
+    version='0.1.9',
+    url='https://github.com/murnanedaniel/ToyTrack',
+    author='Author Name',
+    author_email='dtmurnane@lbl.gov',
+    description='A package for generating toy tracking data.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=['numpy >= 1.11.1', 'pandas >= 0.18.1'],
+)
