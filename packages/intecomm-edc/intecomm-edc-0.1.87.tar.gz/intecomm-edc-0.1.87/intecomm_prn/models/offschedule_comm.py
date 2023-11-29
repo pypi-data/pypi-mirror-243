89f5f29b4416cf1ae624da35a47fb385f6376a53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 15:04:11 +0000

    Add package intecomm-edc/0.1.87/intecomm-edc-0.1.87.tar.gz

---
+++ packages/intecomm-edc/intecomm-edc-0.1.87.tar.gz/intecomm-edc-0.1.87/intecomm_prn/models/offschedule_comm.py
@@ -0,0 +1,15 @@
+from edc_action_item.models import ActionModelMixin
+from edc_model.models import BaseUuidModel
+from edc_sites.models import SiteModelMixin
+from edc_visit_schedule.model_mixins import OffScheduleModelMixin
+
+from ..constants import OFFSCHEDULE_COMM_ACTION
+
+
+class OffScheduleComm(SiteModelMixin, ActionModelMixin, OffScheduleModelMixin, BaseUuidModel):
+    action_name = OFFSCHEDULE_COMM_ACTION
+    offschedule_compare_dates_as_datetimes = False
+
+    class Meta(OffScheduleModelMixin.Meta, BaseUuidModel.Meta):
+        verbose_name = "Off-schedule community-based integrated care"
+        verbose_name_plural = "Off-schedule community-based integrated care"
