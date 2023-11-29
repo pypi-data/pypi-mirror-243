b16e09a9d4572675f1f12497f48b0fb0ee44b0d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 14:04:28 +0000

    Add package gyptis/1.0.1/gyptis-1.0.1-py3-none-any.whl

---
+++ packages/gyptis/gyptis-1.0.1-py3-none-any.whl/gyptis/sources/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Benjamin Vial
+# This file is part of gyptis
+# License: MIT
+# See the documentation at gyptis.gitlab.io
+
+
+from .dipole import *
+from .gaussian import *
+from .ls import *
+from .pw import *
+from .source import *
+from .spharm import *
+from .stack import *
---
+++ packages/gyptis/gyptis-1.0.1-py3-none-any.whl/gyptis/utils/log.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Benjamin Vial
+# This file is part of gyptis
+# License: MIT
+# See the documentation at gyptis.gitlab.io
+
+__all__ = ["logger", "set_log_level"]
+
+
+import logging
+import sys
+
+from loguru import logger
+
+from .. import dolfin
+
+
+def set_log_level(level, level_dolfin=logging.ERROR):
+    global logger
+    logger.remove()
+    logformat = "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> -- <level>{message}</level>"
+    logger.add(sys.stderr, format=logformat, level=level, colorize=True)
+    dolfin.set_log_level(level_dolfin)
+
+
+set_log_level(logging.INFO)
