258e39c647eb27c9383bc47c4b5919ab962fd8be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 04:03:37 +0000

    Add package hezar/0.33.2/hezar-0.33.2-py2.py3-none-any.whl

---
+++ packages/hezar/hezar-0.33.2-py2.py3-none-any.whl/hezar/trainer/trainer_utils.py
@@ -0,0 +1,85 @@
+import os.path
+
+import pandas as pd
+from torch.utils.tensorboard import SummaryWriter
+
+__all__ = [
+    "AverageMeter",
+    "MetricsTracker",
+    "CSVLogger",
+    "write_to_tensorboard",
+]
+
+
+class AverageMeter:
+    """Compute and store the average and current value"""
+
+    def __init__(self, name, fmt=":f"):
+        self.name = name
+        self.fmt = fmt
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = "{name} {val" + self.fmt + "} ({avg" + self.fmt + "})"
+        return fmtstr.format(**self.__dict__)
+
+
+class MetricsTracker:
+    def __init__(self, metrics):
+        self.metrics = metrics or []
+        self.trackers = {}
+        if len(self.metrics):
+            for m in self.metrics.values():
+                for metric_key in m.config.output_keys:
+                    self.trackers[metric_key] = AverageMeter(metric_key)
+        if "loss" not in self.trackers:
+            self.trackers["loss"] = AverageMeter("loss")
+
+    def update(self, results):
+        for metric_name, tracker in self.trackers.items():
+            tracker.update(results[metric_name])
+
+    def reset(self):
+        for tracker in self.trackers.values():
+            tracker.reset()
+
+    def avg(self):
+        avg_results = {}
+        for metric_name, tracker in self.trackers.items():
+            avg_results[metric_name] = tracker.avg
+
+        return avg_results
+
+
+class CSVLogger:
+    def __init__(self, logs_dir: str, csv_filename: str):
+        self.save_path = os.path.join(logs_dir, csv_filename)
+        self.df = pd.DataFrame({})
+
+    def write(self, logs: dict, step: int):
+        all_logs = {"step": step}
+        all_logs.update({k: [v] for k, v in logs.items()})
+        row = pd.DataFrame(all_logs)
+        self.df = pd.concat([self.df, row])
+        self.df.to_csv(self.save_path, index=False)
+
+
+def write_to_tensorboard(writer: SummaryWriter, logs: dict, step: int):
+    for metric_name, value in logs.items():
+        writer.add_scalar(metric_name, value, step)
