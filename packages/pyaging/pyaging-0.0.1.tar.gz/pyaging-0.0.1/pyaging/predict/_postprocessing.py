136030a559709e55fca3264c67761a5a5800664a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 16:04:37 +0000

    Add package pyaging/0.0.1/pyaging-0.0.1.tar.gz

---
+++ packages/pyaging/pyaging-0.0.1.tar.gz/pyaging-0.0.1/pyaging/predict/_postprocessing.py
@@ -0,0 +1,37 @@
+import math
+
+
+def anti_log_linear(x, adult_age=20):
+    """
+    Applies an anti-logarithmic linear transformation to a value.
+    """
+    if x < 0:
+        # Apply exponential transformation for negative values
+        return (1 + adult_age) * math.exp(x) - 1
+    else:
+        # Apply linear transformation for non-negative values
+        return (1 + adult_age) * x + adult_age
+
+
+def anti_logp2(x):
+    """
+    Applies an anti-logarithmic transformation with an offset of -2.
+    """
+    # Exponential transformation with an offset
+    return math.exp(x) - 2
+
+
+def anti_log(x):
+    """
+    Applies a simple anti-logarithmic transformation.
+    """
+    # Simple exponential transformation
+    return math.exp(x)
+
+
+def anti_log_log(x):
+    """
+    Applies a double transformation: logarithmic followed by anti-logarithmic.
+    """
+    # Double transformation: logarithmic followed by anti-logarithmic
+    return math.exp(-math.exp(-x))
