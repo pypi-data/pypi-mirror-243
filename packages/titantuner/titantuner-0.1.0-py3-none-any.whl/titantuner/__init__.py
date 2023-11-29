9e00b147ae0387a8f2beef2e431bd691d378035f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 15:04:05 +0000

    Add package titantuner/0.1.0/titantuner-0.1.0-py3-none-any.whl

---
+++ packages/titantuner/titantuner-0.1.0-py3-none-any.whl/titantuner/__init__.py
@@ -0,0 +1,18 @@
+import os
+import pkgutil
+import sys
+import numpy as np
+
+
+VERSION = "0.1.0"
+__all__ = []
+for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
+    if module_name != "__main__":
+        __all__.append(module_name)
+        _module = loader.find_module(module_name).load_module(module_name)
+        globals()[module_name] = _module
+
+def main():
+    import titantuner.__main__
+    titantuner.__main__.main()
+
