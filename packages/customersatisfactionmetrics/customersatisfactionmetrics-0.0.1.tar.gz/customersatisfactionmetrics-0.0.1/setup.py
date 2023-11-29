1f4473f9123cef2eabe910875e9e051c2a5231f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 05:04:03 +0000

    Add package customersatisfactionmetrics/0.0.1/customersatisfactionmetrics-0.0.1.tar.gz

---
+++ packages/customersatisfactionmetrics/customersatisfactionmetrics-0.0.1.tar.gz/customersatisfactionmetrics-0.0.1/setup.py
@@ -0,0 +1,37 @@
+"""
+Setup configuration for the customersatisfactionmetrics package.
+
+This script is used to handle the packaging and distribution of the
+'customersatisfactionmetrics' package, including metadata, dependencies,
+and other necessary package information.
+"""
+
+from setuptools import find_packages, setup
+
+# Using 'with' statement for safe file handling
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='customersatisfactionmetrics',
+    version="0.0.1",
+    author='Bram Mittendorff',
+    author_email='bram@pescheck.io',
+    description='A short description of your package',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='http://github.com/pescheckit/customersatisfactionmetrics',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Django>=3.0',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        # Other classifiers...
+    ],
+    # Optional settings
+    # keywords='...',
+    # license='MIT',
+    # python_requires='>=3.6',
+)
