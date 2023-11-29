d0ff103316cd1f842168230bb80b9ee3eaefd6d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 19:05:30 +0000

    Add package permchain/0.0.8/permchain-0.0.8-py3-none-any.whl

---
+++ packages/permchain/permchain-0.0.8-py3-none-any.whl/permchain/__init__.py
@@ -0,0 +1,10 @@
+from permchain.checkpoint.base import BaseCheckpointAdapter, CheckpointAt
+from permchain.pregel import Channel, Pregel, ReservedChannels
+
+__all__ = [
+    "Channel",
+    "Pregel",
+    "ReservedChannels",
+    "BaseCheckpointAdapter",
+    "CheckpointAt",
+]
