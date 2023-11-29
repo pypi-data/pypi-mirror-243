1a0f48c6a12935f6af90679881f1938e85886807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 16:04:32 +0000

    Add package SySApp/0.0.5/SySApp-0.0.5.tar.gz

---
+++ packages/SySApp/SySApp-0.0.5.tar.gz/SySApp-0.0.5/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="SySApp",
+    version="0.0.5",
+    description="SyS App official module in Python. (Work in progress...)",
+    author="Runkang Chen",
+    author_email="help.informatic365@gmail.com",
+    packages=["sysapp"],
+)
---
+++ packages/SySApp/SySApp-0.0.5.tar.gz/SySApp-0.0.5/sysapp/adv_module.py
@@ -0,0 +1,13 @@
+# -----------------------------------------------------------------------
+# 
+# © Copyright 2023 SyS App all rights reserved
+# Powered By Runkang Chen
+# email: help.informatic365@gmail.com
+# Website: https://www.sysapp.org/
+# 
+# -----------------------------------------------------------------------
+
+class ADV:
+    def json_to_strjson(json_: dict):
+        convert = str(f"""{json_}""")
+        return convert
