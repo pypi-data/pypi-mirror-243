132c73abf4f5a044ae66f9be5ec322d9476bf654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 16:05:24 +0000

    Add package dtj/0.1.7/dtj-0.1.7.tar.gz

---
+++ packages/dtj/dtj-0.1.7.tar.gz/dtj-0.1.7/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dtj',
+    version='0.1.7',
+    packages=find_packages(),
+    include_package_data=True,
+    description='A tool to convert directory contents to JSON',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/adriangalilea/dtj',
+    author='Adrian Galilea Delgado',
+    author_email='adriangalilea@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'rich',
+        'pyperclip'
+    ],
+    entry_points={
+        'console_scripts': [
+            'dtj=dtj.main:main',
+        ],
+    },
+)
