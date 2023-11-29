1400330b751df412f60c5d9a43a34daf04846272
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 16:04:30 +0000

    Add package tt42/0.0/tt42-0.0.tar.gz

---
+++ packages/tt42/tt42-0.0.tar.gz/tt42-0.0/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+	name='tt42',
+	version='0.0',
+	author='qdzzzxc',
+	author_email='',
+	description='asdasdasd',
+	packages=['tt42'],
+	include_package_data=True,
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
