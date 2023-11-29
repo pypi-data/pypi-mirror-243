016e031de9a8a157e90edced790720e29f18573e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 14:04:33 +0000

    Add package netsys-client/0.13.0/netsys_client-0.13.0-py3-none-any.whl

---
+++ packages/netsys-client/netsys_client-0.13.0-py3-none-any.whl/netsys_client/gateway.py
@@ -0,0 +1,37 @@
+from . import ConfController, PrintController, Response
+import json
+
+class Gateway:
+
+    _erros = []
+
+    def __init__(self):
+
+        self._response = Response()
+
+    def change_config(self, data):
+        
+        self._conf = ConfController(data)
+        
+        if self._conf.get_erros():
+
+            print(self._conf.get_erros())
+
+    def search_printers(self):
+
+        self._print = PrintController()
+        self._message = self._print.get_printer_list(self._token)
+        self._response.send_message(json.dumps(self._message))
+
+    def print_cupom_fiscal(self):
+
+        self._message = {'token': self._token}
+        self._response.send_message(json.dumps(self._message))
+    
+    def set_erro(self, erro):
+
+        self._erros.append(erro)
+
+    def set_token(self, token):
+
+        self._token = token
