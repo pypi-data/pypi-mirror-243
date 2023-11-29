b5423065a8ab133e6eee399ca6a1df0e714e0802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 18:06:58 +0000

    Add package dark-emulator/1.1.2/dark_emulator-1.1.2-py3-none-any.whl

---
+++ packages/dark-emulator/dark_emulator-1.1.2-py3-none-any.whl/dark_emulator/__init__.py
@@ -0,0 +1,5 @@
+__author__ = 'Takahiro Nishimichi, Hironao Miyatake, Sunao Sugiyama'
+__version__ = '1.1.2'
+__url__ = 'https://dark-emulator.readthedocs.io'
+from . import darkemu
+from . import model_hod
