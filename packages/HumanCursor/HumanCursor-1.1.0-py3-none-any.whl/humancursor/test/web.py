3cec3a5e94a56898f0659caa9833d80b7af896f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 05:03:55 +0000

    Add package HumanCursor/1.1.0/HumanCursor-1.1.0-py3-none-any.whl

---
+++ packages/HumanCursor/HumanCursor-1.1.0-py3-none-any.whl/humancursor/test/web.py
@@ -0,0 +1,41 @@
+import sys
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+from humancursor.web_cursor import WebCursor
+
+
+def start_web_demo():
+    # Disclaimer: This script is for demonstration purposes only and should not be used for any cheating activity.
+    print('Initializing Web Demo')
+    driver = webdriver.Chrome()
+    cursor = WebCursor(driver)
+
+    driver.get('https://humanbenchmark.com/tests/chimp')
+    driver.maximize_window()
+    cursor.show_cursor()
+
+    sleep(1.5)
+
+    start_button = driver.find_element(By.XPATH, '//button[text()="Start Test"]')
+
+    cursor.click_on(start_button)
+
+    sleep(1.2)
+
+    for attempt in range(5):
+        blocks = driver.find_elements(By.XPATH, '//div[@data-cellnumber]')
+        blocks_sorted = sorted(blocks, key=lambda x: int(x.get_attribute('data-cellnumber')))
+        for block in blocks_sorted:
+            cursor.click_on(block)
+
+        continue_button = driver.find_element(By.XPATH, '//button[text()="Continue"]')
+        cursor.click_on(continue_button)
+        sleep(1.2)
+
+    sleep(3)
+    print('Web Demo ended')
+    driver.quit()
+    sys.exit()
