076b34c75354f8151e0e08f1cff72a1b422d8ad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 05:03:48 +0000

    Add package mopipe/0.0.3/mopipe-0.0.3.tar.gz

---
+++ packages/mopipe/mopipe-0.0.3.tar.gz/mopipe-0.0.3/src/mopipe/common/datastructs.py
@@ -0,0 +1,47 @@
+"""datastructs.py
+
+This module contains the data structures used by the mopipe package.
+"""
+
+import sys
+
+if sys.version_info >= (3, 11):
+    from enum import IntEnum, StrEnum
+else:
+    from enum import IntEnum
+
+    from strenum import StrEnum
+
+from pandas import DataFrame
+
+
+class DataLevel(IntEnum):
+    """DataLevel
+
+    Enum for the different levels of data that can be read by the
+    mopipe package.
+    """
+
+    EXPERIMENT = 0
+    TRIAL = 1
+    SUBJECT = 2
+
+
+class MocapMetadata(StrEnum):
+    """MocapMetadata
+
+    Common metadata for all MoCap data, and their transformed names.
+    This allows a common interface for all MoCap data.
+    """
+
+    cam_count = ("n_cameras",)
+    frame_count = ("n_frames",)
+    marker_names = ("marker_names",)
+    marker_count = ("n_markers",)
+    sample_rate = ("sample_rate",)
+    time_stamp = ("time_stamp",)
+
+
+class MocapTimeSeries:
+    tsdata: DataFrame
+    metadata: MocapMetadata
