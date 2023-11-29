61f5b3ed40a138cb1bdfdee2fab41bd050e7f99a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-29 18:06:57 +0000

    Add package flexsrc/0.1.3/flexsrc-0.1.3.tar.gz

---
+++ packages/flexsrc/flexsrc-0.1.3.tar.gz/flexsrc-0.1.3/setup.py
@@ -0,0 +1,41 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+def load_version(module_path):
+    for line in open(module_path + '/__init__.py', 'r').read().splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    return '0.0.0'
+
+
+def load_description():
+    with open('README.md', 'r') as file:
+        line = file.readline()
+        prev = ''
+        while line:
+            if prev == '# FlexSrc\n':
+                return line.rstrip()
+            prev = line
+            line = file.readline()
+    return ''
+
+
+def load_long_description():
+    with open('README.md', 'r') as file:
+        return file.read()
+
+
+setup(
+    name='flexsrc',
+    version=load_version('flexsrc'),
+    description=load_description(),
+    long_description=load_long_description(),
+    long_description_content_type='text/markdown',  
+    author='tkms',
+    author_email='tkmnet@users.noreply.github.com',
+    url='https://github.com/tkmnet/flexsrc',
+    packages=find_packages(),
+    install_requires=['pyyaml', 'xxhash', 'pandas', 'cfcf'],
+)
