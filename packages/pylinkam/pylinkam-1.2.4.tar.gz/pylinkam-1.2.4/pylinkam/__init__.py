ca1d97b488736c07e4ee4c1b1b88b9c8f9671821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 15:04:44 +0000

    Add package pylinkam/1.2.4/pylinkam-1.2.4.tar.gz

---
+++ packages/pylinkam/pylinkam-1.2.4.tar.gz/pylinkam-1.2.4/pylinkam/__init__.py
@@ -0,0 +1,10 @@
+__app_name__ = 'pylinkam'
+__version__ = '1.2.4'
+
+__author__ = 'Chris Harrison'
+__credits__ = [__author__]
+
+__maintainer__ = 'Chris Harrison'
+__email__ = '629204+ravngr@users.noreply.github.com'
+
+__status__ = 'Development'
