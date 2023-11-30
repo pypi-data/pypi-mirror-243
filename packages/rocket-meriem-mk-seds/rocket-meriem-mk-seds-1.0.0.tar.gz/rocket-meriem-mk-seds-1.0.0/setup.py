9f791f4ea7e9eb95c1ba0a657f370ba0c2805a3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 06:04:28 +0000

    Add package rocket-meriem-mk-seds/1.0.0/rocket-meriem-mk-seds-1.0.0.tar.gz

---
+++ packages/rocket-meriem-mk-seds/rocket-meriem-mk-seds-1.0.0.tar.gz/rocket-meriem-mk-seds-1.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+   name='rocket-meriem-mk-seds',
+   version='1.0.0',
+   author='Meriem MEKKI',
+   author_email='me.mekki@esi-sba.dz',
+   packages=['rocket-meriem-mk-seds'],
+   url='http://pypi.python.org/pypi/rocket-meriem-mk-seds/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   #  install_requires=[
+   #      'dependency1==1.0.0',
+   #      'dependency2>=2.1.0,<3.0.0',
+   #      # Add more dependencies as needed
+   #  ],
+)
