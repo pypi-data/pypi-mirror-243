f7f82fdca70252ff56f0f68d8164211aefa361ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 16:04:50 +0000

    Add package NetFuse/0.0.30rc0/NetFuse-0.0.30rc0-py3-none-any.whl

---
+++ packages/NetFuse/NetFuse-0.0.30rc0-py3-none-any.whl/netfuse/modules/netgear.py
@@ -0,0 +1,34 @@
+from typing import List
+
+# noinspection PyProtectedMember
+from multiprocessing.context import TimeoutError
+from multiprocessing.pool import ThreadPool
+
+
+from netfuse.config import settings, ValidationError, MissingRequirement, Error
+
+
+def attached_devices():
+    """Get all attached devices in a Netgear router.
+
+    Returns:
+        List[pynetgear.Device]:
+        List of device objects.
+    """
+    try:
+        import pynetgear
+    except ImportError as err:
+        raise MissingRequirement(
+            f"\n\n{err.name}\n\tpip install netfuse[netgear]"
+        )
+    if not settings.router_pass:
+        raise ValidationError(
+            "\n\nrouter_pass\n\tEnvironment variable should be a valid string "
+            f"[type=string_type, input_value={settings.router_pass}, input_type={type(settings.router_pass)}]\n"
+        )
+    netgear = pynetgear.Netgear(password=settings.router_pass)
+    process = ThreadPool(processes=1).apply_async(func=netgear.get_attached_devices)
+    try:
+        return process.get(60)  # default is 120, we don't want to wait that long
+    except TimeoutError:
+        raise Error("Failed to get attached devices.")
