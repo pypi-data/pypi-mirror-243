d3804c391584a53c090d8143d73c6e3b4ed0050f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 03:05:53 +0000

    Add package sbpykit/0.3.0/sbpykit-0.3.0-py3-none-any.whl

---
+++ packages/sbpykit/sbpykit-0.3.0-py3-none-any.whl/sbpykit/utils/__init__.py
@@ -0,0 +1,34 @@
+"""
+Additional utils for error handling.
+"""
+from typing import Any, Never, NoReturn
+
+from sbpykit.utils._info import ObjectInfo
+
+
+def stringify(value: dict, separator: str = ", ") -> str:
+    """
+    Transforms dictionary into string representation.
+    """
+    option_strs: list[str] = []
+
+    for k, v in value.items():
+        option_strs.append(f"{k}={v}")
+
+    return separator.join(option_strs)
+
+
+def get_titled_value(
+    title: str,
+    value: Any | None = None,
+) -> str:
+    titled_value: str = title
+    if value:
+        titled_value = f"{title} <{value!s}>"
+
+    return titled_value
+
+
+def never(_: Never) -> NoReturn:
+    error_message: str = "unhandled case"
+    raise NotImplementedError(error_message)
