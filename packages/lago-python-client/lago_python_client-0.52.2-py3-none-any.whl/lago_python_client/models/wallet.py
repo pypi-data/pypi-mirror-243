ad146cc15f8a46c7fd8732e95d6935d2a08cc6fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 03:06:12 +0000

    Add package lago-python-client/0.52.2/lago_python_client-0.52.2-py3-none-any.whl

---
+++ packages/lago-python-client/lago_python_client-0.52.2-py3-none-any.whl/lago_python_client/models/wallet.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+from ..base_model import BaseResponseModel
+
+
+class Wallet(BaseModel):
+    external_customer_id: Optional[str]
+    rate_amount: Optional[str]
+    name: Optional[str]
+    paid_credits: Optional[str]
+    granted_credits: Optional[str]
+    expiration_at: Optional[str]
+    currency: Optional[str]
+
+
+class WalletResponse(BaseResponseModel):
+    lago_id: str
+    lago_customer_id: str
+    external_customer_id: str
+    status: str
+    currency: str
+    name: Optional[str]
+    rate_amount: str
+    credits_balance: str
+    balance_cents: int
+    consumed_credits: str
+    created_at: str
+    expiration_at: Optional[str]
+    last_balance_sync_at: Optional[str]
+    last_consumed_credit_at: Optional[str]
+    terminated_at: Optional[str]
+    balance: str # NOTE(legacy)
