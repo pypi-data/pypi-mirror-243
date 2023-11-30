da40319a832439f5bb19a87b1384b4aa7ee21062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 03:05:36 +0000

    Add package hiddify-reality-scanner/1.8.0/hiddify_reality_scanner-1.8.0.tar.gz

---
+++ packages/hiddify-reality-scanner/hiddify_reality_scanner-1.8.0.tar.gz/hiddify_reality_scanner-1.8.0/hiddify_reality_scanner/base.py
@@ -0,0 +1,17 @@
+"""
+hiddify_reality_scanner base module.
+
+This is the principal module of the hiddify_reality_scanner project.
+here you put your main classes and objects.
+
+Be creative! do whatever you want!
+
+If you want to replace this with a Flask application run:
+
+    $ make init
+
+and then choose `flask` as template.
+"""
+
+# example constant variable
+NAME = "hiddify_reality_scanner"
