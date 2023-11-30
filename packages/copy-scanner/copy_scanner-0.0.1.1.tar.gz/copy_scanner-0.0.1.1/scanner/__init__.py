a5bec707b4704d4dff4f4b682892a2e7d0478e9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 05:05:21 +0000

    Add package copy-scanner/0.0.1.1/copy_scanner-0.0.1.1.tar.gz

---
+++ packages/copy-scanner/copy_scanner-0.0.1.1.tar.gz/copy_scanner-0.0.1.1/scanner/__init__.py
@@ -0,0 +1,13 @@
+from .version import __version__
+from . import pp, tl, pl
+
+print(
+    f"""
+    **************************************************************************
+    * Welcome to scanner (v{__version__})!                                         *
+    * Making the invisible visible - one cell at a time.                     *
+    * For help and documentation, visit https://github.com/parklab/scanner   *
+    * Happy scanning!                                                        *
+    **************************************************************************
+    """
+)
