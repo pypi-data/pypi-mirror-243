bddd6166d34a31e8fba393bc65744631ff6fccbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 06:04:34 +0000

    Add package minourocketpyx/1.0.0/minourocketpyx-1.0.0.tar.gz

---
+++ packages/minourocketpyx/minourocketpyx-1.0.0.tar.gz/minourocketpyx-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='minourocketpyx',
+   version='1.0.0',
+   author='Minou',
+   author_email='a.deghbouche@esi-sba.dz',
+   packages=['minourocketpyx'],
+   url='http://pypi.python.org/pypi/minourocketpyx/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=['minourocketpyx']
+)
