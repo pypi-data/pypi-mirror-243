f347690c426ded4f37ebc0ed7ac05f93489d36e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 14:04:42 +0000

    Add package magma-lang/2.6.8/magma-lang-2.6.8.tar.gz

---
+++ packages/magma-lang/magma-lang-2.6.8.tar.gz/magma-lang-2.6.8/magma/generator.py
@@ -0,0 +1,180 @@
+from abc import abstractmethod, ABCMeta
+import collections
+import functools
+import warnings
+import weakref
+
+from hwtypes import BitVector
+
+from magma.circuit import (
+    Circuit,
+    DefineCircuitKind,
+    DebugCircuit,
+    DebugDefineCircuitKind,
+    NamerDict,
+)
+from . import cache_definition
+from magma.common import ParamDict
+from magma.config import config
+from magma.debug import get_debug_info
+
+
+class GeneratorMeta(type):
+    def __new__(mcs, name, bases, attrs):
+        warnings.warn(
+            "m.Generator is deprecated. Use m.Generator2 instead",
+            category=DeprecationWarning,
+            stacklevel=2,
+        )
+
+        attrs["bind_generators"] = []
+        cls = super().__new__(mcs, name, bases, attrs)
+        old_generate = cls.generate
+
+        def generate_wrapper(*args, **kwargs):
+            result = old_generate(*args, **kwargs)
+            if hasattr(result, "circuit_definition"):
+                result = result.circuit_definition
+            for gen in cls.bind_generators:
+                gen.generate_bind(result, *args, **kwargs)
+            return result
+
+        cls.generate = generate_wrapper
+        # Cache after bind logic so we don't run it twice on a cached entry
+        if cls.cache:
+            cls.generate = cache_definition(cls.generate)
+        return cls
+
+    def __call__(cls, *args, name=None, **kwargs):
+        """
+        name is reserved kwarg for naming instances
+
+        TODO: Generalize handling of instance parameters
+        """
+        inst_kwargs = {}
+        if name is not None:
+            inst_kwargs["name"] = name
+        return cls.generate(*args, **kwargs)(**inst_kwargs)
+
+
+class Generator(metaclass=GeneratorMeta):
+    # User can disable cacheing by setting this attribute to False
+    cache = True
+
+    @staticmethod
+    @abstractmethod
+    def generate(*args, **kwargs):
+        raise NotImplementedError()
+
+    @classmethod
+    def bind(cls, monitor):
+        cls.bind_generators.append(monitor)
+
+
+def _make_key(cls, *args, **kwargs):
+    _SECRET_KEY = "__magma_generator2_secret_key__"
+    dct = {f"{_SECRET_KEY}{i}": v for i, v in enumerate(args)}
+    dct.update(kwargs)
+    for k, v in dct.items():
+        if isinstance(v, BitVector):
+            # Custom hash for BitVector to avoid inconsistent size error in key
+            # comparison
+            # TODO(leonardt/array2): We could move this to the BV.__hash__ if
+            # this might be more generally useful as a pattern
+            dct[k] = (int(v), len(v))
+    return (cls, ParamDict(dct))
+
+
+def _make_type(cls, *args, **kwargs):
+    dummy = type.__new__(cls, "", (), {
+        "_namer_dict": NamerDict()
+    })
+    name = cls.__name__
+    bases = (cls._base_cls_,)
+    dct = cls._base_metacls_.__prepare__(name, bases)
+    cls.__init__(dummy, *args, **kwargs)
+    dct.update(dict(dummy.__dict__))
+    # NOTE(leonardt): We need to override the Generator2 classmethod bind with
+    # DefineCircuitKind.bind for generator instances (circuits).
+    dct["bind"] = classmethod(cls._base_metacls_.bind)
+    ckt = cls._base_metacls_.__new__(cls, name, bases, dct)
+    ckt._args_ = args
+    ckt._kwargs_ = kwargs
+    for gen in cls.bind_generators:
+        gen.generate_bind(ckt, *args, **kwargs)
+    return ckt
+
+
+class _Generator2Meta(type):
+    _cache = weakref.WeakValueDictionary()
+    _base_cls_ = Circuit
+    _base_metacls_ = DefineCircuitKind
+
+    def __new__(metacls, name, bases, dct):
+        bases = bases + (metacls._base_metacls_,)
+        assert dct.setdefault("bind_generators", []) == []
+        return type.__new__(metacls, name, bases, dct)
+
+    def __call__(cls, *args, **kwargs):
+        # NOTE(leonardt): This is a hacky way to determine that this is a
+        # type(...) call, i.e. it is invoked in the class creation pipeline. In
+        # that case, there are exactly 3 arguments of the form name, bases, dct.
+        # In theory, it would be possible for a Generator subclass to have the
+        # same argument number and types, but having the "__module__" member of
+        # the dct would be highly unlikely, furthmore they should be using a
+        # hashable dict, so it shouldn't satisfy that check. Note that this is
+        # why we use "type is" instead of isinstance.
+        is_base_cls = (
+            len(args) == 3
+            and type(args[0]) is str
+            and type(args[1]) is tuple
+            and (type(args[2]) is dict or type(args[2]) is NamerDict)
+            and "__module__" in args[2]
+        )
+        if is_base_cls:
+            return type.__new__(cls, *args)
+        if not getattr(cls, "_cache_", True):
+            return _make_type(cls, *args, **kwargs)
+        key = _make_key(cls, *args, **kwargs)
+        try:
+            return _Generator2Meta._cache[key]
+        except KeyError:
+            pass
+        this = _make_type(cls, *args, **kwargs)
+        _Generator2Meta._cache[key] = this
+        return this
+
+
+Generator2Kind = _Generator2Meta
+
+
+class Generator2(metaclass=_Generator2Meta):
+    def __new__(metacls, name, bases, dct):
+        return type.__new__(metacls, name, bases, dct)
+
+    def __call__(cls, *args, **kwargs):
+        if "debug_info" not in kwargs:
+            kwargs["debug_info"] = get_debug_info(3)
+        return type(cls)._base_metacls_.__call__(cls, *args, **kwargs)
+
+    @classmethod
+    def bind(cls, monitor):
+        cls.bind_generators.append(monitor)
+
+    def __setattr__(cls, key, value):
+        if config.use_namer_dict:
+            cls._namer_dict[key] = value
+        super().__setattr__(key, value)
+
+
+class _DebugGeneratorMeta(_Generator2Meta):
+    _base_cls_ = DebugCircuit
+    _base_metacls_ = DebugDefineCircuitKind
+
+
+class DebugGenerator2(Generator2, metaclass=_DebugGeneratorMeta):
+    pass
+
+
+def reset_generator_cache():
+    _Generator2Meta._cache = weakref.WeakValueDictionary()
---
+++ packages/magma-lang/magma-lang-2.6.8.tar.gz/magma-lang-2.6.8/magma/inline_verilog.py
@@ -0,0 +1,208 @@
+import contextlib
+import hashlib
+import string
+import warnings
+from typing import Mapping, Optional, Union
+
+from ast_tools.stack import _SKIP_FRAME_DEBUG_STMT, get_symbol_table
+
+from magma.array import Array
+from magma.bit import Bit
+from magma.circuit import Circuit
+from magma.clock import ClockTypes
+from magma.definition_context import DefinitionContext, get_definition_context
+from magma.digital import Digital
+from magma.interface import IO
+from magma.passes.passes import CircuitPass
+from magma.primitives.wire import Wire
+from magma.ref import DefnRef, InstRef, ArrayRef, TupleRef
+from magma.t import Type, Direction, In
+from magma.tuple import Tuple
+from magma.view import PortView, InstView
+from magma.wire_utils import wire_value_or_port_view, WiringError
+
+
+ValueLike = Union[Type, PortView]
+
+
+class InlineVerilogError(RuntimeError):
+    pass
+
+
+def _get_view_inst_parent(view):
+    while not isinstance(view, InstView):
+        assert isinstance(view, PortView), type(view)
+        return _get_view_inst_parent(view.parent_view)
+    return view
+
+
+def _make_inline_value(
+        inline_value_map: Mapping[str, ValueLike], value: ValueLike
+) -> str:
+    if (
+            (
+                isinstance(value, Array)
+                and not issubclass(value.T, Digital)
+            )
+            or isinstance(value, Tuple)
+    ):
+        key = ", ".join(
+            _make_inline_value(inline_value_map, t)
+            for t in reversed(value)
+        )
+        # NOTE(leonardt): previously we used a tick (') prefix, but this is only
+        # needed in verilog for wire assignments. In the past, this was fine
+        # because inline verilog modules used the ' prefix in the instance
+        # statement which is the same as declaring and assigning a wire. Now, we
+        # can inline values more generally, so we don't insert the ' and instead
+        # the user can insert it one if it is inlining a value into a wire
+        # assignment or instance port statement.
+        return f"{{{key}}}"
+    key = f"__magma_inline_value_{len(inline_value_map)}"
+    inline_value_map[key] = value
+    return f"{{{key}}}"
+
+
+def _build_io(inline_value_map: Mapping[str, ValueLike]) -> IO:
+    if not inline_value_map:
+        # Add dummy port so IO is not empty.
+        io = IO(I=In(Bit))
+        io.I.unused()
+        return io
+    io = IO()
+    for key, value in inline_value_map.items():
+        if isinstance(value, PortView):
+            T = value.T
+        else:
+            T = type(value)
+        io += IO(**{key: In(T)})
+    return io
+
+
+def _inline_verilog(
+        context: DefinitionContext,
+        format_str: str,
+        format_args: Mapping[str, ValueLike],
+        inline_value_map: Mapping[str, ValueLike],
+        inline_wire_prefix: str):
+    inline_verilog_modules = context.set_default_metadata(
+        "inline_verilog_modules", [])
+    format_args = format_args.copy()
+    for key, arg in format_args.items():
+        if isinstance(arg, (Type, PortView)):
+            arg = _make_inline_value(inline_value_map, arg)
+        format_args[key] = arg
+    inline_str = format_str.format(**format_args)
+
+    # Because modules/instances are sorted lexigraphically in the generated
+    # verilog, in order for the inline verilog in the output to be in
+    # statement-order, we need to generate a lexicographically increasing string
+    # based on the order of the inline verilog statement in the python. A trick
+    # to do this with numbers is, for every 10 modules, to insert a prefix 9 so
+    # that it comes after the previous 1-9 digits.
+    i = len(inline_verilog_modules)
+    prefix = "9" * (i // 10)
+    suffix = i % 10
+
+    class _InlineVerilog(Circuit):
+        name = f"{context.placer.name}_inline_verilog_{prefix}{suffix}"
+        io = _build_io(inline_value_map)
+        # Since each inline value (key) maps to a port, we populate the
+        # connect_references dictionary with a map from key to port.
+        connect_references = {}
+        for key in inline_value_map:
+            port = getattr(io, key)
+            port.unused()  # these are needed so CoreIR knows it's a definition
+            connect_references[key] = port
+        inline_verilog_strs = [(inline_str, connect_references)]
+
+    inline_verilog_modules.append(_InlineVerilog)
+    inst_name = f"{context.placer.name}_inline_verilog_inst_{prefix}{suffix}"
+    inst = _InlineVerilog(name=inst_name)
+    # If there no interpolated values, _build_io adds a dummy port to avoid an
+    # empty IO. We drive that with 0 here to avoid a hanging input.
+    if not inline_value_map:
+        inst.I @= 0
+
+    for key, value in inline_value_map.items():
+        try:
+            wire_value_or_port_view(getattr(inst, key), value)
+        except WiringError:
+            raise InlineVerilogError(
+                f"Found reference to undriven input port: {repr(value)}"
+            ) from None
+
+
+def _process_fstring_syntax(
+        format_str: str,
+        format_args: Mapping[str, ValueLike],
+        inline_value_map: Mapping[str, ValueLike],
+        symbol_table: Mapping) -> str:
+    fieldnames = [
+        fname
+        for _, fname, _, _ in string.Formatter().parse(format_str)
+        if fname
+    ]
+    for field in fieldnames:
+        if field in format_args:
+            continue
+        try:
+            value = eval(field, {}, symbol_table)
+        except NameError:
+            continue
+        if isinstance(value, (Type, PortView)):
+            # Magma values (more precisely value-like objects, i.e. values or
+            # PortView objects) are interpolated into strings specially. We
+            # handle that here by creating a wire instance to make sure the
+            # values persist.
+            value = _make_inline_value(inline_value_map, value)
+            # Stage for subsequent format call for regular kwargs inside
+            # _inline_verilog.
+            value = value.replace("{", "{{").replace("}", "}}")
+        format_str = format_str.replace(f"{{{field}}}", str(value))
+    return format_str
+
+
+def _process_inline_verilog(
+        context: DefinitionContext,
+        format_str: str,
+        format_args: Mapping[str, ValueLike],
+        symbol_table: Mapping,
+        inline_wire_prefix: str):
+    inline_value_map = {}
+    if symbol_table is not None:
+        format_str = _process_fstring_syntax(
+            format_str, format_args, inline_value_map, symbol_table)
+    _inline_verilog(
+        context, format_str, format_args, inline_value_map, inline_wire_prefix)
+
+
+def inline_verilog_impl(
+        format_str: str,
+        format_args: Mapping[str, ValueLike],
+        symbol_table: Mapping,
+        inline_wire_prefix: str):
+    context = get_definition_context()
+    _process_inline_verilog(
+        context, format_str, format_args, symbol_table, inline_wire_prefix)
+
+
+def inline_verilog(
+        format_str: str,
+        inline_wire_prefix: str = "_magma_inline_wire",
+        **kwargs):
+    exec(_SKIP_FRAME_DEBUG_STMT)
+    warnings.warn(
+        "m.inline_verilog() is deprecated. Use m.inline_verilog2() instead",
+        category=DeprecationWarning,
+        stacklevel=2,
+    )
+    format_args = kwargs
+    symbol_table = get_symbol_table([inline_verilog], copy_locals=True)
+    inline_verilog_impl(
+        format_str, format_args, symbol_table, inline_wire_prefix)
+
+
+class ProcessInlineVerilogPass(CircuitPass):
+    def __call__(self, _):
+        pass
