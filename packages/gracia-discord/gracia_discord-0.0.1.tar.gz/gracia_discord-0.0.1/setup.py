156ff1352fff88776d8e1beab9a99e8af34087e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 16:04:42 +0000

    Add package gracia-discord/0.0.1/gracia_discord-0.0.1.tar.gz

---
+++ packages/gracia-discord/gracia_discord-0.0.1.tar.gz/gracia_discord-0.0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="gracia_discord",
+    version="0.0.1",
+    license='MIT',
+    author="alocados",
+    author_email="develop@alocados.io",
+    description="gracia_discord",
+    packages=["gracia_discord"],
+    zip_safe=False
+)
