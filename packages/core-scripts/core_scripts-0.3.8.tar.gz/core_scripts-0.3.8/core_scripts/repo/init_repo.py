72ca070f49908a3c4afd0a285f8420fc7bdcd979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 05:04:31 +0000

    Add package core-scripts/0.3.8/core_scripts-0.3.8.tar.gz

---
+++ packages/core-scripts/core_scripts-0.3.8.tar.gz/core_scripts-0.3.8/core_scripts/repo/init_repo.py
@@ -0,0 +1,7 @@
+def init_repo(repo,opts):
+    """
+        crea el repositorio en github, y inicia el reopsitio con git, hace un commit y lo publica a github.
+    """
+    # Verifica que el repositorio no este vacio, ya que no podria hacer el primer commit.
+    
+    pass
