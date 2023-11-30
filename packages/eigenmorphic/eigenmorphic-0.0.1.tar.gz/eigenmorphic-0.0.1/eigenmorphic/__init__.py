0ecea3fd787379abe6de7efda383ab8fa79137dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 03:04:58 +0000

    Add package eigenmorphic/0.0.1/eigenmorphic-0.0.1.tar.gz

---
+++ packages/eigenmorphic/eigenmorphic-0.0.1.tar.gz/eigenmorphic-0.0.1/eigenmorphic/__init__.py
@@ -0,0 +1,6 @@
+from sage.misc.lazy_import import lazy_import
+
+lazy_import('eigenmorphic.eigenvalues', ['morphic_eigenvalues'])
+
+lazy_import('eigenmorphic.coboundary', ['coboundary_basis', 'return_substitution'])
+
