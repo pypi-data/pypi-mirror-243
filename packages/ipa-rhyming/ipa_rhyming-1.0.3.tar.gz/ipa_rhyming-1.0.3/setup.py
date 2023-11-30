2f51c0ff2beee07ccdb7c0fa6daaa780c5cf6646
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 03:05:12 +0000

    Add package ipa-rhyming/1.0.3/ipa_rhyming-1.0.3.tar.gz

---
+++ packages/ipa-rhyming/ipa_rhyming-1.0.3.tar.gz/ipa_rhyming-1.0.3/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(name='ipa_rhyming',
+                 version='1.0.3',
+                 packages=['ipa_rhyming'],
+                 package_data={'ipa_rhyming': ['static']},
+                 classifiers=[
+                     'Programming Language :: Python :: 3',
+                     'Operating System :: OS Independent',
+                     'Topic :: Scientific/Engineering'
+                 ],
+                 python_requires='>=3',
+                 author_email='neminova2.0@gmail.com'
+                 )
