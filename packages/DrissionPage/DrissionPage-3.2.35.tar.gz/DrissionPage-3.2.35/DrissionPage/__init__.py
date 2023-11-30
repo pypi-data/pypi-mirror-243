3cc0bafcb8c350e047f0885b7f6c1ca41ec73506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 16:04:59 +0000

    Add package DrissionPage/3.2.35/DrissionPage-3.2.35.tar.gz

---
+++ packages/DrissionPage/DrissionPage-3.2.35.tar.gz/DrissionPage-3.2.35/DrissionPage/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+"""
+@Author  :   g1879
+@Contact :   g1879@qq.com
+"""
+# 常用页面类
+from .chromium_page import ChromiumPage
+from .session_page import SessionPage
+from .web_page import WebPage
+
+# 启动配置类
+from .configs.chromium_options import ChromiumOptions
+from .configs.session_options import SessionOptions
+
+# 旧版页面类和启动配置类
+try:
+    from .mixpage.mix_page import MixPage
+    from .mixpage.drission import Drission
+    from .configs.driver_options import DriverOptions
+except ModuleNotFoundError:
+    pass
+
+__version__ = '3.2.35'
