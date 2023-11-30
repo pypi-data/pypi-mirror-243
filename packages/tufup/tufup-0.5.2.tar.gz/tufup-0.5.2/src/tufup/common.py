4dd64f2d6bdd30742f2aae4e6bf149fd7976b0ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 17:05:50 +0000

    Add package tufup/0.5.2/tufup-0.5.2.tar.gz

---
+++ packages/tufup/tufup-0.5.2.tar.gz/tufup-0.5.2/src/tufup/common.py
@@ -0,0 +1,156 @@
+import logging
+import pathlib
+import re
+from typing import Optional, Union
+
+import bsdiff4
+from packaging.version import Version, InvalidVersion
+
+logger = logging.getLogger(__name__)
+
+SUFFIX_ARCHIVE = '.tar.gz'
+SUFFIX_PATCH = '.patch'
+
+
+class TargetMeta(object):
+    filename_pattern = '{name}-{version}{suffix}'
+    filename_regex = re.compile(
+        r'^(?P<name>[\w-]+)-(?P<version>.+)(?P<suffix>\.tar\.gz|\.patch)$'
+    )
+
+    def __init__(
+        self,
+        target_path: Union[None, str, pathlib.Path] = None,
+        name: Optional[str] = None,
+        version: Optional[str] = None,
+        is_archive: Optional[bool] = True,
+    ):
+        """
+        Initialize either with target_path, or with name, version, archive.
+
+        BEWARE: whitespace is not allowed in the filename,
+        nor in the `name` or `version` arguments
+        """
+        super().__init__()
+        if target_path is None:
+            target_path = TargetMeta.compose_filename(
+                name=name, version=version, is_archive=is_archive
+            )
+        self.target_path_str = str(target_path)  # keep the original for reference
+        self.path = pathlib.Path(target_path)
+        if ' ' in self.filename:
+            logger.critical(
+                f'invalid filename "{self.filename}": whitespace not allowed'
+            )
+
+    def __str__(self):
+        return str(self.target_path_str)
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}(target_path="{self.target_path_str}")'
+
+    def __hash__(self):
+        """
+        This makes the object hashable, so it can be used as dict key or set
+        member.
+
+        https://docs.python.org/3/glossary.html#term-hashable
+
+        """
+        return hash(tuple(self.__dict__.items()))
+
+    def __eq__(self, other):
+        if type(other) != type(self):
+            return NotImplemented
+        return vars(self) == vars(other)
+
+    def __lt__(self, other):
+        """
+        This makes the object sortable, based on the *version* property,
+        without having to specify an explicit sorting key. Note this
+        disregards app name, platform, and suffixes.
+        """
+        if type(other) != type(self):
+            return NotImplemented
+        return self.version < other.version
+
+    @property
+    def filename(self):
+        return self.path.name
+
+    @property
+    def name(self) -> Optional[str]:
+        """The app name."""
+        match_dict = self.parse_filename(self.filename)
+        return match_dict.get('name')
+
+    @property
+    def version(self) -> Optional[Version]:
+        match_dict = self.parse_filename(self.filename)
+        try:
+            version = Version(match_dict.get('version', ''))
+        except InvalidVersion:
+            version = None
+            logger.critical(f'No valid version in filename: {self.filename}')
+        return version
+
+    @property
+    def suffix(self) -> Optional[str]:
+        """Returns the filename suffix, either '.tar.gz', '.patch', or None."""
+        match_dict = self.parse_filename(self.filename)
+        return match_dict.get('suffix')
+
+    @property
+    def is_archive(self) -> bool:
+        return self.suffix == SUFFIX_ARCHIVE
+
+    @property
+    def is_patch(self) -> bool:
+        return self.suffix == SUFFIX_PATCH
+
+    @property
+    def is_other(self) -> bool:
+        return self.suffix not in [SUFFIX_ARCHIVE, SUFFIX_PATCH]
+
+    @classmethod
+    def parse_filename(cls, filename: str) -> dict:
+        """
+        Parse a filename to extract app name, version, and suffix.
+
+        We do not impose any versioning requirements yet, such as defined in
+        packaging.version.VERSION_PATTERN.
+        """
+        match = cls.filename_regex.search(string=filename)
+        return match.groupdict() if match else {}
+
+    @classmethod
+    def compose_filename(cls, name: str, version: str, is_archive: bool):
+        suffix = SUFFIX_ARCHIVE if is_archive else SUFFIX_PATCH
+        return cls.filename_pattern.format(name=name, version=version, suffix=suffix)
+
+
+class Patcher(object):
+    @classmethod
+    def create_patch(
+        cls, src_path: pathlib.Path, dst_path: pathlib.Path
+    ) -> pathlib.Path:
+        """
+        Create a binary patch file based on source and destination files.
+
+        Patch file path matches destination file path, except for suffix.
+        """
+        # replace suffix twice, in case we have a .tar.gz
+        patch_path = dst_path.with_suffix('').with_suffix(SUFFIX_PATCH)
+        bsdiff4.file_diff(src_path=src_path, dst_path=dst_path, patch_path=patch_path)
+        return patch_path
+
+    @classmethod
+    def apply_patch(cls, src_path: pathlib.Path, patch_path: pathlib.Path):
+        """
+        Apply binary patch file to source file to create destination file.
+
+        Destination file path matches patch file path, except for suffix.
+        """
+        dst_path = patch_path.with_suffix(SUFFIX_ARCHIVE)
+        bsdiff4.file_patch(src_path=src_path, dst_path=dst_path, patch_path=patch_path)
+        return dst_path
---
+++ packages/tufup/tufup-0.5.2.tar.gz/tufup-0.5.2/tests/test_common.py
@@ -0,0 +1,172 @@
+import logging
+import pathlib
+
+import bsdiff4
+from packaging.version import Version
+
+from tests import TempDirTestCase
+from tufup.common import Patcher, TargetMeta
+
+
+class TestTargetMeta(TempDirTestCase):
+    def test_init_whitespace(self):
+        for kwargs in [
+            dict(target_path='w h i t e s p a c e-1.2.3.tar.gz'),
+            dict(name='w h i t e s p a c e'),
+        ]:
+            with self.subTest(msg=kwargs):
+                with self.assertLogs(level=logging.CRITICAL):
+                    TargetMeta(**kwargs)
+
+    def test_eq_ne(self):
+        target_meta = TargetMeta()
+        self.assertEqual(target_meta, TargetMeta())
+        self.assertNotEqual(target_meta, TargetMeta(target_path='something'))
+        # two ways to initialize a TargetMeta instance:
+        self.assertEqual(
+            TargetMeta(target_path='my-app-1.2.3a0.patch'),
+            TargetMeta(name='my-app', version='1.2.3a0', is_archive=False),
+        )
+
+    def test_repr(self):
+        # https://docs.python.org/3/reference/datamodel.html#object.__repr__
+        target_meta = TargetMeta(target_path='some.file')
+        # the input to eval is known and trusted, so that should be safe, right?
+        self.assertEqual(eval(repr(target_meta)), target_meta)
+
+    def test_str(self):
+        # see issue #3
+        for target_path in ['str_path', pathlib.Path('pathlib_path')]:
+            with self.subTest(msg=target_path):
+                self.assertIsInstance(
+                    TargetMeta(target_path=target_path).__str__(), str
+                )
+
+    def test_hashable(self):
+        obj = TargetMeta()
+        self.assertEqual(obj.__hash__(), hash(tuple(vars(obj).items())))
+        # we can use the obj as a set member or as dict key
+        self.assertEqual({obj, obj}, {obj})
+
+    def test_sortable(self):
+        version_1 = TargetMeta(target_path='my-app-win-1.0.tar.gz')
+        version_2 = TargetMeta(target_path='my-app-win-2.0.tar.gz')
+        info_list = [version_2, version_1]
+        self.assertEqual([version_1, version_2], sorted(info_list))
+
+    def test_filename(self):
+        target_path = 'url/path/somefile'
+        target_meta = TargetMeta(target_path=target_path)
+        self.assertEqual('somefile', target_meta.filename)
+
+    def test_name(self):
+        target_meta = TargetMeta(target_path='url/path/my-app-1.0.tar.gz')
+        self.assertEqual('my-app', target_meta.name)
+
+    def test_version(self):
+        for version_str in ['1.2.3a4', '']:
+            expected = Version(version_str) if version_str else None
+            with self.subTest(msg=version_str):
+                target_meta = TargetMeta(target_path=f'x-{version_str}.tar.gz')
+                self.assertEqual(expected, target_meta.version)
+
+    def test_suffix(self):
+        valid_suffixes = ['.tar.gz', '.patch']
+        for suffix in ['', '.zip', '.tar.gz', '.patch']:
+            with self.subTest(msg=suffix):
+                target_meta = TargetMeta(target_path=f'my-app-1.2.3a4{suffix}')
+                if suffix in valid_suffixes:
+                    self.assertEqual(suffix, target_meta.suffix)
+                else:
+                    self.assertIsNone(target_meta.suffix)
+
+    def test_is_archive(self):
+        self.assertTrue(TargetMeta(target_path='my-app-1.0.tar.gz').is_archive)
+        self.assertFalse(TargetMeta(target_path='my-app-1.0.patch').is_archive)
+        self.assertFalse(TargetMeta(target_path='my-app-1.0.zip').is_archive)
+
+    def test_is_patch(self):
+        self.assertTrue(TargetMeta(target_path='my-app-1.0.patch').is_patch)
+        self.assertFalse(TargetMeta(target_path='my-app-1.0.tar.gz').is_patch)
+        self.assertFalse(TargetMeta(target_path='my-app-1.0.zip').is_patch)
+
+    def test_is_other(self):
+        self.assertTrue(TargetMeta(target_path='my-app-1.0.zip').is_other)
+        self.assertFalse(TargetMeta(target_path='my-app-1.0.patch').is_other)
+        self.assertFalse(TargetMeta(target_path='my-app-1.0.tar.gz').is_other)
+
+    def test_parse_filename(self):
+        cases = [
+            # PEP440 versions are allowed (we do not parse them here...)
+            ('app-1.patch', ('app', '1', '.patch')),
+            ('app-1.tar.gz', ('app', '1', '.tar.gz')),
+            ('app-1.2.tar.gz', ('app', '1.2', '.tar.gz')),
+            ('app-1.2.3.tar.gz', ('app', '1.2.3', '.tar.gz')),
+            ('app-1a.tar.gz', ('app', '1a', '.tar.gz')),
+            ('app-1b.tar.gz', ('app', '1b', '.tar.gz')),
+            ('app-1rc.tar.gz', ('app', '1rc', '.tar.gz')),
+            ('app-1rc0.tar.gz', ('app', '1rc0', '.tar.gz')),
+            ('app-2022.0.tar.gz', ('app', '2022.0', '.tar.gz')),
+            # we don't impose a specific version format at this point (that
+            # is deferred to packaging.Version)
+            ('app-invalidversion.tar.gz', ('app', 'invalidversion', '.tar.gz')),
+            # underscores, dashes, capitals, etc.
+            ('app-name---1.tar.gz', ('app-name--', '1', '.tar.gz')),
+            ('CAPS-1.tar.gz', ('CAPS', '1', '.tar.gz')),
+            ('un_der_scores-1.0.tar.gz', ('un_der_scores', '1.0', '.tar.gz')),
+            # invalid filenames
+            ('sp ac es-1.zip', ()),
+            ('app-1.gz', ()),
+            ('app-1.xz', ()),
+            ('anything', ()),
+        ]
+        for filename, expected in cases:
+            match_dict = TargetMeta.parse_filename(filename=filename)
+            with self.subTest(msg=filename):
+                self.assertEqual(expected, tuple(match_dict.values()))
+
+    def test_compose_filename(self):
+        filename = TargetMeta.compose_filename(
+            name='app', version='1.0', is_archive=True
+        )
+        self.assertEqual('app-1.0.tar.gz', filename)
+
+
+class PatcherTests(TempDirTestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        # dummy paths
+        self.old_archive_path = self.temp_dir_path / 'my_app-1.0.tar.gz'
+        self.new_archive_path = self.temp_dir_path / 'my_app-2.0.tar.gz'
+        self.new_patch_path = self.temp_dir_path / 'my_app-2.0.patch'
+        # write dummy archive data to files
+        self.old_archive_path.write_bytes(b'old archive data')
+        self.new_archive_data = b'new archive data'
+        self.new_archive_path.write_bytes(self.new_archive_data)
+        # create patch file (see Patcher.create_patch)
+        bsdiff4.file_diff(
+            src_path=self.old_archive_path,
+            dst_path=self.new_archive_path,
+            patch_path=self.new_patch_path,
+        )
+        self.new_patch_data = self.new_patch_path.read_bytes()
+
+    def test_create_patch(self):
+        # remove existing patch file, just to be sure
+        self.new_patch_path.unlink()
+        # test
+        new_patch_path = Patcher.create_patch(
+            src_path=self.old_archive_path, dst_path=self.new_archive_path
+        )
+        self.assertTrue(new_patch_path.exists())
+        self.assertEqual(self.new_patch_data, new_patch_path.read_bytes())
+
+    def test_apply_patch(self):
+        # remove existing "new archive" file, just to be sure
+        self.new_archive_path.unlink()
+        # test
+        new_archive_path = Patcher.apply_patch(
+            src_path=self.old_archive_path, patch_path=self.new_patch_path
+        )
+        self.assertTrue(new_archive_path.exists())
+        self.assertEqual(self.new_archive_data, new_archive_path.read_bytes())
