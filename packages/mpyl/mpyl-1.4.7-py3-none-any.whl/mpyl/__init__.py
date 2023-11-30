53fe8ccc985bcca31b0c675c11137e6fd0de1745
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 17:05:22 +0000

    Add package mpyl/1.4.7/mpyl-1.4.7-py3-none-any.whl

---
+++ packages/mpyl/mpyl-1.4.7-py3-none-any.whl/mpyl/__init__.py
@@ -0,0 +1,38 @@
+"""
+.. include:: ../../README.md
+
+.. include:: ../../README-usage.md
+
+.. include:: ../../README-dev.md
+
+.. include:: ../../releases/README.md
+"""
+
+import click
+
+from .cli.build import build
+from .cli.health import health
+from .cli.meta_info import get_version
+from .cli.meta_info import version
+from .cli.projects import projects
+from .cli.repository import repository
+from .utilities.pyaml_env import parse_config
+from .utilities.repo import RepoConfig, Repository
+
+
+@click.group(name="mpyl", help="Command Line Interface for MPyL")
+def main_group():
+    """Command Line Interface for MPyL"""
+
+
+def add_commands():
+    main_group.add_command(projects)
+    main_group.add_command(build)
+    main_group.add_command(version)
+    main_group.add_command(health)
+    main_group.add_command(repository)
+
+
+def main():
+    add_commands()
+    main_group()  # pylint: disable = no-value-for-parameter
