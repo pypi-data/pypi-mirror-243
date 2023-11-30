8fe6bb1963c32c7224d9b86d4fbd88b73542235c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 14:04:46 +0000

    Add package guidance/0.1.5/guidance-0.1.5-py3-none-any.whl

---
+++ packages/guidance/guidance-0.1.5-py3-none-any.whl/tests/test_grammar.py
@@ -0,0 +1,19 @@
+from guidance import models, select
+from .utils import get_model
+
+def test_select_reset_pos():
+    model = models.Mock()
+    model += 'This is' + select(options=['bad', 'quite bad'])
+    assert str(model) in ["This isbad", "This isquite bad"]
+
+def test_select_simple():
+    lm = get_model("transformers:gpt2")
+    options = ['baad I think', 'bad I think', 'bad']
+    lm = lm + 'Scott is quite ' + select(name='bad', options=options)
+    assert lm['bad'] in options
+
+def test_select_longer():
+    '''This tests to ensure that the grammar is extended greedily.'''
+    lm = models.Mock(b"<s>Scott is a very nice man.")
+    lm += "Scott is a very " + select(name='text', options=['nice', 'nice man.'])
+    assert lm["text"] == 'nice man.'
