362371b1ff1da6a40e395ffa3e46fd5b1cbdb79b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 05:05:23 +0000

    Add package scNodes/1.2.2/scNodes-1.2.2.tar.gz

---
+++ packages/scNodes/scNodes-1.2.2.tar.gz/scNodes-1.2.2/scNodes/nodes/spatialfilter.py
@@ -0,0 +1,122 @@
+from scNodes.core.node import *
+
+import pywt
+from scipy.ndimage import gaussian_filter
+from scipy.signal import medfilt
+
+def create():
+    return SpatialFilterNode()
+
+
+class SpatialFilterNode(Node):
+    description = "Apply various filters to the pixel data of input frames. "
+    title = "Spatial filter"
+    group = "Image processing"
+    colour = (44 / 255, 217 / 255, 158 / 255, 1.0)
+    sortid = 101
+
+    FILTERS = ["Wavelet", "Gaussian", "Median", "Difference of Gaussians", "Derivative of Gaussian"]
+    WAVELETS = dict()
+    WAVELETS["Haar"] = 'haar'
+    WAVELETS["Symlet 2"] = 'sym2'
+    WAVELETS["Symlet 3"] = 'sym3'
+    WAVELETS["Daubechies 2"] = 'db2'
+    WAVELETS["Biorthogonal 1.3"] = 'bior1.3'
+    WAVELETS["Reverse biorthogonal 2.2"] = 'rbio2.2'
+    WAVELETS["Other..."] = None
+
+    WAVELET_NAMES = list(WAVELETS.keys())
+    WAVELET_OTHER_IDX = WAVELET_NAMES.index("Other...")
+
+    def __init__(self):
+        super().__init__()  # Was: super(LoadDataNode, self).__init__()
+        self.size = 210
+
+        # Set up connectable attributes
+        self.connectable_attributes["dataset_in"] = ConnectableAttribute(ConnectableAttribute.TYPE_DATASET, ConnectableAttribute.INPUT, parent = self)
+        self.connectable_attributes["dataset_out"] = ConnectableAttribute(ConnectableAttribute.TYPE_DATASET, ConnectableAttribute.OUTPUT, parent = self)
+
+        # Set up node-specific vars
+        self.params["filter"] = 1
+        self.params["level"] = 1
+        self.params["sigma"] = 2.0
+        self.params["kernel"] = 3
+        self.params["wavelet"] = 0
+        self.params["custom_wavelet"] = "bior6.8"
+        self.params["dog_s1"] = 1.0
+        self.params["dog_s2"] = 5.0
+        self.params["deriv_sigma"] = 2.0
+        self.params["deriv_order"] = 1
+
+    def render(self):
+        if super().render_start():
+            self.connectable_attributes["dataset_out"].render_start()
+            self.connectable_attributes["dataset_in"].render_start()
+            self.connectable_attributes["dataset_out"].render_end()
+            self.connectable_attributes["dataset_in"].render_end()
+            imgui.spacing()
+            imgui.separator()
+            imgui.spacing()
+            imgui.push_item_width(140)
+            _c, self.params["filter"] = imgui.combo("Filter", self.params["filter"], SpatialFilterNode.FILTERS)
+            self.any_change = self.any_change or _c
+            imgui.pop_item_width()
+            imgui.push_item_width(70)
+            if self.params["filter"] == 0:
+                imgui.push_item_width(140)
+                _c, self.params["wavelet"] = imgui.combo("Wavelet", self.params["wavelet"], SpatialFilterNode.WAVELET_NAMES)
+                imgui.pop_item_width()
+                self.any_change = self.any_change or _c
+                if self.params["wavelet"] == SpatialFilterNode.WAVELET_OTHER_IDX:
+                    _c, self.params["custom_wavelet"] = imgui.input_text("pywt name", self.params["custom_wavelet"], 16)
+                    self.any_change = self.any_change or _c
+                    Node.tooltip("Enter any of the pywavelets dicrete wavelet names.\n"
+                                 "See: http://wavelets.pybytes.com/ ")
+                _c, self.params["level"] = imgui.input_int("Level", self.params["level"], 0, 0)
+                self.any_change = self.any_change or _c
+            elif self.params["filter"] == 1:
+                _c, self.params["sigma"] = imgui.input_float("Sigma (px)", self.params["sigma"], 0.0, 0.0, format="%.1f")
+                self.any_change = self.any_change or _c
+            elif self.params["filter"] == 2:
+                _c, self.params["kernel"] = imgui.input_int("Kernel (px)", self.params["kernel"], 0, 0)
+                if self.params["kernel"] % 2 == 0:
+                    self.params["kernel"] += 1
+                self.any_change = self.any_change or _c
+            elif self.params["filter"] == 3:
+                _c, self.params["dog_s1"] = imgui.input_float("Sigma 1 (px)", self.params["dog_s1"], 0.0, 0.0, format="%.1f")
+                self.any_change = self.any_change or _c
+                _c, self.params["dog_s2"] = imgui.input_float("Sigma 2 (px)", self.params["dog_s2"], 0.0, 0.0, format="%.1f")
+                self.any_change = self.any_change or _c
+            elif self.params["filter"] == 4:
+                _c, self.params["deriv_sigma"] = imgui.input_float("Sigma 1 (px)", self.params["deriv_sigma"], 0.0, 0.0, format="%.1f")
+                self.any_change = self.any_change or _c
+                _c, self.params["deriv_order"] = imgui.input_int("Order", self.params["deriv_order"], 0, 0)
+                self.any_change = self.any_change or _c
+            imgui.pop_item_width()
+            super().render_end()
+
+    def get_image_impl(self, idx=None):
+        data_source = self.connectable_attributes["dataset_in"].get_incoming_node()
+        if data_source:
+            input_image = data_source.get_image(idx)
+            outframe = input_image.clone()
+            pxd = input_image.load()
+            if self.params["filter"] == 0:
+                chosen_wavelet = SpatialFilterNode.WAVELETS[SpatialFilterNode.WAVELET_NAMES[self.params["wavelet"]]]
+                w, h = pxd.shape
+                pxd = pxd[:(w//2)*2, :(h//2)*2]
+                if chosen_wavelet is None:
+                    chosen_wavelet = self.params["custom_wavelet"]
+                pxd = pywt.swt2(pxd, wavelet=chosen_wavelet, level=self.params["level"], norm=True, trim_approx=True)[0]
+            elif self.params["filter"] == 1:
+                pxd = gaussian_filter(pxd, self.params["sigma"])
+            elif self.params["filter"] == 2:
+                pxd = medfilt(pxd, self.params["kernel"])
+            elif self.params["filter"] == 3:
+                pxd = gaussian_filter(pxd, self.params["dog_s1"]) - gaussian_filter(pxd, self.params["dog_s2"])
+            elif self.params["filter"] == 4:
+                pxd = gaussian_filter(pxd, self.params["deriv_sigma"], order=self.params["deriv_order"])
+            outframe.data = pxd
+            return outframe
+        else:
+            return None
---
+++ packages/scNodes/scNodes-1.2.2.tar.gz/scNodes-1.2.2/scNodes/nodes/temporalfilter.py
@@ -0,0 +1,101 @@
+from scNodes.core.node import *
+
+
+def create():
+    return TemporalFilterNode()
+
+
+class TemporalFilterNode(Node):
+    description = "This node performs 'temporal filtering', by processing multiple of the input dataset's frames to compute\b" \
+                  "one output frame. Different options are available that perform different temporal filters; e.g. a window-\n" \
+                  "ed average, forward/backward/central differences, or 'grouped difference' filtering.\n" \
+                  "This latter option can be useful when processing photo-activated fluorescence imaging datasets (e.g. PALM)\n" \
+                  "where the sample fluorescence is bright even after prolonged exposure (as is typical in, e.g., cryo-PALM).\n" \
+                  "If for example the acquisition routine was '1 activation UV pulse then 20 fluorescence images', it can help\n" \
+                  "to use the last fluorescence image in such a group of 20 images as a background template for the next group\n" \
+                  "of 20 images."
+    title = "Temporal filter"
+    group = "Image processing"
+    colour = (55 / 255, 236 / 255, 54 / 255, 1.0)
+    sortid = 102
+
+    FILTERS = ["Forward difference", "Backward difference", "Central difference", "Grouped difference", "Windowed average"]
+    NEGATIVE_MODES = ["Absolute", "Zero", "Retain"]
+    INCOMPLETE_GROUP_MODES = ["Discard"]
+
+    def __init__(self):
+        super().__init__()  # Was: super(LoadDataNode, self).__init__()
+        self.size = 250
+
+        # Set up connectable attributes
+        self.connectable_attributes["dataset_in"] = ConnectableAttribute(ConnectableAttribute.TYPE_DATASET, ConnectableAttribute.INPUT, parent=self)
+        self.connectable_attributes["dataset_out"] = ConnectableAttribute(ConnectableAttribute.TYPE_DATASET, ConnectableAttribute.OUTPUT, parent=self)
+
+        self.params["filter"] = 0
+        self.params["negative_handling"] = 2
+        self.params["skip"] = 1
+        self.params["group_size"] = 11
+        self.params["group_background_index"] = 1
+        self.params["window"] = 3
+
+    def render(self):
+        if super().render_start():
+            self.connectable_attributes["dataset_out"].render_start()
+            self.connectable_attributes["dataset_in"].render_start()
+            self.connectable_attributes["dataset_out"].render_end()
+            self.connectable_attributes["dataset_in"].render_end()
+            imgui.spacing()
+            imgui.separator()
+            imgui.spacing()
+
+            imgui.push_item_width(170)
+            _c, self.params["filter"] = imgui.combo("Filter", self.params["filter"], TemporalFilterNode.FILTERS)
+            self.any_change = self.any_change or _c
+            imgui.pop_item_width()
+            imgui.push_item_width(110)
+            _c, self.params["negative_handling"] = imgui.combo("Negative handling", self.params["negative_handling"], TemporalFilterNode.NEGATIVE_MODES)
+            self.any_change = self.any_change or _c
+            imgui.pop_item_width()
+            imgui.push_item_width(90)
+            if self.params["filter"] == 0 or self.params["filter"] == 1 or self.params["filter"] == 2:
+                _c, self.params["skip"] = imgui.input_int("Step (# frames)", self.params["skip"], 0, 0)
+                self.any_change = self.any_change or _c
+            elif self.params["filter"] == 3:
+                _c, self.params["group_size"] = imgui.input_int("Images per cycle", self.params["group_size"], 0, 0)
+                self.any_change = self.any_change or _c
+                _c, self.params["group_background_index"] = imgui.input_int("Background index", self.params["group_background_index"], 0, 0)
+                self.any_change = self.any_change or _c
+            elif self.params["filter"] == 4:
+                _c, self.params["window"] = imgui.input_int("Window size", self.params["window"], 0, 0)
+                self.any_change = self.any_change or _c
+            imgui.pop_item_width()
+            super().render_end()
+
+    def get_image_impl(self, idx=None):
+        data_source = self.connectable_attributes["dataset_in"].get_incoming_node()
+        if data_source:
+            pxd = None
+            if self.params["filter"] == 0:
+                pxd = data_source.get_image(idx + self.params["skip"]).load() - data_source.get_image(idx).load()
+            elif self.params["filter"] == 1:
+                pxd = data_source.get_image(idx).load() - data_source.get_image(idx - self.params["skip"]).load()
+            elif self.params["filter"] == 2:
+                pxd = data_source.get_image(idx + self.params["skip"]).load() - data_source.get_image(idx - self.params["skip"]).load()
+            elif self.params["filter"] == 3:
+                pxd = data_source.get_image(idx).load() - data_source.get_image(self.params["group_size"] * (idx // self.params["group_size"]) + self.params["group_background_index"]).load()
+            elif self.params["filter"] == 4:
+                pxd = np.zeros_like(data_source.get_image(idx).load())
+                for i in range(-self.params["window"], self.params["window"] + 1):
+                    pxd += data_source.get_image(idx + i).load()
+                pxd /= (2 * self.params["window"] + 1)
+
+            if self.params["negative_handling"] == 0:
+                pxd = np.abs(pxd)
+            elif self.params["negative_handling"] == 1:
+                pxd[pxd < 0] = 0
+            elif self.params["negative_handling"] == 2:
+                pass
+
+            out_image = data_source.get_image(idx)
+            out_image.data = pxd
+            return out_image
