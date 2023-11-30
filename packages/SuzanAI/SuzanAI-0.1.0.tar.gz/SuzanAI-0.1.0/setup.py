ae70319e1b23ae594ddef600c4d0984c4975f6ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 04:06:30 +0000

    Add package SuzanAI/0.1.0/SuzanAI-0.1.0.tar.gz

---
+++ packages/SuzanAI/SuzanAI-0.1.0.tar.gz/SuzanAI-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SuzanAI',
+    version='0.1.0',
+    author='famat',
+    author_email='support@suzan.ai',
+    description='Suzan AI API',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.6',
+)
+
