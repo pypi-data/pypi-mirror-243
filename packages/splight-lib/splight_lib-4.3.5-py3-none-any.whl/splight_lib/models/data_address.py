56fd6a3099cca143f8659d8e123ff57c2062ec4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 05:05:17 +0000

    Add package splight-lib/4.3.5/splight_lib-4.3.5-py3-none-any.whl

---
+++ packages/splight-lib/splight_lib-4.3.5-py3-none-any.whl/splight_lib/models/data_address.py
@@ -0,0 +1,33 @@
+from typing import List
+
+import pandas as pd
+from pydantic import BaseModel
+
+from splight_lib.models.native import Boolean, NativeOutput, Number, String
+
+
+class DataAddresses(BaseModel):
+    asset: str
+    attribute: str
+    type: str
+
+    _type_map = {
+        "Number": Number,
+        "String": String,
+        "Boolean": Boolean,
+    }
+
+    def get(self, **params) -> List["NativeOutput"]:
+        params["asset"] = self.asset
+        params["attribute"] = self.attribute
+        return self._type_map[self.type].get(**params)
+
+    def get_dataframe(self, **params) -> pd.DataFrame:
+        params["asset"] = self.asset
+        params["attribute"] = self.attribute
+        return self._type_map[self.type].get_dataframe(**params)
+
+    def save_dataframe(self, dataframe: pd.DataFrame):
+        dataframe["asset"] = self.asset
+        dataframe["attribute"] = self.attribute
+        self._type_map[self.type].save_dataframe(dataframe)
