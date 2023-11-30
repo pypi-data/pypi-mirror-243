b40c4b85c572ceaebd06169b7c005e26530f8355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 06:04:23 +0000

    Add package miwa-package-esi-sba/1.0.0/miwa_package_esi_sba-1.0.0.tar.gz

---
+++ packages/miwa-package-esi-sba/miwa_package_esi_sba-1.0.0.tar.gz/miwa_package_esi_sba-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='miwa_package_esi_sba',
+   version='1.0.0',
+   author='marouabel',
+   author_email='m.belmiloud@esi-sba.dz',
+   packages=['miwa_package_esi_sba'],
+   url='http://pypi.python.org/pypi/miwa_package_esi_sba/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=['miwa_package_esi_sba']
+)
