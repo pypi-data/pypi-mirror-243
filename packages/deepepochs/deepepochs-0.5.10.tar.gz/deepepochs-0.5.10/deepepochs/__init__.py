e483c112ff36221ec4be85dea4bbbbeb38552897
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 16:04:58 +0000

    Add package deepepochs/0.5.10/deepepochs-0.5.10.tar.gz

---
+++ packages/deepepochs/deepepochs-0.5.10.tar.gz/deepepochs-0.5.10/deepepochs/__init__.py
@@ -0,0 +1,12 @@
+"""
+@author: hitlic
+TODO:
+"""
+__version__ = '0.5.10'
+
+from .loops import *
+from .trainer import Trainer, TrainerBase, EpochTask, GradAccumulateTask
+from .tools import *
+from .callbacks import *
+from .optimizer import Optimizer, Optimizers
+from .patches import PatchBase, ValuePatch, TensorPatch, MeanPatch, ConfusionPatch
