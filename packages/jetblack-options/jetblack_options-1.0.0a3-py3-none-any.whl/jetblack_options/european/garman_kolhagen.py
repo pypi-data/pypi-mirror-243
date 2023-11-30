d37ff0bb0737c6d46b5968ee3fb91f015c450353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 03:05:31 +0000

    Add package jetblack-options/1.0.0a3/jetblack_options-1.0.0a3-py3-none-any.whl

---
+++ packages/jetblack-options/jetblack_options-1.0.0a3-py3-none-any.whl/jetblack_options/european/garman_kolhagen.py
@@ -0,0 +1,50 @@
+"""Garman and Kolhagen (1983) Currency options"""
+
+from math import exp, log, sqrt
+from statistics import NormalDist
+
+from ..numeric_greeks.with_dividend_yield import NumericGreeks
+
+norm = NormalDist()
+cdf = norm.cdf
+pdf = norm.pdf
+inv_cdf = norm.inv_cdf
+
+
+def price(
+        is_call: bool,
+        S: float,
+        K: float,
+        T: float,
+        r: float,
+        rf: float,
+        v: float,
+) -> float:
+    """Garman and Kolhagen (1983) Currency options.
+
+    Args:
+        is_call (bool): True for a call, false for a put.
+        S (float): The asset price.
+        K (float): The strike price.
+        T (float): The time to expiry in years.
+        r (float): The risk free rate of the base currency.
+        rf (float): The risk free rate of the quote currency.
+        v (float): The asset volatility.
+
+    Returns:
+        float: _description_
+    """
+    # Garman and Kolhagen (1983) Currency options
+
+    d1 = (log(S / K) + (r - rf + v ** 2 / 2) * T) / (v * sqrt(T))
+    d2 = d1 - v * sqrt(T)
+    if is_call:
+        return S * exp(-rf * T) * cdf(d1) - K * exp(-r * T) * cdf(d2)
+    else:
+        return K * exp(-r * T) * cdf(-d2) - S * exp(-rf * T) * cdf(-d1)
+
+
+def make_bumper(is_call: bool) -> NumericGreeks:
+    def evaluate(S: float, K: float, T: float, r: float, rf: float, v: float) -> float:
+        return price(is_call, S, K, T, r, rf, v)
+    return NumericGreeks(evaluate)
