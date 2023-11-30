e294ed4a2d8431a766b3fa31a49b6623410f8519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 03:06:30 +0000

    Add package imio.smartweb.core/1.2.12/imio.smartweb.core-1.2.12-py3-none-any.whl

---
+++ packages/imio.smartweb.core/imio.smartweb.core-1.2.12-py3-none-any.whl/imio/smartweb/core/rest/authentic_sources.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+
+from imio.smartweb.core.config import DIRECTORY_URL
+from imio.smartweb.core.config import EVENTS_URL
+from imio.smartweb.core.config import NEWS_URL
+from imio.smartweb.core.contents.rest.search.endpoint import get_default_view_url
+from imio.smartweb.core.utils import get_wca_token
+from plone.protect.interfaces import IDisableCSRFProtection
+from plone.restapi.deserializer import json_body
+from plone.restapi.services import Service
+from zope.interface import alsoProvides
+from zope.interface import implementer
+from zope.publisher.interfaces import IPublishTraverse
+
+import os
+import requests
+
+
+@implementer(IPublishTraverse)
+class BaseRequestForwarder(Service):
+    def __init__(self, context, request):
+        super().__init__(context, request)
+        self.traversal_stack = []
+
+    def reply(self):
+        alsoProvides(self.request, IDisableCSRFProtection)
+        url = "/".join(self.traversal_stack)
+        auth_source_url = f"{self.base_url}/{url}"
+        response = self.forward_request(auth_source_url)
+        response = self.add_smartweb_urls(response)
+        return response
+
+    def publishTraverse(self, request, name):
+        self.traversal_stack.append(name)
+        return self
+
+    def forward_request(self, url):
+        method = self.request.method
+        token = get_wca_token(self.client_id, self.client_secret)
+        headers = {"Accept": "application/json", "Authorization": token}
+        params = self.request.form
+        params = self.add_missing_metadatas(params)
+        data = json_body(self.request)
+
+        # Forward the request to the authentic source
+        auth_source_response = requests.request(
+            method, url, params=params, headers=headers, data=data
+        )
+
+        response = self.request.response
+        # Set the status code and headers from the authentic source server response
+        response.setStatus(auth_source_response.status_code)
+        for header, value in auth_source_response.headers.items():
+            response.setHeader(header, value)
+
+        return auth_source_response.json()
+
+    def add_smartweb_urls(self, json_data):
+        if "items" not in json_data:
+            return json_data
+        for item in json_data.get("items", []):
+            if "@id" in item and "UID" in item:
+                # we can construct a Smartweb-related URL for item
+                # TODO: handle other views & translations (use/refactor code in
+                # search endpoint)
+                default_view_url = get_default_view_url(self.request_type)
+                item_uid = item["UID"]
+                item_id = item["id"]
+                item["smartweb_url"] = f"{default_view_url}#/{item_id}?u={item_uid}"
+        return json_data
+
+    def add_missing_metadatas(self, params):
+        if "fullobjects" in params:
+            return params
+        if "metadata_fields" not in params:
+            params["metadata_fields"] = ["id", "UID"]
+        else:
+            if "id" not in params["metadata_fields"]:
+                params["metadata_fields"].append("id")
+            if "UID" not in params["metadata_fields"]:
+                params["metadata_fields"].append("UID")
+        return params
+
+
+class DirectoryRequestForwarder(BaseRequestForwarder):
+    request_type = "directory"
+    client_id = os.environ.get("RESTAPI_DIRECTORY_CLIENT_ID")
+    client_secret = os.environ.get("RESTAPI_DIRECTORY_CLIENT_SECRET")
+    base_url = DIRECTORY_URL
+
+
+class EventsRequestForwarder(BaseRequestForwarder):
+    request_type = "events"
+    client_id = os.environ.get("RESTAPI_EVENTS_CLIENT_ID")
+    client_secret = os.environ.get("RESTAPI_EVENTS_CLIENT_SECRET")
+    base_url = EVENTS_URL
+
+
+class NewsRequestForwarder(BaseRequestForwarder):
+    request_type = "news"
+    client_id = os.environ.get("RESTAPI_NEWS_CLIENT_ID")
+    client_secret = os.environ.get("RESTAPI_NEWS_CLIENT_SECRET")
+    base_url = NEWS_URL
