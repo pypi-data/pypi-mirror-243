112ae7a8f539755539a1e8f2f9ba385aee085e7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 15:04:44 +0000

    Add package uips/0.0.2/uips-0.0.2-py3-none-any.whl

---
+++ packages/uips/uips-0.0.2-py3-none-any.whl/uips/__init__.py
@@ -0,0 +1,8 @@
+"""Phase space sampling"""
+import os
+
+PSS_DIR = os.path.dirname(os.path.realpath(__file__))
+PSS_INPUT_DIR = os.path.join(PSS_DIR, "../inputs")
+PSS_DATA_DIR = os.path.join(PSS_DIR, "../data")
+PSS_DOC_DIR = os.path.join(PSS_DIR, "../documentation")
+PSS_ML_DIR = os.path.join(PSS_DIR, "../data-efficientML")
