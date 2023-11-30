fefae69d6b8df476e3622a5c4a826c800d632b4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 16:05:01 +0000

    Add package mfio/1.3.5/mfio-1.3.5.tar.gz

---
+++ packages/mfio/mfio-1.3.5.tar.gz/mfio-1.3.5/mfio/iolib/mcsv.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-#
+# -------------------------------------------------------------------------------
+# Author:       yunhgu
+# Date:         2023/8/3
+# -------------------------------------------------------------------------------
+import csv
+from .base import IO
+
+
+class Csv(IO):
+    @classmethod
+    def read(cls, path) -> list:
+        data_list = []
+        with open(path, "r") as f:
+            # 这里reader是一个生成器
+            content = csv.reader(f)
+            for row in content:
+                data_list.append(row)
+            return data_list
+
+    @classmethod
+    def write(cls, path, content):
+        """
+        :param file_path:生成文件路径
+        :param content:csv内容 [(1, 2, 3), (4, 5, 6)]
+        """
+        with open(path, "w", newline='', encoding='cp936') as f:
+            write = csv.writer(f)
+            write.writerows(content)
+
+    @staticmethod
+    def name():
+        """
+        :return: string with name of geometry
+        """
+        return "csv"
