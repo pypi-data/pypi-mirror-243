e23ac68c66b7dfe051949a202f3399ef4d4b1134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 06:04:47 +0000

    Add package prefect-databricks/0.2.3/prefect_databricks-0.2.3-py3-none-any.whl

---
+++ packages/prefect-databricks/prefect_databricks-0.2.3-py3-none-any.whl/prefect_databricks/credentials.py
@@ -0,0 +1,78 @@
+"""Credential classes used to perform authenticated interactions with Databricks"""
+
+from typing import Any, Dict, Optional
+
+from httpx import AsyncClient
+from prefect.blocks.core import Block
+from pydantic import VERSION as PYDANTIC_VERSION
+
+if PYDANTIC_VERSION.startswith("2."):
+    from pydantic.v1 import Field, SecretStr
+else:
+    from pydantic import Field, SecretStr
+
+
+class DatabricksCredentials(Block):
+    """
+    Block used to manage Databricks authentication.
+
+    Attributes:
+        databricks_instance:
+            Databricks instance used in formatting the endpoint URL.
+        token: The token to authenticate with Databricks.
+        client_kwargs: Additional keyword arguments to pass to AsyncClient.
+
+    Examples:
+        Load stored Databricks credentials:
+        ```python
+        from prefect_databricks import DatabricksCredentials
+        databricks_credentials_block = DatabricksCredentials.load("BLOCK_NAME")
+        ```
+    """
+
+    _block_type_name = "Databricks Credentials"
+    _logo_url = "https://cdn.sanity.io/images/3ugk85nk/production/ff9a2573c23954bedd27b0f420465a55b1a99dfd-250x250.png"  # noqa
+    _documentation_url = "https://prefecthq.github.io/prefect-databricks/credentials/#prefect_databricks.credentials.DatabricksCredentials"  # noqa
+
+    databricks_instance: str = Field(
+        default=...,
+        description="Databricks instance used in formatting the endpoint URL.",
+    )
+    token: SecretStr = Field(
+        default=..., description="The token to authenticate with Databricks."
+    )
+    client_kwargs: Optional[Dict[str, Any]] = Field(
+        default=None, description="Additional keyword arguments to pass to AsyncClient."
+    )
+
+    def get_client(self) -> AsyncClient:
+        """
+        Gets an Databricks REST AsyncClient.
+
+        Returns:
+            An Databricks REST AsyncClient.
+
+        Example:
+            Gets a Databricks REST AsyncClient.
+            ```python
+            from prefect import flow
+            from prefect_databricks import DatabricksCredentials
+
+            @flow
+            def example_get_client_flow():
+                token = "consumer_key"
+                databricks_credentials = DatabricksCredentials(token=token)
+                client = databricks_credentials.get_client()
+                return client
+
+            example_get_client_flow()
+            ```
+        """
+        base_url = f"https://{self.databricks_instance}/api/"
+
+        client_kwargs = self.client_kwargs or {}
+        client_kwargs["headers"] = {
+            "Authorization": f"Bearer {self.token.get_secret_value()}"
+        }
+        client = AsyncClient(base_url=base_url, **client_kwargs)
+        return client
