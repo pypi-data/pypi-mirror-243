e6dde3c4a7050c0e812168544cb58dfe31fffb9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 03:05:02 +0000

    Add package meilisync/0.1.2/meilisync-0.1.2.tar.gz

---
+++ packages/meilisync/meilisync-0.1.2.tar.gz/meilisync-0.1.2/meilisync/schemas.py
@@ -0,0 +1,29 @@
+import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+
+from meilisync.enums import EventType
+
+
+class ProgressEvent(BaseModel):
+    progress: dict | None = None
+
+
+class Event(ProgressEvent):
+    type: EventType
+    table: str | None = None
+    data: dict
+
+    def mapping_data(self, fields_mapping: Optional[dict] = None):
+        data = {}
+        for k, v in self.data.items():
+            if isinstance(v, datetime.datetime):
+                v = int(v.timestamp())
+
+            if fields_mapping is not None and k in fields_mapping:
+                real_k = fields_mapping[k] or k
+                data[real_k] = v
+            else:
+                data[k] = v
+        return data or self.data
