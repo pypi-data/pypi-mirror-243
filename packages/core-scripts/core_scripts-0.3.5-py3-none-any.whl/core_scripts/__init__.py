f81e1c8190c479e99673607a9899e0773bd1afba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 05:04:38 +0000

    Add package core-scripts/0.3.5/core_scripts-0.3.5-py3-none-any.whl

---
+++ packages/core-scripts/core_scripts-0.3.5-py3-none-any.whl/core_scripts/__init__.py
@@ -0,0 +1,10 @@
+import fileSystem_files
+
+# TODO: ver si funciona
+def name_main(name):
+    """
+    Verifica si el script fue ejecutado localmente o fue importado.
+    """
+    if name == "__main__":
+        return True
+    return False
