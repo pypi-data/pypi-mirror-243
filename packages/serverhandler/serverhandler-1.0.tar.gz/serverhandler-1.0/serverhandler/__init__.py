3a128e327d78624f6a6dccba11f858c4507798f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 06:04:26 +0000

    Add package serverhandler/1.0/serverhandler-1.0.tar.gz

---
+++ packages/serverhandler/serverhandler-1.0.tar.gz/serverhandler-1.0/serverhandler/__init__.py
@@ -0,0 +1,14 @@
+import urllib.request
+import urllib.parse
+import random
+def open():
+    key = random.randint(0, 9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999)
+    if not f"\n{str(key)}\n" in urllib.request.urlopen("http://advanced.figit.club/serverhandler/servers.txt"):
+        urllib.request.urlopen("http://advanced.figit.club/serverhandler/open.php", data=urllib.parse.urlencode({"key": key}).encode())
+    return key
+def receive(server):
+    return urllib.request.urlopen(f"http://advanced.figit.club/serverhandler/{server}/output.txt").read().decode("utf-8")
+def send(server, data):
+    urllib.request.urlopen("http://advanced.figit.club/serverhandler/alter.php", data=urllib.parse.urlencode({"server": server, "data": data}).encode())
+def append(server, data):
+    urllib.request.urlopen("http://advanced.figit.club/serverhandler/append.php", data=urllib.parse.urlencode({"server": server, "data": data}).encode())
