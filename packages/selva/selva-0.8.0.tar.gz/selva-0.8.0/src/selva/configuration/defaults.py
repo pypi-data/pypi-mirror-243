d7121c637a52c9e6d64733647ea3f961b3046ce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 14:04:35 +0000

    Add package selva/0.8.0/selva-0.8.0.tar.gz

---
+++ packages/selva/selva-0.8.0.tar.gz/selva-0.8.0/src/selva/configuration/defaults.py
@@ -0,0 +1,16 @@
+default_settings = {
+    "components": [],
+    "middleware": [],
+    "logging": {
+        "setup": "selva.logging.setup.setup_logger",
+        "root": "WARNING",
+        "level": {},
+        "enable": [],
+        "disable": [],
+    },
+    "templates": {
+        "jinja": {
+            "path": "resources/templates",
+        },
+    },
+}
