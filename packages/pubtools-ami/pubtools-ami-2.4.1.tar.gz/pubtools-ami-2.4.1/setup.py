d5bbc7432bf2c3121480f8604e41480125010021
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-30 04:07:57 +0000

    Add package pubtools-ami/2.4.1/pubtools-ami-2.4.1.tar.gz

---
+++ packages/pubtools-ami/pubtools-ami-2.4.1.tar.gz/pubtools-ami-2.4.1/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup, find_packages
+
+
+def get_description():
+    return "Publishing tools for Amazon machine Images(AMI)"
+
+
+def get_long_description():
+    with open("README.md") as f:
+        text = f.read()
+
+    # Long description is everything after README's initial heading
+    idx = text.find("\n\n")
+    return text[idx:]
+
+
+def get_requirements():
+    with open("requirements.in") as f:
+        return f.read().splitlines()
+
+
+setup(
+    name="pubtools-ami",
+    version="2.4.1",
+    packages=find_packages(exclude=["tests"]),
+    url="https://github.com/release-engineering/pubtools-ami",
+    license="GNU General Public License",
+    description=get_description(),
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    install_requires=get_requirements(),
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "pubtools-ami-push = pubtools._ami.tasks.push:entry_point",
+            "pubtools-ami-delete = pubtools._ami.tasks.delete:entry_point",
+        ]
+    },
+    project_urls={
+        "Documentation": "https://release-engineering.github.io/pubtools-ami/",
+        "Changelog": "https://github.com/release-engineering/pubtools-ami/blob/master/CHANGELOG.md",
+    },
+)
