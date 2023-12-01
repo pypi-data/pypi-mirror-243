0391b8487e7bbc4fb48f0366ae559c2d809aaff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 04:05:37 +0000

    Add package to-pip/3.11.32/to-pip-3.11.32.tar.gz

---
+++ packages/to-pip/to-pip-3.11.32.tar.gz/to-pip-3.11.32/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = [line.strip() for line in f.readlines()]
+
+setup(
+    name="to-pip",
+    version="3.11.32",
+    packages=find_packages(),
+    py_modules=['to_pip'],
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'to_pip = to_pip:main',
+        ],
+    },
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',)
