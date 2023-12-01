f7d155309471923e6870487a11c9b21d33672798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 03:06:29 +0000

    Add package educommon/3.5.4/educommon-3.5.4.tar.gz

---
+++ packages/educommon/educommon-3.5.4.tar.gz/educommon-3.5.4/src/educommon/audit_log/helpers.py
@@ -0,0 +1,32 @@
+from collections.abc import (
+    Iterable,
+)
+from typing import (
+    TYPE_CHECKING,
+    List,
+    Union,
+)
+
+from educommon.audit_log.models import (
+    Table,
+)
+
+
+if TYPE_CHECKING:
+    from django.db.models import (
+        Model,
+    )
+
+
+def get_models_table_ids(models: Union['Model', List['Model']]) -> List[int]:
+    """
+    Возвращает перечень id таблиц из AuditLog соответствующих указанным моделям.
+    """
+    if not isinstance(models, Iterable):
+        models = (models, )
+
+    table_ids = Table.objects.filter(
+        name__in=(model._meta.db_table for model in models),
+    ).values_list('id', flat=True)
+
+    return table_ids
