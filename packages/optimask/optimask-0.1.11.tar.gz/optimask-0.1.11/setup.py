a1ef6669831d34b435cc2d093f0459b0fe660e05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 06:04:55 +0000

    Add package optimask/0.1.11/optimask-0.1.11.tar.gz

---
+++ packages/optimask/optimask-0.1.11.tar.gz/optimask-0.1.11/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='optimask',
+      version='0.1.11',
+      packages=find_packages(),
+      install_requires=['numpy', 'pandas'],
+      long_description_content_type='text/markdown',
+      long_description=long_description
+      )
