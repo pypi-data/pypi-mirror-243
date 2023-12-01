bb2949254c929d65dc8e227bd4c36398db7e99c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 03:07:54 +0000

    Add package glade-api-python/0.0.1/glade_api_python-0.0.1.tar.gz

---
+++ packages/glade-api-python/glade_api_python-0.0.1.tar.gz/glade_api_python-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='gladepython-dev',
+    version='1.0.0',
+    description='An API client python package for Gladefinance',
+    author='Oluwatobi Adelabu',
+    packages=['src'],
+    install_requires=['requests'],
+)
---
+++ packages/glade-api-python/glade_api_python-0.0.1.tar.gz/glade_api_python-0.0.1/src/__init__.py
@@ -0,0 +1,6 @@
+from .glade_functions import Glade
+name = "gladepython-dev"
+__version__ = '0.1.0'
+__author__ = "Oluwatobi Adelabu"
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2023. Gladefinance'
