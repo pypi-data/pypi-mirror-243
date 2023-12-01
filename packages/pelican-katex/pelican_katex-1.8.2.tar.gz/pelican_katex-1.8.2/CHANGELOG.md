0c264f3458c5964be8e16e4c2786eeefbc74a366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 06:04:59 +0000

    Add package pelican-katex/1.8.2/pelican_katex-1.8.2.tar.gz

---
+++ packages/pelican-katex/pelican_katex-1.8.2.tar.gz/pelican_katex-1.8.2/CHANGELOG.md
@@ -0,0 +1,46 @@
+# Changelog
+
+## 1.8.2
+
+- Render markdown math following non-alpha-numeric characters to allow, e.g., inline math
+  in parentheses, see #15
+- Upgrade the bundled katex to 0.16.9
+
+## 1.8.1
+
+- Ignore delimiters in markdown that are followed by alphanumeric characters
+
+## 1.8.0
+
+- Allow escaping dollar delimiters in markdown with backslash
+
+## 1.7.0
+
+- Update the bundled katex to 0.16.7
+
+## 1.6.1
+
+- Serialize the namespace of `math` elements in markdown properly
+
+## 1.6.0
+
+- Add per-file preambles
+- Two small bugfixes
+
+## 1.5.0
+
+- Add support for a shared preamble across math blocks
+
+## 1.4.0
+
+- Make it work on Windows
+
+## 1.2.0
+
+- Restructure the plugin into a client-server architecture to eliminate the
+  overhead of loading nodejs and KaTeX once per math block/role
+
+### Fixes
+
+- Do not fail on newlines in interpreted text
+- Reraise errors that happen during rendering of interpreted text
---
+++ packages/pelican-katex/pelican_katex-1.8.2.tar.gz/pelican_katex-1.8.2/test/test_markdown.py
@@ -0,0 +1,86 @@
+import markdown
+from pelican_katex.markdown import KatexExtension
+
+from html_template import HTMLTemplate
+
+
+def test_renders_standalone_inline_math():
+    input = "Hello $x^2$ world!"
+    output = markdown.markdown(input, extensions=[KatexExtension()])
+
+    template = HTMLTemplate('<p>Hello <span class="katex">...</span> world!</p>')
+    assert template == output
+
+
+def test_renders_inline_math_at_beginning():
+    input = "$x^2$ world!"
+    output = markdown.markdown(input, extensions=[KatexExtension()])
+
+    template = HTMLTemplate('<p><span class="katex">...</span> world!</p>')
+    assert template == output
+
+
+def test_renders_inline_math_in_front_of_punctuation():
+    input = "Hello $x^2$!"
+    output = markdown.markdown(input, extensions=[KatexExtension()])
+
+    template = HTMLTemplate('<p>Hello <span class="katex">...</span>!</p>')
+    assert template == output
+
+
+def test_renders_inline_math_between_parentheses():
+    input = "orthogonal ($u \cot v$) basis"
+    output = markdown.markdown(input, extensions=[KatexExtension()])
+
+    template = HTMLTemplate('<p>orthogonal (<span class="katex">...</span>) basis</p>')
+    assert template == output
+
+
+def test_leaves_currencies_alone():
+    input = "I have 10$ and 20$."
+    output = markdown.markdown(input, extensions=[KatexExtension()])
+
+    template = HTMLTemplate("<p>I have 10$ and 20$.</p>")
+    assert template == output
+
+
+def test_ignores_dollar_followed_by_alnum_character():
+    input = "A coin is $1 and stored in $A register."
+    output = markdown.markdown(input, extensions=[KatexExtension()])
+
+    template = HTMLTemplate("<p>A coin is $1 and stored in $A register.</p>")
+    assert template == output
+
+
+def test_double_dollar_renders_display_math():
+    input = "$$x^2$$"
+    output = markdown.markdown(input, extensions=[KatexExtension()])
+
+    template = HTMLTemplate('<p><span class="katex-display">...</span></p>')
+    assert template == output
+
+
+def test_preamble_block_does_not_show_up():
+    input = "$$@\\def\\pelican{x^2}$$\nUse it\n$$\\pelican = 1$$"
+    output = markdown.markdown(input, extensions=[KatexExtension()])
+
+    template = HTMLTemplate('<p>\nUse it\n<span class="katex-display">...</span></p>')
+    assert template == output
+
+
+def test_backslash_escapes_dollar_delimiter():
+    input = r"My bank account has \$1 and my wallet \$0."
+    output = markdown.markdown(input, extensions=[KatexExtension()])
+
+    target = input.replace("\\", "")
+    template = HTMLTemplate(f"<p>{target}</p>")
+    assert template == output
+
+
+def test_backslash_escapes_double_dollar_delimiter():
+    input = r"A is \$$1 and B is \$$10."
+    output = markdown.markdown(input, extensions=[KatexExtension()])
+
+    target = input.replace("\\", "")
+    template = HTMLTemplate(f"<p>{target}</p>")
+    assert template == output
