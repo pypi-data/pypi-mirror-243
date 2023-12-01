797b8302a45a0ed5458b99d81f0802de123f377c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 04:04:47 +0000

    Add package auto-config/0.0.13/auto_config-0.0.13-py312-none-any.whl

---
+++ packages/auto-config/auto_config-0.0.13-py312-none-any.whl/auto_config/field.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from pydantic import BaseModel, Field
+
+
+class BaseExtraField(BaseModel):
+    pass
+
+
+class SSHField(BaseModel):
+    port: int
+    user: str
+    forward_agent: bool = True
+
+
+class AnsibleField(BaseModel):
+    router: bool = False
+    aliases: list[str] = Field(default_factory=list)
+
+
+class DNSField(BaseModel):
+    target: str
+
+
+class DefaultExtraField(BaseExtraField):
+    ssh: SSHField | None = None
+    ansible: AnsibleField | None = None
+    dns: DNSField | None = None
