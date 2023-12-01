49ead1467f157e93c6458f850249391a3cb7c0d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 04:05:10 +0000

    Add package cg/53.2.0/cg-53.2.0.tar.gz

---
+++ packages/cg/cg-53.2.0.tar.gz/cg-53.2.0/cg/constants/archiving.py
@@ -0,0 +1,10 @@
+from enum import StrEnum
+
+
+class ArchiveLocations(StrEnum):
+    """Archive locations for the different customers' Spring files."""
+
+    KAROLINSKA_BUCKET: str = "karolinska_bucket"
+
+
+PDC_ARCHIVE_LOCATION: str = "PDC"
