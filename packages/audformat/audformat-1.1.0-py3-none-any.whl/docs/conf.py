48026681ff8fec26472a7981a1cfe5d178c3244a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 03:05:52 +0000

    Add package audformat/1.1.0/audformat-1.1.0-py3-none-any.whl

---
+++ packages/audformat/audformat-1.1.0-py3-none-any.whl/docs/conf.py
@@ -0,0 +1,110 @@
+from datetime import date
+import os
+import shutil
+
+import toml
+
+import audeer
+
+
+config = toml.load(audeer.path('..', 'pyproject.toml'))
+
+
+# Project -----------------------------------------------------------------
+project = config['project']['name']
+copyright = f'2019-{date.today().year} audEERING GmbH'
+author = ', '.join(author['name'] for author in config['project']['authors'])
+version = audeer.git_repo_version()
+title = 'Documentation'
+
+
+# General -----------------------------------------------------------------
+master_doc = 'index'
+extensions = []
+source_suffix = '.rst'
+exclude_patterns = [
+    'api-src',
+    'build',
+    'tests',
+    'Thumbs.db',
+    '.DS_Store',
+    '**.ipynb_checkpoints',
+    'emodb-src',
+    '__pycache__',
+]
+templates_path = ['_templates']
+pygments_style = None
+extensions = [
+    'sphinx.ext.graphviz',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',  # support for Google-style docstrings
+    'sphinx.ext.autosummary',
+    'sphinx_autodoc_typehints',
+    'sphinx.ext.autosectionlabel',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.intersphinx',
+    'sphinx_copybutton',
+    'sphinxcontrib.katex',  # has to be before jupyter_sphinx
+    'jupyter_sphinx',
+]
+
+napoleon_use_ivar = True  # List of class attributes
+autodoc_inherit_docstrings = True  # disable docstring inheritance
+# autosummary_generate_overwrite = True
+
+intersphinx_mapping = {
+    'audeer': ('https://audeering.github.io/audeer/', None),
+    'pandas': ('https://pandas.pydata.org/pandas-docs/stable/', None),
+    'python': ('https://docs.python.org/3/', None),
+    'numpy': ('https://numpy.org/doc/stable/', None),
+}
+# Ignore package dependencies during building the docs
+autodoc_mock_imports = [
+    'tqdm',
+]
+
+# Reference with :ref:`data-header:Database`
+autosectionlabel_prefix_document = True
+autosectionlabel_maxdepth = 2
+
+# Do not copy prompt output
+copybutton_prompt_text = r'>>> |\.\.\. |$ '
+copybutton_prompt_is_regexp = True
+
+linkcheck_ignore = [
+    'https://www.isca-speech.org',
+    'http://emodb.bilderbar.info',
+]
+
+# Graphviz figures
+graphviz_output_format = 'svg'
+
+# Disable auto-generation of TOC entries in the API
+# https://github.com/sphinx-doc/sphinx/issues/6316
+toc_object_entries = False
+
+
+# HTML --------------------------------------------------------------------
+html_theme = 'sphinx_audeering_theme'
+html_theme_options = {
+    'display_version': True,
+    'logo_only': False,
+    'footer_links': False,
+    'wide_pages': ['data-example'],
+}
+html_context = {
+    'display_github': True,
+}
+html_title = title
+
+
+# Copy API (sub-)module RST files to docs/api/ folder ---------------------
+audeer.rmdir('api')
+audeer.mkdir('api')
+api_src_files = audeer.list_file_names('api-src')
+api_dst_files = [
+    audeer.path('api', os.path.basename(src_file))
+    for src_file in api_src_files
+]
+for src_file, dst_file in zip(api_src_files, api_dst_files):
+    shutil.copyfile(src_file, dst_file)
