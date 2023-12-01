7a1c15b783e49b22d3710f5c26b2e405b7d564d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 03:06:04 +0000

    Add package RealTimeTTS/0.3.1/RealTimeTTS-0.3.1-py3-none-any.whl

---
+++ packages/RealTimeTTS/RealTimeTTS-0.3.1-py3-none-any.whl/RealtimeTTS/__init__.py
@@ -0,0 +1,6 @@
+from .text_to_stream import TextToAudioStream
+from .engines import BaseEngine
+from .engines import SystemEngine, SystemVoice
+from .engines import AzureEngine, AzureVoice
+from .engines import ElevenlabsEngine, ElevenlabsVoice
+from .engines import CoquiEngine
