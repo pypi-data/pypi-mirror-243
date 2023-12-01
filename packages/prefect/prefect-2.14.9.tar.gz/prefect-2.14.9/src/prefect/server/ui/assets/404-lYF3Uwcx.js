04424adf6663606d1da9a91fbaf4f77108a327a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 05:05:57 +0000

    Add package prefect/2.14.9/prefect-2.14.9.tar.gz

---
+++ packages/prefect/prefect-2.14.9.tar.gz/prefect-2.14.9/src/prefect/server/ui/assets/404-lYF3Uwcx.js
@@ -0,0 +1,1 @@
+import{d as e,c as o,u as a,o as t,ce as r}from"./index-gRcOmcxb.js";import{u as s}from"./usePageTitle-ew0jDH_R.js";const m=e({__name:"404",setup(n){return s("Page Not Found"),(c,u)=>(t(),o(a(r)))}});export{m as default};
