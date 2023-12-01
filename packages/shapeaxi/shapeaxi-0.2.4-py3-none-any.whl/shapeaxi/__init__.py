9f70e10a81aa2cfe0ccbeebedc3fbaec3462b761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 06:05:06 +0000

    Add package shapeaxi/0.2.4/shapeaxi-0.2.4-py3-none-any.whl

---
+++ packages/shapeaxi/shapeaxi-0.2.4-py3-none-any.whl/shapeaxi/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2023-present FlorianDAVAUX <91245912+FlorianDAVAUX@users.noreply.github.com>
+#
+# SPDX-License-Identifier: MIT
+
+# from src.saxi_nets import DentalModelSeg as dentalmodelseg
+from src import saxi_nets
+from saxi_nets import DentalModelSeg as dentalmodelseg
