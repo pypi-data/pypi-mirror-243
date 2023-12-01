25f0dc226db04db87b98b10ce0ecc3642147fb1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 04:04:35 +0000

    Add package RocketSim/2.0.0/RocketSim-2.0.0.tar.gz

---
+++ packages/RocketSim/RocketSim-2.0.0.tar.gz/RocketSim-2.0.0/python-mtheall/Array.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "PyRef.h"
+
+#include <numpy/arrayobject.h>
+
+namespace RocketSim::Python
+{
+class PyArrayRef final : public PyRef<PyArrayObject>
+{
+public:
+	PyArrayRef () noexcept = delete;
+
+	PyArrayRef (nullptr_t) noexcept;
+
+	PyArrayRef (unsigned dim0_, unsigned dim1_ = 0) noexcept;
+
+	float &operator() (unsigned dim0_, unsigned dim1_ = 0) noexcept;
+
+	float const &operator() (unsigned dim0_, unsigned dim1_ = 0) const noexcept;
+
+	bool isnan () const noexcept;
+
+private:
+	unsigned const m_dim0;
+	unsigned const m_dim1;
+};
+}
