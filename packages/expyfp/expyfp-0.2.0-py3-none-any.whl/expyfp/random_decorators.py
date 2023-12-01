45d02997c0e46383aaad41078d3ab2a8d1730ece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 05:05:58 +0000

    Add package expyfp/0.2.0/expyfp-0.2.0-py3-none-any.whl

---
+++ packages/expyfp/expyfp-0.2.0-py3-none-any.whl/expyfp/random_decorators.py
@@ -0,0 +1,16 @@
+from typing import Callable
+
+
+def override(names: set[str]) -> Callable[[type], type]:
+    def override_deco(cls: type) -> type:
+        @classmethod
+        def __init_subclass__(self: type, *args, **kwargs):
+            super(self).__init_subclass__(*args, **kwargs)
+            for name in names:
+                if not hasattr(self, name) or getattr(cls, name, None) is getattr(self, name, None):
+                    raise TypeError(f"Child classes need to have & override {name}")
+
+        cls.__init_subclass__ = __init_subclass__
+        return cls
+
+    return override_deco
