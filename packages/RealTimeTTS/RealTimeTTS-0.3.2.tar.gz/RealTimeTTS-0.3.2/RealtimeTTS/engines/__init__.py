d811665f598cc12a85c3317bd2d772b05a6d7ac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 03:06:40 +0000

    Add package RealTimeTTS/0.3.2/RealTimeTTS-0.3.2.tar.gz

---
+++ packages/RealTimeTTS/RealTimeTTS-0.3.2.tar.gz/RealTimeTTS-0.3.2/RealtimeTTS/engines/__init__.py
@@ -0,0 +1,5 @@
+from .base_engine import BaseEngine
+from .azure_engine import AzureEngine, AzureVoice
+from .system_engine import SystemEngine, SystemVoice
+from .elevenlabs_engine import ElevenlabsEngine, ElevenlabsVoice
+from .coqui_engine import CoquiEngine
