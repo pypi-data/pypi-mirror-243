0a841e46497dd9765027950412ede6dcd78e8de6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 05:05:05 +0000

    Add package data-check/0.18.0/data_check-0.18.0.tar.gz

---
+++ packages/data-check/data_check-0.18.0.tar.gz/data_check-0.18.0/data_check/cli/append.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+from typing import List, Optional, Union
+
+import click
+
+from data_check.config import DataCheckConfig
+
+from .common import common_options
+from .load import load_cmd
+
+
+@click.command()
+@common_options
+@click.option(
+    "--table",
+    type=str,
+    help="table name to load data into",
+)
+@click.argument("files", nargs=-1, type=click.Path())
+@click.pass_context
+def append(
+    ctx: click.Context,
+    connection: str = "",
+    workers: int = DataCheckConfig.parallel_workers,
+    use_process: bool = DataCheckConfig.use_process,
+    config: Union[str, Path] = DataCheckConfig.config_path,
+    verbose: bool = False,
+    traceback: bool = False,
+    quiet: bool = False,
+    log: Optional[Union[str, Path]] = None,
+    table: Optional[str] = None,
+    files: List[Union[str, Path]] = [],
+):
+    """Append data from files into tables."""
+    load_cmd(
+        ctx,
+        connection,
+        workers,
+        use_process,
+        config,
+        verbose,
+        traceback,
+        quiet,
+        log,
+        table,
+        mode="append",
+        files=files,
+    )
