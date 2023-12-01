429fec3a52dbda0f1a22c8930dc2bc7943f22cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 03:06:47 +0000

    Add package locklib/0.0.10/locklib-0.0.10.tar.gz

---
+++ packages/locklib/locklib-0.0.10.tar.gz/locklib-0.0.10/tests/life_lock/test_graph.py
@@ -0,0 +1,93 @@
+import pytest
+
+from locklib.locks.smart_lock.graph import LocksGraph
+from locklib.errors import DeadLockError
+
+
+def test_multiple_set_and_get():
+    graph = LocksGraph()
+
+    graph.add_link(1, 2)
+    graph.add_link(1, 3)
+    graph.add_link(1, 4)
+
+    assert graph.get_links_from(1) == {2, 3, 4}
+
+    assert graph.get_links_from(2) == set()
+
+    assert graph.get_links_from(5) == set()
+
+
+def test_reverse_deleting_of_nodes():
+    graph = LocksGraph()
+
+    graph.add_link(1, 6)
+
+    graph.add_link(6, 3)
+    graph.add_link(6, 4)
+    graph.add_link(6, 5)
+
+    assert len(graph.search_cycles(1, 5)) == 3
+
+
+def test_set_get_delete_and_get():
+    graph = LocksGraph()
+
+    graph.add_link(1, 2)
+    graph.add_link(1, 3)
+    graph.add_link(1, 4)
+
+    assert graph.get_links_from(1) == {2, 3, 4}
+
+    graph.delete_link(1, 2)
+
+    assert graph.get_links_from(1) == {3, 4}
+
+
+def test_detect_simple_cycle():
+    graph = LocksGraph()
+
+    graph.add_link(1, 2)
+
+    with pytest.raises(DeadLockError):
+        graph.add_link(2, 1)
+
+
+def test_detect_difficult_cycle():
+    graph = LocksGraph()
+
+    graph.add_link(1, 2)
+    graph.add_link(2, 3)
+    graph.add_link(3, 4)
+    graph.add_link(4, 5)
+    graph.add_link(5, 6)
+    graph.add_link(6, 7)
+    graph.add_link(7, 8)
+    graph.add_link(8, 9)
+
+    with pytest.raises(DeadLockError):
+        graph.add_link(9, 1)
+
+
+def test_simple_exception_message():
+    graph = LocksGraph()
+
+    graph.add_link(1, 2)
+
+    with pytest.raises(DeadLockError) as e:
+        graph.add_link(2, 1)
+
+    assert str(e.value) == 'A cycle between 2th and 1th threads has been detected.'
+
+
+def test_exception_message_not_so_simple():
+    graph = LocksGraph()
+
+    graph.add_link(1, 2)
+    graph.add_link(2, 3)
+    graph.add_link(3, 4)
+
+    with pytest.raises(DeadLockError) as e:
+        graph.add_link(4, 1)
+
+    assert str(e.value) == 'A cycle between 4th and 1th threads has been detected. The full path of the cycle: 4, 3, 2, 1.'
---
+++ packages/locklib/locklib-0.0.10.tar.gz/locklib-0.0.10/tests/life_lock/test_lock.py
@@ -0,0 +1,182 @@
+from time import sleep
+from queue import Queue
+from threading import Thread, Lock
+
+import pytest
+
+from locklib import SmartLock, DeadLockError
+
+
+def test_release_unlocked():
+    lock = SmartLock()
+
+    with pytest.raises(RuntimeError):
+        lock.release()
+
+
+def test_normal_using():
+    number_of_threads = 5
+    number_of_attempts_per_thread = 100000
+
+    lock = SmartLock()
+    index = 0
+
+    def function():
+        nonlocal index
+
+        for _ in range(number_of_attempts_per_thread):
+            with lock:
+                index += 1
+
+    threads = [Thread(target=function) for _ in range(number_of_threads)]
+
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+
+    assert index == number_of_threads * number_of_attempts_per_thread
+
+
+@pytest.mark.timeout(3)
+def test_raise_when_simple_deadlock():
+    number_of_attempts = 50
+
+    lock_1 = SmartLock()
+    lock_2 = SmartLock()
+
+    queue = Queue()
+
+    for _ in range(number_of_attempts):
+        flag = False
+        def function_1():
+            nonlocal flag
+            try:
+                while True:
+                    with lock_1:
+                        with lock_2:
+                            if flag:
+                                break
+            except DeadLockError as e:
+                flag = True
+                queue.put(True)
+                raise e
+
+        def function_2():
+            nonlocal flag
+            try:
+                while True:
+                    with lock_2:
+                        with lock_1:
+                            if flag:
+                                break
+            except DeadLockError as e:
+                flag = True
+                queue.put(True)
+                raise e
+
+
+        thread_1 = Thread(target=function_1)
+        thread_2 = Thread(target=function_2)
+        thread_1.start()
+        thread_2.start()
+
+        thread_1.join()
+        thread_2.join()
+
+        assert queue.get()
+
+
+@pytest.mark.timeout(3)
+def test_raise_when_not_so_simple_deadlock():
+    number_of_attempts = 50
+
+    lock_1 = SmartLock()
+    lock_2 = SmartLock()
+    lock_3 = SmartLock()
+
+    queue = Queue()
+
+    for _ in range(number_of_attempts):
+        flag = False
+        cycles = 0
+        lock = Lock()
+        def function_1():
+            nonlocal flag
+            nonlocal cycles
+            try:
+                while True:
+                    with lock_1:
+                        sleep(0.0001)
+                        with lock_2:
+                            sleep(0.0001)
+                            with lock_3:
+                                if flag:
+                                    break
+            except DeadLockError as e:
+                with lock:
+                    cycles += 1
+                    if cycles == 2:
+                        flag = True
+                queue.put(True)
+                raise e
+
+        def function_2():
+            nonlocal flag
+            nonlocal cycles
+            try:
+                while True:
+                    with lock_2:
+                        sleep(0.0001)
+                        with lock_3:
+                            sleep(0.0001)
+                            with lock_1:
+                                if flag:
+                                    break
+            except DeadLockError as e:
+                with lock:
+                    cycles += 1
+                    if cycles == 2:
+                        flag = True
+                queue.put(True)
+                raise e
+
+        def function_3():
+            nonlocal flag
+            nonlocal cycles
+            try:
+                while True:
+                    with lock_3:
+                        sleep(0.0001)
+                        with lock_1:
+                            sleep(0.0001)
+                            with lock_2:
+                                if flag:
+                                    break
+            except DeadLockError as e:
+                with lock:
+                    cycles += 1
+                    if cycles == 2:
+                        flag = True
+                queue.put(True)
+                raise e
+
+
+        thread_1 = Thread(target=function_1)
+        thread_2 = Thread(target=function_2)
+        thread_3 = Thread(target=function_3)
+        thread_1.start()
+        thread_2.start()
+        thread_3.start()
+
+        thread_1.join()
+        thread_2.join()
+        thread_3.join()
+
+        counter = 0
+
+        for _ in range(2):
+            queue.get()
+            counter += 1
+
+        assert counter == 2
