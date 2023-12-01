9581124ed0e6862627449790a9e948b69a3d40e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 03:05:53 +0000

    Add package cg/53.1.10/cg-53.1.10-py3-none-any.whl

---
+++ packages/cg/cg-53.1.10-py3-none-any.whl/cg/constants/archiving.py
@@ -0,0 +1,7 @@
+from enum import StrEnum
+
+
+class ArchiveLocations(StrEnum):
+    """Archive locations for the different customers' Spring files."""
+
+    KAROLINSKA_BUCKET: str = "karolinska_bucket"
