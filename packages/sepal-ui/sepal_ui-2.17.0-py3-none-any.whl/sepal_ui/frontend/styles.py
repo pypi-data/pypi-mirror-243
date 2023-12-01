166ff8351ea8ff923d0e8af38342850227c9e02f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 04:05:14 +0000

    Add package sepal-ui/2.17.0/sepal_ui-2.17.0-py3-none-any.whl

---
+++ packages/sepal-ui/sepal_ui-2.17.0-py3-none-any.whl/sepal_ui/frontend/styles.py
@@ -0,0 +1,167 @@
+"""Helpers to customize the display of sepal-ui widgets and maps."""
+
+from pathlib import Path
+from types import SimpleNamespace
+from typing import Dict, Tuple
+
+import ipyvuetify as v
+from IPython.display import HTML, Javascript, display
+from traitlets import Bool, HasTraits, observe
+
+import sepal_ui.scripts.utils as su
+from sepal_ui.conf import config
+
+################################################################################
+# access the folders where style information is stored (layers, widgets)
+#
+
+# the colors are set using tables as follow.
+# 1 (True): dark theme
+# 0 (false): light theme
+JSON_DIR: Path = Path(__file__).parent / "json"
+"The path to the json style folder"
+
+CSS_DIR: Path = Path(__file__).parent / "css"
+"The path to the css style folder"
+
+JS_DIR: Path = Path(__file__).parent / "js"
+"The path to the js style folder"
+
+################################################################################
+# define all the colors that we want to use in the theme
+#
+
+DARK_THEME: Dict[str, str] = {
+    "primary": "#b3842e",
+    "accent": "#a1458e",
+    "secondary": "#324a88",
+    "success": "#3f802a",
+    "info": "#79b1c9",
+    "warning": "#b8721d",
+    "error": "#a63228",
+    "main": "#24221f",  # Are not traits
+    "darker": "#1a1a1a",  # Are not traits
+    "bg": "#121212",  # Are not traits
+    "menu": "#424242",  # Are not traits
+}
+"colors used for the dark theme"
+
+LIGHT_THEME: Dict[str, str] = {
+    "primary": v.theme.themes.light.primary,
+    "accent": v.theme.themes.light.accent,
+    "secondary": v.theme.themes.light.secondary,
+    "success": v.theme.themes.light.success,
+    "info": v.theme.themes.light.info,
+    "warning": v.theme.themes.light.warning,
+    "error": v.theme.themes.light.error,
+    "main": "#2e7d32",
+    "darker": "#005005",
+    "bg": "#FFFFFF",
+    "menu": "#FFFFFF",
+}
+"colors used for the light theme"
+
+TYPES: Tuple[str, ...] = (
+    "info",
+    "primary",
+    "secondary",
+    "accent",
+    "error",
+    "success",
+    "warning",
+    "anchor",
+)
+"The different types defined by ipyvuetify"
+
+################################################################################
+# define classes and method to make the application responsive
+#
+
+
+def get_theme() -> str:
+    """Get theme name from the config file (default to dark).
+
+    Returns:
+        The theme to use
+    """
+    return config.get("sepal-ui", "theme", fallback="dark")
+
+
+class SepalColor(HasTraits, SimpleNamespace):
+
+    _dark_theme: Bool = Bool(True if get_theme() == "dark" else False).tag(sync=True)
+    "Whether to use dark theme or not. By changing this value, the theme value will be stored in the conf file. Is only intended to be accessed in development mode."
+
+    new_colors: dict = {}
+    "Dictionary with name:color structure."
+
+    @observe("_dark_theme")
+    def __init__(self, *_, **new_colors) -> None:
+        """Custom simple name space to store and access to the sepal_ui colors and with a magic method to display theme.
+
+        Args:
+            **new_colors (optional): the new colors to set in hexadecimal as a dict (experimental)
+        """
+        # set vuetify theme
+        v.theme.dark = self._dark_theme
+
+        # Get get current theme name
+        self.theme_name = "dark" if self._dark_theme else "light"
+
+        # Save "new" theme in configuration file
+        su.set_config("theme", self.theme_name)
+
+        self.kwargs = DARK_THEME if self._dark_theme else LIGHT_THEME
+        self.kwargs = new_colors or self.kwargs
+
+        # Even if the theme.themes.dark_theme trait could trigger the change on all elms
+        # we have to replace the default values every time:
+        theme = getattr(v.theme.themes, self.theme_name)
+
+        # TODO: Would be awesome to find a way to create traits for the new colors and
+        # assign them here directly
+        [setattr(theme, color_name, color) for color_name, color in self.kwargs.items()]
+
+        # Now instantiate the namespace
+        SimpleNamespace.__init__(self, **self.kwargs)
+        HasTraits.__init__(self)
+
+        return
+
+    def _repr_html_(self, *_) -> str:
+        """Rich display of the color palette in an HTML frontend."""
+        s = 60
+        html = f"<h3>Current theme: {self.theme_name}</h3><table>"
+        items = {k: v for k, v in self.kwargs.items()}.items()
+
+        for name, color in items:
+            c = su.to_colors(color)
+            html += f"""
+            <th>
+                <svg width='{s}' height='{s}'>
+                    <rect width='{s}' height='{s}' style='fill:{c};
+                    stroke-width:1;stroke:rgb(255,255,255)'/>
+                </svg>
+            </th>
+            """
+
+        html += "</tr><tr>"
+        html += "".join([f"<td>{name}</br>{color}</td>" for name, color in items])
+        html += "</tr></table>"
+
+        return html
+
+
+# load custom styling of sepal_ui
+sepal_ui_css = HTML(f"<style>{(CSS_DIR / 'custom.css').read_text()}</style>")
+
+# load fa-6
+fa_css = HTML(
+    '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"/>'
+)
+
+# create a small hack to remove fontawesome from the html output
+clean_fa_js = Javascript((JS_DIR / "fontawesome.js").read_text())
+
+# display all
+display(sepal_ui_css, fa_css, clean_fa_js)
---
+++ packages/sepal-ui/sepal_ui-2.17.0-py3-none-any.whl/sepal_ui/mapping/fullscreen_control.py
@@ -0,0 +1,103 @@
+"""Customized control to toggle the fullscreen state of the map."""
+
+from pathlib import Path
+from typing import List, Optional
+
+import ipyvuetify as v
+from ipyleaflet import Map, WidgetControl
+from IPython.display import Javascript, display
+
+from sepal_ui.mapping.map_btn import MapBtn
+
+
+class FullScreenControl(WidgetControl):
+
+    ICONS: List[str] = ["fa-solid fa-expand", "fa-solid fa-compress"]
+    "list: The icons that will be used to toggle between expand and compressed mode"
+
+    METHODS: List[str] = ["embed", "fullscreen"]
+    "list: The javascript methods name to be used to switch from expand to compress mode"
+
+    zoomed: bool = False
+    "bool: the current zoomed level: ``True`` for expanded and ``False`` for compressed"
+
+    w_btn: Optional[v.Btn] = None
+    "the btn to display on the map"
+
+    template: Optional[v.VuetifyTemplate] = None
+    "Embeds the 2 javascripts methods to change the rendering of the map"
+
+    def __init__(
+        self, m: Map, fullscreen: bool = False, fullapp: bool = False, **kwargs
+    ) -> None:
+        """A custom Fullscreen Button ready to be embed in a map object.
+
+        This button will force the display of the map in fullscreen mode. It should be used instead of the built-in ipyleaflet FullscreenControl if your map is embedding ipyvuetify widgets. I tends to solve the issue raised here: https://github.com/widgetti/ipyvuetify/issues/141. The idea is to fake the fullscreen display by forcing the map container to extend to the full extend of the screen without using a z-index superior to the ipyvuetify overlay.
+        simply click on it and the map will automatically expand
+
+        .. versionadded:: 2.7.0
+
+        Args:
+            m: the map on which the mutated CSS will be applied (Only work with SepalMap as we are querying the _id)
+            fullscreen: either the map should be displayed in fullscreen by default. default to false.
+            fullapp: either or not the map will be used as the sole widget/tile of an application
+            kwargs: any available arguments from a ipyleaflet WidgetControl
+        """
+        # set the offset
+        offset = "48px" if fullapp else "0px"
+
+        # register the required zoom value
+        self.zoomed = fullscreen
+
+        # create a btn
+        self.w_btn = MapBtn(self.ICONS[self.zoomed])
+
+        # overwrite the widget set in the kwargs (if any)
+        kwargs["widget"] = self.w_btn
+        kwargs.setdefault("position", "topleft")
+        kwargs["transparent_bg"] = True
+
+        # create the widget
+        super().__init__(**kwargs)
+
+        # add javascrip behaviour
+        self.w_btn.on_event("click", self.toggle_fullscreen)
+
+        # save the 2 fullscrenn js code in a table 0 for embedded and 1 for fullscreen
+        js_dir = Path(__file__).parents[1] / "frontend/js"
+        embed = (js_dir / "jupyter_embed.js").read_text() % m._id
+        full = (js_dir / "jupyter_fullscreen.js").read_text() % (m._id, offset)
+
+        # template with js behaviour
+        # "jupyter_fullscreen" place the "leaflet-container element on the front screen
+        # and expand it's display to the full screen
+        # "jupyter_embed" reset all the changed parameter
+        # both trigger the resize event to force the reload of the Tilelayers
+        self.template = v.VuetifyTemplate(
+            template=(
+                "<script>{methods: {jupyter_embed(){%s}, jupyter_fullscreen(){%s}}}</script>"
+                % (embed, full)
+            )
+        )
+        display(self.template)
+
+        # display the map in the requested default state
+        js = full if self.zoomed else embed
+        display(Javascript(js))
+
+    def toggle_fullscreen(self, *args) -> None:
+        """Toggle fullscreen state.
+
+        Toggle the fullscreen state of the map by sending the required javascript method,
+        changing the w_btn icons and the zoomed state of the control.
+        """
+        # change the zoom state
+        self.zoomed = not self.zoomed
+
+        # change button icon
+        self.w_btn.children[0].children = [self.ICONS[self.zoomed]]
+
+        # zoom
+        self.template.send({"method": self.METHODS[self.zoomed], "args": []})
+
+        return
