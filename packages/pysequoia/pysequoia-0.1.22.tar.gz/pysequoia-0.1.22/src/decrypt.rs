217060b07a3f9d48ed5ee7b77c0d62be1827b5db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 04:05:14 +0000

    Add package pysequoia/0.1.22/pysequoia-0.1.22.tar.gz

---
+++ packages/pysequoia/pysequoia-0.1.22.tar.gz/pysequoia-0.1.22/src/decrypt.rs
@@ -0,0 +1,73 @@
+use std::sync::{Arc, Mutex};
+
+use openpgp::crypto::SessionKey;
+use openpgp::parse::{stream::*, Parse};
+use openpgp::policy::StandardPolicy as P;
+use openpgp::types::SymmetricAlgorithm;
+use pyo3::prelude::*;
+use sequoia_openpgp as openpgp;
+
+use crate::Decrypted;
+
+#[pyclass]
+#[derive(Clone)]
+pub struct PyDecryptor {
+    inner: Arc<Mutex<Box<dyn openpgp::crypto::Decryptor + Send + Sync + 'static>>>,
+}
+
+impl PyDecryptor {
+    pub fn new(inner: Box<dyn openpgp::crypto::Decryptor + Send + Sync + 'static>) -> Self {
+        Self {
+            inner: Arc::new(Mutex::new(inner)),
+        }
+    }
+}
+
+#[pyfunction]
+pub fn decrypt(decryptor: PyDecryptor, bytes: &[u8]) -> PyResult<Decrypted> {
+    let policy = &P::new();
+
+    let mut decryptor =
+        DecryptorBuilder::from_bytes(bytes)?.with_policy(policy, None, decryptor)?;
+
+    let mut sink = Vec::new();
+    std::io::copy(&mut decryptor, &mut sink)?;
+    Ok(Decrypted { content: sink })
+}
+
+impl VerificationHelper for PyDecryptor {
+    fn get_certs(&mut self, _ids: &[openpgp::KeyHandle]) -> openpgp::Result<Vec<openpgp::Cert>> {
+        Ok(Vec::new())
+    }
+
+    fn check(&mut self, _structure: MessageStructure) -> openpgp::Result<()> {
+        Ok(())
+    }
+}
+
+impl DecryptionHelper for PyDecryptor {
+    fn decrypt<D>(
+        &mut self,
+        pkesks: &[openpgp::packet::PKESK],
+        _skesks: &[openpgp::packet::SKESK],
+        sym_algo: Option<SymmetricAlgorithm>,
+        mut decrypt: D,
+    ) -> openpgp::Result<Option<openpgp::Fingerprint>>
+    where
+        D: FnMut(SymmetricAlgorithm, &SessionKey) -> bool,
+    {
+        let pair = &mut *self.inner.lock().unwrap();
+
+        for pkesk in pkesks.iter() {
+            if pkesk
+                .decrypt(pair, sym_algo)
+                .map(|(algo, session_key)| decrypt(algo, &session_key))
+                .is_some()
+            {
+                return Ok(None);
+            }
+        }
+
+        Err(anyhow::anyhow!("No key to decrypt message"))
+    }
+}
---
+++ packages/pysequoia/pysequoia-0.1.22.tar.gz/pysequoia-0.1.22/src/lib.rs
@@ -0,0 +1,79 @@
+use std::borrow::Cow;
+
+use openpgp::serialize::stream::Armorer;
+use pyo3::prelude::*;
+
+mod card;
+mod cert;
+mod decrypt;
+mod encrypt;
+mod notation;
+mod sign;
+mod signature;
+mod signer;
+mod store;
+mod user_id;
+mod verify;
+
+use openpgp::armor::Kind;
+use openpgp::packet::Packet;
+use openpgp::serialize::{stream::Message, Marshal};
+use sequoia_openpgp as openpgp;
+
+pub(crate) fn serialize<T>(p: Packet, armor_kind: T) -> openpgp::Result<Vec<u8>>
+where
+    T: Into<Option<Kind>>,
+{
+    let mut sink = vec![];
+    let mut message = Message::new(&mut sink);
+    if let Some(kind) = armor_kind.into() {
+        message = Armorer::new(message).kind(kind).build()?
+    }
+    p.serialize(&mut message)?;
+    message.finalize()?;
+    Ok(sink)
+}
+
+#[pyclass]
+#[derive(Clone)]
+pub struct Decrypted {
+    content: Vec<u8>,
+}
+
+#[pymethods]
+impl Decrypted {
+    #[getter]
+    pub fn bytes(&self) -> Cow<[u8]> {
+        Cow::Borrowed(&self.content)
+    }
+}
+
+#[pymodule]
+fn pysequoia(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<cert::Cert>()?;
+    m.add_class::<store::Store>()?;
+    m.add_class::<card::Card>()?;
+    m.add_class::<notation::Notation>()?;
+    m.add_function(wrap_pyfunction!(sign::sign, m)?)?;
+    m.add_function(wrap_pyfunction!(encrypt::encrypt, m)?)?;
+    m.add_function(wrap_pyfunction!(decrypt::decrypt, m)?)?;
+    m.add_function(wrap_pyfunction!(verify::verify, m)?)?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use openpgp::cert::prelude::*;
+    use sequoia_openpgp as openpgp;
+    use sequoia_openpgp::serialize::SerializeInto;
+    use testresult::TestResult;
+
+    #[test]
+    fn test_armoring() -> TestResult {
+        let cert = CertBuilder::general_purpose(None, Some("test@example.com"))
+            .generate()?
+            .0;
+        assert!(cert.armored().to_vec().is_ok());
+        Ok(())
+    }
+}
