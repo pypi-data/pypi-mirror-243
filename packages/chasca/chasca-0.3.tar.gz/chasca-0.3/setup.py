47cc9c64e7b7f6e56b4e22eef04da5f35960cb40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 05:05:14 +0000

    Add package chasca/0.3/chasca-0.3.tar.gz

---
+++ packages/chasca/chasca-0.3.tar.gz/chasca-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='chasca',
+      version='0.3',
+      description='Tools to plan astronomical observations',
+      url='https://github.com/ramstojh/chasca',
+      author='Jhon Yana',
+      author_email='ramstojh@alumni.usp.br',
+      license='MIT',
+      packages=['chasca'],
+      zip_safe=False)
