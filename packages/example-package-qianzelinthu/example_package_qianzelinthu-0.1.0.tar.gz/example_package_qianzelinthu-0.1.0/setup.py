9483174b3688c1e883b7f6309813f401d9a9fd91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 03:08:03 +0000

    Add package example-package-qianzelinthu/0.1.0/example_package_qianzelinthu-0.1.0.tar.gz

---
+++ packages/example-package-qianzelinthu/example_package_qianzelinthu-0.1.0.tar.gz/example_package_qianzelinthu-0.1.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="example_package_qianzelinthu",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # 'numpy',
+        # 'matplotlib',
+        # 'torch',# 在这里列出你的库所需的其他Python包, 这不是requirements
+    ],
+
+    author="Zelin Qian",
+    author_email="qzl22@mails.tsinghua.edu.cn",
+    description="testttt",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/ZelinQian/SPIDER",
+    # classifiers=[
+    #     "Development Status :: 3 - Alpha",
+    #     "Intended Audience :: Developers",
+    #     "License :: OSI Approved :: MIT License",
+    #     "Programming Language :: Python",
+    #     "Programming Language :: Python :: 3",
+    #     "Programming Language :: Python :: 3.6",
+    #     "Programming Language :: Python :: 3.7",
+    #     "Programming Language :: Python :: 3.8",
+    #     "Programming Language :: Python :: 3.9",
+    # ],
+    python_requires=">=3.6"
+)
