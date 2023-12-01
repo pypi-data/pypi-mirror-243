4167ef0e09f09ba31ea3dd20dd6aaf0fd9e6c09c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 03:07:02 +0000

    Add package deciphon-snap/0.8.2/deciphon_snap-0.8.2-py3-none-any.whl

---
+++ packages/deciphon-snap/deciphon_snap-0.8.2-py3-none-any.whl/deciphon_snap/query_interval.py
@@ -0,0 +1,24 @@
+from deciphon_snap.interval import PyInterval
+from deciphon_snap.match import MatchList, MatchListInterval
+
+__all__ = ["QueryInterval", "QueryIntervalBuilder"]
+
+
+class QueryInterval(PyInterval):
+    ...
+
+
+class QueryIntervalBuilder:
+    def __init__(self, match_list: MatchList):
+        self._offset = []
+        offset = 0
+        for x in match_list:
+            self._offset.append(offset)
+            offset += x.query_size
+        self._offset.append(offset)
+
+    def make(self, match_list_interval: MatchListInterval) -> QueryInterval:
+        i = match_list_interval
+        start = self._offset[i.pyinterval.start]
+        stop = self._offset[i.pyinterval.stop]
+        return QueryInterval(start=start, stop=stop)
