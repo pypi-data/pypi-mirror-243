af0db6c1935b2dcb8156edfa6954e5f2028df778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 04:04:47 +0000

    Add package mep-tools/1.2.0/mep_tools-1.2.0.tar.gz

---
+++ packages/mep-tools/mep_tools-1.2.0.tar.gz/mep_tools-1.2.0/mep_tools/connectors/__init__.py
@@ -0,0 +1,10 @@
+# GENERATED FILE DO NOT CHANGE !!!
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent))
+from connector_pb2 import *
+from connector_pb2_grpc import *
+from metric_pb2 import *
+from metric_pb2_grpc import *
+from service_pb2 import *
+from service_pb2_grpc import *
