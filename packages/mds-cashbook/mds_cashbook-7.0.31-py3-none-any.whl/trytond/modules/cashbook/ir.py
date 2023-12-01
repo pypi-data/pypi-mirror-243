87482d44f6eb56ca4dba1ad171a39edcd2240adc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 03:06:22 +0000

    Add package mds-cashbook/7.0.31/mds_cashbook-7.0.31-py3-none-any.whl

---
+++ packages/mds-cashbook/mds_cashbook-7.0.31-py3-none-any.whl/trytond/modules/cashbook/ir.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# This file is part of the cashbook-module from m-ds.de for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the
+# full copyright notices and license terms.
+
+from trytond.transaction import Transaction
+from trytond.pool import PoolMeta
+
+
+class Rule(metaclass=PoolMeta):
+    __name__ = 'ir.rule'
+
+    @classmethod
+    def _get_context(cls, model_name):
+        context = super()._get_context(model_name)
+        if model_name in {'cashbook.book', 'cashbook.line', 'cashbook.recon'}:
+            context['user_id'] = Transaction().user
+        return context
+
+    @classmethod
+    def _get_cache_key(cls, model_name):
+        key = super()._get_cache_key(model_name)
+        if model_name in {'cashbook.book', 'cashbook.line', 'cashbook.recon'}:
+            key = (*key, Transaction().user)
+        return key
+
+# end Rule
