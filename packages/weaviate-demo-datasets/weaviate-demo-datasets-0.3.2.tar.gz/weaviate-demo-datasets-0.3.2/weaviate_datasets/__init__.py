5539028442f415da6122ac389b7a4fdff4447941
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 04:04:23 +0000

    Add package weaviate-demo-datasets/0.3.2/weaviate-demo-datasets-0.3.2.tar.gz

---
+++ packages/weaviate-demo-datasets/weaviate-demo-datasets-0.3.2.tar.gz/weaviate-demo-datasets-0.3.2/weaviate_datasets/__init__.py
@@ -0,0 +1,15 @@
+from .datasets import (
+    WineReviews,
+    WineReviewsMT,
+    JeopardyQuestions1k,
+    JeopardyQuestions10k,
+    Wiki100,
+)
+
+from .v3_datasets import (
+    WineReviews as WineReviews_V3,
+    WineReviewsMT as WineReviewsMT_V3,
+    JeopardyQuestions1k as JeopardyQuestions1k_V3,
+    JeopardyQuestions10k as JeopardyQuestions10k_V3,
+    Wiki100 as Wiki100_V3,
+)
