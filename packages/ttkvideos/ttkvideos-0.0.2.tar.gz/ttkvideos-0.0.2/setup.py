febd8ece1ea6e3014faedac73fd61b30bc6ce762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 04:05:14 +0000

    Add package ttkvideos/0.0.2/ttkvideos-0.0.2.tar.gz

---
+++ packages/ttkvideos/ttkvideos-0.0.2.tar.gz/ttkvideos-0.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup,find_packages
+
+classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='ttkvideos',
+    version='0.0.2',
+    description='playing video (with audio) in tkinter label',
+    long_description=open('readme.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='https://github.com/Vishal24102002/ttkvideos',
+    author='Vishal Sharma',
+    author_email='vishalsharma659615@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='videos',
+    packages=find_packages(),
+    python_requires = ">=3.6"
+
+
+)
