1568c16c2b68fe5ac2193209ab81fe4c5b7066b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 03:06:48 +0000

    Add package pythereum/1.1.8/pythereum-1.1.8.tar.gz

---
+++ packages/pythereum/pythereum-1.1.8.tar.gz/pythereum-1.1.8/pythereum/exceptions.py
@@ -0,0 +1,61 @@
+class ERPCBaseException(Exception):
+    """
+    Base exception class for Ethereum RPC interactions.
+    """
+
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self.message)
+
+
+class ERPCRequestException(ERPCBaseException):
+    """
+    Raised when an error is returned from the Ethereum RPC.
+    """
+
+    def __init__(self, code: int, message: str = "Generic ERPC Error"):
+        self.code = code  # Error code, e.g., HTTP error code or custom ERPC code
+        full_message = f"Error {code}: {message}\nPlease consult your endpoint's documentation for info on error codes."
+        super().__init__(full_message)
+
+
+class ERPCInvalidReturnException(ERPCBaseException):
+    """
+    Raised when the Ethereum RPC returns a value which is incorrectly formatted.
+    """
+
+
+class ERPCDecoderException(ERPCBaseException):
+    """
+    Raised when invalid data is input to a decoder and an error is thrown.
+    """
+
+
+class ERPCEncoderException(ERPCBaseException):
+    """
+    Raised when invalid data is input to an encoder and an error is thrown.
+    """
+
+
+class ERPCSubscriptionException(ERPCBaseException):
+    """
+    Raised when a subscription request is rejected by a host or for other generic subscription errors.
+    """
+
+
+class ERPCBuilderException(ERPCBaseException):
+    """
+    Raised for exceptions related to builders and the BuilderRPC
+    """
+
+
+class ERPCManagerException(ERPCBaseException):
+    """
+    Raised for exceptions related to manager classes such as nonce managers or gas managers
+    """
+
+
+class ERPCGenericException(ERPCBaseException):
+    """
+    Raised for exceptions which do not fall into any of the above categories, things like utility functions will use it
+    """
