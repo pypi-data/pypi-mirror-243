bf01307b44ed56bcac896376be2602cb0410242f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-01 06:05:04 +0000

    Add package python-sdk-local/0.0.44/python_sdk_local-0.0.44-py3-none-any.whl

---
+++ packages/python-sdk-local/python_sdk_local-0.0.44-py3-none-any.whl/sdk/src/validate.py
@@ -0,0 +1,13 @@
+import os
+
+def validate_enviroment_variables():
+    if(os.getenv("ENVIRONMENT_NAME") is None):
+        raise Exception("logger-local-python-package LoggerLocal.py please add Environment Variable called ENVIRONMENT_NAME=local or play1 (instead of ENVIRONMENT)")
+    if(os.getenv("PRODUCT_USER_IDENTIFIER") is None):
+        raise Exception("logger-local-python-package LoggerLocal.py please add Environment Variable called PRODUCT_USER_IDENTIFIER (instead of PRODUCT_USERNAME)")
+    if(os.getenv("PRODUCT_PASSWORD") is None):
+        raise Exception("logger-local-python-package LoggerLocal.py please add Environment Variable called PRODUCT_PASSWORD")
+    if(os.getenv("LOGZIO_TOKEN") is None):
+        raise Exception("logger-local-python-package LoggerLocal.py please add Environment Variable called LOGZIO_TOKEN=cXNHuVkkffkilnkKzZlWExECRlSKqopE")
+    
+ 
